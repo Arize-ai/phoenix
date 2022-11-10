4ede28797d810f424b9a8eec2142ec5df8310dc0
Mikyo King <mikyo@arize.com>
2022-11-09 21:41:26 -0700

    feat(2): react inside of notebook

--- ui/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom/client";
-
-let Greet = () => <h1>Hello, world!</h1>;
-
-const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(<Greet />);
