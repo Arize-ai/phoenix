e44639859004fbfeb3c1ced7a5dfac94347c5641
Xander Song <xandersong@Xanders-MacBook-Pro.local>
2022-11-28 14:45:11 -0800

    refactor: Change return type of Euclidean distance function from np.array to float.

--- src/phoenix/metrics/embeddings.py
+++ src/phoenix/metrics/embeddings.py
@@ -8,9 +8,9 @@ def euclidean_distance(
     primary: Dataset,
     reference: Dataset,
     embedding_feature_name: str,
-):
+) -> float:
     primary_vec_col: pd.Series = primary.get_embedding_vector_column(embedding_feature_name)
     reference_vec_col: pd.Series = reference.get_embedding_vector_column(embedding_feature_name)
     primary_centroid = primary_vec_col.mean()
     reference_centroid = reference_vec_col.mean()
-    return np.linalg.norm(primary_centroid - reference_centroid)
+    return np.linalg.norm(primary_centroid - reference_centroid).item()
