45badb652cf24e8a5e3bcb9384d3191ce90755be
Mikyo King <mikyo@arize.com>
2022-11-30 16:21:28 -0700

    feat: point cloud selection

--- ui/src/components/canvas/ControlPanel.tsx
+++ ui/src/components/canvas/ControlPanel.tsx
@@ -1,6 +1,6 @@
 import { css } from "@emotion/css";
 import React, { ReactNode } from "react";
-import { Text, theme } from "@arizeai/components";
+import { theme } from "@arizeai/components";
 
 const controlsCSS = css`
     position: absolute;
@@ -14,7 +14,7 @@ const controlsCSS = css`
     }
     border-radius: 6px;
     border: 1px solid ${theme.colors.gray200};
-    background-color: ${theme.colors.gray400};
+    background-color: ${theme.colors.gray700};
     box-shadow: 0 2px 5px 5px rgba(0, 0, 0, 0.1);
     overflow: hidden;
     z-index: 1;
@@ -46,31 +46,5 @@ export function ControlPanel({
         >
             {children}
         </section>
-    );
-}
-
-export function ControlPanelHeader({
-    children,
-    extra,
-}: {
-    children: ReactNode;
-    extra?: ReactNode;
-}) {
-    return (
-        <header
-            className={css`
-                padding: ${theme.spacing.margin8}px ${theme.spacing.margin16}px;
-                border-bottom: 1px solid ${theme.colors.gray500};
-                display: flex;
-                flex-direction: row;
-                align-items: center;
-                justify-content: space-between;
-            `}
-        >
-            <Text textSize="medium" weight="heavy">
-                {children}
-            </Text>
-            <div>{extra}</div>
-        </header>
     );
 }
--- ui/src/components/canvas/DriftPointCloud.tsx
+++ ui/src/components/canvas/DriftPointCloud.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { ReactNode, useCallback, useMemo, useState } from "react";
 import {
     ThreeDimensionalCanvas,
     ThreeDimensionalControls,
@@ -7,19 +7,26 @@ import {
     ThreeDimensionalPoint,
     ThreeDimensionalBounds,
     LassoSelect,
+    PointBaseProps,
 } from "@arizeai/point-cloud";
 import { ErrorBoundary } from "../ErrorBoundary";
 import {
     Accordion,
     AccordionItem,
+    Form,
     Icon,
     Icons,
+    Item,
+    Picker,
     Radio,
     RadioGroup,
     theme,
 } from "@arizeai/components";
 import { css } from "@emotion/css";
+import { shade } from "polished";
 import { ControlPanel } from "./ControlPanel";
+
+const DIM_AMOUNT = 0.5;
 
 export type ThreeDimensionalPointItem = {
     position: ThreeDimensionalPoint;
@@ -56,7 +63,9 @@ function CanvasTools(props: {
         <div
             className={css`
                 position: absolute;
-                left: ${CONTROL_PANEL_WIDTH + 2 * theme.spacing.margin8}px;
+                /* left: ${CONTROL_PANEL_WIDTH +
+                2 * theme.spacing.margin8}px; */
+                left: ${theme.spacing.margin8}px;
                 top: ${theme.spacing.margin8}px;
                 z-index: 1;
             `}
@@ -77,10 +86,22 @@ function CanvasTools(props: {
                     <Icon svg={<Icons.MoveFilled />} />
                 </Radio>
                 <Radio label="Select" value={CanvasMode.select}>
-                    <Icon svg={<Icons.PlusCircleOutline />} />
+                    <Icon svg={<Icons.LassoOutline />} />
                 </Radio>
             </RadioGroup>
         </div>
+    );
+}
+
+function AccordionSection({ children }: { children: ReactNode }) {
+    return (
+        <section
+            className={css`
+                margin: ${theme.spacing.margin8};
+            `}
+        >
+            {children}
+        </section>
     );
 }
 
@@ -89,13 +110,87 @@ function DisplayControlPanel() {
         <ControlPanel position="top-left" width={CONTROL_PANEL_WIDTH}>
             <Accordion variant="compact">
                 <AccordionItem title="Display" id="display">
-                    {"Display"}
-                </AccordionItem>
-                <AccordionItem title="Cluster" id="clusters">
-                    {"Clusters"}
+                    <AccordionSection>
+                        <Form>
+                            <Picker
+                                label="Color by"
+                                defaultSelectedKey={"dataset"}
+                            >
+                                <Item key="dataset">Dataset</Item>
+                            </Picker>
+                        </Form>
+                    </AccordionSection>
                 </AccordionItem>
             </Accordion>
         </ControlPanel>
+    );
+}
+
+function SelectionControlPanel({ selectedIds }: { selectedIds: Set<string> }) {
+    return (
+        <ControlPanel position="top-right" width={CONTROL_PANEL_WIDTH}>
+            <ul>
+                {[...selectedIds].map((id) => (
+                    <li key={id}>{id}</li>
+                ))}
+            </ul>
+        </ControlPanel>
+    );
+}
+
+function UMAPPoints({
+    primaryData,
+    referenceData,
+    selectedIds,
+}: DriftPointCloudProps & { selectedIds: Set<string> }) {
+    const primaryColor = "#7BFFFF";
+    const referenceColor = "#d57bff";
+    /** Colors to represent a dimmed variant of the color for "un-selected" */
+    const dimmedPrimaryColor = useMemo(() => {
+        // if (typeof primaryColor === "function") {
+        //     return (p: PointBaseProps) => shade(DIM_AMOUNT)(primaryColor(p));
+        // }
+        return shade(DIM_AMOUNT, primaryColor);
+    }, [primaryColor]);
+
+    const dimmedReferenceColor = useMemo(() => {
+        // if (typeof referenceColor === "function") {
+        //     return (p: PointBaseProps) => shade(DIM_AMOUNT)(referenceColor(p));
+        // }
+        return shade(DIM_AMOUNT, referenceColor);
+    }, [referenceColor]);
+
+    const primaryColorByFn = useCallback(
+        (p: PointBaseProps) => {
+            if (!selectedIds.has(p.metaData.id) && selectedIds.size > 0) {
+                return dimmedPrimaryColor;
+            }
+            return primaryColor;
+        },
+        [selectedIds, primaryColor, dimmedPrimaryColor]
+    );
+
+    const referenceColorByFn = useCallback(
+        (p: PointBaseProps) => {
+            if (!selectedIds.has(p.metaData.id) && selectedIds.size > 0) {
+                return dimmedReferenceColor;
+            }
+            return referenceColor;
+        },
+        [referenceColor, selectedIds, dimmedReferenceColor]
+    );
+
+    return (
+        <>
+            <Points
+                data={primaryData}
+                pointProps={{ color: primaryColorByFn }}
+            />
+            <Points
+                data={referenceData}
+                pointProps={{ color: referenceColorByFn }}
+            />
+        </>
     );
 }
 
@@ -106,6 +201,7 @@ export function DriftPointCloud({
     // AutoRotate the canvas on initial load
     const [autoRotate, setAutoRotate] = useState<boolean>(true);
     const [canvasMode, setCanvasMode] = useState<CanvasMode>(CanvasMode.move);
+    const [selectedIds, setSelectedIds] = useState<Set<string>>(new Set());
     const allPoints = useMemo(() => [...primaryData, ...referenceData], []);
     const bounds = useMemo(() => {
         return getThreeDimensionalBounds(allPoints.map((p) => p.position));
@@ -113,7 +209,8 @@ export function DriftPointCloud({
     const isMoveMode = canvasMode === CanvasMode.move;
     return (
         <ErrorBoundary>
-            <DisplayControlPanel />
+            {/* <DisplayControlPanel /> */}
+            {/* <SelectionControlPanel selectedIds={selectedIds} /> */}
             <CanvasTools mode={canvasMode} onChange={setCanvasMode} />
             <ThreeDimensionalCanvas camera={{ position: [0, 0, 10] }}>
                 <ThreeDimensionalControls
@@ -130,19 +227,17 @@ export function DriftPointCloud({
                     <LassoSelect
                         points={allPoints}
                         onChange={(selection) => {
-                            // setSelectedUuids(
-                            //     new Set(selection.map((s) => s.metaData.uuid))
-                            // );
+                            setSelectedIds(
+                                new Set(selection.map((s) => s.metaData.id))
+                            );
                         }}
                         enabled={canvasMode === CanvasMode.select}
                     />
-                    <Points
-                        data={primaryData}
-                        pointProps={{ color: "#7BFFFF" }}
-                    />
-                    <Points
-                        data={referenceData}
-                        pointProps={{ color: "#d57bff" }}
+
+                    <UMAPPoints
+                        primaryData={primaryData}
+                        referenceData={referenceData}
+                        selectedIds={selectedIds}
                     />
                 </ThreeDimensionalBounds>
             </ThreeDimensionalCanvas>
