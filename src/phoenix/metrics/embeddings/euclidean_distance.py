93a0f1322c18d002cddb13ca483377b998fecb70
Xander Song <xandersong@Xanders-MacBook-Pro.local>
2022-12-01 10:52:36 -0800

    Renamed parameter in Euclidean distance method.

--- src/phoenix/metrics/embeddings/euclidean_distance.py
+++ src/phoenix/metrics/embeddings/euclidean_distance.py
@@ -7,10 +7,10 @@ from phoenix.datasets import Dataset
 def euclidean_distance(
     primary: Dataset,
     reference: Dataset,
-    embedding_feature_name: str,
+    embedding_column_name: str,
 ) -> float:
-    primary_vec_col: pd.Series = primary.get_embedding_vector_column(embedding_feature_name)
-    reference_vec_col: pd.Series = reference.get_embedding_vector_column(embedding_feature_name)
+    primary_vec_col: pd.Series = primary.get_embedding_vector_column(embedding_column_name)
+    reference_vec_col: pd.Series = reference.get_embedding_vector_column(embedding_column_name)
     primary_centroid = primary_vec_col.mean()
     reference_centroid = reference_vec_col.mean()
     return np.linalg.norm(primary_centroid - reference_centroid).item()
