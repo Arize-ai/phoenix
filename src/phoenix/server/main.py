05de264ac5ef777f2ba466d87fcf8add19f340a2
Mikyo King <mikyo@arize.com>
2022-12-06 17:59:11 -0700

    plumb through the port number

--- src/phoenix/server/main.py
+++ src/phoenix/server/main.py
@@ -1,7 +1,12 @@
+import argparse
+
 import uvicorn
 
+import phoenix.config as config
 from phoenix.server.app import app
 
 if __name__ == "__main__":
-
-    uvicorn.run(app)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", type=int, default=config.port)
+    args = parser.parse_args()
+    uvicorn.run(app, port=args.port)
