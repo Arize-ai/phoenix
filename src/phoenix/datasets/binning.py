7de12a0cc45af6b32b6f39cfc15d85ab37a06e04
Xander Song <xandersong@Xanders-MacBook-Pro.local>
2022-12-15 20:12:59 -0800

    add in optimized binning code

--- src/phoenix/datasets/binning.py
+++ src/phoenix/datasets/binning.py
@@ -5,6 +5,7 @@ Methods for binning and creating histograms.
 from functools import partial
 
 import numpy as np
+import numpy.typing as npt
 import pandas as pd
 
 
@@ -14,12 +15,8 @@ def compute_default_bins(df: pd.DataFrame) -> pd.DataFrame:
     """
 
     def compute_default_bins_from_stats(stats_column: "pd.Series[float]") -> "pd.Series[float]":
-        bin_boundaries = np.concatenate(
-            [
-                np.array([-np.inf]),
-                np.linspace(-4 / 3, 4 / 3, 9) * stats_column["std"] + stats_column["median"],
-                np.array([np.inf]),
-            ]
+        bin_boundaries = (
+            np.linspace(-4 / 3, 4 / 3, 9) * stats_column["std"] + stats_column["median"]
         )
         bins_column: "pd.Series[float]" = pd.Series(bin_boundaries)
         return bins_column
@@ -36,15 +33,16 @@ def compute_histogram(df: pd.DataFrame, bins_df: pd.DataFrame) -> pd.DataFrame:
     for column_name in df.columns:
         column_name_to_func_map[column_name] = partial(
             _compute_histogram_for_column_using_bins,
-            bins=pd.IntervalIndex.from_breaks(bins_df[column_name]),
+            interior_bin_boundaries=bins_df[column_name].to_numpy(),
         )
     return df.agg(column_name_to_func_map)
 
 
 def _compute_histogram_for_column_using_bins(
-    column: "pd.Series[float]", bins: pd.IntervalIndex
-) -> "pd.Series[int]":
-    histogram = column.value_counts(sort=False, bins=bins)  # type: ignore
-    histogram = histogram[bins]
-    histogram = histogram.set_axis(np.arange(histogram.shape[0]))
-    return histogram
+    array: npt.NDArray[np.float64], interior_bin_boundaries: npt.NDArray[np.float64]
+) -> "pd.Series[float]":
+    bin_indexes = np.searchsorted(interior_bin_boundaries, array)
+    histogram = np.bincount(bin_indexes)
+    pad_width = interior_bin_boundaries.shape[0] - histogram.shape[0] + 1
+    histogram = np.pad(histogram, (0, pad_width))
+    return pd.Series(histogram)
