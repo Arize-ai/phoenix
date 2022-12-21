b94c89d38ec19a222b35ec7b629faefb69fd98e7
axiomofjoy <axiomofjoy@gmail.com>
2022-12-21 00:35:09 -0800

    types;

--- src/phoenix/metrics/cardinality.py
+++ src/phoenix/metrics/cardinality.py
@@ -12,8 +12,8 @@ from pandas.core.algorithms import value_counts
 def cardinality(df: pd.DataFrame, max_workers: Optional[int] = None) -> Dict[str, "pd.Series[Any]"]:
     data: Dict[str, pd.Series[Any]] = {}
     with cf.ThreadPoolExecutor(max_workers=max_workers) as executor:
-        future_to_column_name = {
-            executor.submit(value_counts, df[col], dropna=False): col for col in df.columns.astype(str)
+        future_to_column_name: Dict[cf.Future[Any], str] = {
+            executor.submit(value_counts, df[col], dropna=False): col for col in df.columns
         }
         for future in cf.as_completed(future_to_column_name):
             column_name: str = future_to_column_name[future]
