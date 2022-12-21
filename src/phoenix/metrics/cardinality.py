18a1329dd4606ccbd1d709c9729650b5c8d2e06a
axiomofjoy <axiomofjoy@gmail.com>
2022-12-21 00:39:38 -0800

    types

--- src/phoenix/metrics/cardinality.py
+++ src/phoenix/metrics/cardinality.py
@@ -13,7 +13,7 @@ def cardinality(df: pd.DataFrame, max_workers: Optional[int] = None) -> Dict[str
     data: Dict[str, pd.Series[Any]] = {}
     with cf.ThreadPoolExecutor(max_workers=max_workers) as executor:
         future_to_column_name: Dict[cf.Future[Any], str] = {
-            executor.submit(value_counts, df[col], dropna=False): col for col in df.columns
+            executor.submit(value_counts, df[col], dropna=False): str(col) for col in df.columns
         }
         for future in cf.as_completed(future_to_column_name):
             column_name: str = future_to_column_name[future]
