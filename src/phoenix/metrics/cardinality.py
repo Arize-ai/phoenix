663ba9e27875dc6e6f02391b223c74bcb837515d
axiomofjoy <axiomofjoy@gmail.com>
2022-12-20 23:59:37 -0800

    fix type check

--- src/phoenix/metrics/cardinality.py
+++ src/phoenix/metrics/cardinality.py
@@ -3,13 +3,13 @@ Cardinality metrics
 """
 
 import concurrent.futures as cf
-from typing import Dict, Optional
+from typing import Any, Dict, Optional
 
 import pandas as pd
 from pandas.core.algorithms import value_counts
 
 
-def cardinality(df: pd.DataFrame, max_workers: Optional[int] = None) -> Dict[str, pd.Series]:
+def cardinality(df: pd.DataFrame, max_workers: Optional[int] = None) -> Dict[str, "pd.Series[Any]"]:
     data = {}
     with cf.ThreadPoolExecutor(max_workers=max_workers) as executor:
         future_to_column_name = {
