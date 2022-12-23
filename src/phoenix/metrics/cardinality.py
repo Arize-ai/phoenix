786db7be3b7facd4afef8e991a0fa787ad31d447
axiomofjoy <axiomofjoy@gmail.com>
2022-12-22 17:05:00 -0800

    remove type alias

--- src/phoenix/metrics/cardinality.py
+++ src/phoenix/metrics/cardinality.py
@@ -3,17 +3,15 @@ Cardinality metrics
 """
 
 import concurrent.futures as cf
-from typing import Any, Dict, List, Optional, TypeAlias
+from typing import Any, Dict, List, Optional
 
 import pandas as pd
 from pandas.core.algorithms import value_counts
 
-Column: TypeAlias = "pd.Series[Any]"
-
 
 def cardinality(
     df: pd.DataFrame, column_names: List[str], max_workers: Optional[int] = None
-) -> Dict[str, Column]:
+) -> Dict[str, pd.Series[Any]]:
     data = {}
     with cf.ThreadPoolExecutor(max_workers=max_workers) as executor:
         future_to_column_name = {
