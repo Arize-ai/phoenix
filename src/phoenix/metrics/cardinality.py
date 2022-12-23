6ec43cae4d7326e8e54e94162879fcafac6a8317
axiomofjoy <axiomofjoy@gmail.com>
2022-12-22 16:03:14 -0800

    simplify and remove types

--- src/phoenix/metrics/cardinality.py
+++ src/phoenix/metrics/cardinality.py
@@ -14,12 +14,11 @@ Column: TypeAlias = pd.Series[Any]
 def cardinality(
     df: pd.DataFrame, column_names: List[str], max_workers: Optional[int] = None
 ) -> Dict[str, Column]:
-    data: Dict[str, Column] = {}
+    data = {}
     with cf.ThreadPoolExecutor(max_workers=max_workers) as executor:
-        future_to_column_name: Dict[cf.Future[Column], str] = {}
-        for col in column_names:
-            future: cf.Future[Column] = executor.submit(value_counts, df[col], dropna=False)
-            future_to_column_name[future] = col
+        future_to_column_name = {
+            executor.submit(value_counts, df[col], dropna=False): col for col in column_names
+        }
         for future in cf.as_completed(future_to_column_name):
             column_name = future_to_column_name[future]
             data[column_name] = future.result()
