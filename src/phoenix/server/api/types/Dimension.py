c33bd2750f6d372274585a240b138ec0bd58b78f
Mikyo King <mikyo@arize.com>
2022-12-27 12:56:48 -0700

    chore: #133 add graphql schema-gen (#134)
    * chore: add graphql codegen
    
    * test data quality resolver
    
    * Add gql schema
    
    * Add resolver

--- src/phoenix/server/api/types/Dimension.py
+++ src/phoenix/server/api/types/Dimension.py
@@ -1,6 +1,18 @@
+from typing import Optional
+
 import strawberry
+
+
+def get_dimension_data_quality() -> "DimensionDataQuality":
+    return DimensionDataQuality(cardinality=None)
+
+
+@strawberry.type
+class DimensionDataQuality:
+    cardinality: Optional[int]
 
 
 @strawberry.type
 class Dimension:
     name: str
+    dataQuality: DimensionDataQuality
