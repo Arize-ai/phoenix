2c5ddf6961fcaf1556f42c0ef394372b5d81478e
Mikyo King <mikyo@arize.com>
2022-12-27 11:46:12 -0700

    test data quality resolver

--- src/phoenix/server/api/types/Dimension.py
+++ src/phoenix/server/api/types/Dimension.py
@@ -1,6 +1,14 @@
+from typing import Optional
+
 import strawberry
+
+
+@strawberry.type
+class DimensionDataQuality:
+    cardinality: Optional[int]
 
 
 @strawberry.type
 class Dimension:
     name: str
+    dataQuality: DimensionDataQuality
