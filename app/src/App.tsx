8da020c08b34513edeea74ed67142ecba41d0512
Mikyo King <mikyo@arize.com>
2022-12-28 12:51:55 -0700

    mv app resources out of the root

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,12 @@
-import React from "react";
 import { Navbar, Brand } from "./components/nav";
 import { Home } from "./pages";
+import { Provider, theme } from "@arizeai/components";
+import React, { Suspense } from "react";
+import ReactDom from "react-dom/client";
+import { GlobalStyles } from "./GlobalStyles";
+import { ThemeProvider } from "@emotion/react";
+import { RelayEnvironmentProvider, graphql, loadQuery } from "react-relay";
+import RelayEnvironment from "./RelayEnvironment";
 
 export function App() {
     return (
@@ -10,5 +16,33 @@ export function App() {
             </Navbar>
             <Home />
         </div>
+    );
+}
+
+// Define a query
+const RootQuery = graphql`
+    query AppRootQuery {
+        primaryDataset {
+            name
+        }
+        referenceDataset {
+            name
+        }
+    }
+`;
+
+export function AppRoot() {
+    const preloadedQuery = loadQuery(RelayEnvironment, RootQuery, {});
+    return (
+        <Provider>
+            <ThemeProvider theme={theme}>
+                <RelayEnvironmentProvider environment={RelayEnvironment}>
+                    <GlobalStyles />
+                    <Suspense fallback={"Loading..."}>
+                        <App />
+                    </Suspense>
+                </RelayEnvironmentProvider>
+            </ThemeProvider>
+        </Provider>
     );
 }
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,18 +1,8 @@
-import { Provider, theme } from "@arizeai/components";
-import { App } from "./App";
-import React from "react";
 import ReactDom from "react-dom/client";
-import { GlobalStyles } from "./GlobalStyles";
-import { ThemeProvider } from "@emotion/react";
+import { AppRoot } from "./App";
 
 const rootEl = document.getElementById("root");
 
 const root = ReactDom.createRoot(rootEl!);
-root.render(
-    <Provider>
-        <ThemeProvider theme={theme}>
-            <GlobalStyles />
-            <App />
-        </ThemeProvider>
-    </Provider>
-);
+
+root.render(<AppRoot />);
