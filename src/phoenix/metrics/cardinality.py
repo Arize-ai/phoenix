6c392495df9fd68dae3822c0bc57689a660184fe
axiomofjoy <axiomofjoy@gmail.com>
2022-12-28 22:27:04 -0800

    fix cardinality to return num unique values per dimension

--- src/phoenix/metrics/cardinality.py
+++ src/phoenix/metrics/cardinality.py
@@ -3,21 +3,22 @@ Cardinality metrics
 """
 
 import concurrent.futures as cf
-from typing import Any, Dict, List, Optional
+from typing import Any, List, Optional
 
 import pandas as pd
-from pandas.core.algorithms import value_counts
 
 
 def cardinality(
     df: pd.DataFrame, column_names: List[str], max_workers: Optional[int] = None
-) -> Dict[str, "pd.Series[Any]"]:
-    data = {}
+) -> "pd.Series[Any]":
+    index = []
+    data = []
     with cf.ThreadPoolExecutor(max_workers=max_workers) as executor:
         future_to_column_name = {
-            executor.submit(value_counts, df[col], dropna=False): col for col in column_names
+            executor.submit(lambda x: x.nunique(dropna=False), df[col]): col for col in column_names
         }
         for future in cf.as_completed(future_to_column_name):
             column_name = future_to_column_name[future]
-            data[column_name] = future.result()
-    return data
+            index.append(column_name)
+            data.append(future.result())
+    return pd.Series(data, index=index)
