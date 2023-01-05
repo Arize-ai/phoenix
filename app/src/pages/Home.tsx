bd790b8821246135d07c57663a15c62fc69aade0
Mikyo King <mikyo@arize.com>
2023-01-04 17:35:11 -0700

    Add paginated fragment

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,13 +1,25 @@
 import { css } from "@emotion/react";
 import { Card } from "@arizeai/components";
+import { ModelSchemaTable } from "../components/model";
 import React from "react";
+import { graphql, useLazyLoadQuery } from "react-relay";
+import { HomeQuery } from "./__generated__/HomeQuery.graphql";
 
 type HomePageProps = {
   primaryDatasetName: string;
   referenceDatasetName: string;
 };
+
 export function Home(props: HomePageProps) {
   const { primaryDatasetName, referenceDatasetName } = props;
+  const data = useLazyLoadQuery<HomeQuery>(
+    graphql`
+      query HomeQuery {
+        ...ModelSchemaTable_dimensions
+      }
+    `,
+    {}
+  );
   return (
     <main
       css={(theme) =>
@@ -21,7 +33,7 @@ export function Home(props: HomePageProps) {
         subTitle={`primary: ${primaryDatasetName}, reference: ${referenceDatasetName}`}
         variant="compact"
       >
-        Schema here
+        <ModelSchemaTable model={data} />
       </Card>
     </main>
   );
