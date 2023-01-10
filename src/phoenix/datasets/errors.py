8b300c51dc3e0189f1228ece02fcaf1cdf93dec6
axiomofjoy <axiomofjoy@gmail.com>
2023-01-10 03:44:24 -0800

    reset --headers

--- src/phoenix/datasets/errors.py
+++ src/phoenix/datasets/errors.py
@@ -1,7 +1,3 @@
-#                  Copyright 2023 Arize AI and contributors.
-#                   Licensed under the Elastic License 2.0;
-# you may not use this file except in compliance with the Elastic License 2.0.
-
 from abc import ABC, abstractmethod
 from typing import Iterable, List, Union
 
