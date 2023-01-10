0fede72487e8228ad65fcf2c84e15e992afcf096
axiomofjoy <axiomofjoy@gmail.com>
2023-01-10 14:00:47 -0800

    got insert header working on non-empty files

--- src/phoenix/pointcloud/__init__.py
+++ src/phoenix/pointcloud/__init__.py
@@ -1,6 +1,2 @@
-#                    Copyright 2023 Arize AI and contributors.
-#                     Licensed under the Elastic License 2.0;
-#   you may not use this file except in compliance with the Elastic License 2.0.
-
 from .pointcloud import DriftPointCloud
 from .projectors import UMAPProjector
