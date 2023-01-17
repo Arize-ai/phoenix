ee7ce50e28b7525e92c8f896d0eadc387dc0fd7e
Mikyo King <mikyo@arize.com>
2023-01-17 11:53:10 -0700

    remove comments"

--- src/phoenix/server/api/types/Model.py
+++ src/phoenix/server/api/types/Model.py
@@ -26,7 +26,6 @@ class Model:
         the necessary books after the offset.
         For simplicity, here we build the list and then slice it accordingly
         """
-        print("resolving")
         return connection_from_list(
             [
                 to_gql_dimension(index, dimension)
@@ -39,30 +38,3 @@ class Model:
                 before=before if isinstance(before, Cursor) else None,
             ),
         )
-
-    # dimensions_with_offset = [
-    #     (to_gql_dimension(index, dimension), index + 1)
-    #     for index, dimension in enumerate(info.context.model.dimensions)
-    # ]
-
-    # offset = 0
-    # if isinstance(after, Cursor):
-    #     offset = cursor_to_offset(after)
-
-    # # Fetch the requested dimensions plus one, just to calculate `has_next_page`
-    # dimensions_with_offset = dimensions_with_offset[offset : offset + first + 1]
-
-    # edges = [
-    #     Edge(node=dim_with_offset[0], cursor=offset_to_cursor(dim_with_offset[1]))
-    #     for index, dim_with_offset in enumerate(dimensions_with_offset)
-    # ]
-
-    # return Connection(
-    #     page_info=PageInfo(
-    #         has_previous_page=False,
-    #         has_next_page=len(dimensions_with_offset) > first,
-    #         start_cursor=edges[0].cursor if edges else None,
-    #         end_cursor=edges[-2].cursor if len(edges) > 1 else None,
-    #     ),
-    #     edges=edges[:-1],  # exclude last one as it was fetched to know if there is a next page
-    # )
