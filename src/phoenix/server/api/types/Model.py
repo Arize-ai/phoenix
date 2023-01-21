9e2c39400828fc6c7e7494fdb5005f484051aa9b
axiomofjoy <axiomofjoy@gmail.com>
2023-01-20 19:07:22 -1000

    implement embedding dimension node

--- src/phoenix/server/api/types/Model.py
+++ src/phoenix/server/api/types/Model.py
@@ -7,6 +7,7 @@ from strawberry.types import Info
 from phoenix.server.api.context import Context
 
 from .Dimension import Dimension, to_gql_dimension
+from .EmbeddingDimension import EmbeddingDimension, to_gql_embedding_dimension
 from .pagination import Connection, ConnectionArgs, Cursor, connection_from_list
 
 
@@ -30,6 +31,33 @@ class Model:
             [
                 to_gql_dimension(index, dimension)
                 for index, dimension in enumerate(info.context.model.dimensions)
+            ],
+            args=ConnectionArgs(
+                first=first,
+                after=after if isinstance(after, Cursor) else None,
+                last=last,
+                before=before if isinstance(before, Cursor) else None,
+            ),
+        )
+
+    @strawberry.field
+    def embedding_dimensions(
+        self,
+        info: Info[Context, None],
+        first: Optional[int] = 50,
+        last: Optional[int] = None,
+        after: Optional[Cursor] = UNSET,
+        before: Optional[Cursor] = UNSET,
+    ) -> Connection[EmbeddingDimension]:
+        """
+        A non-trivial implementation should efficiently fetch only
+        the necessary books after the offset.
+        For simplicity, here we build the list and then slice it accordingly
+        """
+        return connection_from_list(
+            [
+                to_gql_embedding_dimension(index, embedding_dimension)
+                for index, embedding_dimension in enumerate(info.context.model.embedding_dimensions)
             ],
             args=ConnectionArgs(
                 first=first,
