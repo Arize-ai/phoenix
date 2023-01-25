07f48ff25230b02afa2ce8c322ec32b5b3af2c05
axiomofjoy <axiomofjoy@gmail.com>
2023-01-24 18:18:01 -1000

    add fixture and datasets commands to cli

--- src/phoenix/server/main.py
+++ src/phoenix/server/main.py
@@ -2,8 +2,7 @@ import atexit
 import errno
 import logging
 import os
-from argparse import ArgumentParser, Namespace
-from typing import Optional, Tuple
+from argparse import ArgumentParser
 
 import uvicorn
 
@@ -16,57 +15,6 @@ from phoenix.server.fixtures import (
 )
 
 logger = logging.getLogger(__name__)
-
-
-def _validate_arguments(
-    arguments: Namespace,
-) -> None:
-    """
-    Validates command line arguments.
-
-    Primary and reference datasets can be specified either explicitly by naming
-    existing datasets (in which case no fixture should be provided) or
-    implicitly by passing the name of a fixture (in which case no datasets
-    should be provided).
-    """
-
-    provided_primary_and_reference_flags_only = (
-        isinstance(arguments.primary, str)
-        and isinstance(arguments.reference, str)
-        and arguments.fixture is None
-    )
-    provided_fixture_flag_only = (
-        arguments.primary is None
-        and arguments.reference is None
-        and isinstance(arguments.fixture, str)
-    )
-    if not (provided_primary_and_reference_flags_only or provided_fixture_flag_only):
-        raise ValueError(
-            'Primary and reference datasets can be specified either explicitly via the "--primary" '
-            'and "--reference" flags (in which case the "--fixture" flag should be omitted) or '
-            'implicitly via the "--fixture" flag (in which case the "--primary" and "--reference" '
-            "flags should be omitted)."
-        )
-
-
-def _get_dataset_and_fixture_names(args: Namespace) -> Tuple[str, str, Optional[str]]:
-    """
-    Gets primary dataset name, reference dataset name, and fixture name from
-    command line arguments. In the case where the fixture name is provided,
-    primary and reference dataset names will be inferred.
-    """
-    primary_dataset_name: str
-    reference_dataset_name: str
-    fixture_name: Optional[str]
-    if args.fixture is not None:
-        primary_dataset_name, reference_dataset_name = get_dataset_names_from_fixture_name(
-            args.fixture
-        )
-        fixture_name = args.fixture
-    else:
-        primary_dataset_name = args.primary
-        reference_dataset_name = args.reference
-    return primary_dataset_name, reference_dataset_name, fixture_name
 
 
 def _write_pid_file() -> None:
@@ -96,19 +44,27 @@ if __name__ == "__main__":
     _write_pid_file()
 
     parser = ArgumentParser()
-    parser.add_argument("--primary", type=str)
-    parser.add_argument("--reference", type=str)
-    parser.add_argument("--fixture", type=str, choices=[fixture.name for fixture in FIXTURES])
+
     parser.add_argument("--port", type=int, default=config.port)
     parser.add_argument("--graphiql", action="store_true")
     parser.add_argument("--debug", action="store_true")
+    subparsers = parser.add_subparsers(dest="command", required=True)
+    datasets_parser = subparsers.add_parser("datasets")
+    datasets_parser.add_argument("--primary", type=str, required=True)
+    datasets_parser.add_argument("--reference", type=str, required=True)
+    fixture_parser = subparsers.add_parser("fixture")
+    fixture_parser.add_argument("fixture", type=str, choices=[fixture.name for fixture in FIXTURES])
     args = parser.parse_args()
 
-    _validate_arguments(args)
-    primary_dataset_name, reference_dataset_name, fixture_name = _get_dataset_and_fixture_names(
-        args
-    )
-    if fixture_name is not None:
+    if args.command == "datasets":
+        primary_dataset_name = args.primary
+        reference_dataset_name = args.reference
+    else:
+        fixture_name = args.fixture
+        primary_dataset_name, reference_dataset_name = get_dataset_names_from_fixture_name(
+            fixture_name
+        )
+        print(f'Downloading fixture "{fixture_name}" if missing')
         download_fixture_if_missing(fixture_name)
 
     print(
