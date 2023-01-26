64b6d10f65a655f75eef6d7c20edc6cd14e075be
Mikyo King <mikyo@arize.com>
2023-01-26 11:25:41 -0700

    initial strawman of UMAPoints

--- src/phoenix/server/api/types/EmbeddingDimension.py
+++ src/phoenix/server/api/types/EmbeddingDimension.py
@@ -3,11 +3,17 @@ import strawberry
 from phoenix.core import EmbeddingDimension as CoreEmbeddingDimension
 
 from .node import Node
+from .UMAPPoints import UMAPPoints
 
 
 @strawberry.type
 class EmbeddingDimension(Node):
     name: str
+
+    @strawberry.field
+    def UMAPPoints(self) -> UMAPPoints:
+        # UMAP code goes here
+        return UMAPPoints(coordinates=[], reference_coordinates=[])
 
 
 def to_gql_embedding_dimension(
