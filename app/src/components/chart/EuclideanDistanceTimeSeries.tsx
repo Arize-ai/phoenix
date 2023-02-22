602b098dac5720a84bc85d4d5ec70ccbb9b2105c
Mikyo King <mikyo@arize.com>
2023-02-22 12:09:25 -0700

    change y axis

--- app/src/components/chart/EuclideanDistanceTimeSeries.tsx
+++ app/src/components/chart/EuclideanDistanceTimeSeries.tsx
@@ -10,10 +10,31 @@ import {
   XAxis,
   YAxis,
   ResponsiveContainer,
+  TooltipProps,
 } from "recharts";
 import { graphql } from "relay-runtime";
 import { EuclideanDistanceTimeSeriesQuery } from "./__generated__/EuclideanDistanceTimeSeriesQuery.graphql";
+import { css } from "@emotion/react";
 
+const timeFormatter = timeFormat("%x");
+function TooltipContent({ active, payload, label }: TooltipProps<any, any>) {
+  if (active && payload && payload.length) {
+    return (
+      <div
+        css={(theme) => css`
+          background-color: ${theme.colors.gray900};
+          border: 1px solid transparent;
+          padding: ${theme.spacing.margin4}px;
+        `}
+      >
+        <p>{`${timeFormatter(new Date(label))}`}</p>
+        <p>{`${payload[0].value}`}</p>
+      </div>
+    );
+  }
+
+  return null;
+}
 export function EuclideanDistanceTimeSeries({
   embeddingDimensionId,
   timeRange,
@@ -53,31 +74,39 @@ export function EuclideanDistanceTimeSeries({
     <ResponsiveContainer width="100%" height="100%">
       <AreaChart
         data={chartData as unknown as any[]}
-        margin={{ top: 20, right: 50, left: 0, bottom: 10 }}
+        margin={{ top: 20, right: 50, left: 30, bottom: 10 }}
       >
         <defs>
           <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-            <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            <stop offset="5%" stopColor="#ffffff8e" stopOpacity={0.8} />
+            <stop offset="95%" stopColor="#ffffff8e" stopOpacity={0} />
           </linearGradient>
         </defs>
         <XAxis
           dataKey="timestamp"
           stroke={theme.colors.gray200}
           // TODO: Fix this to be a cleaner interface
-          tickFormatter={(x) => timeFormat("%x")(new Date(x))}
+          tickFormatter={(x) => timeFormatter(new Date(x))}
         />
-        <YAxis stroke={theme.colors.gray200} />
+        <YAxis
+          stroke={theme.colors.gray200}
+          label={{
+            value: "Euc. Distance",
+            angle: -90,
+            position: "insideLeft",
+            style: { textAnchor: "middle" },
+          }}
+        />
         <CartesianGrid
           strokeDasharray="3 3"
           stroke={theme.colors.gray200}
           strokeOpacity={0.3}
         />
-        <Tooltip />
+        <Tooltip content={<TooltipContent />} />
         <Area
           type="monotone"
           dataKey="value"
-          stroke="#8884d8"
+          stroke="#ffffff8e"
           fillOpacity={1}
           fill="url(#colorUv)"
         />
