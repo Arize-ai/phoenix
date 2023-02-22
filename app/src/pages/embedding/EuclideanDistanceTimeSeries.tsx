2b76c5f492ae74037dd1f71acf309b04f021b2c9
Mikyo King <mikyo@arize.com>
2023-02-22 16:55:57 -0700

    relay codegen

--- app/src/pages/embedding/EuclideanDistanceTimeSeries.tsx
+++ app/src/pages/embedding/EuclideanDistanceTimeSeries.tsx
@@ -60,8 +60,6 @@ export function EuclideanDistanceTimeSeries({
                 value
               }
             }
-            trafficTimeSeries: dataQualityTimeSeries(
-              m
           }
         }
       }
