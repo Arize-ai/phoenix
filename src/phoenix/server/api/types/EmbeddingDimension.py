326628fcc3255b680d44196bc9832b2c44b59427
Roger Yang <roger.h.yang@gmail.com>
2023-03-06 10:20:05 -0800

    remove try/except

--- src/phoenix/server/api/types/EmbeddingDimension.py
+++ src/phoenix/server/api/types/EmbeddingDimension.py
@@ -316,17 +316,11 @@ class EmbeddingDimension(Node):
             except SchemaError:
                 pass
 
-            try:
-                if col := dataset.get_embedding_link_to_data_column(self.name):
-                    link_to_data = col[row_id]
-            except SchemaError:
-                pass
+            if col := dataset.get_embedding_link_to_data_column(self.name):
+                link_to_data = col[row_id]
 
-            try:
-                if col := dataset.get_embedding_raw_data_column(self.name):
-                    raw_data = col[row_id]
-            except SchemaError:
-                pass
+            if col := dataset.get_embedding_raw_data_column(self.name):
+                raw_data = col[row_id]
 
             points[dataset_id].append(
                 UMAPPoint(
