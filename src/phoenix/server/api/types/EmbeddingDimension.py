f4d35223882498d55e603808a27928208bf12a85
Roger Yang <roger.h.yang@gmail.com>
2023-03-06 17:28:03 -0800

    fix conditional

--- src/phoenix/server/api/types/EmbeddingDimension.py
+++ src/phoenix/server/api/types/EmbeddingDimension.py
@@ -316,10 +316,12 @@ class EmbeddingDimension(Node):
             except SchemaError:
                 pass
 
-            if col := dataset.get_embedding_link_to_data_column(self.name):
+            col = dataset.get_embedding_link_to_data_column(self.name)
+            if len(col):
                 link_to_data = col[row_id]
 
-            if col := dataset.get_embedding_raw_data_column(self.name):
+            col = dataset.get_embedding_raw_data_column(self.name)
+            if len(col):
                 raw_data = col[row_id]
 
             points[dataset_id].append(
