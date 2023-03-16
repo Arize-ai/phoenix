f9d5a202f512029a9220b48f35da928d21346f20
Mikyo King <mikyo@arize.com>
2023-03-16 16:36:20 -0600

    docs: add module level docstring

--- src/phoenix/__init__.py
+++ src/phoenix/__init__.py
@@ -1,2 +1,18 @@
 from .datasets import Dataset, EmbeddingColumnNames, Schema, load_example
 from .session.session import active_session, close_app, launch_app
+
+# module level doc-string
+__doc__ = """
+arize-phoenix - ML Observability in a notebook
+=====================================================================
+**phoenix** is a Python package providing Phoenix provides MLOps insights at
+lightning speed with zero-config observability for model drift, performance, and
+data quality. 
+
+Main Features 
+------------- 
+Here are just a few of the things that phoenix does well:
+  - Compare two sets of model inferences against one another
+  - Identify problematic embeddings cohorts using UMAP and clustering
+  - Explore model performance, drift, and data quality metrics
+"""
