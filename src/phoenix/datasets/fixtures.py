1385a9bde8ab0936160f7cdf9eb989a247804100
Mikyo King <mikyo@arize.com>
2023-03-16 14:29:14 -0600

    rewrap

--- src/phoenix/datasets/fixtures.py
+++ src/phoenix/datasets/fixtures.py
@@ -37,17 +37,18 @@ sentiment_classification_language_drift_fixture = Fixture(
     name="sentiment_classification_language_drift",
     description="""
     Highlights issues that occur maintaining a sentiment classification model.
-    This  model takes online reviews of your U.S.-based product as the input and predicts whether
-    the reviewer's sentiment was positive, negative, or neutral.
+    This  model takes online reviews of your U.S.-based product as the input and
+    predicts whether the reviewer's sentiment was positive, negative, or
+    neutral.
 
-    You trained your sentiment classification model on English reviews.
-    However, once the model was released into production, you notice that the performance of the
-    model has degraded over a period of time.
+    You trained your sentiment classification model on English reviews. However,
+    once the model was released into production, you notice that the performance
+    of the model has degraded over a period of time.
 
-    Phoenix is able to surface the reason for this performance degradation.
-    In this example, the presence of reviews written in Spanish impact the model's performance.
-    You can surface and troubleshoot this issue by analyzing the embedding vectors associated
-    with the online review text.
+    Phoenix is able to surface the reason for this performance degradation. In
+    this example, the presence of reviews written in Spanish impact the model's
+    performance. You can surface and troubleshoot this issue by analyzing the
+    embedding vectors associated with the online review text.
     """,
     primary_schema=sentiment_classification_language_drift_schema,
     reference_schema=sentiment_classification_language_drift_schema,
@@ -77,11 +78,12 @@ fashion_mnist_reference_schema = replace(fashion_mnist_primary_schema, timestamp
 fashion_mnist_fixture = Fixture(
     name="fashion_mnist",
     description="""
-    Fashion-MNIST is a dataset of Zalando's article images consisting of a training set of
-    60,000 examples and a test set of 10,000 examples. Each example is a 28x28 grayscale image,
-    associated with a label from 10 classes. Fashion-MNIST serves as a direct drop-in replacement
-    for the original MNIST dataset for benchmarking machine learning algorithms.
-    It shares the same image size and structure of training and testing splits.
+    Fashion-MNIST is a dataset of Zalando's article images consisting of a
+    training set of 60,000 examples and a test set of 10,000 examples. Each
+    example is a 28x28 grayscale image, associated with a label from 10 classes.
+    Fashion-MNIST serves as a direct drop-in replacement for the original MNIST
+    dataset for benchmarking machine learning algorithms. It shares the same
+    image size and structure of training and testing splits.
     """,
     primary_schema=fashion_mnist_primary_schema,
     reference_schema=fashion_mnist_reference_schema,
@@ -110,15 +112,17 @@ ner_token_drift_fixture = Fixture(
     name="ner_token_drift",
     description="""
     You are in charge of maintaining a Named Entity Recognition (NER) model.
-    This simple model can automatically scan text, pull out some fundamental entities within it,
-    and classify them into predefined categories: Person, Location, or Organization.
-    However, once the model was released into production, you notice that the performance of the
-    model has degraded over a period of time.
+    This simple model can automatically scan text, pull out some fundamental
+    entities within it, and classify them into predefined categories: Person,
+    Location, or Organization. However, once the model was released into
+    production, you notice that the performance of the model has degraded over a
+    period of time.
 
-    Phoenix is able to surface the reason for this performance degradation.
-    In this example, text including locations is under-represented in the training set.
-    This label imbalance impacts the model's performance. You can surface and troubleshoot this
-    issue by analyzing the embedding vectors associated with the input text.
+    Phoenix is able to surface the reason for this performance degradation. In
+    this example, text including locations is under-represented in the training
+    set. This label imbalance impacts the model's performance. You can surface
+    and troubleshoot this issue by analyzing the embedding vectors associated
+    with the input text.
     """,
     primary_schema=ner_token_drift_schema,
     reference_schema=ner_token_drift_schema,
@@ -143,15 +147,17 @@ credit_card_fraud_schema = Schema(
 credit_card_fraud_fixture = Fixture(
     name="credit_card_fraud",
     description="""
-    Use-case for a credit card fraud detection model at a large bank or payment processing company.
+    Use-case for a credit card fraud detection model at a large bank or payment
+    processing company.
 
-    You have been alerted by a spike in credit card chargebacks leading you to suspect that
-    fraudsters are getting away with committing fraud undetected!
+    You have been alerted by a spike in credit card chargebacks leading you to
+    suspect that fraudsters are getting away with committing fraud undetected!
 
-    Realizing that this flaw in your model's performance has a heavy cost on your
-    company and customers, you understand the need for a powerful tools to troubleshoot and prevent
-    costly model degradations. You turn to Phoenix to find out what changed in your credit card
-    fraud detection model and how you can improve it.
+    Realizing that this flaw in your model's performance has a heavy cost on
+    your company and customers, you understand the need for a powerful tools to
+    troubleshoot and prevent costly model degradations. You turn to Phoenix to
+    find out what changed in your credit card fraud detection model and how you
+    can improve it.
     """,
     primary_schema=credit_card_fraud_schema,
     reference_schema=credit_card_fraud_schema,
@@ -174,17 +180,18 @@ click_through_rate_schema = Schema(
 click_through_rate_fixture = Fixture(
     name="click_through_rate",
     description="""
-    Investigate various performance related aspects of an online advertisement use-case.
-    These datasets are designed for analyzing Click-through Rate (CTR) performance.
+    Investigate various performance related aspects of an online advertisement
+    use-case. These datasets are designed for analyzing Click-through Rate (CTR)
+    performance.
 
-    You manage the models for an online advertising platform.
-    You have spent a great deal of your time collecting online data and training models
-    for best performance. With your models now in production you have no tools available to your
-    disposal to monitor the performance of your models, identify any issues, or get insights into
-    how to improve your models.
+    You manage the models for an online advertising platform. You have spent a
+    great deal of your time collecting online data and training models for best
+    performance. With your models now in production you have no tools available
+    to your disposal to monitor the performance of your models, identify any
+    issues, or get insights into how to improve your models.
 
-    This use-case highlights a common advertisement use-case and is tailored for analyzing CTR for
-    an ad or ad group.
+    This use-case highlights a common advertisement use-case and is tailored for
+    analyzing CTR for an ad or ad group.
     """,
     primary_schema=click_through_rate_schema,
     reference_schema=click_through_rate_schema,
@@ -205,8 +212,8 @@ wide_data_reference_schema = replace(wide_data_primary_schema, timestamp_column_
 wide_data_fixture = Fixture(
     name="wide_data",
     description="""
-    Use-case that for a wide data model (e.g. a large amount of features).
-    For developer use only.
+    Use-case that for a wide data model (e.g. a large amount of features). For
+    developer use only.
     """,
     primary_schema=wide_data_primary_schema,
     reference_schema=wide_data_reference_schema,
@@ -333,8 +340,7 @@ def load_example(use_case: str) -> DatasetDict:
     Parameters
     ----------
         use_case: str
-            Name of the phoenix supported use case
-            Valid values include:
+            Name of the phoenix supported use case Valid values include:
                 - "sentiment_classification_language_drift"
                 - "fashion_mnist"
                 - "ner_token_drift"
@@ -345,7 +351,8 @@ def load_example(use_case: str) -> DatasetDict:
     Returns
     _______
         datasets: DatasetDict
-            A dictionary of datasets, split out by dataset type (primary, reference).
+            A dictionary of datasets, split out by dataset type (primary,
+            reference).
 
     """
     fixture = _get_fixture_by_name(use_case)
