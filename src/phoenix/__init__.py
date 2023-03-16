c303fcc4b1ed440ccb4beea4b340e64a8cb55d80
Mikyo King <mikyo@arize.com>
2023-03-16 00:52:55 -0600

    feat: add links to app / docs on launch (#383)

--- src/phoenix/__init__.py
+++ src/phoenix/__init__.py
@@ -1,2 +1,2 @@
 from .datasets import Dataset, EmbeddingColumnNames, Schema, load_datasets
-from .session.session import close_app, launch_app
+from .session.session import active_session, close_app, launch_app
