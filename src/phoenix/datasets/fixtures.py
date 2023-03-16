25a10b050d44e34316512980959fb0c7e7724be3
Mikyo King <mikyo@arize.com>
2023-03-16 14:23:13 -0600

    pr comments

--- src/phoenix/datasets/fixtures.py
+++ src/phoenix/datasets/fixtures.py
@@ -77,7 +77,7 @@ fashion_mnist_reference_schema = replace(fashion_mnist_primary_schema, timestamp
 fashion_mnist_fixture = Fixture(
     name="fashion_mnist",
     description="""
-    Fashion-MNIST is a dataset of Zalando's article images—consisting of a training set of
+    Fashion-MNIST is a dataset of Zalando's article images consisting of a training set of
     60,000 examples and a test set of 10,000 examples. Each example is a 28x28 grayscale image,
     associated with a label from 10 classes. Fashion-MNIST serves as a direct drop-in replacement
     for the original MNIST dataset for benchmarking machine learning algorithms.
