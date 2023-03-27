af2ded5eee147b514b97ad0f08b486e352f532f3
Alexander Song <axiomofjoy@gmail.com>
2023-03-26 22:03:04 -0700

    chore: add ExampleDatasets as a top-level import

--- src/phoenix/datasets/__init__.py
+++ src/phoenix/datasets/__init__.py
@@ -1,5 +1,5 @@
 from .dataset import Dataset
-from .fixtures import load_example
+from .fixtures import load_example, ExampleDatasets
 from .schema import EmbeddingColumnNames, Schema
 
-__all__ = ["Dataset", "Schema", "EmbeddingColumnNames", "load_example"]
+__all__ = ["Dataset", "Schema", "EmbeddingColumnNames", "load_example", "ExampleDatasets"]
