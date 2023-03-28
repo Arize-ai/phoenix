7b1a6fcd63ab0f843f65c386fcfcba541a4c6d81
Roger Yang <roger.yang@arize.com>
2023-03-28 16:16:44 -0700

    allow download

--- src/phoenix/config.py
+++ src/phoenix/config.py
@@ -1,6 +1,7 @@
-import errno
 import tempfile
+from heapq import nlargest
 from pathlib import Path
+from typing import Iterator
 
 
 def _get_temp_path() -> Path:
@@ -14,15 +15,7 @@ def get_pids_path() -> Path:
     on the host machine. The directory will be created if it does not exist.
     """
     path = _get_temp_path() / "pids"
-    try:
-        path.mkdir(parents=True, exist_ok=True)
-    except OSError as e:
-        if e.errno == errno.EEXIST:
-            pass
-        else:
-            raise
-    else:
-        path.chmod(0o777)
+    path.mkdir(parents=True, exist_ok=True)
     return path
 
 
@@ -38,3 +31,30 @@ PHOENIX_DIR = Path(__file__).resolve().parent
 SERVER_DIR = PHOENIX_DIR / "server"
 # The port the server will run on after launch_app is called
 PORT = 6060
+
+
+def get_exported_files(
+    n_latest: int = 5,
+    directory: Path = EXPORT_DIR,
+    extension: str = "parquet",
+) -> Iterator[Path]:
+    """
+    Yields n most recently exported files by descending modification time.
+
+    Parameters
+    ----------
+    n_latest: int, optional, default=5
+        Specifies the number of the most recent exported files to return. If
+        there are fewer than n exported files then fewer than n files will
+        be returned.
+
+    Returns
+    -------
+    iterator: Iterator[Path]
+        An iterable of file path for the n most recent exported files.
+    """
+    yield from nlargest(
+        n_latest,
+        directory.glob("*." + extension),
+        lambda p: p.stat().st_mtime,
+    )
