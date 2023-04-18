92ed9a13045c0f68d0f7bc88195c359847a4b161
Mikyo King <mikyo@arize.com>
2023-04-18 10:58:20 -0600

    rm dummy data

--- app/src/components/pointcloud/EventItem.tsx
+++ app/src/components/pointcloud/EventItem.tsx
@@ -7,9 +7,6 @@ import { assertUnreachable } from "@phoenix/typeUtils";
 
 import { Shape, ShapeIcon } from "./ShapeIcon";
 
-// Dummy data
-const loremIpsum =
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 type EventItemSize = "small" | "medium" | "large";
 
 type PromptResponse = {
@@ -168,11 +165,7 @@ export function EventItem(props: EventItemProps) {
           }
         `}
       >
-        <EventPreview
-          previewType={primaryPreviewType}
-          {...props}
-          promptAndResponse={{ prompt: loremIpsum, response: loremIpsum }}
-        />
+        <EventPreview previewType={primaryPreviewType} {...props} />
         {secondaryPreviewType != null && (
           <EventPreview previewType={secondaryPreviewType} {...props} />
         )}
