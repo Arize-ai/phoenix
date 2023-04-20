931b7fb6e2b4b98ab2213b21f03d08a51b607012
Mikyo King <mikyo@arize.com>
2023-04-20 13:06:11 -0600

    fix: restore schema JSON serialization

--- tests/datasets/test_schema.py
+++ tests/datasets/test_schema.py
@@ -2,11 +2,28 @@ from phoenix.datasets import EmbeddingColumnNames, Schema
 
 
 def test_json_serialization():
-    schema = Schema(
+    s = Schema(
         feature_column_names=["feature_1"],
         embedding_feature_column_names={
             "embedding_feature": EmbeddingColumnNames(vector_column_name="embedding_vector")
         },
+    )
+
+    # serialize and deserialize.
+    p = s.to_json()
+    schema_from_json = Schema.from_json(p)
+
+    assert schema_from_json.embedding_feature_column_names is not None
+    assert schema_from_json.embedding_feature_column_names["embedding_feature"] is not None
+    assert (
+        schema_from_json.embedding_feature_column_names["embedding_feature"].vector_column_name
+        == "embedding_vector"
+    )
+
+
+def test_json_serialization_with_LLM():
+    s = Schema(
+        feature_column_names=["feature_1"],
         prompt_column_names=EmbeddingColumnNames(
             vector_column_name="prompt_vector",
             raw_data_column_name="prompt_text",
@@ -18,6 +35,8 @@ def test_json_serialization():
     )
 
     # serialize and deserialize.
-    schema_from_json = Schema.from_json(schema.to_json())
+    p = s.to_json()
+    schema_from_json = Schema.from_json(p)
 
-    assert schema_from_json == schema
+    assert schema_from_json.prompt_column_names is not None
+    assert schema_from_json.prompt_column_names.vector_column_name == "prompt_vector"
