4a9fd1034c208c3a100e9c8d4334f339591b22ea
Roger Yang <80478925+RogerHYang@users.noreply.github.com>
2023-04-21 17:27:13 -0700

    fix: floor datetime to minute (#638)

--- src/phoenix/core/model_schema.py
+++ src/phoenix/core/model_schema.py
@@ -478,8 +478,8 @@ class Events(ModelData):
         # open and one minute is the smallest interval allowed.
         stop_time = end_time + timedelta(minutes=1)
         # Round down to the nearest minute.
-        start = start_time.replace(second=0, microsecond=0)
-        stop = stop_time.replace(second=0, microsecond=0)
+        start = _floor_to_minute(start_time)
+        stop = _floor_to_minute(stop_time)
         return TimeRange(start, stop)
 
     def __iter__(self) -> Iterator[Event]:
@@ -1072,3 +1072,16 @@ _id_pat = re.compile(r"\bid\b", re.IGNORECASE)
 def _title_case_no_underscore(name: str) -> str:
     """E.g. `PREDICTION_ID` turns into `Prediction ID`"""
     return _id_pat.sub("ID", name.replace("_", " ").title())
+
+
+MINUTE_DATETIME_FORMAT = "%Y-%m-%dT%H:%M:00%z"
+
+
+def _floor_to_minute(t: datetime) -> datetime:
+    """Floor datetime to the minute by taking a round-trip through string
+    format because there isn't always an available function to strip the
+    nanoseconds if present."""
+    return datetime.strptime(
+        t.astimezone(timezone.utc).strftime(MINUTE_DATETIME_FORMAT),
+        MINUTE_DATETIME_FORMAT,
+    ).astimezone(timezone.utc)
