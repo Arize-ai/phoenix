23a8dcfe4d34f8ef6460cd54c6fce8666782f201
Mikyo King <mikyo@arize.com>
2023-04-27 14:43:39 -0600

    v0.0.18

--- src/phoenix/__init__.py
+++ src/phoenix/__init__.py
@@ -7,7 +7,7 @@ from .datasets import (
 )
 from .session import Session, active_session, close_app, launch_app
 
-__version__ = "0.0.17"
+__version__ = "0.0.18"
 
 # module level doc-string
 __doc__ = """
