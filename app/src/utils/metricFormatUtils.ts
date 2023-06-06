c8d7f204d5ecf86e50555d9d6bb07e0fea0423f2
Mikyo King <mikyo@arize.com>
2023-06-06 14:04:17 -0600

    cleanup

--- app/src/utils/metricFormatUtils.ts
+++ app/src/utils/metricFormatUtils.ts
@@ -1,27 +1,32 @@
-type MetricDescription = {
+type MetricFormattingDefinition = {
   name: string;
   shortName: string;
-  description?: string;
+  definition?: string;
 };
 
-const METRIC_DESCRIPTIONS: Record<string, MetricDescription> = {
+const METRIC_DEFINITIONS: Record<
+  string,
+  MetricFormattingDefinition | undefined
+> = {
   euclideanDistance: {
     name: "Euclidean Distance",
     shortName: "Euc. Distance",
-    description: `Euclidean distance over time captures how much your primary dataset's embeddings are drifting from the reference data. Euclidean distance of the embeddings is calculated by taking the centroid of the embedding vectors for each dataset and calculating the distance between the two centroids.`,
+    definition: `Euclidean distance over time captures how much your primary dataset's embeddings are drifting from the reference data. Euclidean distance of the embeddings is calculated by taking the centroid of the embedding vectors for each dataset and calculating the distance between the two centroids.`,
   },
 } as const;
 
 export function getMetricNameByMetricKey(metricKey: string): string {
-  if (METRIC_DESCRIPTIONS[metricKey]) {
-    return METRIC_DESCRIPTIONS[metricKey].name;
+  const definition = METRIC_DEFINITIONS[metricKey];
+  if (definition != null) {
+    return definition.name;
   }
   return metricKey;
 }
 
 export function getMetricShortNameByMetricKey(metricKey: string): string {
-  if (METRIC_DESCRIPTIONS[metricKey]) {
-    return METRIC_DESCRIPTIONS[metricKey].shortName;
+  const definition = METRIC_DEFINITIONS[metricKey];
+  if (definition != null) {
+    return definition.shortName;
   }
   return metricKey;
 }
@@ -29,11 +34,9 @@ export function getMetricShortNameByMetricKey(metricKey: string): string {
 export function getMetricDescriptionByMetricKey(
   metricKey: string
 ): string | null {
-  if (
-    METRIC_DESCRIPTIONS[metricKey] &&
-    METRIC_DESCRIPTIONS[metricKey].description != null
-  ) {
-    return METRIC_DESCRIPTIONS[metricKey].description || null;
+  const definition = METRIC_DEFINITIONS[metricKey];
+  if (definition && definition.definition != null) {
+    return definition.definition || null;
   }
   return null;
 }
