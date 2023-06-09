64f33fb0b03876d71d64eed05f02406c5f210116
Roger Yang <roger.yang@arize.com>
2023-06-09 13:20:13 -0700

    add prediction id

--- src/phoenix/server/api/types/EventMetadata.py
+++ src/phoenix/server/api/types/EventMetadata.py
@@ -9,6 +9,7 @@ from phoenix.server.api.interceptor import GqlValueMediator
 class EventMetadata:
     """A partial record of a model event. E.g. the predictions and actuals"""
 
+    prediction_id: Optional[str] = strawberry.field(default=GqlValueMediator())
     prediction_score: Optional[float] = strawberry.field(default=GqlValueMediator())
     prediction_label: Optional[str] = strawberry.field(default=GqlValueMediator())
     actual_score: Optional[float] = strawberry.field(default=GqlValueMediator())
