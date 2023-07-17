b67d1abe2c432728b575e5835ef134dce77fa662
Mikyo King <mikyo@arize.com>
2023-07-17 13:59:44 -0600

    ci: bump hdbscan to deal with cython builds (#936)
    * v0.0.28
    
    * ci: bump hdbscan
    
    * Update pyproject.toml
    
    Co-authored-by: Kiko Castillo <fjcasti1@asu.edu>
    
    * Update pyproject.toml
    
    ---------
    
    Co-authored-by: Kiko Castillo <fjcasti1@asu.edu>

--- src/phoenix/__init__.py
+++ src/phoenix/__init__.py
@@ -3,7 +3,7 @@ from .datasets.fixtures import ExampleDatasets, load_example
 from .datasets.schema import EmbeddingColumnNames, RetrievalEmbeddingColumnNames, Schema
 from .session.session import Session, active_session, close_app, launch_app
 
-__version__ = "0.0.27"
+__version__ = "0.0.28"
 
 # module level doc-string
 __doc__ = """
