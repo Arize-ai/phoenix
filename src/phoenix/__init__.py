440eef784a986eb91ae2fae292c38ab40733d7f0
Mikyo King <mikyo@arize.com>
2023-07-25 19:08:45 -0600

    v0.0.30

--- src/phoenix/__init__.py
+++ src/phoenix/__init__.py
@@ -3,7 +3,7 @@ from .datasets.fixtures import ExampleDatasets, load_example
 from .datasets.schema import EmbeddingColumnNames, RetrievalEmbeddingColumnNames, Schema
 from .session.session import Session, active_session, close_app, launch_app
 
-__version__ = "0.0.29rc8"
+__version__ = "0.0.30"
 
 # module level doc-string
 __doc__ = """
