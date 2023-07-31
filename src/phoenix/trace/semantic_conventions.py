1d9ad3448b186d406dad1395543f4fc043e5fc6b
Mikyo King <mikyo@arize.com>
2023-07-31 13:16:30 -0600

    feat(tracing): define span schema

---
+++ src/phoenix/trace/semantic_conventions.py
@@ -0,0 +1,37 @@
+"""
+Semantic conventions for the attributes of a span
+
+Inspiration from OpenTelemetry:
+https://opentelemetry.io/docs/specs/otel/trace/semantic_conventions/span-general/
+"""
+from dataclasses import dataclass
+from typing import Any, Dict
+
+
+@dataclass(frozen=True)
+class AttributeDescription(Dict[str, Any]):
+    id: str
+    brief: str
+    type: str
+
+
+class AttributeGroup:
+    """
+    AttributeGroup is a collection of attributes that are
+    semantically related to each other
+    """
+
+    id: str
+    attributes: dict[str, AttributeDescription]
+
+
+class DeploymentAttributes(AttributeGroup):
+    id = "deployment"
+    prefix = "deployment"
+    attributes = {
+        "environment": AttributeDescription(
+            id="deployment.environment",
+            brief="The environment where the service is deployed",
+            type="string",
+        ),
+    }
---
+++ tests/trace/test_schemas.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+from uuid import uuid4
+
+from phoenix.trace.schemas import Span, SpanContext, SpanException
+from phoenix.trace.semantic_conventions import DeploymentAttributes
+
+
+def test_span_construction():
+    span = Span(
+        name="test",
+        parent_id=None,
+        start_time=datetime.now(),
+        end_time=datetime.now(),
+        span_kind="TOOL",
+        status_code="OK",
+        status_message="",
+        attributes={},
+        events=[],
+        context=SpanContext(trace_id=uuid4(), span_id=uuid4()),
+        conversation=None,
+    )
+    assert span.name == "test"
+
+
+def test_span_with_exception():
+    span = Span(
+        name="exception-span",
+        parent_id=None,
+        start_time=datetime.now(),
+        end_time=datetime.now(),
+        span_kind="TOOL",
+        status_code="OK",
+        status_message="",
+        attributes={
+            DeploymentAttributes.attributes["environment"].id: "dev",
+        },
+        events=[SpanException(timestamp=datetime.now(), message="")],
+        context=SpanContext(trace_id=uuid4(), span_id=uuid4()),
+        conversation=None,
+    )
+    assert span.name == "exception-span"
+    assert span.events[0].name == "exception"
+    assert span.attributes["deployment.environment"] == "dev"
