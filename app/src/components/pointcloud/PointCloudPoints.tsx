2fc5e64a1475009cf84978e9b1c18ad4d7d49507
Mikyo King <mikyo@arize.com>
2023-08-01 18:49:13 -0600

    fix

--- app/src/components/pointcloud/PointCloudPoints.tsx
+++ app/src/components/pointcloud/PointCloudPoints.tsx
@@ -158,7 +158,6 @@ export function PointCloudPoints({
     debouncedSetHoveredEventId(null);
   }, [debouncedSetHoveredEventId]);
 
-  console.log(pointSizeScale);
   return (
     <>
       {datasetVisibility.primary ? (
