3928e85a8a3def78fd2f25194c742845d09fbe66
Mikyo King <mikyo@arize.com>
2023-08-08 14:31:00 -0700

    feat(tracing): spans table

---
+++ app/src/pages/trace/SpansTable.tsx
@@ -0,0 +1,70 @@
+import React, { useMemo } from "react";
+import { graphql, usePaginationFragment } from "react-relay";
+import { Column } from "react-table";
+
+import { Table } from "@phoenix/components/table";
+
+import { SpansTable_spans$key } from "./__generated__/SpansTable_spans.graphql";
+type SpansTableProps = {
+  query: SpansTable_spans$key;
+};
+export function SpansTable(props: SpansTableProps) {
+  const { data } = usePaginationFragment(
+    graphql`
+      fragment SpansTable_spans on Query
+      @refetchable(queryName: "SpansTableSpansQuery")
+      @argumentDefinitions(
+        count: { type: "Int", defaultValue: 50 }
+        cursor: { type: "String", defaultValue: null }
+      ) {
+        spans(first: $count, after: $cursor)
+          @connection(key: "SpansTable_spans") {
+          edges {
+            span: node {
+              spanKind
+              name
+              context {
+                spanId
+                traceId
+              }
+            }
+          }
+        }
+      }
+    `,
+    props.query
+  );
+
+  const tableData = useMemo(
+    () =>
+      data.spans.edges.map(({ span }) => {
+        // Normalize the data
+        return {
+          ...span,
+          trace_id: span.context.traceId,
+          span_id: span.context.spanId,
+        };
+      }),
+    [data]
+  );
+  type TableRow = (typeof tableData)[number];
+  const columns: Column<TableRow>[] = [
+    {
+      Header: "trace_id",
+      accessor: "trace_id",
+    },
+    {
+      Header: "span_id",
+      accessor: "span_id",
+    },
+    {
+      Header: "kind",
+      accessor: "spanKind",
+    },
+    {
+      Header: "name",
+      accessor: "name",
+    },
+  ];
+  return <Table columns={columns} data={tableData} />;
+}
