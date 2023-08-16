aebe4fb864cb6fb4f273db7f716230a54dfc453e
Alexander Song <axiomofjoy@gmail.com>
2023-08-15 23:38:01 -0700

    implement and test llm attributes for invocation params, function calls, and tokens

--- src/phoenix/experimental/callbacks/langchain_tracer.py
+++ src/phoenix/experimental/callbacks/langchain_tracer.py
@@ -10,15 +10,23 @@ from phoenix.trace.schemas import Span, SpanKind, SpanStatusCode
 from phoenix.trace.semantic_conventions import (
     INPUT_MIME_TYPE,
     INPUT_VALUE,
+    LLM_COMPLETION_TOKENS,
+    LLM_FUNCTION_CALL,
     LLM_INVOCATION_PARAMETERS,
+    LLM_MODEL_NAME,
     LLM_PROMPT_TEMPLATE,
     LLM_PROMPT_TEMPLATE_VARIABLES,
     LLM_PROMPT_TEMPLATE_VERSION,
+    LLM_PROMPT_TOKENS,
+    LLM_TOTAL_TOKENS,
     OUTPUT_MIME_TYPE,
     OUTPUT_VALUE,
     MimeType,
 )
 from phoenix.trace.tracer import Tracer
+
+Role = str
+Message = str
 
 logger = logging.getLogger(__name__)
 
@@ -69,13 +77,56 @@ def _prompt_template(serialized: Dict[str, Any]) -> Iterator[Tuple[str, Any]]:
                 yield LLM_PROMPT_TEMPLATE_VARIABLES, kwargs.get("input_variables", [])
                 yield LLM_PROMPT_TEMPLATE_VERSION, "unknown"
                 break
-        except TypeError:
+        except (AttributeError, KeyError, TypeError):
             continue
 
 
-def _invocation_params(extra: Dict[str, Any]) -> Iterator[Tuple[str, Any]]:
-    if "invocation_params" in extra:
-        yield LLM_INVOCATION_PARAMETERS, extra["invocation_params"]
+def _invocation_parameters(extra: Dict[str, Any]) -> Iterator[Tuple[str, Any]]:
+    for key, value in extra.get("invocation_params", {}).items():
+        yield ".".join([LLM_INVOCATION_PARAMETERS, key]), value
+
+
+def _model_name(extra: Dict[str, Any]) -> Iterator[Tuple[str, str]]:
+    for key in ["model_name", "model"]:
+        try:
+            yield LLM_MODEL_NAME, extra["invocation_params"][key]
+            break
+        except (KeyError, TypeError):
+            continue
+
+
+def _token_usage(outputs: Dict[str, Any]) -> Iterator[Tuple[str, int]]:
+    for attribute_name, key in [
+        (LLM_PROMPT_TOKENS, "prompt_tokens"),
+        (LLM_COMPLETION_TOKENS, "completion_tokens"),
+        (LLM_TOTAL_TOKENS, "total_tokens"),
+    ]:
+        try:
+            yield attribute_name, outputs["llm_output"]["token_usage"][key]
+        except (KeyError, TypeError):
+            continue
+
+
+def _function_calls(
+    serialized: Dict[str, Any], outputs: Dict[str, Any]
+) -> Iterator[Tuple[str, int]]:
+    try:
+        function_name = outputs["generations"][0][0]["message"]["kwargs"]["additional_kwargs"][
+            "function_call"
+        ]["name"]
+        arguments = json.loads(
+            outputs["generations"][0][0]["message"]["kwargs"]["additional_kwargs"]["function_call"][
+                "arguments"
+            ]
+        )
+        yield ".".join([LLM_FUNCTION_CALL, function_name]), arguments
+    except (KeyError, IndexError, TypeError):
+        ...
+
+
+def _messages(inputs: Dict[str, Any]) -> Iterator[Tuple[str, Tuple[Role, Message]]]:
+    # TODO: implement
+    ...
 
 
 class OpenInferenceTracer(Tracer, BaseTracer):
@@ -91,7 +142,11 @@ class OpenInferenceTracer(Tracer, BaseTracer):
         }.items():
             attributes.update(zip(io_attributes, _convert_io(run.get(io_key))))
         attributes.update(_prompt_template(run["serialized"]))
-        # attributes.update(_invocation_params(run["extra"]))
+        attributes.update(_invocation_parameters(run["extra"]))
+        attributes.update(_model_name(run["extra"]))
+        attributes.update(_token_usage(run["outputs"]))
+        attributes.update(_function_calls(run["serialized"], run["outputs"]))
+        attributes.update(_messages(run["inputs"]))
         span = self.create_span(
             name=run["name"],
             span_kind=_langchain_run_type_to_span_kind(run["run_type"]),
--- tests/experimental/callbacks/test_langchain_tracer.py
+++ tests/experimental/callbacks/test_langchain_tracer.py
@@ -1,12 +1,21 @@
 from json import loads
+from typing import Type
 from uuid import UUID
 
 import numpy as np
+import pytest
+from langchain import LLMChain, PromptTemplate
+from langchain.agents import AgentType, initialize_agent
+from langchain.agents.agent import AgentExecutor
 from langchain.chains import RetrievalQA
+from langchain.chains.openai_functions import create_openai_fn_chain
 from langchain.chains.retrieval_qa.prompt import PROMPT as RETRIEVAL_QA_PROMPT
+from langchain.chat_models import ChatOpenAI
 from langchain.embeddings.fake import FakeEmbeddings
+from langchain.llms import BaseLLM, OpenAI
 from langchain.llms.fake import FakeListLLM
 from langchain.retrievers import KNNRetriever
+from langchain.tools import tool
 from phoenix.experimental.callbacks.langchain_tracer import OpenInferenceTracer
 from phoenix.trace.schemas import SpanKind, SpanStatusCode
 from phoenix.trace.semantic_conventions import (
@@ -21,6 +30,7 @@ from phoenix.trace.semantic_conventions import (
 )
 from phoenix.trace.span_json_decoder import json_string_to_span
 from phoenix.trace.span_json_encoder import span_to_json
+from pydantic import BaseModel, Field
 
 
 def test_tracer_llm() -> None:
@@ -96,3 +106,120 @@ def test_tracer_llm() -> None:
 
     for span in spans.values():
         assert json_string_to_span(span_to_json(span)) == span
+
+
+@pytest.fixture
+def tracer() -> OpenInferenceTracer:
+    """An OpenInference tracer"""
+    return OpenInferenceTracer()
+
+
+@pytest.mark.parametrize(
+    "llm_class,model_name",
+    [
+        pytest.param(OpenAI, "gpt-3.5-turbo", id="openai-llm"),
+        pytest.param(ChatOpenAI, "gpt-3.5-turbo", id="openai-chat-model"),
+    ],
+)
+def test_tracer_records_common_llm_attributes_for_llm_chain(
+    llm_class: Type[BaseLLM], model_name: str, tracer: OpenInferenceTracer
+) -> None:
+    temperature = 0.75
+    llm = llm_class()
+    llm = llm_class(model_name=model_name, temperature=temperature)
+    prompt_template = PromptTemplate.from_template("What is the complementary color of {color}?")
+    chain = LLMChain(prompt=prompt_template, llm=llm)
+    response = chain.run({"color": "blue"}, callbacks=[tracer])
+
+    assert "orange" in response.lower()
+    span = next(span for span in tracer.span_buffer if span.span_kind == SpanKind.LLM)
+    attributes = span.attributes
+    assert attributes["llm.invocation_parameters.temperature"] == temperature
+    assert attributes["llm.model_name"] == model_name
+    assert isinstance(attributes["llm.prompt_tokens"], int)
+    assert isinstance(attributes["llm.completion_tokens"], int)
+    assert isinstance(attributes["llm.total_tokens"], int)
+
+
+class TestTracerFunctionCallAttributes:
+    def test_tracer_records_function_call_attributes_for_openai_function_chain(
+        self, tracer: OpenInferenceTracer
+    ) -> None:
+        functions = [
+            {
+                "name": "get_current_weather",
+                "description": "Get the current weather in a given location",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "city": {
+                            "type": "string",
+                            "description": "The city name, e.g., San Francisco",
+                        },
+                    },
+                    "required": ["city"],
+                },
+            },
+            {
+                "name": "get_current_time",
+                "description": "Get the current time in a given location",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "city": {
+                            "type": "string",
+                            "description": "The city name, e.g., San Francisco",
+                        },
+                    },
+                    "required": ["city"],
+                },
+            },
+        ]
+        prompt_template = PromptTemplate.from_template("What is the current weather in {location}?")
+        llm = ChatOpenAI(model_name="gpt-4-0613")
+        chain = create_openai_fn_chain(functions=functions, llm=llm, prompt=prompt_template)
+        tracer = OpenInferenceTracer()
+        chain.run("the capital city of England", callbacks=[tracer])
+
+        span = next(span for span in tracer.span_buffer if span.name == "ChatOpenAI")
+        assert span.attributes["llm.function_call.get_current_weather"] == {"city": "London"}
+
+    def test_tracer_records_function_call_attributes_when_openai_function_agent_uses_tool(
+        self, agent: AgentExecutor, tracer: OpenInferenceTracer
+    ) -> None:
+        response = agent.run('How many characters are in the word "hello"?', callbacks=[tracer])
+
+        assert "5" in response or "five" in response.lower()
+        span = next(span for span in tracer.span_buffer if span.name == "ChatOpenAI")
+        attributes = span.attributes
+        assert "llm.function_call.tool_selection" in attributes
+
+    def test_tracer_omits_function_call_attributes_when_openai_function_agent_skips_tool(
+        self, agent: AgentExecutor, tracer: OpenInferenceTracer
+    ) -> None:
+        response = agent.run("Who won the World Cup in 2018?", callbacks=[tracer])
+
+        assert "France" in response or "French" in response
+        span = next(span for span in tracer.span_buffer if span.name == "ChatOpenAI")
+        attributes = span.attributes
+        assert "llm.function_call.tool_selection" not in attributes
+
+    @pytest.fixture
+    def agent(self) -> AgentExecutor:
+        """Returns an OpenAI functions agent that uses a tool to count the letters in a word."""
+
+        class CountCharactersToolArgumentSchema(BaseModel):
+            """A schema for the arguments of the count_characters tool."""
+
+            string: str = Field(description="input string whose characters will be counted")
+
+        @tool("count_letter", args_schema=CountCharactersToolArgumentSchema)
+        def count_characters(string: str) -> int:
+            """A tool that counts the characters in an input string."""
+            return len(string)
+
+        return initialize_agent(
+            tools=[count_characters],
+            llm=ChatOpenAI(temperature=0, model="gpt-4-0613"),
+            agent=AgentType.OPENAI_MULTI_FUNCTIONS,
+        )
