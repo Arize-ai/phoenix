91f9243c8cc9b231ae6b9d50bfa47134d0881367
Roger Yang <roger.yang@arize.com>
2023-08-17 10:00:30 -0700

    clean up

--- src/phoenix/core/traces.py
+++ src/phoenix/core/traces.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from typing import DefaultDict, Iterable, List, cast
+from typing import Iterable, Tuple, cast
 
 from pandas import DataFrame
 
@@ -16,18 +16,14 @@ class Traces:
     def __init__(self, dataframe: DataFrame):
         dataframe = dataframe.set_index("context.span_id", drop=False)
         self._dataframe = dataframe
-        self._adjacency_lists = _build_adjacency_lists(dataframe)
+        self._adjacency_lists = defaultdict(list)
+        for span_id, parent_id in cast(
+            Iterable[Tuple[SpanID, SpanID]],
+            dataframe.loc[:, "parent_id"].dropna().items(),
+        ):
+            self._adjacency_lists[parent_id].append(span_id)
 
     def get_descendant_span_ids(self, span_id: SpanID) -> Iterable[SpanID]:
         for span_id in self._adjacency_lists[span_id]:
             yield span_id
             yield from self.get_descendant_span_ids(span_id)
-
-
-def _build_adjacency_lists(
-    df: DataFrame,
-) -> DefaultDict[SpanID, List[SpanID]]:
-    adjacency_lists = defaultdict(list)
-    for span_id, parent_id in df.loc[:, "parent_id"].dropna().items():
-        adjacency_lists[parent_id].append(span_id)
-    return cast(DefaultDict[SpanID, List[SpanID]], adjacency_lists)
