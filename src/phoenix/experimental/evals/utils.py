9eadbdce17df172747c37792a513e10a41908ee4
Alexander Song <axiomofjoy@gmail.com>
2023-08-30 22:04:11 -0700

    incorporate pr feedback

--- src/phoenix/experimental/evals/utils.py
+++ src/phoenix/experimental/evals/utils.py
@@ -3,8 +3,10 @@ Utility functions for evaluations.
 """
 
 import json
+from io import BytesIO
 from urllib.error import HTTPError
 from urllib.request import urlopen
+from zipfile import ZipFile
 
 import pandas as pd
 
@@ -19,9 +21,13 @@ def download_benchmark_dataset(task: str, dataset_name: str) -> pd.DataFrame:
     Returns:
         pandas.DataFrame: A pandas dataframe containing the data.
     """
-    url = f"http://storage.googleapis.com/arize-assets/phoenix/evals/{task}/{dataset_name}.jsonl"
+    jsonl_file_name = f"{dataset_name}.jsonl"
+    url = f"http://storage.googleapis.com/arize-assets/phoenix/evals/{task}/{jsonl_file_name}.zip"
     try:
-        with urlopen(url) as f:
-            return pd.DataFrame([json.loads(line.decode()) for line in f.readlines()])
+        with urlopen(url) as response:
+            zip_byte_stream = BytesIO(response.read())
+            with ZipFile(zip_byte_stream) as zip_file:
+                with zip_file.open(jsonl_file_name) as jsonl_file:
+                    return pd.DataFrame(map(json.loads, jsonl_file.readlines()))
     except HTTPError:
         raise ValueError(f'Dataset "{dataset_name}" for "{task}" task does not exist.')
