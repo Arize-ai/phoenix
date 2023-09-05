d41e48f1a7c26dff5de09cc8959486a8654deb44
Roger Yang <roger.yang@arize.com>
2023-09-05 13:19:27 -0700

    remove proto script

--- scripts/recompile_protos.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd "$(git rev-parse --show-toplevel)"
-python3 -m grpc_tools.protoc \
-    -I src/phoenix/proto/ \
-    --python_out=src/phoenix/. \
-    --mypy_out=src/phoenix/. \
-    src/phoenix/proto/trace/v1/trace.proto
