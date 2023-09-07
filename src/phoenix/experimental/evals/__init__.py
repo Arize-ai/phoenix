7da51107a88403d455df8a6080a94e3197439ef5
Xander Song <axiomofjoy@gmail.com>
2023-09-07 02:37:38 -0700

    feat: add run_eval function (#1250)
    * add run_eval function
    
    * cleanup
    
    * tweak
    
    * handle none values
    
    * add unit tests
    
    * add docstring
    
    * change variable name to match
    
    * incorporate pr feedback
    
    * remove warning, add in progress bar

--- src/phoenix/experimental/evals/__init__.py
+++ src/phoenix/experimental/evals/__init__.py
@@ -1,12 +1,15 @@
-from .functions import llm_eval_binary
+from .functions import llm_eval_binary, run_relevance_eval
 from .models import OpenAiModel
+from .retrievals import compute_precisions_at_k
 from .templates import RAG_RELEVANCY_PROMPT_TEMPLATE_STR, PromptTemplate
 from .utils.downloads import download_benchmark_dataset
 
 __all__ = [
+    "compute_precisions_at_k",
     "download_benchmark_dataset",
     "llm_eval_binary",
     "OpenAiModel",
     "PromptTemplate",
     "RAG_RELEVANCY_PROMPT_TEMPLATE_STR",
+    "run_relevance_eval",
 ]
