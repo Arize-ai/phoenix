708ed75673c500970dbea8996f7f252fb2673580
Alexander Song <axiomofjoy@gmail.com>
2023-09-11 22:41:15 -0700

    feat: hallucination evals

--- src/phoenix/experimental/evals/__init__.py
+++ src/phoenix/experimental/evals/__init__.py
@@ -1,7 +1,11 @@
 from .functions import llm_eval_binary, run_relevance_eval
 from .models import OpenAiModel
 from .retrievals import compute_precisions_at_k
-from .templates import RAG_RELEVANCY_PROMPT_TEMPLATE_STR, PromptTemplate
+from .templates import (
+    HALLUCINATION_PROMPT_TEMPLATE_STR,
+    RAG_RELEVANCY_PROMPT_TEMPLATE_STR,
+    PromptTemplate,
+)
 from .utils.downloads import download_benchmark_dataset
 
 __all__ = [
@@ -10,6 +14,7 @@ __all__ = [
     "llm_eval_binary",
     "OpenAiModel",
     "PromptTemplate",
+    "HALLUCINATION_PROMPT_TEMPLATE_STR",
     "RAG_RELEVANCY_PROMPT_TEMPLATE_STR",
     "run_relevance_eval",
 ]
