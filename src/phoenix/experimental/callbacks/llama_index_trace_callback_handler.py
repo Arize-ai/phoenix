78e7e966c57ceb0ea82d1642abb1d260e2e49a0d
Roger Yang <80478925+RogerHYang@users.noreply.github.com>
2023-09-14 14:03:51 -0700

    feat: add LLM invocation parameters for llama-index (#1334)
    * add invocation params

--- src/phoenix/experimental/callbacks/llama_index_trace_callback_handler.py
+++ src/phoenix/experimental/callbacks/llama_index_trace_callback_handler.py
@@ -9,6 +9,7 @@ observability solutions such as Arize and Phoenix.
 For more information on the specification, see
 https://github.com/Arize-ai/open-inference-spec
 """
+import json
 import logging
 from collections import defaultdict
 from datetime import datetime
@@ -39,6 +40,7 @@ from phoenix.trace.semantic_conventions import (
     EMBEDDING_VECTOR,
     INPUT_MIME_TYPE,
     INPUT_VALUE,
+    LLM_INVOCATION_PARAMETERS,
     LLM_MESSAGES,
     LLM_MODEL_NAME,
     LLM_TOKEN_COUNT_COMPLETION,
@@ -149,6 +151,14 @@ def payload_to_semantic_attributes(
         if event_type is CBEventType.LLM:
             if model_name := serialized.get("model"):
                 attributes[LLM_MODEL_NAME] = model_name
+                attributes[LLM_INVOCATION_PARAMETERS] = json.dumps(
+                    {
+                        "model": model_name,
+                        "temperature": serialized["temperature"],
+                        "max_tokens": serialized["max_tokens"],
+                        **serialized["additional_kwargs"],
+                    }
+                )
     return attributes
 
 
