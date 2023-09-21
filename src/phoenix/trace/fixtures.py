33a6af18044fecaae95ad602e132217ca4e5c593
Mikyo King <mikyo@arize.com>
2023-09-20 23:34:29 -0400

    add more styles

--- src/phoenix/trace/fixtures.py
+++ src/phoenix/trace/fixtures.py
@@ -22,7 +22,7 @@ llama_index_rag_fixture = TracesFixture(
 llama_index_calculator_agent = TracesFixture(
     name="llama_index_calculator_agent",
     description="Traces from running the llama_index with calculator tools.",
-    file_name="llama_index_calculator_agent.jsonl",
+    file_name="llama_index_calculator_agent_v2.jsonl",
 )
 
 llama_index_rag_fixture_with_davinci = TracesFixture(
