34aab6f7760d1ee925ea1eccf7f52687a3ee250e
Mikyo King <mikyo@arize.com>
2023-09-26 16:12:51 -0600

    pr comments

--- src/phoenix/trace/langchain/instrumentor.py
+++ src/phoenix/trace/langchain/instrumentor.py
@@ -15,26 +15,26 @@ class LangChainInstrumentor:
     def instrument(self) -> None:
         try:
             from langchain.callbacks.base import BaseCallbackManager
-
-            source_init = BaseCallbackManager.__init__
-
-            # Keep track of the source init so we can tell if the patching occurred
-            self._source_callback_manager_init = source_init
-
-            tracer = self._tracer
-
-            # Patch the init method of the BaseCallbackManager to add the tracer
-            # to all callback managers
-            def patched_init(self: BaseCallbackManager, *args: Any, **kwargs: Any) -> None:
-                source_init(self, *args, **kwargs)
-                self.add_handler(tracer, True)
-
-            BaseCallbackManager.__init__ = patched_init  # type: ignore
         except ImportError:
             # Raise a cleaner error if LangChain is not installed
             raise ImportError(
                 "LangChain is not installed. Please install LangChain first to use the instrumentor"
             )
+
+        source_init = BaseCallbackManager.__init__
+
+        # Keep track of the source init so we can tell if the patching occurred
+        self._source_callback_manager_init = source_init
+
+        tracer = self._tracer
+
+        # Patch the init method of the BaseCallbackManager to add the tracer
+        # to all callback managers
+        def patched_init(self: BaseCallbackManager, *args: Any, **kwargs: Any) -> None:
+            source_init(self, *args, **kwargs)
+            self.add_handler(tracer, True)
+
+        BaseCallbackManager.__init__ = patched_init  # type: ignore
 
     from langchain.callbacks.base import BaseCallbackManager
 
