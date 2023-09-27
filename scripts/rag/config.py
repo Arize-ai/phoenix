82bfea5389a7be1a906bfede1b9a5967feb92115
Mikyo King <mikyo@arize.com>
2023-09-26 21:31:38 -0600

    mikyo: spelling / config

--- scripts/rag/config.py
+++ scripts/rag/config.py
@@ -1,2 +1,4 @@
-open_ai_key = ""
-cohere_key = ""
+import os
+
+open_ai_key = os.environ.get("OPENAI_API_KEY", "MISSING_OPENAI_API_KEY")
+cohere_key = os.environ.get("COHERE_API_KEY", "COHERE_API_KEY")
