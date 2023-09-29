e77175b8cc4892d4677498333e0732089d764f72
Francisco Castillo <kiko@arize.com>
2023-09-29 14:44:01 -0700

    wip

--- src/phoenix/experimental/evals/functions/__init__.py
+++ src/phoenix/experimental/evals/functions/__init__.py
@@ -1,5 +1,4 @@
 from .binary import llm_eval_binary, run_relevance_eval
-from .common import NOT_PARSABLE
 from .generate import llm_generate
 
-__all__ = ["llm_eval_binary", "run_relevance_eval", "llm_generate", "NOT_PARSABLE"]
+__all__ = ["llm_eval_binary", "run_relevance_eval", "llm_generate"]
