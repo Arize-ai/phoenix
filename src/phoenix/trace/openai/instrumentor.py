290d61262e75f92427734e7cfc3aade9f76963c3
Alexander Song <axiomofjoy@gmail.com>
2023-10-03 18:47:24 -0700

    fix type issues;

--- src/phoenix/trace/openai/instrumentor.py
+++ src/phoenix/trace/openai/instrumentor.py
@@ -7,14 +7,16 @@ from typing import (
     Any,
     Callable,
     Dict,
-    Iterable,
     List,
     Mapping,
     Optional,
+    Sequence,
     Union,
+    cast,
 )
 
 from phoenix.trace.schemas import (
+    AttributeValue,
     Message,
     SpanAttributes,
     SpanEvent,
@@ -48,23 +50,21 @@ if TYPE_CHECKING:
     from openai.openai_response import OpenAIResponse
 
 
-class RequestType(Enum):
-    CHAT_COMPLETION = "chat_completion"
-    COMPLETION = "completion"
-    EMBEDDING = "embedding"
-
+Parameters = Mapping[str, Any]
 
 INSTRUMENTED_ATTRIBUTE_NAME = "is_instrumented_with_openinference_tracer"
 
 
 class NotSet:
+    """Indicates that no value is to be set for a given attribute."""
+
     pass
 
 
-def tracer_val(k, v):
-    if isinstance(v, NotSet):
-        return dict()
-    return {k: v}
+class RequestType(Enum):
+    CHAT_COMPLETION = "chat_completion"
+    COMPLETION = "completion"
+    EMBEDDING = "embedding"
 
 
 class OpenAIInstrumentor:
@@ -121,19 +121,23 @@ def _wrap_openai_api_requestor(
             events: List[SpanEvent] = []
             attributes: SpanAttributes = dict()
 
-            for attribute_name, openinference_converter in OPENINFERENCE_INPUT_MAPPING.items():
-                attributes.update(tracer_val(attribute_name, openinference_converter(parameters)))
-
+            for (
+                attribute_name,
+                get_parameter_attribute_fn,
+            ) in _PARAMETER_ATTRIBUTE_FUNCTIONS.items():
+                attributes.update(
+                    _as_span_attributes(attribute_name, get_parameter_attribute_fn(parameters))
+                )
             try:
                 outputs = request_fn(*args, **kwargs)
                 response = outputs[0]
-
                 for (
                     attribute_name,
-                    openinference_converter,
-                ) in OPENINFERENCE_RESPONSE_MAPPING.items():
-                    attributes.update(tracer_val(attribute_name, openinference_converter(response)))
-
+                    get_response_attribute_fn,
+                ) in _RESPONSE_ATTRIBUTE_FUNCTIONS.items():
+                    attributes.update(
+                        _as_span_attributes(attribute_name, get_response_attribute_fn(response))
+                    )
                 current_status_code = SpanStatusCode.OK
                 return outputs
             except Exception as error:
@@ -164,15 +168,15 @@ def _wrap_openai_api_requestor(
     return wrapped
 
 
-def _input_value(parameters: Mapping[str, Any]) -> Union[str, NotSet]:
+def _input_value(parameters: Parameters) -> Union[str, NotSet]:
     return json.dumps(messages) if (messages := parameters.get("messages")) else NotSet()
 
 
-def _input_mime_type(parameters: Mapping[str, Any]) -> str:  # type: ignore
+def _input_mime_type(parameters: Parameters) -> str:
     return MimeType.JSON.value
 
 
-def _llm_input_messages(parameters: Mapping[str, Any]) -> Union[Iterable[Message], NotSet]:
+def _llm_input_messages(parameters: Parameters) -> Union[Sequence[Message], NotSet]:
     if not (messages := parameters.get("messages")):
         return NotSet()
 
@@ -191,7 +195,7 @@ def _llm_input_messages(parameters: Mapping[str, Any]) -> Union[Iterable[Message
 
 
 def _llm_invocation_parameters(
-    parameters: Mapping[str, Any],
+    parameters: Parameters,
 ) -> str:
     return json.dumps(parameters)
 
@@ -200,32 +204,31 @@ def _output_value(response: "OpenAIResponse") -> str:
     return json.dumps(response.data["choices"])
 
 
-def _output_mime_type(response: "OpenAIResponse") -> str:  # type: ignore
+def _output_mime_type(response: "OpenAIResponse") -> str:
     return MimeType.JSON.value
 
 
 def _llm_token_count_prompt(response: "OpenAIResponse") -> Union[int, NotSet]:
     if token_usage := response.data.get("usage"):
-        return token_usage["prompt_tokens"]
+        return cast(int, token_usage["prompt_tokens"])
     return NotSet()
 
 
 def _llm_token_count_completion(response: "OpenAIResponse") -> Union[int, NotSet]:
     if token_usage := response.data.get("usage"):
-        return token_usage["completion_tokens"]
+        return cast(int, token_usage["completion_tokens"])
     return NotSet()
 
 
 def _llm_token_count_total(response: "OpenAIResponse") -> Union[int, NotSet]:
     if token_usage := response.data.get("usage"):
-        return token_usage["total_tokens"]
+        return cast(int, token_usage["total_tokens"])
     return NotSet()
 
 
 def _llm_function_call(
     response: "OpenAIResponse",
 ) -> Union[str, NotSet]:
-    """Yields function call data if present"""
     choices = response.data["choices"]
     choice = choices[0]
     if choice.get("finish_reason") == "function_call" and (
@@ -246,15 +249,23 @@ def _get_request_type(url: str) -> Optional[RequestType]:
     return None
 
 
-OPENINFERENCE_INPUT_MAPPING: Dict[str, Callable] = {
+def _as_span_attributes(
+    attribute_name: str, value: Union[AttributeValue, NotSet]
+) -> SpanAttributes:
+    if isinstance(value, NotSet):
+        return dict()
+    return {attribute_name: value}
+
+
+_PARAMETER_ATTRIBUTE_FUNCTIONS: Dict[str, Callable[[Parameters], Union[NotSet, AttributeValue]]] = {
     INPUT_VALUE: _input_value,
     INPUT_MIME_TYPE: _input_mime_type,
     LLM_INPUT_MESSAGES: _llm_input_messages,
     LLM_INVOCATION_PARAMETERS: _llm_invocation_parameters,
 }
-
-
-OPENINFERENCE_RESPONSE_MAPPING: Dict[str, Callable] = {
+_RESPONSE_ATTRIBUTE_FUNCTIONS: Dict[
+    str, Callable[["OpenAIResponse"], Union[NotSet, AttributeValue]]
+] = {
     OUTPUT_VALUE: _output_value,
     OUTPUT_MIME_TYPE: _output_mime_type,
     LLM_TOKEN_COUNT_PROMPT: _llm_token_count_prompt,
