61ad16913d25e7e16e0e6959e1479e64d2390f72
Dustin Ngo <dustin@arize.com>
2023-10-06 09:20:38 -0400

    Restore missing import

--- src/phoenix/experimental/evals/models/bedrock.py
+++ src/phoenix/experimental/evals/models/bedrock.py
@@ -3,6 +3,8 @@ import logging
 from dataclasses import dataclass, field
 from typing import TYPE_CHECKING, Any, Dict, List, Optional
 
+from phoenix.experimental.evals.models.base import BaseEvalModel
+
 if TYPE_CHECKING:
     from tiktoken import Encoding
 
