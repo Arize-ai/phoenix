6c374d61da0f4dda893564eb13bdf6c827b4bc28
Alexander Song <axiomofjoy@gmail.com>
2023-10-10 13:24:44 -0700

    refactor configuration

--- src/phoenix/evals.py
+++ src/phoenix/evals.py
@@ -1,138 +1,236 @@
 from dataclasses import dataclass
-from typing import Any, Dict, List, Literal, Optional, Protocol, TypeAlias, Union
+from typing import (
+    Any,
+    List,
+    Literal,
+    Mapping,
+    Optional,
+    Protocol,
+    Sequence,
+    TypeAlias,
+    TypedDict,
+    Union,
+)
 
-import pandas as pd
 from langchain.chat_models.base import BaseChatModel as LangChainBaseChatModel
 from langchain.llms.base import BaseLLM as LangChainBaseLLM
+from litellm.llms.base import BaseLLM as LiteLLMBaseLLM
 from llama_index.llms.base import BaseModel as LlamaIndexBaseModel
 
 JobID: TypeAlias = str
-Record: TypeAlias = Dict[str, Any]
-ClassificationPrediction: TypeAlias = Union[str, int]
+Record: TypeAlias = Mapping[str, Any]
 Evaluation: TypeAlias = Union[str, int, float]
 Evaluations: TypeAlias = List[Evaluation]
 
 
-# Model interface.
-class BaseModel(Protocol):
-    def generate(self, prompt: str, *, system_prompt: Optional[str] = None) -> str:
+class Message(TypedDict):
+    role: str
+    content: str
+
+
+# LLM and chat model interfaces. The `generate` method for both are meant to encapsulate a single request.
+
+
+@dataclass
+class Classification:
+    content: str
+    explanation: Optional[str] = None
+
+
+# LLM interface.
+class BaseCompletionModel(Protocol):
+    def generate(self, prompts: List[str]) -> List[List[str]]:
+        # Support batch input (e.g., the OpenAI completions API accepts batches of prompts as input)
+        # Support multiple choices in the output (e.g., the OpenAI completions API returns multiple
+        # completions per prompt when the parameter n is set to a value greater than 1)
         ...
 
 
-# Natively supported models.
-class OpenAIModel:
+# Chat model interface.
+class BaseChatModel(Protocol):
+    def generate(self, messages: List[Message]) -> List[Message]:
+        # The output is a list of messages because some APIs can produce multiple candidate responses per input.
+        # This is assuming that we don't need to support batch inputs for chat models.
+        # Question: Do we need to support batch inputs for chat models? LangChain does this, not sure why.
+        ...
+
+
+BaseModel = Union[BaseCompletionModel, BaseChatModel]
+
+
+# Natively supported models (specific model parameters are omitted)
+class OpenAICompletionModel:
     def __init__(self, model_name: str) -> None:
         # dynamically import the OpenAI SDK to contain the dependency
         # (add model parameters to the function signature as needed)
         raise NotImplementedError()
 
-    def generate(self, prompt: str, *, system_prompt: Optional[str] = None) -> str:
+    def generate(self, prompts: List[str]) -> List[List[str]]:
         raise NotImplementedError()
 
 
-class AnthropicModel:
+class OpenAIChatModel:
     def __init__(self, model_name: str) -> None:
-        # dynamically import the Anthropic SDK to contain the dependency
+        # dynamically import the OpenAI SDK to contain the dependency
         # (add model parameters to the function signature as needed)
         raise NotImplementedError()
 
-    def generate(self, prompt: str, *, system_prompt: Optional[str] = None) -> str:
+    def generate(self, messages: List[Message]) -> List[Message]:
         raise NotImplementedError()
 
 
 # Adapters for LLM orchestration frameworks
-# (users will not typically interact with these directly)
-class LangChainLLMAdapter:
+class LangChainCompletionModel:
     def __init__(self, model: LangChainBaseLLM) -> None:
         # dynamically imports LangChain to contain the dependency
         raise NotImplementedError()
 
-    def generate(self, prompt: str, *, system_prompt: Optional[str] = None) -> str:
-        # delegates to the wrapped LangChain LLM
+    def generate(self, prompts: List[str]) -> List[List[str]]:
         raise NotImplementedError()
 
 
-class LangChainChatModelAdapter:
+class LangChainChatModel:
     def __init__(self, model: LangChainBaseChatModel) -> None:
         # dynamically imports LangChain to contain the dependency
         raise NotImplementedError()
 
-    def generate(self, prompt: str, *, system_prompt: Optional[str] = None) -> str:
-        # delegates to the wrapped LangChain chat model
+    def generate(self, messages: List[Message]) -> List[Message]:
         raise NotImplementedError()
 
 
-class LlamaIndexModelAdapter:
+class LlamaIndexCompletionModel:
     def __init__(self, model: LlamaIndexBaseModel) -> None:
         # dynamically imports LlamaIndex to contain the dependency
         raise NotImplementedError()
 
-    def generate(self, prompt: str, *, system_prompt: Optional[str] = None) -> str:
-        # delegates to the wrapped LlamaIndex model
+    def generate(self, prompts: List[str]) -> List[List[str]]:
         raise NotImplementedError()
 
 
-# Prompt template.
-class ClassificationPromptTemplate:
-    def __init__(self, template: str, rails: List[str]) -> None:
+class LlamaIndexChatModel:
+    def __init__(self, model: LlamaIndexBaseModel) -> None:
+        # dynamically imports LlamaIndex to contain the dependency
         raise NotImplementedError()
 
-    def format(self, *args, **kwargs) -> str:
+    def generate(self, messages: List[Message]) -> List[Message]:
         raise NotImplementedError()
 
 
-RELEVANCE_PROMPT_TEMPLATE = ClassificationPromptTemplate(
-    "some prompt we've test to death", ["relevant", "irrelevant"]
+class LiteLLMCompletionModel:
+    def __init__(self, model: LiteLLMBaseLLM) -> None:
+        # dynamically imports LiteLLM to contain the dependency
+        raise NotImplementedError()
+
+    def generate(self, prompts: List[str]) -> List[List[str]]:
+        raise NotImplementedError()
+
+
+class LiteLLMChatModel:
+    def __init__(self, model: LiteLLMBaseLLM) -> None:
+        # dynamically imports LlamaIndex to contain the dependency
+        raise NotImplementedError()
+
+    def generate(self, messages: List[Message]) -> List[Message]:
+        raise NotImplementedError()
+
+
+# Prompt Template
+class PromptTemplate(Protocol):
+    @property
+    def template_string(self) -> str:
+        ...
+
+    def format(self, **kwargs) -> str:
+        ...
+
+
+# LLM classification config
+@dataclass
+class LLMClassificationConfig:
+    # Question: Are we making the correct assumption that there is one template and an optional system message string?
+    template: PromptTemplate
+    rails: Sequence[str]
+    system_message: Optional[str] = None
+    default_rail: str = "UNPARSABLE"
+
+
+@dataclass
+class RAGVariableNames:
+    query_variable_name: str
+    reference_variable_name: str
+
+
+class RAGRelevanceClassificationConfig(RAGVariableNames, LLMClassificationConfig):
+    ...
+
+
+DefaultRAGRelevanceClassificationConfig = RAGRelevanceClassificationConfig(
+    template=PromptTemplate(template_string="Query: {query}\nReference: {reference}\nResponse: "),
+    rails=("relevant", "irrelevant"),
+    system_message='You are an assistant whose purpose is to classify a document as relevant or irrelevant to a query. You must respond with a single word, either "relevant" or "irrelevant".',
+    query_variable_name="query",
+    reference_variable_name="reference",
 )
-TOXICITY_PROMPT_TEMPLATE = ClassificationPromptTemplate(
-    "some prompt we've test to death", ["toxic", "non-toxic"]
-)
-HALLUCINATION_PROMPT_TEMPLATE = ClassificationPromptTemplate(
-    "some prompt we've test to death", ["hallucinated", "factual"]
-)
+
+
+@dataclass
+class FunctionCallingPrompts:
+    function_name: str  # e.g., "record_relevance_classification"
+    function_description: str  # e.g., "Classifies a record as relevant or irrelevant to a query."
+    argument_name: str  # e.g., "relevancy"
+    argument_description: str  # e.g., "A string indicating whether the record is relevant or irrelevant to the query"
+
+
+class LLMFunctionsClassificationConfig(FunctionCallingPrompts, LLMClassificationConfig):
+    ...
 
 
 # Configuration for running batches of predictions
 @dataclass
-class RetrySettings:
+class RetryConfig:
     ...
 
 
 @dataclass
-class JobConfig:
-    max_requests_per_minute: int
-    max_tokens_per_minute: int
-    thread_requests: bool
-    retry_settings: RetrySettings
+class ManualRollingWindow:
+    rolling_window_duration: int  # duration of the rolling window in seconds
+    max_requests_per_window: int
+    max_tokens_per_window: int
+
+
+# In the case of certain APIs such as the OpenAI API, we're able to get the
+# rate-limits from the API at runtime and can infer the parameters in the rolling window
+@dataclass
+class AutomaticRollingWindow:
+    ...
+
+
+@dataclass
+class RequestConfig:
+    retry_settings: RetryConfig
+    rolling_window: Optional[Union[ManualRollingWindow, AutomaticRollingWindow]]
+    completions_per_request: Optional[
+        int
+    ] = None  # for completion APIs supporting batch completions
+    thread_requests: bool = True
 
 
 # Recommended batch configurations for different foundation models and APIs.
-# (tested by us to maximize throughput while avoiding rate limiting)
-class DefaultOpenAIGPT4JobConfig(JobConfig):
-    ...
+DefaultOpenAIGPT4RequestConfig = RequestConfig(
+    retry_settings=RetryConfig(),
+    rolling_window=AutomaticRollingWindow(),
+)
 
 
-class DefaultOpenAIGPT35TurboJobConfig(JobConfig):
-    ...
+DefaultOpenAIGPT35TurboRequestConfig = ...
 
 
-class DefaultAnthropicClaude2JobConfig(JobConfig):
-    ...
+DefaultAnthropicClaude2RequestConfig = ...
 
 
 # LLM classifiers.
 class BaseLLMClassifier(Protocol):
-    def predict(self, record: Record) -> Optional[ClassificationPrediction]:
-        ...
-
-    def predict_batch(
-        self, record: Record, *, job_config: Optional[JobConfig] = None
-    ) -> List[Optional[ClassificationPrediction]]:
-        ...
-
-    def predict_dataframe(
-        self, dataframe: pd.DataFrame, *, job_config: Optional[JobConfig] = None
-    ) -> List[Optional[ClassificationPrediction]]:
+    def predict(self, record: Record) -> Classification:
         ...
 
 
@@ -140,74 +238,35 @@ class LLMClassifier:
     def __init__(
         self,
         model: BaseModel,
-        prompt_template: ClassificationPromptTemplate,
-        *,
-        system_message: Optional[str] = None,
-        default_return_value: Optional[str] = None,
+        classification_config: LLMClassificationConfig,
     ) -> None:
         raise NotImplementedError()
 
-    @classmethod
-    def from_llama_index_model(
-        cls, model: LlamaIndexBaseModel, prompt_template: ClassificationPromptTemplate
-    ) -> "LLMClassifier":
-        return cls(LlamaIndexModelAdapter(model), prompt_template)
-
-    @classmethod
-    def from_langchain_llm(
-        cls, model: LangChainBaseLLM, prompt_template: ClassificationPromptTemplate
-    ) -> "LLMClassifier":
-        return cls(LangChainLLMAdapter(model), prompt_template)
-
-    @classmethod
-    def from_langchain_chat_model(
-        cls,
-        model: LangChainBaseChatModel,
-        prompt_template: ClassificationPromptTemplate,
-    ) -> "LLMClassifier":
-        return cls(LangChainChatModelAdapter(model), prompt_template)
-
-    def predict(self, record: Record) -> ClassificationPrediction:
-        raise NotImplementedError()
-
-    def predict_batch(
-        self, record: Record, *, job_config: Optional[JobConfig] = None
-    ) -> List[Optional[ClassificationPrediction]]:
-        raise NotImplementedError()
-
-    def predict_dataframe(
-        self, dataframe: pd.DataFrame, *, job_config: Optional[JobConfig] = None
-    ) -> List[Optional[ClassificationPrediction]]:
+    def predict(self, record: Record) -> Classification:
         raise NotImplementedError()
 
 
-class OpenAIFunctionsClassifier:
+class LLMClassifierWithExplanation:
     def __init__(
         self,
-        prompt_template: ClassificationPromptTemplate,
-        model: OpenAIModel,
-        function_name: str,
-        function_description: str,
-        class_argument_name: str,
-        class_argument_description: str,
-        *,
-        default_return_value: Optional[str] = None,
+        model: BaseModel,
+        classification_config: LLMClassificationConfig,  # This classification config object is the same as before, but the prompts need to contain explicit instructions about providing explanations and how to format the explanation and classifier
     ) -> None:
         raise NotImplementedError()
 
-    def predict(
-        self, record: Record, *, require_explanation: bool = False
-    ) -> ClassificationPrediction:
+    def predict(self, record: Record) -> Classification:
         raise NotImplementedError()
 
-    def predict_batch(
-        self, record: Record, *, job_config: Optional[JobConfig] = None
-    ) -> List[Optional[ClassificationPrediction]]:
+
+class FunctionsClassifier:
+    def __init__(
+        self,
+        model: OpenAIChatModel,  # function calling is a feature of only the OpenAI API
+        classification_config: LLMFunctionsClassificationConfig,
+    ) -> None:
         raise NotImplementedError()
 
-    def predict_dataframe(
-        self, dataframe: pd.DataFrame, *, job_config: Optional[JobConfig] = None
-    ) -> List[Optional[ClassificationPrediction]]:
+    def predict(self, record: Record, *, provide_explanation: bool = False) -> Classification:
         raise NotImplementedError()
 
 
@@ -215,7 +274,7 @@ class OpenAIFunctionsClassifier:
 class OpenAIFineTuningJob:
     def __init__(
         self,
-        prompt_template: ClassificationPromptTemplate,
+        classification_config: LLMClassificationConfig,
         base_model_name: str,
         *,
         num_epochs: Optional[int] = None,
@@ -271,7 +330,9 @@ class ClassificationEvaluator:
 
 # evals
 class Evals:
-    def __init__(self, evaluators: List[Evaluator], job_config: Optional[JobConfig] = None) -> None:
+    def __init__(
+        self, evaluators: List[Evaluator], job_config: Optional[RequestConfig] = None
+    ) -> None:
         raise NotImplementedError()
 
     @classmethod
@@ -280,7 +341,7 @@ class Evals:
         eval_names: List[str],
         *,
         model: Optional[BaseModel] = None,
-        job_config: Optional[JobConfig] = None,
+        job_config: Optional[RequestConfig] = None,
     ) -> "Evals":
         raise NotImplementedError()
 
