20d9cafb11b80b48c31e628184eab0182340e66e
Dustin Ngo <dustin@arize.com>
2023-10-12 09:38:42 -0400

    Add documentation

--- src/phoenix/utilities/error_handling.py
+++ src/phoenix/utilities/error_handling.py
@@ -10,6 +10,24 @@ F = TypeVar("F", bound=Callable[..., Any])
 def graceful_fallback(
     fallback_method: Callable[..., Any], exceptions: Optional[Iterable[Type[BaseException]]] = None
 ) -> Callable[[F], F]:
+    """
+    Decorator that reroutes failing functions to a specified fallback method.
+
+    While it is generally not advisable to catch all exceptions, this decorator can be used to
+    gracefully degrade a function in situations when raising an error might be too disruptive.
+    Exceptions supprssed by this decorator will be logged to the root logger, and all inputs
+    to the wrapped function will be passed to the fallback method.
+
+
+    Args:
+
+    fallback_method (Callable[..., Any]): The fallback method to be called when the wrapped
+    function fails.
+
+    exceptions: An optional iterable of exceptions that should be suppressed by this decorator. If
+    unset, all exceptions will be suppressed.
+    """
+
     exceptions = (BaseException,) if exceptions is None else tuple(exceptions)
 
     def decorator(func: F) -> F:
