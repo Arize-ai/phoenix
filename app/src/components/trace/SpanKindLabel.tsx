27e9b8178b243ee28d4538df8ad50168d5e26134
Mikyo King <mikyo@arize.com>
2023-10-27 14:37:37 -0600

    fix(UI): fix icon and label colors for light mode (#1678)

--- app/src/components/trace/SpanKindLabel.tsx
+++ app/src/components/trace/SpanKindLabel.tsx
@@ -5,7 +5,7 @@ import { Label, LabelProps } from "@arizeai/components";
 export function SpanKindLabel(props: { spanKind: string }) {
   const { spanKind } = props;
   const color = useMemo<LabelProps["color"]>(() => {
-    let color: LabelProps["color"] = "gray";
+    let color: LabelProps["color"] = "grey-500";
     switch (spanKind) {
       case "llm":
         color = "orange-1000";
