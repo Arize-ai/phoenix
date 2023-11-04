a4bfeabda8390baf46112fd63bd802008e70573a
Dustin Ngo <dustin@arize.com>
2023-11-04 01:41:17 -0400

    feat: Use adaptive rate limiting (#1705)
    * Start spiking out adaptive rate limiter
    
    * Fix bugs, wire up adaptive limiter
    
    * Remove model-specific rate-limiting details
    
    * Remove needless imports
    
    * Update rate limiter tests
    
    * Finish updating tests
    
    * Fix typing issues
    
    * Remove deprecated rate limit tester

--- src/phoenix/experimental/evals/models/openai.py
+++ src/phoenix/experimental/evals/models/openai.py
@@ -4,15 +4,13 @@ import asyncio
 import logging
 import os
 from dataclasses import dataclass, field
-from typing import TYPE_CHECKING, Any, Dict, List, Mapping, Optional, Tuple, Union
+from typing import TYPE_CHECKING, Any, Dict, List, Optional, Tuple, Union
 
 import nest_asyncio
-import requests
-from openai.openai_object import OpenAIObject
 from tqdm.asyncio import tqdm as async_tqdm
 
 from phoenix.experimental.evals.models.base import BaseEvalModel
-from phoenix.experimental.evals.models.rate_limiters import OpenAIRateLimiter
+from phoenix.experimental.evals.models.rate_limiters import RateLimiter
 from phoenix.experimental.evals.utils.types import is_list_of
 from phoenix.utilities.logging import printif
 
@@ -38,64 +36,6 @@ TQDM_BAR_FORMAT = (
     "({n_fmt}/{total_fmt}) "
     "[{elapsed}<{remaining}, {rate_fmt}{postfix}]"
 )
-
-# rate limits conservatively pulled from https://learn.microsoft.com/en-us/azure/ai-services/openai/quotas-limits
-AZURE_OPENAI_TOKEN_RATE_LIMITS = {
-    "gpt-35-turbo": 240000,
-    "gpt-35-turbo-16k": 240000,
-    "gpt-35-turbo-instruct": 240000,
-    "gpt-4": 20000,
-    "gpt-4-32k": 60000,
-    "text-embedding-ada-002": 240000,
-    "babbage-002": 50000,
-    "davinci-002": 50000,
-    "gpt-35-turbo-0613": 50000,
-}
-
-
-def openai_token_usage(chat_completion: OpenAIObject) -> int:
-    try:
-        return int(chat_completion.usage.total_tokens)
-    except (AttributeError, ValueError):
-        return 0
-
-
-def openai_rate_limit_info(
-    model_name: str, api_key: Optional[str], base_url: Optional[str] = None
-) -> Mapping[str, int]:
-    if base_url is not None and "openai.azure" in base_url:
-        token_limit = AZURE_OPENAI_TOKEN_RATE_LIMITS.get(model_name, 120000)
-        limit_info = {
-            "token_rate_limit": token_limit,
-        }
-    else:
-        if api_key is None:
-            # TODO: Create custom AuthenticationError
-            raise RuntimeError(
-                "OpenAI's API key not provided. Pass it as an argument to 'openai_api_key' "
-                "or set it in your environment: 'export OPENAI_API_KEY=sk-****'"
-            )
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {api_key}",
-        }
-        data = {
-            "model": model_name,
-            "messages": [
-                {"role": "user", "content": "Test"},
-            ],
-        }
-        response = requests.post(
-            "https://api.openai.com/v1/chat/completions", headers=headers, json=data
-        )
-        # default to tier 1 rate limits (https://platform.openai.com/docs/guides/rate-limits/overview)
-        request_limit = int(response.headers.get("x-ratelimit-limit-requests", 500))
-        token_limit = int(response.headers.get("x-ratelimit-limit-tokens", 10000))
-        limit_info = {
-            "request_rate_limit": request_limit,
-            "token_rate_limit": token_limit,
-        }
-    return limit_info
 
 
 @dataclass
@@ -141,22 +81,7 @@ class OpenAIModel(BaseEvalModel):
         self._init_environment()
         self._init_open_ai()
         self._init_tiktoken()
-        self._rate_limiter: Optional[OpenAIRateLimiter] = None
-
-    @property
-    def rate_limiter(self, rate_limit_multiplier: float = 0.8) -> OpenAIRateLimiter:
-        if self._rate_limiter is None:
-            self._rate_limiter = OpenAIRateLimiter()
-            limit_info = openai_rate_limit_info(
-                self.model_name, self.openai_api_key, base_url=self.openai_api_base
-            )
-            # throttle our requests to some multiple of the absolute rate limit
-            limits = {
-                limit_type: limit * rate_limit_multiplier
-                for limit_type, limit in limit_info.items()
-            }
-            self._rate_limiter.set_rate_limits(self.model_name, **limits)
-        return self._rate_limiter
+        self._init_rate_limiter()
 
     def _init_environment(self) -> None:
         try:
@@ -228,6 +153,11 @@ class OpenAIModel(BaseEvalModel):
             logger.warning("Warning: model not found. Using cl100k_base encoding.")
             encoding = self._tiktoken.get_encoding("cl100k_base")
         self._tiktoken_encoding = encoding
+
+    def _init_rate_limiter(self) -> None:
+        self._rate_limiter: RateLimiter = RateLimiter(
+            rate_limit_error=self._openai_error.RateLimitError
+        )
 
     @staticmethod
     def _build_messages(
@@ -346,17 +276,16 @@ class OpenAIModel(BaseEvalModel):
             self._openai_error.Timeout,
             self._openai_error.APIError,
             self._openai_error.APIConnectionError,
-            self._openai_error.RateLimitError,
             self._openai_error.ServiceUnavailableError,
         ]
 
         async def metered_openai_chat_completion(**kwargs: Any) -> Any:
-            limit = self.rate_limiter.alimit(self.model_name, openai_token_usage)
+            limit = self._rate_limiter.alimit
             response = await limit(self._openai.ChatCompletion.acreate)(**kwargs)
             return response
 
         async def metered_openai_completion(**kwargs: Any) -> Any:
-            limit = self.rate_limiter.alimit(self.model_name, openai_token_usage)
+            limit = self._rate_limiter.alimit
             response = await limit(self._openai.Completion.acreate)(**kwargs)
             return response
 
--- src/phoenix/experimental/evals/models/rate_limiters.py
+++ src/phoenix/experimental/evals/models/rate_limiters.py
@@ -1,12 +1,11 @@
 import asyncio
 import time
-from collections import defaultdict
 from functools import wraps
-from typing import Any, Awaitable, Callable, Dict, Optional, TypeVar, Union, cast
+from math import exp
+from typing import Any, Awaitable, Callable, Type, TypeVar, cast
 
 from typing_extensions import ParamSpec
 
-Numeric = Union[int, float]
 ParameterSpec = ParamSpec("ParameterSpec")
 GenericType = TypeVar("GenericType")
 AsyncCallable = TypeVar("AsyncCallable", bound=Callable[..., Awaitable[Any]])
@@ -16,222 +15,134 @@ class UnavailableTokensError(Exception):
     pass
 
 
-class TokenRateLimiter:
+class AdaptiveTokenBucket:
     """
-    A in-memory rate-limiter implemented using the token bucket algorithm.
+    An adaptive rate-limiter that adjusts the rate based on the number of rate limit errors.
 
-    A description of the technique can be found here: (https://en.wikipedia.org/wiki/Token_bucket).
-    A collection of arbitrary tokens replenish at `rate` tokens per second up to a maximum. These
-    tokens can be spent to process requests.
+    This rate limiter does not need to know the exact rate limit. Instead, it starts with a high
+    rate and reduces it whenever a rate limit error occurs. The rate is increased slowly over time
+    if no further errors occur.
 
     Args:
-    per_minute_rate (float): The rate at which tokens replenish.
-
-    starting_tokens (float): The number of tokens the rate limiter starts with.
-
-    max_tokens (float): The maximum number of tokens this rate limiter can hold. Tokens will not
-    replenish past this value.
-
-    rate_multiplier (float, Optional): A multiplier used to adjust the rate limit applied by this
-    object. For example, a rate_multiplier of 0.8 can be used to ensure that all actions
-    controlled by this rate limiter will never exceed 0.8 times the originally specified rate.
+    initial_per_second_request_rate (float): The allowed request rate.
+    enforcement_window_minutes (float): The time window over which the rate limit is enforced.
+    rate_reduction_factor (float): Factor used to reduce the rate limit after a rate limit error.
+    rate_increase_factor (float): Factor increasing the rate limit over time.
     """
 
     def __init__(
         self,
-        per_second_rate: Numeric,
-        starting_tokens: Numeric,
-        max_tokens: Numeric,
-        rate_multiplier: Numeric = 1,
+        initial_per_second_request_rate: float,
+        enforcement_window_minutes: float,
+        rate_reduction_factor: float = 0.5,
+        rate_increase_factor: float = 0.01,
     ):
-        self.rate_multiplier = rate_multiplier
-        self.rate = per_second_rate * self.rate_multiplier
-        self.tokens = starting_tokens
-        self.max_tokens = max_tokens
-        self.created = time.time()
-        self.last_checked = self.created
-        self.total_tokens: Numeric = 0
-
-    def update_limit(self, per_second_rate: Numeric, max_tokens: Numeric) -> None:
-        new_rate = per_second_rate * self.rate_multiplier
-        if self.rate != new_rate:
-            self.rate = new_rate
-            self.tokens = 0  # reset tokens as conservatively as possible
-            self.max_tokens = max_tokens
-            self.last_checked = time.time()
-
-    def available_tokens(self) -> float:
-        time_since_last_checked = time.time() - self.last_checked
-        return min(self.max_tokens, self.rate * time_since_last_checked + self.tokens)
-
-    def spend_tokens_if_available(self, token_cost: Numeric) -> None:
-        if token_cost > self.available_tokens():
-            raise UnavailableTokensError
         now = time.time()
-        current_tokens = min(self.max_tokens, self.tokens + (now - self.last_checked) * self.rate)
-        self.tokens = current_tokens - token_cost
+        self.rate = initial_per_second_request_rate
+        self.rate_reduction_factor = rate_reduction_factor
+        self.enforcement_window = enforcement_window_minutes * 60
+        self.rate_increase_factor = rate_increase_factor
+        self.last_rate_update = now
         self.last_checked = now
-        self.total_tokens += token_cost
+        self.tokens = 0.0
 
-    def spend_tokens(self, token_cost: Numeric) -> None:
-        self.tokens -= token_cost
-        self.total_tokens += token_cost
+    def _increase_rate(self) -> None:
+        time_since_last_update = time.time() - self.last_rate_update
+        self.rate *= exp(self.rate_increase_factor * time_since_last_update)
+        self.last_rate_update = time.time()
 
-    def wait_for_then_spend_available_tokens(
-        self, token_cost: Numeric, max_wait_time: Numeric = 300
+    def max_tokens(self) -> float:
+        return self.rate * self.enforcement_window
+
+    def on_rate_limit_error(self) -> None:
+        self.rate *= self.rate_reduction_factor
+        self.rate = max(self.rate, 1 / self.enforcement_window)
+        self.last_rate_update = time.time()
+
+    def available_requests(self) -> float:
+        now = time.time()
+        time_since_last_checked = time.time() - self.last_checked
+        self.tokens = min(self.max_tokens(), self.rate * time_since_last_checked + self.tokens)
+        self.last_checked = now
+        return self.tokens
+
+    def make_request_if_ready(self) -> None:
+        if self.available_requests() <= 1:
+            raise UnavailableTokensError
+        self.tokens -= 1
+
+    def wait_until_ready(
+        self,
+        max_wait_time: float = 300,
     ) -> None:
         start = time.time()
         while (time.time() - start) < max_wait_time:
             try:
-                self.spend_tokens_if_available(token_cost)
+                self._increase_rate()
+                self.make_request_if_ready()
                 break
             except UnavailableTokensError:
-                time.sleep(1 / self.rate)
+                time.sleep(0.1 / self.rate)
                 continue
 
-    async def async_wait_for_then_spend_available_tokens(
-        self, token_cost: Numeric, max_wait_time: Numeric = 300
+    async def async_wait_until_ready(
+        self,
+        max_wait_time: float = 300,
     ) -> None:
         start = time.time()
         while (time.time() - start) < max_wait_time:
             try:
-                self.spend_tokens_if_available(token_cost)
+                self._increase_rate()
+                self.make_request_if_ready()
                 break
             except UnavailableTokensError:
-                await asyncio.sleep(1 / self.rate)
+                await asyncio.sleep(0.1 / self.rate)
                 continue
 
-    def effective_rate(self) -> Numeric:
-        return self.total_tokens / (time.time() - self.created)
 
-
-class LimitStore:
-    """
-    A singleton store for collections of rate limits grouped by service key.
-
-    LimitStore is a singleton because all calls to a single endpoint should share the same rate
-    limits. Rate limiter implementations can use a LimitStore instance to manage rate limits for
-    a specific endpoint indexed by a key. The LimitStore singleton will ensure that rate limits
-    are correctly shared across all calls to the same endpoint.
-
-    This implementation is not threadsafe, but is async-safe.
-    """
-
-    _singleton = None
-
-    def __new__(cls) -> "LimitStore":
-        if not cls._singleton:
-            cls._singleton = super().__new__(cls)
-            cls._singleton._rate_limits = defaultdict(dict)
-        return cls._singleton
-
-    _rate_limits: Dict[str, Dict[str, TokenRateLimiter]]
-
-    def set_rate_limit(
+class RateLimiter:
+    def __init__(
         self,
-        key: str,
-        limit_type: str,
-        per_minute_rate_limit: Numeric,
-        enforcement_window_minutes: Optional[int] = None,
+        rate_limit_error: Type[BaseException],
+        initial_per_second_request_rate: float = 200,
+        enforcement_window_minutes: float = 1,
+        rate_reduction_factor: float = 0.5,
+        rate_increase_factor: float = 0.01,
     ) -> None:
-        # default to 1 minute enforcement window
-        enforcement_window_minutes = (
-            enforcement_window_minutes if enforcement_window_minutes is not None else 1
+        self._rate_limit_error = rate_limit_error
+        self._rate_limiter = AdaptiveTokenBucket(
+            initial_per_second_request_rate=initial_per_second_request_rate,
+            enforcement_window_minutes=enforcement_window_minutes,
+            rate_reduction_factor=rate_reduction_factor,
+            rate_increase_factor=rate_increase_factor,
         )
-        per_second_rate_limit = round(per_minute_rate_limit / 60, 3)
-        max_tokens = per_minute_rate_limit * enforcement_window_minutes
-        if limits := self._rate_limits[key]:
-            if limit := limits.get(limit_type):
-                limit.update_limit(per_second_rate_limit, max_tokens)
-                return
-        limits[limit_type] = TokenRateLimiter(
-            per_second_rate_limit,
-            0,
-            max_tokens,
-        )
-
-    def get_rate_limits(self, key: str) -> Dict[str, TokenRateLimiter]:
-        return self._rate_limits[key]
-
-    def wait_for_rate_limits(self, key: str, rate_limit_costs: Dict[str, Numeric]) -> None:
-        rate_limits = self._rate_limits[key]
-        for limit_type, cost in rate_limit_costs.items():
-            if limit := rate_limits.get(limit_type):
-                limit.wait_for_then_spend_available_tokens(cost)
-
-    async def async_wait_for_rate_limits(
-        self, key: str, rate_limit_costs: Dict[str, Numeric]
-    ) -> None:
-        rate_limits = self._rate_limits[key]
-        for limit_type, cost in rate_limit_costs.items():
-            if limit := rate_limits.get(limit_type):
-                await limit.async_wait_for_then_spend_available_tokens(cost)
-
-    def spend_rate_limits(self, key: str, rate_limit_costs: Dict[str, Numeric]) -> None:
-        rate_limits = self._rate_limits[key]
-        for limit_type, cost in rate_limit_costs.items():
-            if limit := rate_limits.get(limit_type):
-                limit.spend_tokens(cost)
-
-
-class OpenAIRateLimiter:
-    def __init__(self) -> None:
-        self._store = LimitStore()
-
-    def key(self, model_name: str) -> str:
-        return f"openai:{model_name}"
-
-    def set_rate_limits(
-        self,
-        model_name: str,
-        request_rate_limit: Optional[Numeric],
-        token_rate_limit: Optional[Numeric],
-    ) -> None:
-        if request_rate_limit is not None:
-            self._store.set_rate_limit(self.key(model_name), "requests", request_rate_limit)
-        if token_rate_limit is not None:
-            self._store.set_rate_limit(self.key(model_name), "tokens", token_rate_limit)
 
     def limit(
-        self,
-        model_name: str,
-        response_cost_fn: Callable[..., Numeric],
-    ) -> Callable[[Callable[ParameterSpec, GenericType]], Callable[ParameterSpec, GenericType]]:
-        def rate_limit_decorator(
-            fn: Callable[ParameterSpec, GenericType]
-        ) -> Callable[ParameterSpec, GenericType]:
-            @wraps(fn)
-            def wrapper(*args: Any, **kwargs: Any) -> GenericType:
-                key = self.key(model_name)
-                self._store.wait_for_rate_limits(key, {"requests": 1, "tokens": 0})
-                result: GenericType = fn(*args, **kwargs)
-                self._store.spend_rate_limits(key, {"tokens": response_cost_fn(result)})
-                return result
+        self, fn: Callable[ParameterSpec, GenericType]
+    ) -> Callable[ParameterSpec, GenericType]:
+        @wraps(fn)
+        def wrapper(*args: Any, **kwargs: Any) -> GenericType:
+            while True:
+                try:
+                    self._rate_limiter.wait_until_ready()
+                    result: GenericType = fn(*args, **kwargs)
+                    return result
+                except self._rate_limit_error:
+                    self._rate_limiter.on_rate_limit_error()
+                    continue
 
-            return wrapper
+        return wrapper
 
-        return rate_limit_decorator
+    def alimit(self, fn: AsyncCallable) -> AsyncCallable:
+        @wraps(fn)
+        async def wrapper(*args: Any, **kwargs: Any) -> GenericType:
+            while True:
+                try:
+                    await self._rate_limiter.async_wait_until_ready()
+                    result: GenericType = await fn(*args, **kwargs)
+                    return result
+                except self._rate_limit_error:
+                    self._rate_limiter.on_rate_limit_error()
+                    continue
 
-    def alimit(
-        self,
-        model_name: str,
-        response_cost_fn: Callable[..., Numeric],
-    ) -> Callable[[AsyncCallable], AsyncCallable]:
-        def rate_limit_decorator(fn: AsyncCallable) -> AsyncCallable:
-            @wraps(fn)
-            async def wrapper(*args: Any, **kwargs: Any) -> GenericType:
-                key = self.key(model_name)
-                await self._store.async_wait_for_rate_limits(
-                    key,
-                    {
-                        "requests": 1,
-                        "tokens": 0,
-                    },  # token costs are 0 up front, infer from the response instead
-                )
-                result: GenericType = await fn(*args, **kwargs)
-                self._store.spend_rate_limits(key, {"tokens": response_cost_fn(result)})
-                return result
-
-            return cast(AsyncCallable, wrapper)
-
-        return rate_limit_decorator
+        return cast(AsyncCallable, wrapper)
