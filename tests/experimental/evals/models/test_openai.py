7efd752c8839231e469687007ed2e489e57ab6be
Mikyo King <mikyo@arize.com>
2023-11-08 11:21:11 -0700

    fix openai tests

--- tests/experimental/evals/models/test_openai.py
+++ tests/experimental/evals/models/test_openai.py
@@ -7,7 +7,7 @@ def test_openai_model():
     """
     Sanity check of the initialization of OpenAI wrapper
     NB: this is intentionally white-box testing since
-    we cannot rely on the OpenAI API to be stable and don't
+    we have very little type safety in the OpenAI wrapper
     """
     with patch.object(OpenAIModel, "_init_tiktoken", return_value=None):
         model = OpenAIModel(model_name="gpt-4-1106-preview")
