e29422b18c8fa6139839a933322dda6395be6c9a
Mikyo King <mikyo@arize.com>
2023-11-13 15:15:02 -0700

    remove brittle test

--- tests/experimental/evals/functions/test_generate.py
+++ tests/experimental/evals/functions/test_generate.py
@@ -61,58 +61,6 @@ def test_llm_generate(monkeypatch: pytest.MonkeyPatch, respx_mock: respx.mock):
 
 
 @pytest.mark.respx(base_url="https://api.openai.com/v1/chat/completions")
-def test_llm_generate_prints_info_with_verbose_flag(
-    monkeypatch: pytest.MonkeyPatch, capfd, respx_mock: respx.mock
-):
-    monkeypatch.setenv(OPENAI_API_KEY_ENVVAR_NAME, "sk-0123456789")
-    dataframe = pd.DataFrame(
-        [
-            {
-                "query": "What is Python?",
-                "reference": "Python is a programming language.",
-            },
-            {
-                "query": "What is Python?",
-                "reference": "Ruby is a programming language.",
-            },
-            {
-                "query": "What is C++?",
-                "reference": "C++ is a programming language.",
-            },
-            {
-                "query": "What is C++?",
-                "reference": "irrelevant",
-            },
-        ]
-    )
-    responses = [
-        "it's a dialect of french",
-        "it's a music notation",
-        "It's a crazy language",
-        "it's a programming language",
-    ]
-    queries = dataframe["query"].tolist()
-    references = dataframe["reference"].tolist()
-    for query, reference, response in zip(queries, references, responses):
-        matcher = M(content__contains=query) & M(content__contains=reference)
-        respx_mock.route(matcher).mock(
-            return_value=httpx.Response(200, json={"choices": [{"message": {"content": response}}]})
-        )
-
-    template = (
-        "Given {query} and a golden answer {reference}, generate an answer that is incorrect."
-    )
-
-    with patch.object(OpenAIModel, "_init_tiktoken", return_value=None):
-        model = OpenAIModel()
-
-    llm_generate(dataframe=dataframe, template=template, model=model, verbose=True)
-
-    out, _ = capfd.readouterr()
-    assert "Generating responses for 4 prompts..." in out, "Response generation should be printed"
-
-
-@pytest.mark.respx(base_url="https://api.openai.com/v1/chat/completions")
 def test_llm_generate_with_output_parser(monkeypatch: pytest.MonkeyPatch, respx_mock: respx.mock):
     monkeypatch.setenv(OPENAI_API_KEY_ENVVAR_NAME, "sk-0123456789")
     dataframe = pd.DataFrame(
