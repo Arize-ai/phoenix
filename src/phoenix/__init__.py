637325ca894a86c17a8b0a9cca9d94693ab4e475
Mikyo King <mikyo@arize.com>
2023-11-20 11:13:34 -0700

    v1.2.1

--- src/phoenix/__init__.py
+++ src/phoenix/__init__.py
@@ -5,7 +5,7 @@ from .session.session import NotebookEnvironment, Session, active_session, close
 from .trace.fixtures import load_example_traces
 from .trace.trace_dataset import TraceDataset
 
-__version__ = "1.2.0"
+__version__ = "1.2.1"
 
 # module level doc-string
 __doc__ = """
