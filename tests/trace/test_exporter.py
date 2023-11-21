f0e6f87a09f3aa56f3c2367c47b52b094908df3f
Mikyo King <mikyo@arize.com>
2023-11-21 13:09:06 -0700

    rename env var

---
+++ tests/trace/test_exporter.py
@@ -0,0 +1,17 @@
+import pytest
+from phoenix.trace.exporter import HttpExporter
+
+
+def test_exporter(monkeypatch: pytest.MonkeyPatch):
+    # Test that it defaults to local
+    exporter = HttpExporter()
+    assert exporter._base_url == "http://127.0.0.1:6006"
+
+    # Test that you can configure an endpoint
+    exporter = HttpExporter(endpoint="https://my-phoenix-server.com/")
+    assert exporter._base_url == "https://my-phoenix-server.com"
+
+    # Test that it supports environment variables
+    monkeypatch.setenv("PHOENIX_COLLECTOR_ENDPOINT", "https://my-phoenix-server.com/")
+    exporter = HttpExporter()
+    assert exporter._base_url == "https://my-phoenix-server.com"
