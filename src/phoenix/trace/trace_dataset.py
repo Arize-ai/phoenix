a218a650cefa8925ed7b6627c121454bfc94ec0d
Mikyo King <mikyo@arize.com>
2023-12-01 14:21:12 -0700

    feat(traces): evaluation annotations on traces for associating spans with eval metrics (#1693)
    * feat: initial associations of evaluations to traces
    
    * add some documentaiton
    
    * wip: add dataframe utils
    
    * Switch to a single evaluation per dataframe
    
    * make copy the default
    
    * fix doc string
    
    * fix name
    
    * fix notebook
    
    * Add immutability
    
    * remove value from being required
    
    * fix tutorials formatting
    
    * make type a string to see if it fixes tests
    
    * fix test to handle un-parsable
    
    * Update src/phoenix/trace/trace_eval_dataset.py
    
    Co-authored-by: Xander Song <axiomofjoy@gmail.com>
    
    * Update src/phoenix/trace/trace_eval_dataset.py
    
    Co-authored-by: Xander Song <axiomofjoy@gmail.com>
    
    * change to trace_evaluations
    
    * cleanup
    
    * Fix formatting
    
    * pr comments
    
    * cleanup notebook
    
    * make sure columns are dropped
    
    * remove unused test
    
    ---------
    
    Co-authored-by: Xander Song <axiomofjoy@gmail.com>

--- src/phoenix/trace/trace_dataset.py
+++ src/phoenix/trace/trace_dataset.py
@@ -1,7 +1,7 @@
 import json
 import uuid
 from datetime import datetime
-from typing import Iterator, List, Optional, cast
+from typing import Iterable, Iterator, List, Optional, cast
 
 import pandas as pd
 from pandas import DataFrame, read_parquet
@@ -10,6 +10,7 @@ from phoenix.datetime_utils import normalize_timestamps
 
 from ..config import DATASET_DIR, GENERATED_DATASET_NAME_PREFIX
 from .schemas import ATTRIBUTE_PREFIX, CONTEXT_PREFIX, Span
+from .span_evaluations import EVALUATIONS_INDEX_NAME, SpanEvaluations
 from .span_json_decoder import json_to_span
 from .span_json_encoder import span_to_json
 
@@ -40,18 +41,33 @@ class TraceDataset:
     """
     A TraceDataset is a wrapper around a dataframe which is a flattened representation
     of Spans. The collection of spans trace the LLM application's execution.
-
-    Parameters
-    __________
-    dataframe: pandas.DataFrame
-        the pandas dataframe containing the tracing data. Each row represents a span.
     """
 
     name: str
     dataframe: pd.DataFrame
+    evaluations: List[SpanEvaluations] = []
     _data_file_name: str = "data.parquet"
 
-    def __init__(self, dataframe: DataFrame, name: Optional[str] = None):
+    def __init__(
+        self,
+        dataframe: DataFrame,
+        name: Optional[str] = None,
+        evaluations: Iterable[SpanEvaluations] = (),
+    ):
+        """
+        Constructs a TraceDataset from a dataframe of spans. Optionally takes in
+        evaluations for the spans in the dataset.
+
+        Parameters
+        __________
+        dataframe: pandas.DataFrame
+            the pandas dataframe containing the tracing data. Each row
+            represents a span.
+        evaluations: Optional[Iterable[SpanEvaluations]]
+            an optional list of evaluations for the spans in the dataset. If
+            provided, the evaluations can be materialized into a unified
+            dataframe as annotations.
+        """
         # Validate the the dataframe has required fields
         if missing_columns := set(REQUIRED_COLUMNS) - set(dataframe.columns):
             raise ValueError(
@@ -59,6 +75,7 @@ class TraceDataset:
             )
         self.dataframe = normalize_dataframe(dataframe)
         self.name = name or f"{GENERATED_DATASET_NAME_PREFIX}{str(uuid.uuid4())}"
+        self.evaluations = list(evaluations)
 
     @classmethod
     def from_spans(cls, spans: List[Span]) -> "TraceDataset":
@@ -133,3 +150,34 @@ class TraceDataset:
             allow_truncated_timestamps=True,
             coerce_timestamps="ms",
         )
+
+    def append_evaluations(self, evaluations: SpanEvaluations) -> None:
+        """adds an evaluation to the traces"""
+        # Append the evaluations to the list of evaluations
+        self.evaluations.append(evaluations)
+
+    def get_evals_dataframe(self) -> DataFrame:
+        """
+        Creates a flat dataframe of all the evaluations for the dataset.
+        """
+        return pd.concat(
+            [evals.get_dataframe(prefix_columns_with_name=True) for evals in self.evaluations],
+            axis=1,
+        )
+
+    def get_spans_dataframe(self, include_evaluations: bool = True) -> DataFrame:
+        """
+        converts the dataset to a dataframe of spans. If evaluations are included,
+        the evaluations are merged into the dataframe.
+
+        Parameters
+        __________
+        include_evaluations: bool
+            if True, the evaluations are merged into the dataframe
+        """
+        if not include_evaluations:
+            return self.dataframe.copy()
+        evals_df = self.get_evals_dataframe()
+        # Make sure the index is set to the span_id
+        df = self.dataframe.set_index(EVALUATIONS_INDEX_NAME, drop=False)
+        return pd.concat([df, evals_df], axis=1)
