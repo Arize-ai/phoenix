9289ff40fc67bac93ee2e60c0c3d116ddbcd9bf7
Alexander Song <axiomofjoy@gmail.com>
2024-01-18 15:50:21 -0800

    fix logic error

--- src/phoenix/session/session.py
+++ src/phoenix/session/session.py
@@ -224,7 +224,7 @@ class Session(ABC):
         return self.evals.export_evaluations()
 
     def get_trace_dataset(self) -> Optional[TraceDataset]:
-        if (dataframe := self.get_spans_dataframe()) is not None:
+        if (dataframe := self.get_spans_dataframe()) is None:
             return None
         evaluations = self.get_evaluations()
         return TraceDataset(dataframe=dataframe, evaluations=evaluations)
