839e704182b2399d9f9c9ebd3827b2ac827a7813
Roger Yang <roger.yang@arize.com>
2024-01-29 16:41:39 -0800

    refactor: more efficient span lookup

--- tests/core/test_traces.py
+++ tests/core/test_traces.py
@@ -1,5 +1,7 @@
-from uuid import uuid4
+from collections import namedtuple
+from itertools import count, islice
 
+import pytest
 from phoenix.core.traces import (
     Traces,
 )
@@ -10,19 +12,29 @@ class MockTraces(Traces):
         pass
 
 
-def test_get_descendant_span_ids() -> None:
-    span_ids = [uuid4() for _ in range(6)]
+def test_get_descendant_span_ids(spans) -> None:
+    spans = list(islice(spans, 6))
+    span_ids = [span.context.span_id for span in spans]
     mock = MockTraces()
-    mock._child_span_ids.update(
+    mock._child_spans.update(
         {
-            span_ids[1]: [span_ids[2], span_ids[3]],
-            span_ids[2]: [span_ids[4]],
-            span_ids[4]: [span_ids[5]],
+            span_ids[1]: {spans[2], spans[3]},
+            span_ids[2]: {spans[4]},
+            span_ids[4]: {spans[5]},
         }
     )
-    assert set(mock.get_descendant_span_ids(span_ids[0])) == set()
-    assert set(mock.get_descendant_span_ids(span_ids[1])) == set(span_ids[2:])
-    assert set(mock.get_descendant_span_ids(span_ids[2])) == set(span_ids[4:])
-    assert set(mock.get_descendant_span_ids(span_ids[3])) == set()
-    assert set(mock.get_descendant_span_ids(span_ids[4])) == set(span_ids[5:])
-    assert set(mock.get_descendant_span_ids(span_ids[5])) == set()
+    assert set(mock._get_descendant_spans(span_ids[0])) == set()
+    assert set(mock._get_descendant_spans(span_ids[1])) == set(spans[2:])
+    assert set(mock._get_descendant_spans(span_ids[2])) == set(spans[4:])
+    assert set(mock._get_descendant_spans(span_ids[3])) == set()
+    assert set(mock._get_descendant_spans(span_ids[4])) == set(spans[5:])
+    assert set(mock._get_descendant_spans(span_ids[5])) == set()
+
+
+Span = namedtuple("Span", "context")
+Context = namedtuple("Context", "span_id")
+
+
+@pytest.fixture
+def spans():
+    return (Span(context=Context(i)) for i in count())
