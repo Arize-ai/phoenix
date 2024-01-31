b6fed96b81b8faecdf3759cc0398234d5111ba71
Roger Yang <roger.yang@arize.com>
2024-01-30 21:01:45 -0800

    run in executor

--- src/phoenix/server/query_handlers.py
+++ src/phoenix/server/query_handlers.py
@@ -1,3 +1,5 @@
+import asyncio
+from functools import partial
 from typing import Optional, cast
 
 import pandas as pd
@@ -20,7 +22,15 @@ class GetSpansDataFrameHandler(HTTPEndpoint):
     async def post(self, request: Request) -> Response:
         payload = await request.json()
         filter_condition = cast(str, payload.pop("filter_condition", None) or "")
-        valid_eval_names = self.evals.get_span_evaluation_names() if self.evals else ()
+        loop = asyncio.get_running_loop()
+        valid_eval_names = (
+            await loop.run_in_executor(
+                None,
+                self.evals.get_span_evaluation_names,
+            )
+            if self.evals
+            else ()
+        )
         try:
             span_filter = SpanFilter(
                 filter_condition,
@@ -32,12 +42,16 @@ class GetSpansDataFrameHandler(HTTPEndpoint):
                 status_code=HTTP_422_UNPROCESSABLE_ENTITY,
                 content=f"Invalid filter condition: {e}",
             )
-        df = get_spans_dataframe(
-            self.traces,
-            span_filter,
-            start_time=payload.get("start_time"),
-            stop_time=payload.get("stop_time"),
-            root_spans_only=payload.get("root_spans_only"),
+        df = await loop.run_in_executor(
+            None,
+            partial(
+                get_spans_dataframe,
+                self.traces,
+                span_filter,
+                start_time=payload.get("start_time"),
+                stop_time=payload.get("stop_time"),
+                root_spans_only=payload.get("root_spans_only"),
+            ),
         )
         if df is None:
             return Response(status_code=HTTP_404_NOT_FOUND)
@@ -54,7 +68,15 @@ class QuerySpansHandler(HTTPEndpoint):
     async def post(self, request: Request) -> Response:
         payload = await request.json()
         queries = payload.pop("queries", [])
-        valid_eval_names = self.evals.get_span_evaluation_names() if self.evals else ()
+        loop = asyncio.get_running_loop()
+        valid_eval_names = (
+            await loop.run_in_executor(
+                None,
+                self.evals.get_span_evaluation_names,
+            )
+            if self.evals
+            else ()
+        )
         try:
             span_queries = [
                 SpanQuery.from_dict(
@@ -69,11 +91,16 @@ class QuerySpansHandler(HTTPEndpoint):
                 status_code=HTTP_422_UNPROCESSABLE_ENTITY,
                 content=f"Invalid query: {e}",
             )
-        results = query_spans(
-            self.traces,
-            *span_queries,
-            start_time=payload.get("start_time"),
-            stop_time=payload.get("stop_time"),
+        results = await loop.run_in_executor(
+            None,
+            partial(
+                query_spans,
+                self.traces,
+                *span_queries,
+                start_time=payload.get("start_time"),
+                stop_time=payload.get("stop_time"),
+                root_spans_only=payload.get("root_spans_only"),
+            ),
         )
         if not results:
             return Response(status_code=HTTP_404_NOT_FOUND)
