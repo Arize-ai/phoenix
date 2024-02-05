cb4381713ce7a89d9e0d0ea2c3a76667cf74ba50
Alexander Song <axiomofjoy@gmail.com>
2024-02-04 23:16:57 -0800

    running

--- src/phoenix/trace/llama_index/callback.py
+++ src/phoenix/trace/llama_index/callback.py
@@ -1,26 +1,27 @@
 import logging
 from typing import (
-    TYPE_CHECKING,
-    Any,
+    Callable,
+    List,
+    Optional,
+    Union,
 )
 
 from openinference.instrumentation.llama_index import (
     OpenInferenceTraceCallbackHandler as _OpenInferenceTraceCallbackHandler,
 )
-from openinference.instrumentation.llama_index.version import __version__
+from openinference.instrumentation.llama_index import (
+    __version__,
+)
 from opentelemetry import trace as trace_api
-from wrapt import ObjectProxy
 
-from phoenix.trace.exporter import HttpExporter
+from phoenix.trace.exporter import HttpExporter, NoOpExporter, OpenInferenceExporter
+from phoenix.trace.schemas import Span
 from phoenix.trace.tracer import OpenInferenceTracer
-
-if TYPE_CHECKING:
-    pass
 
 logger = logging.getLogger(__name__)
 
 
-class OpenInferenceTraceCallbackHandler(ObjectProxy):  # type: ignore
+class OpenInferenceTraceCallbackHandler(_OpenInferenceTraceCallbackHandler):
     """Callback handler for storing LLM application trace data in OpenInference format.
     OpenInference is an open standard for capturing and storing AI model
     inferences. It enables production LLMapp servers to seamlessly integrate
@@ -32,18 +33,20 @@ class OpenInferenceTraceCallbackHandler(ObjectProxy):  # type: ignore
 
     def __init__(
         self,
-        *args: Any,
-        **kwargs: Any,
+        callback: Optional[Callable[[List[Span]], None]] = None,
+        exporter: Optional[Union[OpenInferenceExporter, HttpExporter, NoOpExporter]] = None,
     ) -> None:
-        exporter = kwargs.get("exporter")
-        if isinstance(exporter, HttpExporter):
-            logger.warning("OpenInferenceTraceCallbackHandler: Using HttpExporter is deprecated. ")
-        self._tracer = OpenInferenceTracer(exporter=exporter)
-        if not (tracer_provider := kwargs.get("tracer_provider")):
-            tracer_provider = trace_api.get_tracer_provider()
-        tracer = trace_api.get_tracer(__name__, __version__, tracer_provider)
-        super().__init__(
-            _OpenInferenceTraceCallbackHandler(
-                tracer=tracer,
+        if callback is not None:
+            logger.warning(
+                "OpenInference has been updated for full OpenTelemetry compliance. "
+                "The legacy `callback` argument has been deprecated and no longer has any effect. "
+                "If you need access to spans for processing, some options include exporting spans "
+                "from an OpenTelemetry collector or adding a SpanProcessor to the OpenTelemetry "
+                "TracerProvider. More examples can be found in the Phoenix docs: "
+                "https://docs.arize.com/phoenix/deployment/instrumentation"
             )
+        compat_tracer = OpenInferenceTracer(exporter=exporter)
+        compat_tracer._configure_otel_tracer()  # temporary until we make a PR to llama_index
+        super().__init__(
+            tracer=trace_api.get_tracer(__name__, __version__, compat_tracer.tracer_provider)
         )
