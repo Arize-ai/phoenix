e0ca44308aff3738a9461e1ae30ffded61a795cf
Dustin Ngo <dustin@arize.com>
2024-02-06 12:10:42 -0500

    Factor out token counting functionality out of models

--- src/phoenix/experimental/evals/functions/processing.py
+++ src/phoenix/experimental/evals/functions/processing.py
@@ -1,6 +1,75 @@
+"""
+Token processing functions for supported models. This module is being deprecated.
+"""
+
+
 from typing import List
 
 from ..models import BaseEvalModel
+
+OPENAI_MODEL_TOKEN_LIMIT_MAPPING = {
+    "gpt-3.5-turbo-instruct": 4096,
+    "gpt-3.5-turbo-0301": 4096,
+    "gpt-3.5-turbo-0613": 4096,  # Current gpt-3.5-turbo default
+    "gpt-3.5-turbo-16k-0613": 16385,
+    "gpt-4-0314": 8192,
+    "gpt-4-0613": 8192,  # Current gpt-4 default
+    "gpt-4-32k-0314": 32768,
+    "gpt-4-32k-0613": 32768,
+    "gpt-4-1106-preview": 128000,
+    "gpt-4-vision-preview": 128000,
+}
+
+ANTHROPIC_MODEL_TOKEN_LIMIT_MAPPING = {
+    "claude-2.1": 200000,
+    "claude-2.0": 100000,
+    "claude-instant-1.2": 100000,
+}
+
+# https://cloud.google.com/vertex-ai/docs/generative-ai/learn/models
+GEMINI_MODEL_TOKEN_LIMIT_MAPPING = {
+    "gemini-pro": 32760,
+    "gemini-pro-vision": 16384,
+}
+
+BEDROCK_MODEL_TOKEN_LIMIT_MAPPING = {
+    "anthropic.claude-instant-v1": 100 * 1024,
+    "anthropic.claude-v1": 100 * 1024,
+    "anthropic.claude-v2": 100 * 1024,
+    "amazon.titan-text-express-v1": 8 * 1024,
+    "ai21.j2-mid-v1": 8 * 1024,
+    "ai21.j2-ultra-v1": 8 * 1024,
+}
+
+MODEL_TOKEN_LIMIT = {
+    **OPENAI_MODEL_TOKEN_LIMIT_MAPPING,
+    **ANTHROPIC_MODEL_TOKEN_LIMIT_MAPPING,
+    **GEMINI_MODEL_TOKEN_LIMIT_MAPPING,
+    **BEDROCK_MODEL_TOKEN_LIMIT_MAPPING,
+}
+
+
+def get_encoder(model: BaseEvalModel) -> None:
+    import tiktoken
+
+    try:
+        encoding = tiktoken.encoding_for_model(model.model_name)
+    except KeyError:
+        encoding = tiktoken.get_encoding("cl100k_base")
+    return encoding
+
+
+def max_context_size(model: BaseEvalModel) -> int:
+    # default to 4096
+    MODEL_TOKEN_LIMIT.get(model.model_name, 4096)
+
+
+def get_tokens_from_text(encoder, text: str) -> List[int]:
+    return encoder.encode(text)
+
+
+def get_text_from_tokens(encoder, tokens: List[int]) -> str:
+    return encoder.decode(tokens)
 
 
 def truncate_text_by_model(model: BaseEvalModel, text: str, token_buffer: int = 0) -> str:
@@ -16,17 +85,17 @@ def truncate_text_by_model(model: BaseEvalModel, text: str, token_buffer: int = 
     Returns:
         str: Truncated text
     """
-    max_token_count = model.max_context_size - token_buffer
-    tokens = model.get_tokens_from_text(text)
+    encoder = get_encoder(model)
+    max_token_count = max_context_size(model) - token_buffer
+    tokens = get_tokens_from_text(encoder, text)
     if len(tokens) > max_token_count:
-        return model.get_text_from_tokens(tokens[:max_token_count]) + "..."
+        return get_text_from_tokens(encoder, tokens[:max_token_count]) + "..."
     return text
 
 
 def concatenate_and_truncate_chunks(
     chunks: List[str], model: BaseEvalModel, token_buffer: int
 ) -> str:
-    """_summary_"""
     """Given a list of `chunks` of text, this function will return the concatenated chunks
     truncated to a token limit given by the `model` and `token_buffer`. See the function
     `truncate_text_by_model` for information on the truncation process.
@@ -39,6 +108,6 @@ def concatenate_and_truncate_chunks(
         truncated such that the resulting text comprises 950 tokens. Defaults to 0.
 
     Returns:
-        str: _description_
+        str: A prompt string that fits within a model's context window.
     """
     return truncate_text_by_model(model=model, text=" ".join(chunks), token_buffer=token_buffer)
