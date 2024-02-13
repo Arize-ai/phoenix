fe3e613002ed5343fa9d98b5417808856d4537bb
Mikyo King <mikyo@arize.com>
2024-02-13 15:01:27 -0700

    fix(trace): perform library version compatibility on llama_index (#2275)
    * refactor: replace semconv with those from openinference (#2270)
    
    * refactor: replace semantic conventions with those from openinference
    
    * lazy import
    
    ---------
    
    Co-authored-by: Roger Yang <80478925+RogerHYang@users.noreply.github.com>

--- src/phoenix/trace/llama_index/callback.py
+++ src/phoenix/trace/llama_index/callback.py
@@ -1,17 +1,8 @@
 import logging
-import re
-from importlib.metadata import PackageNotFoundError
+from importlib.metadata import PackageNotFoundError, version
 from importlib.util import find_spec
-from typing import (
-    Any,
-)
+from typing import Any
 
-from openinference.instrumentation.llama_index._callback import (
-    OpenInferenceTraceCallbackHandler as _OpenInferenceTraceCallbackHandler,
-)
-from openinference.instrumentation.llama_index.version import (
-    __version__,
-)
 from opentelemetry import trace as trace_api
 from opentelemetry.sdk import trace as trace_sdk
 from opentelemetry.sdk.trace.export import SimpleSpanProcessor
@@ -25,41 +16,45 @@ logger = logging.getLogger(__name__)
 LLAMA_INDEX_MODERN_VERSION = (0, 10, 0)
 INSTRUMENTATION_MODERN_VERSION = (1, 0, 0)
 
-
-def _parse_version(version: str) -> tuple[int, int, int]:
-    # Use a regular expression to match the major, minor, and patch components
-    # The expression captures digits in three groups and ignores any suffixes like "-post1"
-    match = re.match(r"(\d+)\.(\d+)\.(\d+)", version)
-    if not match:
-        raise ValueError(f"Invalid version format: {version}")
-
-    # Extract the major, minor, and patch numbers and convert them to integers
-    major, minor, patch = map(int, match.groups())
-
-    return (major, minor, patch)
-
-
-def _check_instrumentation_compatibility() -> None:
-    import llama_index as llama_index
-    import openinference.instrumentation.llama_index as instrumentation
-
+def _check_instrumentation_compatibility() -> bool:
     # split the version string into a tuple of integers
-    llama_index_version_str = llama_index.__version__
-    instrumentation_version_str = instrumentation.__version__  # type: ignore
-    llama_index_version = _parse_version(llama_index_version_str)
-    instrumentation_version = _parse_version(instrumentation_version_str)
+    llama_index_version = tuple(map(int, version("llama-index").split(".")[:3]))
+    instrumentation_version = tuple(
+        map(int, version("openinference-instrumentation-llama-index").split(".")[:3])
+    )
     # check if the llama_index version is compatible with the instrumentation version
     if (
         llama_index_version < LLAMA_INDEX_MODERN_VERSION
         and instrumentation_version >= INSTRUMENTATION_MODERN_VERSION
     ):
         raise IncompatibleLibraryVersionError(
-            f"""llama-index v{llama_index_version_str} is not compatible with
+            f"""llama-index v{'.'.join(map(str, llama_index_version))} is not compatible with
              openinference-instrumentation-llama-index
-             v{instrumentation_version_str}. Please either migrate llama-index to
+             v{instrumentation_version}. Please either migrate llama-index to
              at least 0.10.0 or downgrade openinference-instrumentation-llama-index via
              `pip install openinference-instrumentation-llama-index<1.0.0`."""
         )
+    elif (
+        llama_index_version >= LLAMA_INDEX_MODERN_VERSION
+        and instrumentation_version < INSTRUMENTATION_MODERN_VERSION
+    ):
+        raise IncompatibleLibraryVersionError(
+            f"""llama-index v{'.'.join(map(str, llama_index_version))} is not compatible with
+             openinference-instrumentation-llama-index
+             v{instrumentation_version}. Please upgrade
+             openinference-instrumentation-llama-index to at least 1.0.0"""
+        )
+    # if the versions are compatible, return True
+    return True
+
+
+if _check_instrumentation_compatibility():
+    from openinference.instrumentation.llama_index._callback import (
+        OpenInferenceTraceCallbackHandler as _OpenInferenceTraceCallbackHandler,
+    )
+    from openinference.instrumentation.llama_index.version import (
+        __version__,
+    )
 
 
 class OpenInferenceTraceCallbackHandler(_OpenInferenceTraceCallbackHandler):
