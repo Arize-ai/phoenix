46c2303207758c83662a573390daaea464fd8f8a
Roger Yang <roger.yang@arize.com>
2024-02-12 20:18:01 -0800

    feat: fiter by metadata keys

--- tests/trace/dsl/test_filter.py
+++ tests/trace/dsl/test_filter.py
@@ -101,7 +101,14 @@ def test_span_filter_by_eval_exceptions(spans, evals, eval_name):
         SpanFilter("evals[123].score < 0.5", evals=evals)
 
 
-Span = namedtuple("Span", "context")
+def test_span_filter_by_metadata(spans):
+    spans = list(islice(spans, 3))
+
+    sf = SpanFilter('metadata["odd index"] == 1')
+    assert list(filter(sf, spans)) == [spans[1]]
+
+
+Span = namedtuple("Span", "context attributes")
 Context = namedtuple("Context", "span_id")
 Evals = namedtuple("Evals", "get_span_evaluation")
 
@@ -121,4 +128,10 @@ def eval_name():
 
 @pytest.fixture
 def spans():
-    return (Span(context=Context(i)) for i in count())
+    return (
+        Span(
+            context=Context(i),
+            attributes={**({"metadata": {"odd index": i}} if i % 2 else {})},
+        )
+        for i in count()
+    )
