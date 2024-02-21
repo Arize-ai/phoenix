f2e52d9c0e4e68ff62d5e80563eb353637e8ed5b
Dustin Ngo <dustin@arize.com>
2024-02-21 10:42:34 -0500

    Add tests

--- packages/phoenix-evals/tests/phoenix/evals/test_dummy.py
+++ packages/phoenix-evals/tests/phoenix/evals/test_dummy.py
@@ -1,8 +1,8 @@
 """
 Dummy test file. Should be removed.
 """
-import phoenix.evals.version
+import evals.version
 
 
 def test_dummy() -> None:
-    assert phoenix.evals.version.__version__
+    assert evals.version.__version__
