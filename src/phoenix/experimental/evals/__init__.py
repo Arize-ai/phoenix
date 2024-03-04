882ded54be1b0b3574acdf49d6a6647d2ef25ab3
Alexander Song <axiomofjoy@gmail.com>
2024-03-04 12:45:35 -0800

    add deprecation warning to phoenix.experimental.evals

--- src/phoenix/experimental/evals/__init__.py
+++ src/phoenix/experimental/evals/__init__.py
@@ -1,3 +1,7 @@
+import logging
+import sys
+from typing import Any
+
 from .evaluators import (
     HallucinationEvaluator,
     LLMEvaluator,
@@ -27,39 +31,58 @@ from .templates import (
 )
 from .utils import NOT_PARSABLE, download_benchmark_dataset
 
-__all__ = [
-    "compute_precisions_at_k",
-    "download_benchmark_dataset",
-    "llm_classify",
-    "llm_generate",
-    "OpenAIModel",
-    "VertexAIModel",
-    "BedrockModel",
-    "LiteLLMModel",
-    "PromptTemplate",
-    "ClassificationTemplate",
-    "CODE_READABILITY_PROMPT_RAILS_MAP",
-    "CODE_READABILITY_PROMPT_TEMPLATE",
-    "HALLUCINATION_PROMPT_RAILS_MAP",
-    "HALLUCINATION_PROMPT_TEMPLATE",
-    "RAG_RELEVANCY_PROMPT_RAILS_MAP",
-    "RAG_RELEVANCY_PROMPT_TEMPLATE",
-    "TOXICITY_PROMPT_RAILS_MAP",
-    "TOXICITY_PROMPT_TEMPLATE",
-    "HUMAN_VS_AI_PROMPT_RAILS_MAP",
-    "HUMAN_VS_AI_PROMPT_TEMPLATE",
-    "QA_PROMPT_RAILS_MAP",
-    "QA_PROMPT_TEMPLATE",
-    "NOT_PARSABLE",
-    "run_relevance_eval",
-    "run_evals",
-    "LLMEvaluator",
-    "CodeReadabilityEvaluator",
-    "HallucinationEvaluator",
-    "HumanVsAIEvaluator",
-    "QAEvaluator",
-    "ReferenceLinkCorrectnessEvaluator",
-    "RelevanceEvaluator",
-    "SummarizationEvaluator",
-    "ToxicityEvaluator",
-]
+logger = logging.getLogger(__name__)
+
+
+exported = {
+    "compute_precisions_at_k": compute_precisions_at_k,
+    "download_benchmark_dataset": download_benchmark_dataset,
+    "llm_classify": llm_classify,
+    "llm_generate": llm_generate,
+    "OpenAIModel": OpenAIModel,
+    "VertexAIModel": VertexAIModel,
+    "BedrockModel": BedrockModel,
+    "LiteLLMModel": LiteLLMModel,
+    "PromptTemplate": PromptTemplate,
+    "ClassificationTemplate": ClassificationTemplate,
+    "CODE_READABILITY_PROMPT_RAILS_MAP": CODE_READABILITY_PROMPT_RAILS_MAP,
+    "CODE_READABILITY_PROMPT_TEMPLATE": CODE_READABILITY_PROMPT_TEMPLATE,
+    "HALLUCINATION_PROMPT_RAILS_MAP": HALLUCINATION_PROMPT_RAILS_MAP,
+    "HALLUCINATION_PROMPT_TEMPLATE": HALLUCINATION_PROMPT_TEMPLATE,
+    "RAG_RELEVANCY_PROMPT_RAILS_MAP": RAG_RELEVANCY_PROMPT_RAILS_MAP,
+    "RAG_RELEVANCY_PROMPT_TEMPLATE": RAG_RELEVANCY_PROMPT_TEMPLATE,
+    "TOXICITY_PROMPT_RAILS_MAP": TOXICITY_PROMPT_RAILS_MAP,
+    "TOXICITY_PROMPT_TEMPLATE": TOXICITY_PROMPT_TEMPLATE,
+    "HUMAN_VS_AI_PROMPT_RAILS_MAP": HUMAN_VS_AI_PROMPT_RAILS_MAP,
+    "HUMAN_VS_AI_PROMPT_TEMPLATE": HUMAN_VS_AI_PROMPT_TEMPLATE,
+    "QA_PROMPT_RAILS_MAP": QA_PROMPT_RAILS_MAP,
+    "QA_PROMPT_TEMPLATE": QA_PROMPT_TEMPLATE,
+    "NOT_PARSABLE": NOT_PARSABLE,
+    "run_relevance_eval": run_relevance_eval,
+    "run_evals": run_evals,
+    "LLMEvaluator": LLMEvaluator,
+    "HallucinationEvaluator": HallucinationEvaluator,
+    "QAEvaluator": QAEvaluator,
+    "RelevanceEvaluator": RelevanceEvaluator,
+    "SummarizationEvaluator": SummarizationEvaluator,
+    "ToxicityEvaluator": ToxicityEvaluator,
+}
+
+
+class _DEPRECATED_MODULE:
+    __all__ = tuple(exported.keys())
+
+    def __getattr__(self, name: str) -> Any:
+        if name not in exported:
+            raise AttributeError(f"module {__name__} has no attribute {name}")
+        logger.warning(
+            "Evals are moving out of experimental."
+            "Install the evals extra with `pip install arize-phoenix[evals]` and "
+            f"import `phoenix.evals.{name}`. "
+            "For more info, see the [migration guide](https://github.com/Arize-ai/phoenix/blob/main/MIGRATION.md)."
+        )
+        return exported[name]
+
+
+# See e.g. https://stackoverflow.com/a/7668273
+sys.modules[__name__] = _DEPRECATED_MODULE()  # type: ignore
