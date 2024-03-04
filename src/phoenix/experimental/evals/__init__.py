94f3d1a6297971ebd493902e8584e6cac63994c3
Alexander Song <axiomofjoy@gmail.com>
2024-03-04 14:32:30 -0800

    use ObjectProxy instead to allow easy pass through to submodules

--- src/phoenix/experimental/evals/__init__.py
+++ src/phoenix/experimental/evals/__init__.py
@@ -2,6 +2,8 @@ import logging
 import sys
 from typing import Any
 
+from wrapt import ObjectProxy
+
 from .evaluators import (
     HallucinationEvaluator,
     LLMEvaluator,
@@ -33,56 +35,51 @@ from .utils import NOT_PARSABLE, download_benchmark_dataset
 
 logger = logging.getLogger(__name__)
 
+__all__ = [
+    "compute_precisions_at_k",
+    "download_benchmark_dataset",
+    "llm_classify",
+    "llm_generate",
+    "OpenAIModel",
+    "VertexAIModel",
+    "BedrockModel",
+    "LiteLLMModel",
+    "PromptTemplate",
+    "ClassificationTemplate",
+    "CODE_READABILITY_PROMPT_RAILS_MAP",
+    "CODE_READABILITY_PROMPT_TEMPLATE",
+    "HALLUCINATION_PROMPT_RAILS_MAP",
+    "HALLUCINATION_PROMPT_TEMPLATE",
+    "RAG_RELEVANCY_PROMPT_RAILS_MAP",
+    "RAG_RELEVANCY_PROMPT_TEMPLATE",
+    "TOXICITY_PROMPT_RAILS_MAP",
+    "TOXICITY_PROMPT_TEMPLATE",
+    "HUMAN_VS_AI_PROMPT_RAILS_MAP",
+    "HUMAN_VS_AI_PROMPT_TEMPLATE",
+    "QA_PROMPT_RAILS_MAP",
+    "QA_PROMPT_TEMPLATE",
+    "NOT_PARSABLE",
+    "run_relevance_eval",
+    "run_evals",
+    "LLMEvaluator",
+    "HallucinationEvaluator",
+    "QAEvaluator",
+    "RelevanceEvaluator",
+    "SummarizationEvaluator",
+    "ToxicityEvaluator",
+]
 
-exported = {
-    "compute_precisions_at_k": compute_precisions_at_k,
-    "download_benchmark_dataset": download_benchmark_dataset,
-    "llm_classify": llm_classify,
-    "llm_generate": llm_generate,
-    "OpenAIModel": OpenAIModel,
-    "VertexAIModel": VertexAIModel,
-    "BedrockModel": BedrockModel,
-    "LiteLLMModel": LiteLLMModel,
-    "PromptTemplate": PromptTemplate,
-    "ClassificationTemplate": ClassificationTemplate,
-    "CODE_READABILITY_PROMPT_RAILS_MAP": CODE_READABILITY_PROMPT_RAILS_MAP,
-    "CODE_READABILITY_PROMPT_TEMPLATE": CODE_READABILITY_PROMPT_TEMPLATE,
-    "HALLUCINATION_PROMPT_RAILS_MAP": HALLUCINATION_PROMPT_RAILS_MAP,
-    "HALLUCINATION_PROMPT_TEMPLATE": HALLUCINATION_PROMPT_TEMPLATE,
-    "RAG_RELEVANCY_PROMPT_RAILS_MAP": RAG_RELEVANCY_PROMPT_RAILS_MAP,
-    "RAG_RELEVANCY_PROMPT_TEMPLATE": RAG_RELEVANCY_PROMPT_TEMPLATE,
-    "TOXICITY_PROMPT_RAILS_MAP": TOXICITY_PROMPT_RAILS_MAP,
-    "TOXICITY_PROMPT_TEMPLATE": TOXICITY_PROMPT_TEMPLATE,
-    "HUMAN_VS_AI_PROMPT_RAILS_MAP": HUMAN_VS_AI_PROMPT_RAILS_MAP,
-    "HUMAN_VS_AI_PROMPT_TEMPLATE": HUMAN_VS_AI_PROMPT_TEMPLATE,
-    "QA_PROMPT_RAILS_MAP": QA_PROMPT_RAILS_MAP,
-    "QA_PROMPT_TEMPLATE": QA_PROMPT_TEMPLATE,
-    "NOT_PARSABLE": NOT_PARSABLE,
-    "run_relevance_eval": run_relevance_eval,
-    "run_evals": run_evals,
-    "LLMEvaluator": LLMEvaluator,
-    "HallucinationEvaluator": HallucinationEvaluator,
-    "QAEvaluator": QAEvaluator,
-    "RelevanceEvaluator": RelevanceEvaluator,
-    "SummarizationEvaluator": SummarizationEvaluator,
-    "ToxicityEvaluator": ToxicityEvaluator,
-}
 
-
-class _DEPRECATED_MODULE:
-    __all__ = tuple(exported.keys())
-
+class _DeprecatedModuleWrapper(ObjectProxy):  # type: ignore
     def __getattr__(self, name: str) -> Any:
-        if name not in exported:
-            raise AttributeError(f"module {__name__} has no attribute {name}")
+        value = self.__wrapped__.__getattr__(name)
         logger.warning(
             "Evals are moving out of experimental."
             "Install the evals extra with `pip install arize-phoenix[evals]` and "
             f"import `phoenix.evals.{name}`. "
             "For more info, see the [migration guide](https://github.com/Arize-ai/phoenix/blob/main/MIGRATION.md)."
         )
-        return exported[name]
+        return value
 
 
-# See e.g. https://stackoverflow.com/a/7668273
-sys.modules[__name__] = _DEPRECATED_MODULE()  # type: ignore
+sys.modules[__name__] = _DeprecatedModuleWrapper(sys.modules[__name__])
