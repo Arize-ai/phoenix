94064d05ee03875817b3cecd53359c20cbfd0e45
Alexander Song <axiomofjoy@gmail.com>
2024-03-06 17:49:06 -0800

    add spans field to project gql type

--- src/phoenix/server/api/types/Project.py
+++ src/phoenix/server/api/types/Project.py
@@ -1,10 +1,25 @@
 from datetime import datetime
-from typing import Optional
+from itertools import chain
+from typing import List, Optional
 
 import strawberry
+from strawberry import ID, UNSET
+from strawberry.types import Info
 
 from phoenix.core.project import Project as CoreProject
+from phoenix.server.api.context import Context
+from phoenix.server.api.input_types.SpanSort import SpanSort
+from phoenix.server.api.input_types.TimeRange import TimeRange
 from phoenix.server.api.types.node import Node
+from phoenix.server.api.types.pagination import (
+    Connection,
+    ConnectionArgs,
+    Cursor,
+    connection_from_list,
+)
+from phoenix.server.api.types.Span import Span, to_gql_span
+from phoenix.trace.dsl import SpanFilter
+from phoenix.trace.schemas import TraceID
 
 
 @strawberry.type
@@ -37,3 +52,51 @@ class Project(Node):
     @strawberry.field
     def latency_ms_p99(self) -> Optional[float]:
         return self.project.root_span_latency_ms_quantiles(0.99)
+
+    @strawberry.field
+    def spans(
+        self,
+        info: Info[Context, None],
+        time_range: Optional[TimeRange] = UNSET,
+        trace_ids: Optional[List[ID]] = UNSET,
+        first: Optional[int] = 50,
+        last: Optional[int] = UNSET,
+        after: Optional[Cursor] = UNSET,
+        before: Optional[Cursor] = UNSET,
+        sort: Optional[SpanSort] = UNSET,
+        root_spans_only: Optional[bool] = UNSET,
+        filter_condition: Optional[str] = UNSET,
+    ) -> Connection[Span]:
+        args = ConnectionArgs(
+            first=first,
+            after=after if isinstance(after, Cursor) else None,
+            last=last,
+            before=before if isinstance(before, Cursor) else None,
+        )
+        if (traces := info.context.traces) is None:
+            return connection_from_list(data=[], args=args)
+        evals = info.context.evals
+        predicate = (
+            SpanFilter(
+                condition=filter_condition,
+                evals=evals,
+            )
+            if filter_condition
+            else None
+        )
+        if not trace_ids:
+            spans = traces.get_spans(
+                start_time=time_range.start if time_range else None,
+                stop_time=time_range.end if time_range else None,
+                root_spans_only=root_spans_only,
+            )
+        else:
+            spans = chain.from_iterable(
+                traces.get_trace(trace_id) for trace_id in map(TraceID, trace_ids)
+            )
+        if predicate:
+            spans = filter(predicate, spans)
+        if sort:
+            spans = sort(spans, evals=evals)
+        data = list(map(to_gql_span, spans))
+        return connection_from_list(data=data, args=args)
