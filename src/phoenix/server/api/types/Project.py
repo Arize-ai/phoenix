ab7354b4f813a757e4a120cb71ff08fcf2a105cd
Xander Song <axiomofjoy@gmail.com>
2024-03-07 11:40:38 -0800

    refactor: add spans field to gql project type (#2478)
    * add spans field to project gql type
    * pull information from project rather than traces

--- src/phoenix/server/api/types/Project.py
+++ src/phoenix/server/api/types/Project.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from itertools import chain
 from typing import List, Optional
 
 import strawberry
@@ -6,12 +7,20 @@ from strawberry import ID, UNSET
 
 from phoenix.core.project import Project as CoreProject
 from phoenix.metrics.retrieval_metrics import RetrievalMetrics
+from phoenix.server.api.input_types.SpanSort import SpanSort
 from phoenix.server.api.input_types.TimeRange import TimeRange
 from phoenix.server.api.types.DocumentEvaluationSummary import DocumentEvaluationSummary
 from phoenix.server.api.types.EvaluationSummary import EvaluationSummary
 from phoenix.server.api.types.node import Node
+from phoenix.server.api.types.pagination import (
+    Connection,
+    ConnectionArgs,
+    Cursor,
+    connection_from_list,
+)
+from phoenix.server.api.types.Span import Span, to_gql_span
 from phoenix.trace.dsl import SpanFilter
-from phoenix.trace.schemas import SpanID
+from phoenix.trace.schemas import SpanID, TraceID
 
 
 @strawberry.type
@@ -44,6 +53,52 @@ class Project(Node):
     @strawberry.field
     def latency_ms_p99(self) -> Optional[float]:
         return self.project.root_span_latency_ms_quantiles(0.99)
+
+    @strawberry.field
+    def spans(
+        self,
+        time_range: Optional[TimeRange] = UNSET,
+        trace_ids: Optional[List[ID]] = UNSET,
+        first: Optional[int] = 50,
+        last: Optional[int] = UNSET,
+        after: Optional[Cursor] = UNSET,
+        before: Optional[Cursor] = UNSET,
+        sort: Optional[SpanSort] = UNSET,
+        root_spans_only: Optional[bool] = UNSET,
+        filter_condition: Optional[str] = UNSET,
+    ) -> Connection[Span]:
+        args = ConnectionArgs(
+            first=first,
+            after=after if isinstance(after, Cursor) else None,
+            last=last,
+            before=before if isinstance(before, Cursor) else None,
+        )
+        if not (project := self.project).span_count:
+            return connection_from_list(data=[], args=args)
+        predicate = (
+            SpanFilter(
+                condition=filter_condition,
+                evals=project,
+            )
+            if filter_condition
+            else None
+        )
+        if not trace_ids:
+            spans = project.get_spans(
+                start_time=time_range.start if time_range else None,
+                stop_time=time_range.end if time_range else None,
+                root_spans_only=root_spans_only,
+            )
+        else:
+            spans = chain.from_iterable(
+                project.get_trace(trace_id) for trace_id in map(TraceID, trace_ids)
+            )
+        if predicate:
+            spans = filter(predicate, spans)
+        if sort:
+            spans = sort(spans, evals=project)
+        data = list(map(to_gql_span, spans))
+        return connection_from_list(data=data, args=args)
 
     @strawberry.field(
         description="Names of all available evaluations for spans. "
