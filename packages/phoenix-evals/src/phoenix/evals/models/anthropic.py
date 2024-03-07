5aa3842d3e3d8a1fe21fb62c594032474899fb81
Dustin Ngo <dustin@arize.com>
2024-03-07 14:11:36 -0500

    feat: Update `AnthropicModel` to use `messages` API (#2489)
    * Update `AnthropicModel` to use `messages` API
    
    * Fix type annotations

--- packages/phoenix-evals/src/phoenix/evals/models/anthropic.py
+++ packages/phoenix-evals/src/phoenix/evals/models/anthropic.py
@@ -1,9 +1,15 @@
 from dataclasses import dataclass, field
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Tuple
 
 from phoenix.evals.exceptions import PhoenixContextLimitExceeded
 from phoenix.evals.models.base import BaseModel
 from phoenix.evals.models.rate_limiters import RateLimiter
+
+MINIMUM_ANTHROPIC_VERSION = "0.18.0"
+
+
+def anthropic_version(version_str: str) -> Tuple[int, ...]:
+    return tuple(map(int, version_str.split(".")))
 
 
 @dataclass
@@ -36,14 +42,25 @@ class AnthropicModel(BaseModel):
     def _init_client(self) -> None:
         try:
             import anthropic
-
-            self._anthropic = anthropic
-            self.client = self._anthropic.Anthropic()
-            self.async_client = self._anthropic.AsyncAnthropic()
         except ImportError:
             self._raise_import_error(
                 package_name="anthropic",
+                package_min_version=MINIMUM_ANTHROPIC_VERSION,
             )
+
+        installed_version = anthropic_version(anthropic.__version__)
+        minimum_version = anthropic_version(MINIMUM_ANTHROPIC_VERSION)
+
+        if installed_version <= minimum_version:
+            raise ImportError(
+                "The installed version of `anthropic` is no longer supported. "
+                "Please upgrade to the latest version with "
+                f"`pip install -U anthropic>={MINIMUM_ANTHROPIC_VERSION}`"
+            )
+
+        self._anthropic = anthropic
+        self.client = self._anthropic.Anthropic()
+        self.async_client = self._anthropic.AsyncAnthropic()
 
     def _init_rate_limiter(self) -> None:
         self._rate_limiter = RateLimiter(
@@ -56,7 +73,7 @@ class AnthropicModel(BaseModel):
 
     def invocation_parameters(self) -> Dict[str, Any]:
         return {
-            "max_tokens_to_sample": self.max_tokens,
+            "max_tokens": self.max_tokens,
             "stop_sequences": self.stop_sequences,
             "temperature": self.temperature,
             "top_p": self.top_p,
@@ -71,7 +88,7 @@ class AnthropicModel(BaseModel):
         invocation_parameters.update(kwargs)
         response = self._rate_limited_completion(
             model=self.model,
-            prompt=self._format_prompt_for_claude(prompt),
+            messages=self._format_prompt_for_claude(prompt),
             **invocation_parameters,
         )
 
@@ -81,8 +98,8 @@ class AnthropicModel(BaseModel):
         @self._rate_limiter.limit
         def _completion(**kwargs: Any) -> Any:
             try:
-                response = self.client.completions.create(**kwargs)
-                return response.completion
+                response = self.client.messages.create(**kwargs)
+                return response.content[0].text
             except self._anthropic.BadRequestError as e:
                 exception_message = e.args[0]
                 if exception_message and "prompt is too long" in exception_message:
@@ -99,7 +116,7 @@ class AnthropicModel(BaseModel):
         invocation_parameters.update(kwargs)
         response = await self._async_rate_limited_completion(
             model=self.model,
-            prompt=self._format_prompt_for_claude(prompt),
+            messages=self._format_prompt_for_claude(prompt),
             **invocation_parameters,
         )
 
@@ -109,8 +126,8 @@ class AnthropicModel(BaseModel):
         @self._rate_limiter.alimit
         async def _async_completion(**kwargs: Any) -> Any:
             try:
-                response = await self.async_client.completions.create(**kwargs)
-                return response.completion
+                response = await self.async_client.messages.create(**kwargs)
+                return response.content[0].text
             except self._anthropic.BadRequestError as e:
                 exception_message = e.args[0]
                 if exception_message and "prompt is too long" in exception_message:
@@ -119,6 +136,8 @@ class AnthropicModel(BaseModel):
 
         return await _async_completion(**kwargs)
 
-    def _format_prompt_for_claude(self, prompt: str) -> str:
-        # Claude requires prompt in the format of Human: ... Assistant:
-        return f"{self._anthropic.HUMAN_PROMPT} {prompt} {self._anthropic.AI_PROMPT}"
+    def _format_prompt_for_claude(self, prompt: str) -> List[Dict[str, str]]:
+        # the Anthropic messages API expects a list of messages
+        return [
+            {"role": "user", "content": prompt},
+        ]
