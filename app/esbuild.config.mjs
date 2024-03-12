a52e9197ccc3efc5bb8d0e67c8913c8f8cbec310
Alexander Song <axiomofjoy@gmail.com>
2024-03-12 14:33:04 -0700

    add commit hash to index file name

--- app/esbuild.config.mjs
+++ app/esbuild.config.mjs
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 /* eslint-disable no-undef */
+import { execSync } from "child_process";
 import esbuild from "esbuild";
 
 import { relay } from "./esbuild.relay.mjs";
@@ -10,9 +11,15 @@ if (process.argv.length > 2 && process.argv[2] === "dev") {
 }
 const isDev = mode === "dev";
 
+let indexFileName = "index.js";
+if (mode !== "dev") {
+  const commitHash = execSync("git rev-parse HEAD").toString().trim().substring(0, 8);
+  indexFileName = `index.${commitHash}.js`;
+}
+
 const buildOptions = {
   entryPoints: ["src/index.tsx"],
-  outfile: "../src/phoenix/server/static/index.js",
+  outfile: `../src/phoenix/server/static/${indexFileName}`,
   minify: !isDev,
   bundle: true,
   target: ["es2020"],
