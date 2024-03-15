8999aac576db3c842a852a2bcbae0bf7d095df8c
Xander Song <axiomofjoy@gmail.com>
2024-03-15 08:37:38 -0700

    Update app/src/pages/projects/ProjectsPage.tsx
    Co-authored-by: Mikyo King <mikyo@arize.com>

--- app/src/pages/projects/ProjectsPage.tsx
+++ app/src/pages/projects/ProjectsPage.tsx
@@ -218,7 +218,7 @@ type ProjectActionDropdownProps = {
 };
 function ProjectActionDropdown({ handleDelete }: ProjectActionDropdownProps) {
   return (
-    <DropdownTrigger placement="bottom left">
+    <DropdownTrigger placement="bottom right">
       <ProjectActionsMenuButton />
       <ProjectDeleteActionButton handleDelete={handleDelete} />
     </DropdownTrigger>
