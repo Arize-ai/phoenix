8202a2d399b448b5d83ce58601e9968a584602f3
jlonge4 <91354480+jlonge4@users.noreply.github.com>
2024-03-20 18:03:55 -0400

    feat: update bedrock.py to use messages API for claude
    Make suggested edits

--- packages/phoenix-evals/src/phoenix/evals/models/bedrock.py
+++ packages/phoenix-evals/src/phoenix/evals/models/bedrock.py
@@ -160,7 +160,7 @@ class BedrockModel(BaseModel):
             return body.get("completions")[0].get("data").get("text")
         elif self.model_id.startswith("anthropic"):
             body = json.loads(response.get("body").read().decode())
-            return body.get("completion")
+            return body.get("content")[0]["text"]
         elif self.model_id.startswith("amazon"):
             body = json.loads(response.get("body").read())
             return body.get("results")[0].get("outputText")
