e66dd62f1e0eecefe896376099677bb608923496
Alexander Song <axiomofjoy@gmail.com>
2024-04-08 18:17:44 -0700

    add back in backticks

--- src/phoenix/db/models.py
+++ src/phoenix/db/models.py
@@ -66,7 +66,7 @@ class Base(DeclarativeBase):
         naming_convention={
             "ix": "ix_%(column_0_label)s",
             "uq": "uq_%(table_name)s_%(column_0_name)s",
-            "ck": "ck_%(table_name)s_%(constraint_name)s",
+            "ck": "ck_%(table_name)s_`%(constraint_name)s`",
             "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
             "pk": "pk_%(table_name)s",
         }
