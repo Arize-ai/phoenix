12221a8535c70f7c065d824a569b47d86ccc6f24
Roger Yang <roger.yang@arize.com>
2024-04-09 07:51:33 -0700

    clean up

--- src/phoenix/server/main.py
+++ src/phoenix/server/main.py
@@ -239,7 +239,7 @@ if __name__ == "__main__":
 
     working_dir = get_working_dir().resolve()
     db_connection_str = get_env_database_connection_str()
-    engine = create_engine(db_connection_str, echo=True)
+    engine = create_engine(db_connection_str)
     app = create_app(
         engine=engine,
         export_path=export_path,
