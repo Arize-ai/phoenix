2821bb4fe9fda331fccc6aa6f4fd40a54661a18b
Mikyo King <mikyo@arize.com>
2024-04-09 18:56:01 -0600

    fix: preserve loggers across migrations (#2835)

--- src/phoenix/db/migrate.py
+++ src/phoenix/db/migrate.py
@@ -1,9 +1,30 @@
 import logging
+from contextlib import contextmanager
 from pathlib import Path
+from typing import Any, Iterator, List
 
 from alembic import command
 from alembic.config import Config
 from sqlalchemy import URL
+
+
+@contextmanager
+def using_log_level(new_level: int, logger_names: List[str]) -> Iterator[Any]:
+    original_levels = {}
+    try:
+        for name in logger_names:
+            logger = logging.getLogger(name)
+            # Save the original log level
+            original_levels[name] = logger.level
+            # Set the new log level
+            logger.setLevel(new_level)
+        yield
+    finally:
+        # Revert log levels to their original states
+        for name, level in original_levels.items():
+            logger = logging.getLogger(name)
+            logger.setLevel(level)
+
 
 logger = logging.getLogger(__name__)
 
@@ -16,7 +37,8 @@ def migrate(url: URL) -> None:
     Args:
         url: The database URL.
     """
-    logger.warning("Running migrations on the database")
+    print("🏃‍♀️‍➡️ Running migrations on the database.")
+    print("---------------------------")
     config_path = str(Path(__file__).parent.resolve() / "alembic.ini")
     alembic_cfg = Config(config_path)
 
@@ -25,4 +47,7 @@ def migrate(url: URL) -> None:
     alembic_cfg.set_main_option("script_location", scripts_location)
     alembic_cfg.set_main_option("sqlalchemy.url", str(url))
 
-    command.upgrade(alembic_cfg, "head")
+    with using_log_level(logging.DEBUG, ["sqlalchemy", "alembic"]):
+        command.upgrade(alembic_cfg, "head")
+    print("---------------------------")
+    print("✅ Migrations complete.")
