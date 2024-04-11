075a8e6c3da12d7b838fbbd25f8090e7306631f6
Mikyo King <mikyo@arize.com>
2024-04-11 15:04:15 -0600

    chore: only log migrations when running as a container

--- src/phoenix/db/migrate.py
+++ src/phoenix/db/migrate.py
@@ -1,33 +1,19 @@
 import logging
-from contextlib import contextmanager
 from pathlib import Path
 from threading import Thread
-from typing import Any, Iterator, List
 
 from alembic import command
 from alembic.config import Config
 from sqlalchemy import URL
 
-
-@contextmanager
-def using_log_level(new_level: int, logger_names: List[str]) -> Iterator[Any]:
-    original_levels = {}
-    try:
-        for name in logger_names:
-            logger = logging.getLogger(name)
-            # Save the original log level
-            original_levels[name] = logger.level
-            # Set the new log level
-            logger.setLevel(new_level)
-        yield
-    finally:
-        # Revert log levels to their original states
-        for name, level in original_levels.items():
-            logger = logging.getLogger(name)
-            logger.setLevel(level)
-
+from phoenix.settings import Settings
 
 logger = logging.getLogger(__name__)
+
+
+def printif(condition: bool, text: str) -> None:
+    if condition:
+        print(text)
 
 
 def migrate(url: URL) -> None:
@@ -38,8 +24,9 @@ def migrate(url: URL) -> None:
     Args:
         url: The database URL.
     """
-    print("🏃‍♀️‍➡️ Running migrations on the database.")
-    print("---------------------------")
+    log_migrations = Settings.log_migrations
+    printif(log_migrations, "🏃‍♀️‍➡️ Running migrations on the database.")
+    printif(log_migrations, "---------------------------")
     config_path = str(Path(__file__).parent.resolve() / "alembic.ini")
     alembic_cfg = Config(config_path)
 
@@ -47,11 +34,9 @@ def migrate(url: URL) -> None:
     scripts_location = str(Path(__file__).parent.resolve() / "migrations")
     alembic_cfg.set_main_option("script_location", scripts_location)
     alembic_cfg.set_main_option("sqlalchemy.url", str(url))
-
-    with using_log_level(logging.DEBUG, ["sqlalchemy", "alembic"]):
-        command.upgrade(alembic_cfg, "head")
-    print("---------------------------")
-    print("✅ Migrations complete.")
+    command.upgrade(alembic_cfg, "head")
+    printif(log_migrations, "---------------------------")
+    printif(log_migrations, "✅ Migrations complete.")
 
 
 def migrate_in_thread(url: URL) -> None:
