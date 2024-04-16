f567ee12d4a9b950dba33ca44ce64f8aeb373acc
Mikyo King <mikyo@arize.com>
2024-04-15 18:13:37 -0600

    chore: remove filesystem based span_store

--- src/phoenix/server/api/routers/v1/traces.py
+++ src/phoenix/server/api/routers/v1/traces.py
@@ -5,7 +5,6 @@ from google.protobuf.message import DecodeError
 from opentelemetry.proto.collector.trace.v1.trace_service_pb2 import (
     ExportTraceServiceRequest,
 )
-from opentelemetry.proto.trace.v1.trace_pb2 import TracesData
 from starlette.requests import Request
 from starlette.responses import Response
 from starlette.status import (
@@ -15,7 +14,6 @@ from starlette.status import (
 )
 
 from phoenix.core.traces import Traces
-from phoenix.storage.span_store import SpanStore
 from phoenix.trace.otel import decode
 from phoenix.utilities.project import get_project_name
 
@@ -46,7 +44,6 @@ async def post_traces(request: Request) -> Response:
     if request.app.state.read_only:
         return Response(status_code=HTTP_403_FORBIDDEN)
     traces: Traces = request.app.state.traces
-    store: SpanStore = request.app.state.store
     content_type = request.headers.get("content-type")
     if content_type != "application/x-protobuf":
         return Response(
@@ -72,8 +69,6 @@ async def post_traces(request: Request) -> Response:
             content="Request body is invalid ExportTraceServiceRequest",
             status_code=HTTP_422_UNPROCESSABLE_ENTITY,
         )
-    if store:
-        store.save(TracesData(resource_spans=req.resource_spans))
     for resource_spans in req.resource_spans:
         project_name = get_project_name(resource_spans.resource.attributes)
         for scope_span in resource_spans.scope_spans:
