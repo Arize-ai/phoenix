801847686f8a67429912e8a0681380741d28d2cf
Dustin Ngo <dustin@arize.com>
2024-04-23 20:43:00 -0400

    Clean up fixtures for parametrization

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,18 @@
-
 import pytest
 from phoenix.db.models import Base
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
+
+
+def create_async_postgres_engine(psycopg_connection):
+    connection = psycopg_connection.cursor().connection
+    user = connection.info.user
+    password = connection.info.password
+    database = connection.info.dbname
+    host = connection.info.host
+    port = connection.info.port
+    async_database_url = f"postgresql+asyncpg://{user}:{password}@{host}:{port}/{database}"
+    return create_async_engine(async_database_url)
 
 
 def create_async_sqlite_engine():
@@ -11,14 +21,7 @@ def create_async_sqlite_engine():
 
 @pytest.fixture()
 async def postgres_engine(postgresql):
-    connection = postgresql.cursor().connection
-    user = connection.info.user
-    password = connection.info.password
-    database = connection.info.dbname
-    host = connection.info.host
-    port = connection.info.port
-    async_database_url = f"postgresql+asyncpg://{user}:{password}@{host}:{port}/{database}"
-    engine = create_async_engine(async_database_url)
+    engine = create_async_postgres_engine(postgresql)
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
     yield engine
@@ -34,15 +37,20 @@ async def sqlite_engine():
     await engine.dispose()
 
 
+@pytest.fixture
+def session(request):
+    return request.getfixturevalue(request.param)
+
+
 @pytest.fixture(scope="function")
-async def sqlite_session(sqlite_engine):
+async def sqlite(sqlite_engine):
     async_session = sessionmaker(sqlite_engine, expire_on_commit=False, class_=AsyncSession)
     async with async_session() as session:
         yield session
 
 
 @pytest.fixture(scope="function")
-async def postgresql_session(postgres_engine):
+async def postgres(postgres_engine):
     async_session = sessionmaker(postgres_engine, expire_on_commit=False, class_=AsyncSession)
     async with async_session() as session:
         yield session
--- tests/db/test_models.py
+++ tests/db/test_models.py
@@ -1,20 +1,13 @@
+import pytest
 from phoenix.db import models
 from sqlalchemy import select
 
 
-async def test_sqlite_initialization(sqlite_session):
+@pytest.mark.parametrize("session", ["sqlite", "postgres"], indirect=["session"])
+async def test_projects(session):
     project = models.Project(name="test_project")
-    sqlite_session.add(project)
-    await sqlite_session.commit()
+    session.add(project)
+    await session.commit()
     statement = select(models.Project).where(models.Project.name == "test_project")
-    result = (await sqlite_session.execute(statement)).scalars().first()
-    assert result is not None
-
-
-async def test_psql_initialization(postgresql_session):
-    project = models.Project(name="test_project")
-    postgresql_session.add(project)
-    await postgresql_session.commit()
-    statement = select(models.Project).where(models.Project.name == "test_project")
-    result = (await postgresql_session.execute(statement)).scalars().first()
+    result = (await session.execute(statement)).scalars().first()
     assert result is not None
