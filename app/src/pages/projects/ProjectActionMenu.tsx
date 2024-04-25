a717ff6c48d2b67dd7505bf2aa1d3db7f2c3e713
Mikyo King <mikyo@arize.com>
2024-04-25 17:41:31 -0600

    feat(persistence): clear traces UI (#2988)
    * feat(persistence): add a clear project dropdown item
    
    * hide the delete

--- app/src/pages/projects/ProjectActionMenu.tsx
+++ app/src/pages/projects/ProjectActionMenu.tsx
@@ -16,28 +16,44 @@ import {
 } from "@arizeai/components";
 import { ActionMenu, Flex, Item, Text } from "@arizeai/components";
 
-import { ProjectActionMenuMutation } from "./__generated__/ProjectActionMenuMutation.graphql";
+import { ProjectActionMenuClearMutation } from "./__generated__/ProjectActionMenuClearMutation.graphql";
+import { ProjectActionMenuDeleteMutation } from "./__generated__/ProjectActionMenuDeleteMutation.graphql";
+
+enum ProjectAction {
+  DELETE = "deleteProject",
+  CLEAR = "clearProject",
+}
 
 export function ProjectActionMenu({
   projectId,
   projectName,
   onProjectDelete,
+  onProjectClear,
 }: {
   projectId: string;
   projectName: string;
+  onProjectClear: () => void;
   onProjectDelete: () => void;
 }) {
   const [confirmDialog, setConfirmDialog] = useState<ReactNode>(null);
-  const [commit] = useMutation<ProjectActionMenuMutation>(graphql`
-    mutation ProjectActionMenuMutation($projectId: GlobalID!) {
+  const canDelete = projectName !== "default";
+  const [commitDelete] = useMutation<ProjectActionMenuDeleteMutation>(graphql`
+    mutation ProjectActionMenuDeleteMutation($projectId: GlobalID!) {
       deleteProject(id: $projectId) {
+        __typename
+      }
+    }
+  `);
+  const [commitClear] = useMutation<ProjectActionMenuClearMutation>(graphql`
+    mutation ProjectActionMenuClearMutation($projectId: GlobalID!) {
+      clearProject(id: $projectId) {
         __typename
       }
     }
   `);
   const handleDelete = useCallback(() => {
     startTransition(() => {
-      commit({
+      commitDelete({
         variables: {
           projectId: projectId,
         },
@@ -45,7 +61,19 @@ export function ProjectActionMenu({
 
       onProjectDelete();
     });
-  }, [commit, projectId, onProjectDelete]);
+  }, [commitDelete, projectId, onProjectDelete]);
+
+  const handleClear = useCallback(() => {
+    startTransition(() => {
+      commitClear({
+        variables: {
+          projectId: projectId,
+        },
+      });
+
+      onProjectClear();
+    });
+  }, [commitClear, projectId, onProjectClear]);
 
   const onDelete = useCallback(() => {
     setConfirmDialog(
@@ -72,6 +100,37 @@ export function ProjectActionMenu({
     );
   }, [handleDelete, projectName]);
 
+  const onClear = useCallback(() => {
+    setConfirmDialog(
+      <Dialog size="S" title="Clear Project">
+        <View padding="size-200">
+          <Text color="danger">
+            {`Are you sure you want to clear project ${projectName}? All traces and evaluations for this project will be deleted. This cannot be undone.`}
+          </Text>
+        </View>
+        <View
+          paddingEnd="size-200"
+          paddingTop="size-100"
+          paddingBottom="size-100"
+          borderTopColor="light"
+          borderTopWidth="thin"
+        >
+          <Flex direction="row" justifyContent="end">
+            <Button
+              variant="danger"
+              onClick={() => {
+                handleClear();
+                setConfirmDialog(null);
+              }}
+            >
+              Clear
+            </Button>
+          </Flex>
+        </View>
+      </Dialog>
+    );
+  }, [handleClear, projectName]);
+
   return (
     <div
       // TODO: add this logic to the ActionMenu component
@@ -85,24 +144,44 @@ export function ProjectActionMenu({
         buttonVariant="quiet"
         align="end"
         onAction={(action) => {
-          switch (action) {
-            case "deleteProject": {
+          switch (action as ProjectAction) {
+            case ProjectAction.DELETE: {
               return onDelete();
+            }
+            case ProjectAction.CLEAR: {
+              return onClear();
             }
           }
         }}
+        disabledKeys={canDelete ? [] : [ProjectAction.DELETE]}
       >
-        <Item key={"deleteProject"}>
+        <Item key={ProjectAction.CLEAR}>
           <Flex
             direction={"row"}
             gap="size-75"
             justifyContent={"start"}
             alignItems={"center"}
           >
-            <Icon svg={<Icons.TrashOutline />} />
-            <Text>Delete</Text>
+            <Icon svg={<Icons.Refresh />} />
+            <Text>Clear Data</Text>
           </Flex>
         </Item>
+        {canDelete ? (
+          <Item key={ProjectAction.DELETE}>
+            <Flex
+              direction={"row"}
+              gap="size-75"
+              justifyContent={"start"}
+              alignItems={"center"}
+            >
+              <Icon svg={<Icons.TrashOutline />} />
+              <Text>Delete</Text>
+            </Flex>
+          </Item>
+        ) : (
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          (null as any)
+        )}
       </ActionMenu>
       <DialogContainer
         type="modal"
