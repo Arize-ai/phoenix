44fe8698134d201907e49b5e14db29f21e527bb7
Alexander Song <axiomofjoy@gmail.com>
2024-04-25 18:08:58 -0700

    update index naming convention

--- src/phoenix/db/models.py
+++ src/phoenix/db/models.py
@@ -97,7 +97,7 @@ class Base(DeclarativeBase):
     # https://alembic.sqlalchemy.org/en/latest/naming.html#integration-of-naming-conventions-into-operations-autogenerate
     metadata = MetaData(
         naming_convention={
-            "ix": "ix_%(column_0_label)s",
+            "ix": "ix_%(table_name)s_%(column_0_N_name)s",
             "uq": "uq_%(table_name)s_%(column_0_N_name)s",
             "ck": "ck_%(table_name)s_`%(constraint_name)s`",
             "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
