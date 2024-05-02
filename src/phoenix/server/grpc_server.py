610c8faa05da0af1697684ef8970105cfefb35a9
Roger Yang <80478925+RogerHYang@users.noreply.github.com>
2024-05-02 09:11:50 -0700

    feat: grpc interceptor for prometheus (#3056)

--- src/phoenix/server/grpc_server.py
+++ src/phoenix/server/grpc_server.py
@@ -13,7 +13,6 @@ from opentelemetry.proto.collector.trace.v1.trace_service_pb2_grpc import (
 from typing_extensions import TypeAlias
 
 from phoenix.config import get_env_grpc_port
-from phoenix.server.prometheus import GRPC_EXCEPTIONS, GRPC_PROCESSING_TIME, GRPC_TOTAL_REQUESTS
 from phoenix.trace.otel import decode_otlp_span
 from phoenix.trace.schemas import Span
 from phoenix.utilities.project import get_project_name
@@ -32,24 +31,18 @@ class Servicer(TraceServiceServicer):
         super().__init__()
         self._callback = callback
 
-    @GRPC_PROCESSING_TIME.time()
     async def Export(
         self,
         request: ExportTraceServiceRequest,
         context: RpcContext,
     ) -> ExportTraceServiceResponse:
-        GRPC_TOTAL_REQUESTS.inc()
-        try:
-            for resource_spans in request.resource_spans:
-                project_name = get_project_name(resource_spans.resource.attributes)
-                for scope_span in resource_spans.scope_spans:
-                    for otlp_span in scope_span.spans:
-                        span = decode_otlp_span(otlp_span)
-                        await self._callback(span, project_name)
-            return ExportTraceServiceResponse()
-        except Exception as e:
-            GRPC_EXCEPTIONS.inc()
-            raise e
+        for resource_spans in request.resource_spans:
+            project_name = get_project_name(resource_spans.resource.attributes)
+            for scope_span in resource_spans.scope_spans:
+                for otlp_span in scope_span.spans:
+                    span = decode_otlp_span(otlp_span)
+                    await self._callback(span, project_name)
+        return ExportTraceServiceResponse()
 
 
 class GrpcServer:
@@ -57,17 +50,27 @@ class GrpcServer:
         self,
         callback: Callable[[Span, ProjectName], Awaitable[None]],
         tracer_provider: Optional["TracerProvider"] = None,
+        enable_prometheus: bool = False,
     ) -> None:
         self._callback = callback
         self._server: Optional[Server] = None
         self._tracer_provider = tracer_provider
+        self._enable_prometheus = enable_prometheus
 
     async def __aenter__(self) -> None:
+        interceptors = []
+        if self._enable_prometheus:
+            from py_grpc_prometheus.prometheus_server_interceptor import PromServerInterceptor
+
+            interceptors.append(PromServerInterceptor())
         if self._tracer_provider is not None:
             from opentelemetry.instrumentation.grpc import GrpcAioInstrumentorServer
 
             GrpcAioInstrumentorServer().instrument(tracer_provider=self._tracer_provider)  # type: ignore
-        server = grpc.aio.server(options=(("grpc.so_reuseport", 0),))
+        server = grpc.aio.server(
+            options=(("grpc.so_reuseport", 0),),
+            interceptors=interceptors,
+        )
         server.add_insecure_port(f"[::]:{get_env_grpc_port()}")
         add_TraceServiceServicer_to_server(Servicer(self._callback), server)  # type: ignore
         await server.start()
