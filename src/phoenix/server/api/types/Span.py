da50552b91b8079a0db1d3ee04f8b7f3e5ed2c15
Roger Yang <roger.yang@arize.com>
2024-05-06 07:34:52 -0700

    clean up

--- src/phoenix/server/api/types/Span.py
+++ src/phoenix/server/api/types/Span.py
@@ -69,7 +69,7 @@ class SpanIOValue:
         description="Truncate value up to `chars` characters, appending '...' if truncated.",
     )  # type: ignore
     def truncated_value(self, chars: int = 100) -> str:
-        return self.value[: max(0, chars - 3)] + "..." if len(self.value) > chars else self.value
+        return f"{self.value[: max(0, chars - 3)]}..." if len(self.value) > chars else self.value
 
 
 @strawberry.enum
