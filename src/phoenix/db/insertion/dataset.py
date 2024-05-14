ef4b8640e6723562bbe2891ab2576263a2d2d63a
Roger Yang <80478925+RogerHYang@users.noreply.github.com>
2024-05-13 18:28:55 -0700

    feat(datasets): dataset upload endpoint (plus fixtures) (#3183)

---
+++ src/phoenix/db/insertion/dataset.py
@@ -0,0 +1,242 @@
+import logging
+from dataclasses import dataclass
+from datetime import datetime, timezone
+from enum import Enum
+from itertools import chain
+from typing import (
+    Any,
+    Awaitable,
+    FrozenSet,
+    Iterable,
+    Iterator,
+    Mapping,
+    Optional,
+    Sequence,
+    Union,
+    cast,
+)
+
+from sqlalchemy import insert, select
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing_extensions import TypeAlias
+
+from phoenix.db import models
+from phoenix.db.insertion.helpers import DataManipulationEvent
+
+logger = logging.getLogger(__name__)
+
+DatasetId: TypeAlias = int
+DatasetVersionId: TypeAlias = int
+DatasetExampleId: TypeAlias = int
+DatasetExampleRevisionId: TypeAlias = int
+SpanRowId: TypeAlias = int
+Examples: TypeAlias = Iterable[Mapping[str, Any]]
+
+
+@dataclass(frozen=True)
+class DatasetCreationEvent(DataManipulationEvent):
+    dataset_id: DatasetId
+
+
+async def insert_dataset(
+    session: AsyncSession,
+    name: str,
+    description: Optional[str] = None,
+    metadata: Optional[Mapping[str, Any]] = None,
+    created_at: Optional[datetime] = None,
+) -> DatasetId:
+    id_ = await session.scalar(
+        insert(models.Dataset)
+        .values(
+            name=name,
+            description=description,
+            metadata_=metadata or {},
+            created_at=created_at,
+        )
+        .returning(models.Dataset.id)
+    )
+    return cast(DatasetId, id_)
+
+
+async def insert_dataset_version(
+    session: AsyncSession,
+    dataset_id: DatasetId,
+    description: Optional[str] = None,
+    metadata: Optional[Mapping[str, Any]] = None,
+    created_at: Optional[datetime] = None,
+) -> DatasetVersionId:
+    id_ = await session.scalar(
+        insert(models.DatasetVersion)
+        .values(
+            dataset_id=dataset_id,
+            description=description,
+            metadata_=metadata or {},
+            created_at=created_at,
+        )
+        .returning(models.DatasetVersion.id)
+    )
+    return cast(DatasetVersionId, id_)
+
+
+async def insert_dataset_example(
+    session: AsyncSession,
+    dataset_id: DatasetId,
+    span_rowid: Optional[SpanRowId] = None,
+    created_at: Optional[datetime] = None,
+) -> DatasetExampleId:
+    id_ = await session.scalar(
+        insert(models.DatasetExample)
+        .values(
+            dataset_id=dataset_id,
+            span_rowid=span_rowid,
+            created_at=created_at,
+        )
+        .returning(models.DatasetExample.id)
+    )
+    return cast(DatasetExampleId, id_)
+
+
+class RevisionKind(Enum):
+    CREATE = "CREATE"
+    PATCH = "PATCH"
+    DELETE = "DELETE"
+
+    @classmethod
+    def _missing_(cls, v: Any) -> "RevisionKind":
+        if isinstance(v, str) and v and v.isascii() and not v.isupper():
+            return cls(v.upper())
+        raise ValueError(f"Invalid revision kind: {v}")
+
+
+async def insert_dataset_example_revision(
+    session: AsyncSession,
+    dataset_version_id: DatasetVersionId,
+    dataset_example_id: DatasetExampleId,
+    input: Mapping[str, Any],
+    output: Mapping[str, Any],
+    metadata: Optional[Mapping[str, Any]] = None,
+    revision_kind: RevisionKind = RevisionKind.CREATE,
+    created_at: Optional[datetime] = None,
+) -> DatasetExampleRevisionId:
+    id_ = await session.scalar(
+        insert(models.DatasetExampleRevisions)
+        .values(
+            dataset_version_id=dataset_version_id,
+            dataset_example_id=dataset_example_id,
+            input=input,
+            output=output,
+            metadata_=metadata or {},
+            revision_kind=revision_kind.value,
+            created_at=created_at,
+        )
+        .returning(models.DatasetExampleRevisions.id)
+    )
+    return cast(DatasetExampleRevisionId, id_)
+
+
+class DatasetAction(Enum):
+    CREATE = "create"
+    APPEND = "append"
+
+    @classmethod
+    def _missing_(cls, v: Any) -> "DatasetAction":
+        if isinstance(v, str) and v and v.isascii() and not v.islower():
+            return cls(v.lower())
+        raise ValueError(f"Invalid dateset action: {v}")
+
+
+async def add_dataset_examples(
+    session: AsyncSession,
+    name: str,
+    examples: Union[Examples, Awaitable[Examples]],
+    input_keys: Sequence[str],
+    output_keys: Sequence[str],
+    metadata_keys: Sequence[str] = (),
+    description: Optional[str] = None,
+    metadata: Optional[Mapping[str, Any]] = None,
+    action: DatasetAction = DatasetAction.CREATE,
+) -> Optional[DatasetCreationEvent]:
+    keys = DatasetKeys(frozenset(input_keys), frozenset(output_keys), frozenset(metadata_keys))
+    created_at = datetime.now(timezone.utc)
+    dataset_id: Optional[DatasetId] = None
+    if action is DatasetAction.APPEND and name:
+        dataset_id = await session.scalar(
+            select(models.Dataset.id).where(models.Dataset.name == name)
+        )
+    if action is DatasetAction.CREATE or dataset_id is None:
+        try:
+            dataset_id = await insert_dataset(
+                session=session,
+                name=name,
+                description=description,
+                metadata=metadata,
+                created_at=created_at,
+            )
+        except Exception:
+            logger.exception(
+                f"Fail to insert dataset: {input_keys=}, {output_keys=}, {metadata_keys=}"
+            )
+            raise
+    try:
+        dataset_version_id = await insert_dataset_version(
+            session=session,
+            dataset_id=dataset_id,
+            created_at=created_at,
+        )
+    except Exception:
+        logger.exception(f"Fail to insert dataset version for {dataset_id=}")
+        raise
+    for example in (await examples) if isinstance(examples, Awaitable) else examples:
+        try:
+            dataset_example_id = await insert_dataset_example(
+                session=session,
+                dataset_id=dataset_id,
+                created_at=created_at,
+            )
+        except Exception:
+            logger.exception(f"Fail to insert dataset example for {dataset_id=}")
+            raise
+        try:
+            await insert_dataset_example_revision(
+                session=session,
+                dataset_version_id=dataset_version_id,
+                dataset_example_id=dataset_example_id,
+                input={key: example.get(key) for key in keys.input},
+                output={key: example.get(key) for key in keys.output},
+                metadata={key: example.get(key) for key in keys.metadata},
+                created_at=created_at,
+            )
+        except Exception:
+            logger.exception(
+                f"Fail to insert dataset example revision for {dataset_version_id=}, "
+                f"{dataset_example_id=}"
+            )
+            raise
+    return DatasetCreationEvent(dataset_id=dataset_id)
+
+
+@dataclass(frozen=True)
+class DatasetKeys:
+    input: FrozenSet[str]
+    output: FrozenSet[str]
+    metadata: FrozenSet[str]
+
+    def __iter__(self) -> Iterator[str]:
+        return chain(self.input, self.output, self.metadata)
+
+    def __post_init__(self) -> None:
+        if overlap := self.input.intersection(self.output):
+            raise ValueError(f"input_keys and output_keys have overlap: {overlap}")
+        if overlap := self.input.intersection(self.metadata):
+            raise ValueError(f"input_keys and metadata_keys have overlap: {overlap}")
+        if overlap := self.output.intersection(self.metadata):
+            raise ValueError(f"output_keys and metadata_keys have overlap: {overlap}")
+
+    def check_differences(self, column_headers_set: FrozenSet[str]) -> None:
+        for category, keys in (
+            ("input", self.input),
+            ("output", self.output),
+            ("metadata", self.metadata),
+        ):
+            if diff := keys.difference(column_headers_set):
+                raise ValueError(f"{category} keys not found in table column headers: {diff}")
---
+++ src/phoenix/server/api/routers/v1/datasets.py
@@ -0,0 +1,288 @@
+import csv
+import gzip
+import io
+import logging
+import zlib
+from asyncio import QueueFull
+from collections import Counter
+from enum import Enum
+from functools import partial
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    FrozenSet,
+    Iterator,
+    List,
+    Optional,
+    Tuple,
+    cast,
+)
+
+import pyarrow as pa
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette.concurrency import run_in_threadpool
+from starlette.datastructures import FormData, UploadFile
+from starlette.requests import Request
+from starlette.responses import Response
+from starlette.status import (
+    HTTP_403_FORBIDDEN,
+    HTTP_422_UNPROCESSABLE_ENTITY,
+    HTTP_429_TOO_MANY_REQUESTS,
+)
+from typing_extensions import TypeAlias, assert_never
+
+from phoenix.db import models
+from phoenix.db.insertion.dataset import DatasetAction, add_dataset_examples
+
+logger = logging.getLogger(__name__)
+
+
+async def post_datasets_upload(request: Request) -> Response:
+    """
+    summary: Upload CSV or PyArrow file as dataset
+    operationId: uploadDataset
+    tags:
+      - datasets
+    requestBody:
+      content:
+        multipart/form-data:
+          schema:
+            type: object
+            required:
+              - name
+              - inputKeys
+              - outputKeys
+              - file
+            properties:
+              name:
+                type: string
+                format: binary
+              description:
+                type: string
+                format: binary
+              inputKeys:
+                type: array
+                items:
+                  type: string
+                  format: binary
+              outputKeys:
+                type: array
+                items:
+                  type: string
+                  format: binary
+              metadataKeys:
+                type: array
+                items:
+                  type: string
+                  format: binary
+              file:
+                type: string
+                format: binary
+    responses:
+      200:
+        description: Success
+      403:
+        description: Forbidden
+      422:
+        description: Request body is invalid
+    """
+    if request.app.state.read_only:
+        return Response(status_code=HTTP_403_FORBIDDEN)
+    async with request.form() as form:
+        try:
+            (
+                action,
+                name,
+                description,
+                input_keys,
+                output_keys,
+                metadata_keys,
+                file,
+            ) = await _parse_form_data(form)
+        except ValueError as e:
+            return Response(
+                content=str(e),
+                status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+            )
+        if action is DatasetAction.CREATE:
+            async with request.app.state.db() as session:
+                if await _check_table_exists(session, name):
+                    return Response(
+                        content=f"Dataset already exists: {name=}",
+                        status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+                    )
+        content = await file.read()
+    try:
+        content_type = FileContentType(file.content_type)
+        if content_type is FileContentType.CSV:
+            get_examples, column_headers = await _process_csv(
+                content,
+                FileContentEncoding(file.headers.get("content-encoding")),
+            )
+        elif content_type is FileContentType.PYARROW:
+            get_examples, column_headers = await _process_pyarrow(
+                content,
+            )
+        else:
+            assert_never(content_type)
+        _check_keys_exist(column_headers, input_keys, output_keys, metadata_keys)
+    except ValueError as e:
+        return Response(
+            content=str(e),
+            status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+        )
+    try:
+        examples = run_in_threadpool(get_examples)
+        request.state.enqueue_operation(
+            partial(
+                add_dataset_examples,
+                examples=examples,
+                action=action,
+                name=name,
+                description=description,
+                input_keys=input_keys,
+                output_keys=output_keys,
+                metadata_keys=metadata_keys,
+            )
+        )
+    except QueueFull:
+        examples.close()
+        return Response(status_code=HTTP_429_TOO_MANY_REQUESTS)
+    return Response()
+
+
+class FileContentType(Enum):
+    CSV = "text/csv"
+    PYARROW = "application/x-pandas-pyarrow"
+
+    @classmethod
+    def _missing_(cls, v: Any) -> "FileContentType":
+        if isinstance(v, str) and v and v.isascii() and not v.islower():
+            return cls(v.lower())
+        raise ValueError(f"Invalid file content type: {v}")
+
+
+class FileContentEncoding(Enum):
+    NONE = "none"
+    GZIP = "gzip"
+    DEFLATE = "deflate"
+
+    @classmethod
+    def _missing_(cls, v: Any) -> "FileContentEncoding":
+        if v is None:
+            return cls("none")
+        if isinstance(v, str) and v and v.isascii() and not v.islower():
+            return cls(v.lower())
+        raise ValueError(f"Invalid file content encoding: {v}")
+
+
+Name: TypeAlias = str
+Description: TypeAlias = Optional[str]
+InputKeys: TypeAlias = FrozenSet[str]
+OutputKeys: TypeAlias = FrozenSet[str]
+MetadataKeys: TypeAlias = FrozenSet[str]
+DatasetId: TypeAlias = int
+Examples: TypeAlias = Iterator[Dict[str, Any]]
+
+
+async def _process_csv(
+    content: bytes,
+    content_encoding: FileContentEncoding,
+) -> Tuple[Callable[[], Examples], FrozenSet[str]]:
+    if content_encoding is FileContentEncoding.GZIP:
+        content = await run_in_threadpool(gzip.decompress, content)
+    elif content_encoding is FileContentEncoding.DEFLATE:
+        content = await run_in_threadpool(zlib.decompress, content)
+    elif content_encoding is not FileContentEncoding.NONE:
+        assert_never(content_encoding)
+    reader = await run_in_threadpool(lambda c: csv.DictReader(io.StringIO(c.decode())), content)
+    if reader.fieldnames is None:
+        raise ValueError("Missing CSV column header")
+    (header, freq), *_ = Counter(reader.fieldnames).most_common(1)
+    if freq > 1:
+        raise ValueError(f"Duplicated column header in CSV file: {header}")
+    column_headers = frozenset(reader.fieldnames)
+
+    def get_examples() -> Iterator[Dict[str, Any]]:
+        yield from reader
+
+    return get_examples, column_headers
+
+
+async def _process_pyarrow(
+    content: bytes,
+) -> Tuple[Callable[[], Examples], FrozenSet[str]]:
+    try:
+        reader = pa.ipc.open_stream(content)
+    except pa.ArrowInvalid as e:
+        raise ValueError("File is not valid pyarrow") from e
+    column_headers = frozenset(reader.schema.names)
+
+    def get_examples() -> Iterator[Dict[str, Any]]:
+        yield from reader.read_pandas().to_dict(orient="records")
+
+    return get_examples, column_headers
+
+
+async def _check_table_exists(session: AsyncSession, name: str) -> bool:
+    return bool(
+        await session.scalar(
+            select(1).select_from(models.Dataset).where(models.Dataset.name == name)
+        )
+    )
+
+
+def _check_keys_exist(
+    column_headers: FrozenSet[str],
+    input_keys: InputKeys,
+    output_keys: OutputKeys,
+    metadata_keys: MetadataKeys,
+) -> None:
+    for desc, keys in (
+        ("input", input_keys),
+        ("output", output_keys),
+        ("metadata", metadata_keys),
+    ):
+        if diff := keys.difference(column_headers):
+            raise ValueError(f"{desc} keys not found in column headers: {diff}")
+
+
+async def _parse_form_data(
+    form: FormData,
+) -> Tuple[
+    DatasetAction,
+    Name,
+    Description,
+    InputKeys,
+    OutputKeys,
+    MetadataKeys,
+    UploadFile,
+]:
+    name = cast(Optional[str], form.get("name"))
+    if not name:
+        raise ValueError("Dataset name must not be empty")
+    action = DatasetAction(cast(Optional[str], form.get("action")) or "create")
+    file = form["file"]
+    if not isinstance(file, UploadFile):
+        raise ValueError("Malformed file in form data.")
+    description = cast(Optional[str], form.get("description")) or file.filename
+    input_keys = frozenset(cast(List[str], form.getlist("input_keys[]")))
+    output_keys = frozenset(cast(List[str], form.getlist("output_keys[]")))
+    metadata_keys = frozenset(cast(List[str], form.getlist("metadata_keys[]")))
+    if overlap := input_keys.intersection(output_keys):
+        raise ValueError(f"input_keys, output_keys have overlap: {overlap}")
+    if overlap := input_keys.intersection(metadata_keys):
+        raise ValueError(f"input_keys and metadata_keys have overlap: {overlap}")
+    if overlap := output_keys.intersection(metadata_keys):
+        raise ValueError(f"output_keys and metadata_keys have overlap: {overlap}")
+    return (
+        action,
+        name,
+        description,
+        input_keys,
+        output_keys,
+        metadata_keys,
+        file,
+    )
