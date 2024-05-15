7c0d36eff85f01f880128138993f7d0b2182b563
Dustin Ngo <dustin@arize.com>
2024-05-14 21:39:08 -0400

    Flesh out test

--- tests/server/api/app/test_datasets.py
+++ tests/server/api/app/test_datasets.py
@@ -1,8 +1,15 @@
 async def test_datasets(test_client, dataset_0):
     response = test_client.get("/v1/datasets/0")
     assert response.status_code == 200
-    assert response.json() == {
+    dataset_json = response.json()
+
+    assert "created_at" in dataset_json
+    assert "updated_at" in dataset_json
+    fixture_values ={
         "id": 0,
         "name": "dataset 0",
         "description": "a test dataset",
+        "metadata": {"info": "a test dataset"},
+        "record_count": 1,
     }
+    assert all(item in dataset_json.items() for item in fixture_values.items())
