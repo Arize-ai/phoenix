8b2740104ab6afcce91b9f7f4616bcfff9c8e57b
Dustin Ngo <dustin@arize.com>
2024-05-16 16:46:23 -0400

    feat: Implement `GET /datasets/id` and `GET /datasets` (#3197)
    * Rename model
    
    * Add backend Dataset schema and `get` route for Datasets by ID
    
    * Refactor app creation and test fixtures for API test harness
    
    * Ruff 🐶
    
    * Add timestamps to payload
    
    * Use correct field names
    
    * Ensure timestamps are serializable
    
    * Flesh out test
    
    * Use async client
    
    * Add test for empty datasets
    
    * Add another nontrivial example
    
    * Ruff 🐶
    
    * Resolve type annotation errors
    
    * Update import
    
    * Use default export path
    
    * Create export directories in test env
    
    * Try creating home directories?
    
    * Do not mount UI route in test client
    
    * Explicitly disable UI route on initialization
    
    * Fix gnarly bug (properly skip prostgresql tests)
    
    * Push example count into model
    
    * Move serialization into route handler
    
    * Use global ids for REST API
    
    * Refactor server instrumentation
    
    * Break up engine creation and instrumentation for clarity
    
    * Remove engine from session factory wrapper
    
    * Use updated httpx client syntax
    
    * Improve type specificity for db dialect property
    
    * Use explicit kwarg for sqlalchemy join
    
    * Flesh out OpenAPI response schema for GET /datasets/id route
    
    * Implement list datasets with cursor-based pagination
    
    * Check dialect type when creating session factory
    
    * Move API tests to `rest_v1` subdirectory for clarity
    
    * Use hybrid property
    
    * Properly try and return a value from hybrid property
    
    * Use inplace expression

--- src/phoenix/server/main.py
+++ src/phoenix/server/main.py
@@ -32,7 +32,13 @@ from phoenix.pointcloud.umap_parameters import (
     DEFAULT_N_SAMPLES,
     UMAPParameters,
 )
-from phoenix.server.app import create_app
+from phoenix.server.app import (
+    SessionFactory,
+    _db,
+    create_app,
+    create_engine_and_run_migrations,
+    instrument_engine_if_enabled,
+)
 from phoenix.settings import Settings
 from phoenix.trace.fixtures import (
     TRACES_FIXTURES,
@@ -240,8 +246,11 @@ if __name__ == "__main__":
         start_prometheus()
 
     working_dir = get_working_dir().resolve()
+    engine = create_engine_and_run_migrations(db_connection_str)
+    instrumentation_cleanups = instrument_engine_if_enabled(engine)
+    factory = SessionFactory(session_factory=_db(engine), dialect=engine.dialect.name)
     app = create_app(
-        database_url=db_connection_str,
+        db=factory,
         export_path=export_path,
         model=model,
         umap_params=umap_params,
@@ -253,6 +262,7 @@ if __name__ == "__main__":
         enable_prometheus=enable_prometheus,
         initial_spans=fixture_spans,
         initial_evaluations=fixture_evals,
+        clean_up_callbacks=instrumentation_cleanups,
     )
     server = Server(config=Config(app, host=host, port=port, root_path=host_root_path))  # type: ignore
     Thread(target=_write_pid_file_when_ready, args=(server,), daemon=True).start()
