42aaa3587af27b2e7f39edf411925cf6f6184945
Simon Bachstein <mail@sbachstein.de>
2024-05-16 09:38:12 +0200

    Guarantee training slashes in host base URLs

--- src/phoenix/session/client.py
+++ src/phoenix/session/client.py
@@ -55,9 +55,11 @@ class Client(TraceDataExtractor):
         host = get_env_host()
         if host == "0.0.0.0":
             host = "127.0.0.1"
-        self._base_url = (
+        base_url = (
             endpoint or get_env_collector_endpoint() or f"http://{host}:{get_env_port()}"
         )
+        self._base_url = base_url if base_url.endswith("/") else base_url + "/"
+
         self._session = Session()
         weakref.finalize(self, self._session.close)
         if warn_if_server_not_running:
