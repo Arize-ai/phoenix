c9b484fd11f8cd7d49959f35d9fd5c691c557324
Mikyo King <mikyo@arize.com>
2024-05-15 20:12:58 -0600

    feat(datasets): gql dataset create (#3203)
    * feat(datasets): gql dataset create
    
    * PR comments
    
    * add defaults

--- src/phoenix/server/api/types/Dataset.py
+++ src/phoenix/server/api/types/Dataset.py
@@ -3,6 +3,7 @@ from typing import Optional
 
 import strawberry
 from strawberry.relay import Node, NodeID
+from strawberry.scalars import JSON
 
 
 @strawberry.type
@@ -10,5 +11,6 @@ class Dataset(Node):
     id_attr: NodeID[int]
     name: str
     description: Optional[str]
+    metadata: JSON
     created_at: datetime
     updated_at: datetime
