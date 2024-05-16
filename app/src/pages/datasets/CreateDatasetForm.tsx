d558b00a37a2de1c1490d540737b0f948c848235
Mikyo King <mikyo@arize.com>
2024-05-16 15:06:35 -0600

    feat(datasets): create dataset UI (#3217)
    * add form
    
    feat(datasets): create datset form
    
    feat(datasets): create datset form
    
    * Update app/src/pages/datasets/CreateDatasetForm.tsx
    
    Co-authored-by: Xander Song <axiomofjoy@gmail.com>
    
    * Update app/src/pages/datasets/CreateDatasetForm.tsx
    
    Co-authored-by: Xander Song <axiomofjoy@gmail.com>
    
    ---------
    
    Co-authored-by: Xander Song <axiomofjoy@gmail.com>

---
+++ app/src/pages/datasets/CreateDatasetForm.tsx
@@ -0,0 +1,144 @@
+import React, { useCallback } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { graphql, useMutation } from "react-relay";
+
+import {
+  Button,
+  Flex,
+  Form,
+  TextArea,
+  TextField,
+  View,
+} from "@arizeai/components";
+
+import {
+  CreateDatasetFormMutation,
+  CreateDatasetFormMutation$data,
+} from "./__generated__/CreateDatasetFormMutation.graphql";
+
+type CreateDatasetParams = {
+  name: string;
+  description: string;
+  metadata: Record<string, unknown>;
+};
+
+export type CreateDatasetFormProps = {
+  onDatasetCreated: (
+    dataset: CreateDatasetFormMutation$data["createDataset"]
+  ) => void;
+  onDatasetCreateError: (error: Error) => void;
+};
+
+export function CreateDatasetForm(props: CreateDatasetFormProps) {
+  const { onDatasetCreated, onDatasetCreateError } = props;
+  const {
+    control,
+    handleSubmit,
+    formState: { isDirty, isValid },
+  } = useForm({
+    defaultValues: {
+      name: "Dataset " + new Date().toISOString(),
+      description: "",
+      metadata: {},
+    } as CreateDatasetParams,
+  });
+  const [commit, isCommitting] = useMutation<CreateDatasetFormMutation>(graphql`
+    mutation CreateDatasetFormMutation(
+      $name: String!
+      $description: String = null
+      $metadata: JSON = null
+    ) {
+      createDataset(
+        name: $name
+        description: $description
+        metadata: $metadata
+      ) {
+        id
+        name
+        description
+      }
+    }
+  `);
+  const onSubmit = useCallback(
+    (params: CreateDatasetParams) => {
+      commit({
+        variables: params,
+        onCompleted: (response) => {
+          onDatasetCreated(response["createDataset"]);
+        },
+        onError: (error) => {
+          // TODO(datasets): cleanup error handling to show human friendly error
+          onDatasetCreateError(error);
+        },
+      });
+    },
+    [commit, onDatasetCreated, onDatasetCreateError]
+  );
+  return (
+    <>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <View padding="size-200">
+          <Controller
+            name="name"
+            control={control}
+            rules={{
+              required: "field is required",
+            }}
+            render={({
+              field: { onChange, onBlur, value },
+              fieldState: { invalid, error },
+            }) => (
+              <TextField
+                label="Dataset Name"
+                description={`The name of the dataset`}
+                errorMessage={error?.message}
+                validationState={invalid ? "invalid" : "valid"}
+                onChange={onChange}
+                onBlur={onBlur}
+                value={value.toString()}
+              />
+            )}
+          />
+          <Controller
+            name="description"
+            control={control}
+            render={({
+              field: { onChange, onBlur, value },
+              fieldState: { invalid, error },
+            }) => (
+              <TextArea
+                label="description"
+                isRequired={false}
+                height={100}
+                errorMessage={error?.message}
+                validationState={invalid ? "invalid" : "valid"}
+                onChange={onChange}
+                onBlur={onBlur}
+                value={value.toString()}
+              />
+            )}
+          />
+        </View>
+        <View
+          paddingEnd="size-200"
+          paddingTop="size-100"
+          paddingBottom="size-100"
+          borderTopColor="light"
+          borderTopWidth="thin"
+        >
+          <Flex direction="row" justifyContent="end">
+            <Button
+              type="submit"
+              isDisabled={!isValid}
+              variant={isDirty ? "primary" : "default"}
+              size="compact"
+              loading={isCommitting}
+            >
+              {isCommitting ? "Creating..." : "Create Dataset"}
+            </Button>
+          </Flex>
+        </View>
+      </Form>
+    </>
+  );
+}
