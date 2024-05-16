154fe5423a467a16924c538c8c0b46217b017c5e
Alexander Song <axiomofjoy@gmail.com>
2024-05-16 00:37:44 -0700

    initial sketch

--- src/phoenix/server/api/schema.py
+++ src/phoenix/server/api/schema.py
@@ -1,16 +1,17 @@
 from collections import defaultdict
-from typing import Dict, List, Optional, Set, Union
+from typing import Dict, List, Literal, Optional, Protocol, Set, Union
 
 import numpy as np
 import numpy.typing as npt
 import strawberry
+from openinference.semconv.trace import OpenInferenceMimeTypeValues, OpenInferenceSpanKindValues
 from sqlalchemy import delete, insert, select
 from sqlalchemy.orm import contains_eager, load_only
 from strawberry import ID, UNSET
 from strawberry.relay import Connection, GlobalID, Node
 from strawberry.scalars import JSON
 from strawberry.types import Info
-from typing_extensions import Annotated
+from typing_extensions import Annotated, Any
 
 from phoenix.config import DEFAULT_PROJECT_NAME
 from phoenix.db import models
@@ -383,6 +384,155 @@ class Mutation(ExportEventsMutation):
                 updated_at=row.updated_at,
                 metadata=row.metadata_,
             )
+
+    @strawberry.mutation
+    async def add_spans_to_dataset(
+        self,
+        info: Info[Context, None],
+        dataset_id: GlobalID,
+        dataset_version_id: GlobalID,
+        span_ids: List[GlobalID],
+    ) -> Query:
+        dataset_rowid = from_global_id_with_expected_type(
+            global_id=dataset_id, expected_type_name="Dataset"
+        )
+        dataset_version_rowid = from_global_id_with_expected_type(
+            global_id=dataset_version_id, expected_type_name="DatasetVersion"
+        )
+        span_rowids = [
+            from_global_id_with_expected_type(global_id=span_id, expected_type_name="Span")
+            for span_id in span_ids
+        ]
+        async with info.context.db() as session:
+            spans = await session.execute(
+                select(
+                    models.Span.id,
+                    models.Span.span_kind,
+                    models.Span.attributes,
+                    models.Span.attributes["input"]["mime_type"].label("input_mime_type"),
+                    models.Span.attributes["input"]["value"].label("input_value"),
+                    models.Span.attributes["output"]["mime_type"].label("output_mime_type"),
+                    models.Span.attributes["input"]["value"].label("output_value"),
+                    models.Span.attributes["llm"]["prompt_template"]["variables"].label(
+                        "prompt_template_variables"
+                    ),
+                    models.Span.attributes["llm"]["input"]["messages"].label("llm_input_messages"),
+                    models.Span.attributes["llm"]["output"]["messages"].label(
+                        "llm_output_messages"
+                    ),
+                    models.Span.attributes["retrieval"]["documents"].label("retrieval_documents"),
+                ).where(models.Span.id.in_(span_rowids))
+            )
+            for span in spans:
+                dataset_example_rowid = await session.scalar(
+                    insert(models.DatasetExample)
+                    .values(dataset_id=dataset_rowid, span_rowid=span.id)
+                    .returning(models.DatasetExample.id)
+                )
+                await session.execute(
+                    insert(models.DatasetExampleRevisions).values(
+                        dataset_example_id=dataset_example_rowid,
+                        dataset_version_id=dataset_version_rowid,
+                        input=get_dataset_example_input(span),
+                        output=get_dataset_example_output(span),
+                        metadata_=span.attributes,
+                        revision_kind="CREATE",
+                    )
+                )
+        return Query()
+
+
+class HasSpanIOAttributes(Protocol):
+    span_kind: Optional[str]
+    input_value: Any
+    input_mime_type: Optional[str]
+    output_value: Any
+    output_mime_type: Optional[str]
+    prompt_template_variables: Any
+    llm_input_messages: Any
+    llm_output_messages: Any
+    retrieval_documents: Any
+
+
+def get_dataset_example_input(span: HasSpanIOAttributes) -> Dict[str, Any]:
+    input_value = span.input_value
+    input_mime_type = span.input_mime_type
+    if span.span_kind == OpenInferenceSpanKindValues.LLM.value:
+        return _get_llm_span_input(
+            llm_input_messages=span.llm_input_messages,
+            input_value=input_value,
+            input_mime_type=input_mime_type,
+            prompt_template_variables=span.prompt_template_variables,
+        )
+    return _get_generic_io_value(io_value=input_value, mime_type=input_mime_type, kind="input")
+
+
+def get_dataset_example_output(span: HasSpanIOAttributes) -> Dict[str, Any]:
+    output_value = span.output_value
+    output_mime_type = span.output_mime_type
+    if (span_kind := span.span_kind) == OpenInferenceSpanKindValues.LLM.value:
+        return _get_llm_span_output(
+            llm_output_messages=span.llm_output_messages,
+            output_value=output_value,
+            output_mime_type=output_mime_type,
+        )
+    if span_kind == OpenInferenceSpanKindValues.RETRIEVER.value:
+        return _get_retriever_span_output(
+            retrieval_documents=span.retrieval_documents,
+            output_value=output_value,
+            output_mime_type=output_mime_type,
+        )
+    return _get_generic_io_value(io_value=output_value, mime_type=output_mime_type, kind="output")
+
+
+def _get_llm_span_input(
+    llm_input_messages: Any,
+    input_value: Any,
+    input_mime_type: Optional[str],
+    prompt_template_variables: Any,
+) -> Dict[str, Any]:
+    input: Dict[str, Any]
+    if llm_input_messages is not None:
+        input = {"input_messages": llm_input_messages}
+    else:
+        input = _get_generic_io_value(io_value=input_value, mime_type=input_mime_type, kind="input")
+    if prompt_template_variables:
+        input = {**input, "prompt_template_variables": prompt_template_variables}
+    return input
+
+
+def _get_llm_span_output(
+    llm_output_messages: Any,
+    output_value: Any,
+    output_mime_type: Optional[str],
+) -> Dict[str, Any]:
+    if llm_output_messages is not None:
+        return {"output_messages": llm_output_messages}
+    return _get_generic_io_value(io_value=output_value, mime_type=output_mime_type, kind="input")
+
+
+def _get_retriever_span_output(
+    retrieval_documents: Any,
+    output_value: Any,
+    output_mime_type: Optional[str],
+) -> Dict[str, Any]:
+    if retrieval_documents is not None:
+        return {"retrieval_documents": retrieval_documents}
+    return _get_generic_io_value(io_value=output_value, mime_type=output_mime_type, kind="input")
+
+
+def _get_generic_io_value(
+    io_value: Any, mime_type: Optional[str], kind: Literal["input", "output"]
+) -> Dict[str, Any]:
+    if isinstance(io_value, str) and (
+        mime_type == OpenInferenceMimeTypeValues.TEXT.value or mime_type is None
+    ):
+        return {kind: io_value}
+    if isinstance(io_value, dict) and (
+        mime_type == OpenInferenceMimeTypeValues.JSON.value or mime_type is None
+    ):
+        return io_value
+    return {}
 
 
 # This is the schema for generating `schema.graphql`.
