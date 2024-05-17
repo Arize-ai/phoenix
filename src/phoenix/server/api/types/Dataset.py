de28b1252e7079c1957f1a67dce1a56f390a1523
Mikyo King <mikyo@arize.com>
2024-05-16 20:30:42 -0600

    feat(dataset): gql dataset versions connection (#3222)
    * wip
    
    * feat(datasets): gql dataset versions connection
    
    * gql

--- src/phoenix/server/api/types/Dataset.py
+++ src/phoenix/server/api/types/Dataset.py
@@ -2,8 +2,21 @@ from datetime import datetime
 from typing import Optional
 
 import strawberry
-from strawberry.relay import Node, NodeID
+from sqlalchemy import select
+from strawberry import UNSET
+from strawberry.relay import Connection, Node, NodeID
 from strawberry.scalars import JSON
+from strawberry.types import Info
+
+from phoenix.db import models
+from phoenix.server.api.context import Context
+from phoenix.server.api.types.pagination import (
+    ConnectionArgs,
+    CursorString,
+    connection_from_list,
+)
+
+from .DatasetVersion import DatasetVersion
 
 
 @strawberry.type
@@ -14,3 +27,35 @@ class Dataset(Node):
     metadata: JSON
     created_at: datetime
     updated_at: datetime
+
+    @strawberry.field
+    async def versions(
+        self,
+        info: Info[Context, None],
+        first: Optional[int] = 50,
+        last: Optional[int] = UNSET,
+        after: Optional[CursorString] = UNSET,
+        before: Optional[CursorString] = UNSET,
+    ) -> Connection[DatasetVersion]:
+        args = ConnectionArgs(
+            first=first,
+            after=after if isinstance(after, CursorString) else None,
+            last=last,
+            before=before if isinstance(before, CursorString) else None,
+        )
+        async with info.context.db() as session:
+            versions = await session.scalars(
+                select(models.DatasetVersion)
+                .filter_by(dataset_id=self.id_attr)
+                .order_by(models.DatasetVersion.created_at.desc())
+            )
+        data = [
+            DatasetVersion(
+                id_attr=version.id,
+                description=version.description,
+                metadata=version.metadata,
+                created_at=version.created_at,
+            )
+            for version in versions
+        ]
+        return connection_from_list(data=data, args=args)
