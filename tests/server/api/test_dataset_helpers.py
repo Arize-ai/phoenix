f57a3fdf7b111054580286d9536421786a528114
Alexander Song <axiomofjoy@gmail.com>
2024-05-16 18:25:02 -0700

    add parameterized test cases for get_dataset_example_input

--- tests/server/api/test_dataset_helpers.py
+++ tests/server/api/test_dataset_helpers.py
@@ -1,29 +1,10 @@
 from dataclasses import dataclass
 from typing import Any, Optional
 
+import pytest
 from phoenix.server.api.datasets_helpers import (
     get_dataset_example_input,
 )
-
-
-def test_get_dataset_example_input():
-    span = MockSpan(
-        span_kind="LLM",
-        input_value="plain-text-input",
-        input_mime_type="text/plain",
-        output_value="plain-text-output",
-        output_mime_type="text/plain",
-        prompt_template_variables={"variable_name": "variable-value"},
-        llm_input_messages=[{"content": "user-message", "role": "user"}],
-        llm_output_messages=[{"content": "assistant-message", "role": "assistant"}],
-        retrieval_documents=None,
-    )
-    expected_input_value = {
-        "input_messages": [{"content": "user-message", "role": "user"}],
-        "prompt_template_variables": {"variable_name": "variable-value"},
-    }
-    input_value = get_dataset_example_input(span)
-    assert expected_input_value == input_value
 
 
 @dataclass(frozen=True)
@@ -37,3 +18,141 @@ class MockSpan:
     llm_input_messages: Any
     llm_output_messages: Any
     retrieval_documents: Any
+
+
+@pytest.mark.parametrize(
+    "span, expected_input_value",
+    [
+        pytest.param(
+            MockSpan(
+                span_kind="LLM",
+                input_value="plain-text-input",
+                input_mime_type="text/plain",
+                output_value="plain-text-output",
+                output_mime_type="text/plain",
+                prompt_template_variables={"variable_name": "variable-value"},
+                llm_input_messages=[{"content": "user-message", "role": "user"}],
+                llm_output_messages=[{"content": "assistant-message", "role": "assistant"}],
+                retrieval_documents=None,
+            ),
+            {
+                "input_messages": [{"content": "user-message", "role": "user"}],
+                "prompt_template_variables": {"variable_name": "variable-value"},
+            },
+            id="llm-span-with-input-messages-and-prompt-template-variables",
+        ),
+        pytest.param(
+            MockSpan(
+                span_kind="LLM",
+                input_value="plain-text-input",
+                input_mime_type="text/plain",
+                output_value="plain-text-output",
+                output_mime_type="text/plain",
+                prompt_template_variables=None,
+                llm_input_messages=[{"content": "user-message", "role": "user"}],
+                llm_output_messages=[{"content": "assistant-message", "role": "assistant"}],
+                retrieval_documents=None,
+            ),
+            {
+                "input_messages": [{"content": "user-message", "role": "user"}],
+            },
+            id="llm-span-with-input-messages-and-no-prompt-template-variables",
+        ),
+        pytest.param(
+            MockSpan(
+                span_kind="LLM",
+                input_value="plain-text-input",
+                input_mime_type="text/plain",
+                output_value="plain-text-output",
+                output_mime_type="text/plain",
+                prompt_template_variables=None,
+                llm_input_messages=None,
+                llm_output_messages=[{"content": "assistant-message", "role": "assistant"}],
+                retrieval_documents=None,
+            ),
+            {"input": "plain-text-input"},
+            id="llm-span-with-no-input-messages-and-plain-text-input",
+        ),
+        pytest.param(
+            MockSpan(
+                span_kind="LLM",
+                input_value="plain-text-input",
+                input_mime_type="text/plain",
+                output_value="plain-text-output",
+                output_mime_type="text/plain",
+                prompt_template_variables={"variable_name": "variable-value"},
+                llm_input_messages=None,
+                llm_output_messages=[{"content": "assistant-message", "role": "assistant"}],
+                retrieval_documents=None,
+            ),
+            {
+                "input": "plain-text-input",
+                "prompt_template_variables": {"variable_name": "variable-value"},
+            },
+            id="llm-span-with-no-input-messages-and-plain-text-input-with-prompt-template-variables",
+        ),
+        pytest.param(
+            MockSpan(
+                span_kind="LLM",
+                input_value={"chain_input": "chain-input"},
+                input_mime_type="application/json",
+                output_value="plain-text-output",
+                output_mime_type="text/plain",
+                prompt_template_variables=None,
+                llm_input_messages=None,
+                llm_output_messages=[{"content": "assistant-message", "role": "assistant"}],
+                retrieval_documents=None,
+            ),
+            {"chain_input": "chain-input"},
+            id="llm-span-with-no-input-messages-and-json-input",
+        ),
+        # pytest.param(
+        #     MockSpan(
+        #         span_kind="RETRIEVER",
+        #         input_value={"chain_input": "chain-input"},
+        #         input_mime_type="application/json",
+        #         output_value="plain-text-output",
+        #         output_mime_type="text/plain",
+        #         prompt_template_variables=None,
+        #         llm_input_messages=None,
+        #         llm_output_messages=None,
+        #         retrieval_documents=[{"id": "1", "score": 0.5, "content": "document-content"}],
+        #     ),
+        #     {"chain_input": "chain-input"},
+        #     id="llm-span-with-no-input-messages-and-json-input",
+        # ),
+        pytest.param(
+            MockSpan(
+                span_kind="CHAIN",
+                input_value="plain-text-input",
+                input_mime_type="text/plain",
+                output_value="plain-text-output",
+                output_mime_type="text/plain",
+                prompt_template_variables=None,
+                llm_input_messages=None,
+                llm_output_messages=[{"content": "assistant-message", "role": "assistant"}],
+                retrieval_documents=None,
+            ),
+            {"input": "plain-text-input"},
+            id="non-llm-span-with-plain-text-input",
+        ),
+        pytest.param(
+            MockSpan(
+                span_kind="CHAIN",
+                input_value={"chain_input": "chain-input"},
+                input_mime_type="application/json",
+                output_value="plain-text-output",
+                output_mime_type="text/plain",
+                prompt_template_variables=None,
+                llm_input_messages=None,
+                llm_output_messages=[{"content": "assistant-message", "role": "assistant"}],
+                retrieval_documents=None,
+            ),
+            {"chain_input": "chain-input"},
+            id="non-llm-span-with-json-input",
+        ),
+    ],
+)
+def test_get_dataset_example_input(span, expected_input_value):
+    input_value = get_dataset_example_input(span)
+    assert expected_input_value == input_value
