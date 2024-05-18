3d06857aec87f954950a34444113ffdba0545e87
Xander Song <axiomofjoy@gmail.com>
2024-05-18 13:07:53 -0700

    refactor(datasets): break contents of schema into query and mutation sub-modules (#3239)

---
+++ src/phoenix/server/api/mutations/__init__.py
@@ -0,0 +1,10 @@
+import strawberry
+
+from phoenix.server.api.mutations.dataset_mutations import DatasetMutationMixin
+from phoenix.server.api.mutations.export_events_mutations import ExportEventsMutationMixin
+from phoenix.server.api.mutations.project_mutations import ProjectMutationMixin
+
+
+@strawberry.type
+class Mutation(ProjectMutationMixin, DatasetMutationMixin, ExportEventsMutationMixin):
+    pass
