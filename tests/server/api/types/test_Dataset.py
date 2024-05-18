04786b15fed13cb36e269780d91eb717556d96d9
Alexander Song <axiomofjoy@gmail.com>
2024-05-18 14:15:45 -0700

    add and pass test case to ensure that deleted examples are fetched

--- tests/server/api/types/test_Dataset.py
+++ tests/server/api/types/test_Dataset.py
@@ -76,9 +76,21 @@ async def test_dataset_examples_resolver_returns_latest_example_revisions_before
 
 
 async def test_dataset_examples_resolver_excludes_deleted_examples(
-    test_client,
+    test_client, dataset_with_deletion
 ) -> None:
-    assert False
+    response = await test_client.post(
+        "/graphql",
+        json={
+            "query": DATASET_EXAMPLES_QUERY,
+            "variables": {
+                "datasetId": str(GlobalID("Dataset", str(1))),
+            },
+        },
+    )
+    assert response.status_code == 200
+    response_json = response.json()
+    assert (errors := response_json.get("errors")) is None, errors
+    assert response_json["data"] == {"node": {"examples": {"edges": []}}}
 
 
 @pytest.fixture
@@ -144,6 +156,74 @@ async def dataset_with_patch_revision(session):
         output={"output": "second-output"},
         metadata_={},
         revision_kind="PATCH",
+    )
+    session.add(dataset_example_revision_2)
+    await session.flush()
+
+
+@pytest.fixture
+async def dataset_with_deletion(session):
+    """
+    A dataset with a single example and two versions. In the first version, the
+    dataset example is created. In the second version, the dataset example is
+    deleted.
+    """
+
+    dataset = models.Dataset(
+        id=1,
+        name="dataset-name",
+        description="this dataset changes over time",
+        metadata_={},
+    )
+    session.add(dataset)
+    await session.flush()
+
+    dataset_example = models.DatasetExample(
+        id=1,
+        dataset_id=1,
+        created_at=datetime(year=2020, month=1, day=1, hour=0, minute=0, tzinfo=pytz.utc),
+    )
+    session.add(dataset_example)
+    await session.flush()
+
+    dataset_version_1 = models.DatasetVersion(
+        id=1,
+        dataset_id=1,
+        description=None,
+        metadata_={},
+    )
+    session.add(dataset_version_1)
+    await session.flush()
+
+    dataset_example_revision_1 = models.DatasetExampleRevision(
+        id=1,
+        dataset_example_id=1,
+        dataset_version_id=1,
+        input={"input": "first-input"},
+        output={"output": "first-output"},
+        metadata_={},
+        revision_kind="CREATE",
+    )
+    session.add(dataset_example_revision_1)
+    await session.flush()
+
+    dataset_version_2 = models.DatasetVersion(
+        id=2,
+        dataset_id=1,
+        description=None,
+        metadata_={},
+    )
+    session.add(dataset_version_2)
+    await session.flush()
+
+    dataset_example_revision_2 = models.DatasetExampleRevision(
+        id=2,
+        dataset_example_id=1,
+        dataset_version_id=2,
+        input={"input": "first-input"},
+        output={"output": "first-output"},
+        metadata_={},
+        revision_kind="DELETE",
     )
     session.add(dataset_example_revision_2)
     await session.flush()
