3b71570bd40e81ed44830de618bd5048b3e4386b
Mikyo King <mikyo@arize.com>
2024-05-20 17:32:27 -0600

    fix lint

--- app/src/pages/project/SpanSelectionToolbar.tsx
+++ app/src/pages/project/SpanSelectionToolbar.tsx
@@ -1,6 +1,5 @@
 import React, { ReactNode, Suspense, useCallback, useState } from "react";
 import { graphql, useMutation } from "react-relay";
-import { useNavigate } from "react-router";
 import { css } from "@emotion/react";
 
 import {
