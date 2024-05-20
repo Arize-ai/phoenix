0c7bfe87285d9ec34484cc88f08a9507afe4508f
Xander Song <axiomofjoy@gmail.com>
2024-05-20 11:43:33 -0700

    feat: gql resolver for dataset examples (#3238)

---
+++ src/phoenix/server/api/types/DatasetExample.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+import strawberry
+from strawberry.relay.types import Node, NodeID
+from strawberry.scalars import JSON
+
+
+@strawberry.type
+class DatasetExample(Node):
+    id_attr: NodeID[int]
+    input: JSON
+    output: JSON
+    metadata: JSON
+    created_at: datetime
