f31f29be14658141bf112591105c057b280bbd9b
Alexander Song <axiomofjoy@gmail.com>
2024-05-22 23:36:05 -0700

    add markup for table

--- app/src/pages/dataset/DatasetPage.tsx
+++ app/src/pages/dataset/DatasetPage.tsx
@@ -1,10 +1,18 @@
 import React, { Suspense, useMemo } from "react";
 import { graphql, useRefetchableFragment } from "react-relay";
 import { useLoaderData } from "react-router";
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
 
 import { Flex, Text, View } from "@arizeai/components";
 
 import { Loading } from "@phoenix/components";
+import { TableEmpty } from "@phoenix/components/table/TableEmpty";
+import { TextCell } from "@phoenix/components/table/TextCell";
 
 import type {
   datasetLoaderQuery,
@@ -47,12 +55,49 @@ function DatasetPageContent({
     `,
     dataset
   );
-  const examples = useMemo(
-    () => data.examples.edges.map((edge) => edge.node),
+  const tableData = useMemo(
+    () =>
+      data.examples.edges.map((edge) => {
+        const { id, input, output, metadata } = edge.node;
+        return {
+          id,
+          input: JSON.stringify(input),
+          output: JSON.stringify(output),
+          metadata: JSON.stringify(metadata),
+        };
+      }),
     [data]
   );
-  // eslint-disable-next-line
-  console.log({ examples });
+  type TableRow = (typeof tableData)[number];
+  const columns: ColumnDef<TableRow>[] = [
+    {
+      header: "id",
+      accessorKey: "id",
+      cell: TextCell,
+    },
+    {
+      header: "input",
+      accessorKey: "input",
+      cell: TextCell,
+    },
+    {
+      header: "output",
+      accessorKey: "output",
+      cell: TextCell,
+    },
+    {
+      header: "metadata",
+      accessorKey: "metadata",
+      cell: TextCell,
+    },
+  ];
+  const table = useReactTable<TableRow>({
+    columns,
+    data: tableData,
+    getCoreRowModel: getCoreRowModel(),
+  });
+  const rows = table.getRowModel().rows;
+  const isEmpty = rows.length === 0;
   return (
     <div>
       <View
@@ -69,6 +114,44 @@ function DatasetPageContent({
           </Flex>
         </Flex>
       </View>
+      <table>
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th key={header.id}>
+                  <div>
+                    {flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
+                  </div>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        {isEmpty ? (
+          <TableEmpty />
+        ) : (
+          <tbody>
+            {rows.map((row) => (
+              <tr key={row.id}>
+                {row.getVisibleCells().map((cell) => {
+                  return (
+                    <td key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </td>
+                  );
+                })}
+              </tr>
+            ))}
+          </tbody>
+        )}
+      </table>
     </div>
   );
 }
