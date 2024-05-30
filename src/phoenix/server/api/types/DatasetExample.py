ce1481cdd22b9736dd52e05a9e4b14a3789c37c5
Alexander Song <axiomofjoy@gmail.com>
2024-05-29 17:54:51 -0700

    remove unnecessary private field from DatasetExample type

--- src/phoenix/server/api/types/DatasetExample.py
+++ src/phoenix/server/api/types/DatasetExample.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-from typing import Optional
 
 import strawberry
 from strawberry.relay.types import Node, NodeID
@@ -11,5 +10,4 @@ from phoenix.server.api.types.DatasetExampleRevision import DatasetExampleRevisi
 class DatasetExample(Node):
     id_attr: NodeID[int]
     created_at: datetime
-    dataset_version_rowid: strawberry.Private[Optional[int]] = None
     revision: DatasetExampleRevision
