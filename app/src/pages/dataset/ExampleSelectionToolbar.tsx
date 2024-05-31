a61dbc4c7e50ea67c7554c3162f4edcce59a92ee
Mikyo King <mikyo@arize.com>
2024-05-31 14:57:06 -0600

    feat(datasets): Delete examples (#3352)
    * feat(datasets): Delete examples
    
    * cleanup

---
+++ app/src/pages/dataset/ExampleSelectionToolbar.tsx
@@ -0,0 +1,128 @@
+import React, { ReactNode, useCallback, useState } from "react";
+import { graphql, useMutation } from "react-relay";
+import { css } from "@emotion/react";
+
+import {
+  Button,
+  DialogContainer,
+  Flex,
+  Icon,
+  Icons,
+  Text,
+  View,
+} from "@arizeai/components";
+
+import { useNotifyError, useNotifySuccess } from "@phoenix/contexts";
+
+interface SelectedExample {
+  id: string;
+}
+
+type ExampleSelectionToolbarProps = {
+  selectedExamples: SelectedExample[];
+  onClearSelection: () => void;
+  onExamplesDeleted: () => void;
+};
+
+export function ExampleSelectionToolbar(props: ExampleSelectionToolbarProps) {
+  const { selectedExamples, onExamplesDeleted, onClearSelection } = props;
+  const [dialog, setDialog] = useState<ReactNode>(null);
+  const notifySuccess = useNotifySuccess();
+  const notifyError = useNotifyError();
+  const [deleteExamples, isDeletingExamples] = useMutation(graphql`
+    mutation ExampleSelectionToolbarDeleteExamplesMutation(
+      $input: DeleteDatasetExamplesInput!
+    ) {
+      deleteDatasetExamples(input: $input) {
+        dataset {
+          id
+        }
+      }
+    }
+  `);
+  const isPlural = selectedExamples.length !== 1;
+  const onDeleteExamples = useCallback(() => {
+    deleteExamples({
+      variables: {
+        input: {
+          exampleIds: selectedExamples.map((example) => example.id),
+        },
+      },
+      onCompleted: () => {
+        notifySuccess({
+          title: "Examples Deleted",
+          message: `${selectedExamples.length} example${isPlural ? "s" : ""} have been deleted.`,
+        });
+        // Clear the selection
+        onExamplesDeleted();
+        onClearSelection();
+      },
+      onError: (error) => {
+        notifyError({
+          title: "An error occurred",
+          message: `Failed to delete examples: ${error.message}`,
+        });
+      },
+    });
+  }, [
+    deleteExamples,
+    selectedExamples,
+    notifySuccess,
+    isPlural,
+    onExamplesDeleted,
+    onClearSelection,
+    notifyError,
+  ]);
+  return (
+    <div
+      css={css`
+        position: absolute;
+        bottom: var(--ac-global-dimension-size-400);
+        left: 50%;
+        transform: translateX(-50%);
+      `}
+    >
+      <View
+        backgroundColor="light"
+        padding="size-200"
+        borderColor="light"
+        borderWidth="thin"
+        borderRadius="medium"
+        minWidth="size-6000"
+      >
+        <Flex
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Text>{`${selectedExamples.length} example${isPlural ? "s" : ""} selected`}</Text>
+          <Flex direction="row" gap="size-100">
+            <Button variant="default" size="compact" onClick={onClearSelection}>
+              Cancel
+            </Button>
+
+            <Button
+              variant="danger"
+              size="compact"
+              icon={<Icon svg={<Icons.TrashOutline />} />}
+              loading={isDeletingExamples}
+              disabled={isDeletingExamples}
+              onClick={onDeleteExamples}
+            >
+              {isDeletingExamples
+                ? "Deleting..."
+                : "Delete Example" + (isPlural ? "s" : "")}
+            </Button>
+          </Flex>
+        </Flex>
+      </View>
+      <DialogContainer
+        onDismiss={() => {
+          setDialog(null);
+        }}
+      >
+        {dialog}
+      </DialogContainer>
+    </div>
+  );
+}
---
+++ app/src/pages/dataset/__generated__/ExampleSelectionToolbarDeleteExamplesMutation.graphql.ts
@@ -0,0 +1,107 @@
+/**
+ * @generated SignedSource<<c13e6b9355125ee3ffbd7b8ac0278c41>>
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+// @ts-nocheck
+
+import { ConcreteRequest, Mutation } from 'relay-runtime';
+export type DeleteDatasetExamplesInput = {
+  datasetVersionDescription?: string | null;
+  datasetVersionMetadata?: any | null;
+  exampleIds: ReadonlyArray<string>;
+};
+export type ExampleSelectionToolbarDeleteExamplesMutation$variables = {
+  input: DeleteDatasetExamplesInput;
+};
+export type ExampleSelectionToolbarDeleteExamplesMutation$data = {
+  readonly deleteDatasetExamples: {
+    readonly dataset: {
+      readonly id: string;
+    };
+  };
+};
+export type ExampleSelectionToolbarDeleteExamplesMutation = {
+  response: ExampleSelectionToolbarDeleteExamplesMutation$data;
+  variables: ExampleSelectionToolbarDeleteExamplesMutation$variables;
+};
+
+const node: ConcreteRequest = (function(){
+var v0 = [
+  {
+    "defaultValue": null,
+    "kind": "LocalArgument",
+    "name": "input"
+  }
+],
+v1 = [
+  {
+    "alias": null,
+    "args": [
+      {
+        "kind": "Variable",
+        "name": "input",
+        "variableName": "input"
+      }
+    ],
+    "concreteType": "DatasetMutationPayload",
+    "kind": "LinkedField",
+    "name": "deleteDatasetExamples",
+    "plural": false,
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "concreteType": "Dataset",
+        "kind": "LinkedField",
+        "name": "dataset",
+        "plural": false,
+        "selections": [
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "id",
+            "storageKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ],
+    "storageKey": null
+  }
+];
+return {
+  "fragment": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "ExampleSelectionToolbarDeleteExamplesMutation",
+    "selections": (v1/*: any*/),
+    "type": "Mutation",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Operation",
+    "name": "ExampleSelectionToolbarDeleteExamplesMutation",
+    "selections": (v1/*: any*/)
+  },
+  "params": {
+    "cacheID": "b63159f3c325b9af3dc469692f9329e5",
+    "id": null,
+    "metadata": {},
+    "name": "ExampleSelectionToolbarDeleteExamplesMutation",
+    "operationKind": "mutation",
+    "text": "mutation ExampleSelectionToolbarDeleteExamplesMutation(\n  $input: DeleteDatasetExamplesInput!\n) {\n  deleteDatasetExamples(input: $input) {\n    dataset {\n      id\n    }\n  }\n}\n"
+  }
+};
+})();
+
+(node as any).hash = "455bf9e5dc4b9cc655095dc829ef7e12";
+
+export default node;
