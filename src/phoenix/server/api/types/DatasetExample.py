9618742796d74ce206d909a1b952068c0eaf35f4
Alexander Song <axiomofjoy@gmail.com>
2024-05-31 20:03:36 -0700

    add revision resolver and private field to cache db revision on DatasetExample

--- src/phoenix/server/api/types/DatasetExample.py
+++ src/phoenix/server/api/types/DatasetExample.py
@@ -1,9 +1,13 @@
 from datetime import datetime
+from typing import Optional
 
 import strawberry
 from strawberry.relay.types import Node, NodeID
+from strawberry.types import Info
 
-from phoenix.server.api.types.DatasetExampleRevision import DatasetExampleRevision
+from phoenix.db import models
+from phoenix.server.api.context import Context
+from phoenix.server.api.types.DatasetExampleRevision import DatasetExampleRevision, RevisionKind
 
 
 @strawberry.type
@@ -11,3 +15,16 @@ class DatasetExample(Node):
     id_attr: NodeID[int]
     created_at: datetime
     revision: DatasetExampleRevision
+    db_revision: strawberry.Private[Optional[models.DatasetExampleRevision]] = None
+
+    async def revision2(
+        self,
+        info: Info[Context, None],
+    ) -> DatasetExampleRevision:
+        return DatasetExampleRevision(
+            input={},
+            output={},
+            metadata={},
+            revision_kind=RevisionKind.CREATE,
+            created_at=datetime.now(),
+        )
