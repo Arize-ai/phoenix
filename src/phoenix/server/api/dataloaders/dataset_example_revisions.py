85c7b6e9becf278f2afef1673d6cbd9991306a72
Alexander Song <axiomofjoy@gmail.com>
2024-06-02 17:49:02 -0700

    fix bug

--- src/phoenix/server/api/dataloaders/dataset_example_revisions.py
+++ src/phoenix/server/api/dataloaders/dataset_example_revisions.py
@@ -40,26 +40,31 @@ class DatasetExampleRevisionsDataLoader(DataLoader[Key, Result]):
             )
             .group_by(models.DatasetExampleRevision.dataset_example_id)
         ).scalar_subquery()
-        async with self._db() as session:
-            revisions = await session.stream_scalars(
-                select(models.DatasetExampleRevision).where(
-                    and_(
-                        or_(
-                            tuple_(
-                                models.DatasetExampleRevision.dataset_example_id,
-                                models.DatasetExampleRevision.dataset_version_id,
-                            ).in_(example_and_version_ids),
-                            models.DatasetExampleRevision.id.in_(latest_revision_ids),
-                        ),
-                        models.DatasetExampleRevision.revision_kind != "DELETE",
-                    )
-                )
+        is_latest_revision_for_example = models.DatasetExampleRevision.id.in_(latest_revision_ids)
+        query = select(
+            models.DatasetExampleRevision,
+            is_latest_revision_for_example,
+        ).where(
+            and_(
+                or_(
+                    tuple_(
+                        models.DatasetExampleRevision.dataset_example_id,
+                        models.DatasetExampleRevision.dataset_version_id,
+                    ).in_(example_and_version_ids),
+                    is_latest_revision_for_example,
+                ),
+                models.DatasetExampleRevision.revision_kind != "DELETE",
             )
-            results: Dict[Key, Result] = {
-                (
-                    revision.dataset_example_id,
-                    revision.dataset_version_id,
-                ): DatasetExampleRevision.from_orm_revision(revision)
-                async for revision in revisions
-            }
+        )
+        results: Dict[Key, Result] = {}
+        async with self._db() as session:
+            for (
+                revision,
+                is_latest_revision,
+            ) in await session.execute(query):
+                orm_revision = DatasetExampleRevision.from_orm_revision(revision)
+                results[(revision.dataset_example_id, revision.dataset_version_id)] = orm_revision
+                if is_latest_revision:
+                    results[(revision.dataset_example_id, None)] = orm_revision
+
         return [results[key] for key in keys]
