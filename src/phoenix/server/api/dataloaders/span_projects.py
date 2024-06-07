e87d431f580f0eb015aabc2cda23822771d42860
Alexander Song <axiomofjoy@gmail.com>
2024-06-06 23:00:55 -0700

    implement dataloader, test failing on to_gql_project

--- src/phoenix/server/api/dataloaders/span_projects.py
+++ src/phoenix/server/api/dataloaders/span_projects.py
@@ -1,9 +1,6 @@
-from typing import (
-    AsyncContextManager,
-    Callable,
-    List,
-)
+from typing import AsyncContextManager, Callable, List, Union
 
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from strawberry.dataloader import DataLoader
 from typing_extensions import TypeAlias
@@ -20,5 +17,17 @@ class SpanProjectsDataLoader(DataLoader[Key, Result]):
         super().__init__(load_fn=self._load_fn)
         self._db = db
 
-    async def _load_fn(self, keys: List[Key]) -> List[Result]:
-        raise NotImplementedError("need to implement dataloader")
+    async def _load_fn(self, keys: List[Key]) -> List[Union[Result, ValueError]]:
+        span_ids = list(set(keys))
+        async with self._db() as session:
+            projects = {
+                span_id: project
+                async for span_id, project in await session.stream(
+                    select(models.Span.id, models.Project)
+                    .select_from(models.Span)
+                    .join(models.Trace, models.Span.trace_rowid == models.Trace.id)
+                    .join(models.Project, models.Trace.project_rowid == models.Project.id)
+                    .where(models.Span.id.in_(span_ids))
+                )
+            }
+        return [projects.get(span_id) or ValueError("Invalid span ID") for span_id in span_ids]
