d1118452e9995df32dcea5eeb5cc517771656482
Dustin Ngo <dustin@arize.com>
2024-06-07 22:01:35 -0400

    feat: Implement experiments REST API (#3411)
    * Create datasets feature branch
    
    * Spike out experiment routes
    
    * Add status column to datasets migration
    
    * Spike out experiment runner
    
    * Ruff 🐶
    
    * Add object payloads to experiment responses
    
    * Spike out basic eval flow
    
    * Migration and ORM fixes
    
    * Ensure we default to the latest dataset version
    
    * Always pull examples associated with correct version
    
    * Flush, don't commit
    
    * Clean up experiment creation route and remove experiment "sealing"
    
    * Implement GET experiment route
    
    * Flesh out experiment runs API
    
    * Remove experiments module spike
    
    * Flesh out evaluation route
    
    * Update migration to use trace id instead of trace rowid
    
    * Clean up API and add basic workflow test
    
    * Fix list experiment runs bugs
    
    * Remove unused exceptions
    
    * Don't use `get` for path params
    
    * Remove more "gets" from global id constructions for type clarity
    
    * Remove accidental nullable trace-rowid addition
    
    * Address PR feedback
    
    * Only use get if we have a good fallback

---
+++ src/phoenix/server/api/routers/v1/experiment_evaluations.py
@@ -0,0 +1,60 @@
+from datetime import datetime
+
+from starlette.requests import Request
+from starlette.responses import JSONResponse, Response
+from starlette.status import HTTP_404_NOT_FOUND
+from strawberry.relay import GlobalID
+
+from phoenix.db import models
+from phoenix.server.api.types.node import from_global_id_with_expected_type
+
+
+async def create_experiment_evaluation(request: Request) -> Response:
+    experiment_run_globalid = GlobalID.from_id(request.path_params["run_id"])
+    try:
+        experiment_run_id = from_global_id_with_expected_type(
+            experiment_run_globalid, "ExperimentRun"
+        )
+    except ValueError:
+        return Response(
+            content=f"ExperimentRun with ID {experiment_run_globalid} does not exist",
+            status_code=HTTP_404_NOT_FOUND,
+        )
+
+    payload = await request.json()
+    name = payload["name"]
+    label = payload["label"]
+    score = payload["score"]
+    explanation = payload["explanation"]
+    error = payload["error"]
+    metadata = payload.get("metadata", {})
+    start_time = payload["start_time"]
+    end_time = payload["end_time"]
+    async with request.app.state.db() as session:
+        experiment_evaluation = models.ExperimentEvaluation(
+            experiment_run_id=experiment_run_id,
+            name=name,
+            label=label,
+            score=score,
+            explanation=explanation,
+            error=error,
+            metadata_=metadata,
+            start_time=datetime.fromisoformat(start_time),
+            end_time=datetime.fromisoformat(end_time),
+        )
+        session.add(experiment_evaluation)
+        await session.flush()
+        evaluation_globalid = GlobalID("ExperimentEvaluation", str(experiment_evaluation.id))
+        eval_payload = {
+            "id": str(evaluation_globalid),
+            "experiment_run_id": str(experiment_run_globalid),
+            "name": experiment_evaluation.name,
+            "label": experiment_evaluation.label,
+            "score": experiment_evaluation.score,
+            "explanation": experiment_evaluation.explanation,
+            "error": experiment_evaluation.error,
+            "metadata": experiment_evaluation.metadata_,
+            "start_time": experiment_evaluation.start_time.isoformat(),
+            "end_time": experiment_evaluation.end_time.isoformat(),
+        }
+        return JSONResponse(content=eval_payload, status_code=200)
---
+++ src/phoenix/server/api/routers/v1/experiment_runs.py
@@ -0,0 +1,104 @@
+from datetime import datetime
+
+from sqlalchemy import select
+from starlette.requests import Request
+from starlette.responses import JSONResponse, Response
+from starlette.status import HTTP_404_NOT_FOUND
+from strawberry.relay import GlobalID
+
+from phoenix.db import models
+from phoenix.server.api.types.node import from_global_id_with_expected_type
+
+
+async def create_experiment_run(request: Request) -> Response:
+    experiment_globalid = GlobalID.from_id(request.path_params["experiment_id"])
+    try:
+        experiment_id = from_global_id_with_expected_type(experiment_globalid, "Experiment")
+    except ValueError:
+        return Response(
+            content=f"Experiment with ID {experiment_globalid} does not exist",
+            status_code=HTTP_404_NOT_FOUND,
+        )
+
+    payload = await request.json()
+
+    dataset_example_globalid = GlobalID.from_id(payload["dataset_example_id"])
+    try:
+        dataset_example_id = from_global_id_with_expected_type(
+            dataset_example_globalid, "DatasetExample"
+        )
+    except ValueError:
+        return Response(
+            content=f"DatasetExample with ID {dataset_example_globalid} does not exist",
+            status_code=HTTP_404_NOT_FOUND,
+        )
+
+    trace_id = payload.get("trace_id", None)
+    output = payload["output"]
+    start_time = payload["start_time"]
+    end_time = payload["end_time"]
+    error = payload["error"]
+
+    async with request.app.state.db() as session:
+        experiment_run = models.ExperimentRun(
+            experiment_id=experiment_id,
+            dataset_example_id=dataset_example_id,
+            trace_id=trace_id,
+            output=output,
+            start_time=datetime.fromisoformat(start_time),
+            end_time=datetime.fromisoformat(end_time),
+            error=error,
+        )
+        session.add(experiment_run)
+        await session.flush()
+
+        run_globalid = GlobalID("ExperimentRun", str(experiment_run.id))
+        run_payload = {
+            "id": str(run_globalid),
+            "experiment_id": str(experiment_globalid),
+            "dataset_example_id": str(dataset_example_globalid),
+            "output": experiment_run.output,
+            "trace_id": experiment_run.trace_id,
+            "start_time": experiment_run.start_time.isoformat(),
+            "end_time": experiment_run.end_time.isoformat(),
+            "error": experiment_run.error,
+        }
+        return JSONResponse(content=run_payload, status_code=200)
+
+
+async def list_experiment_runs(request: Request) -> Response:
+    experiment_globalid = GlobalID.from_id(request.path_params["experiment_id"])
+    try:
+        experiment_id = from_global_id_with_expected_type(experiment_globalid, "Experiment")
+    except ValueError:
+        return Response(
+            content=f"Experiment with ID {experiment_globalid} does not exist",
+            status_code=HTTP_404_NOT_FOUND,
+        )
+
+    async with request.app.state.db() as session:
+        experiment_runs = await session.execute(
+            select(models.ExperimentRun)
+            .where(models.ExperimentRun.experiment_id == experiment_id)
+            # order by dataset_exaple_id to be consistent with `list_dataset_examples`
+            .order_by(models.ExperimentRun.dataset_example_id.asc())
+        )
+        experiment_runs = experiment_runs.scalars().all()
+        runs = []
+        for run in experiment_runs:
+            run_gid = GlobalID("ExperimentRun", str(run.id))
+            experiment_gid = GlobalID("Experiment", str(run.experiment_id))
+            example_gid = GlobalID("DatasetExample", str(run.dataset_example_id))
+            runs.append(
+                {
+                    "id": str(run_gid),
+                    "experiment_id": str(experiment_gid),
+                    "dataset_example_id": str(example_gid),
+                    "output": run.output,
+                    "trace_id": run.trace_id,
+                    "start_time": run.start_time.isoformat(),
+                    "end_time": run.end_time.isoformat(),
+                    "error": run.error,
+                }
+            )
+        return JSONResponse(content=runs, status_code=200)
