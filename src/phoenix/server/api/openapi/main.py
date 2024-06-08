a4eed2888b1a8e6086a774aaa0d49b9a9cf6743f
Alexander Song <axiomofjoy@gmail.com>
2024-06-07 19:47:42 -0700

    export to yaml rather than json

--- src/phoenix/server/api/openapi/main.py
+++ src/phoenix/server/api/openapi/main.py
@@ -1,6 +1,6 @@
-import json
-
 from .schema import get_openapi_schema
 
 if __name__ == "__main__":
-    print(json.dumps(get_openapi_schema(), indent=2))
+    import yaml
+
+    print(yaml.dump(get_openapi_schema(), indent=2))
