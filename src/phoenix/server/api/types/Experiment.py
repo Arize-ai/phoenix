f526025a1164557923dac4fb7e51a98e41943bc1
Xander Song <axiomofjoy@gmail.com>
2024-06-10 16:09:36 -0700

    feat: add experiments resolver to DatasetExample gql type (#3446)
    Adds an experiments resolver to the DatasetExample type and adds corresponding tests.

--- src/phoenix/server/api/types/Experiment.py
+++ src/phoenix/server/api/types/Experiment.py
@@ -5,6 +5,8 @@ import strawberry
 from strawberry.relay import Node, NodeID
 from strawberry.scalars import JSON
 
+from phoenix.db import models
+
 
 @strawberry.type
 class Experiment(Node):
@@ -13,3 +15,16 @@ class Experiment(Node):
     metadata: JSON
     created_at: datetime
     updated_at: datetime
+
+
+def to_gql_experiment(experiment: models.Experiment) -> Experiment:
+    """
+    Converts an ORM experiment to a GraphQL Experiment.
+    """
+    return Experiment(
+        id_attr=experiment.id,
+        description=experiment.description,
+        metadata=experiment.metadata_,
+        created_at=experiment.created_at,
+        updated_at=experiment.updated_at,
+    )
