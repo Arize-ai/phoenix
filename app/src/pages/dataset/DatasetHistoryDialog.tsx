22ff4910f3acb9c7d4a4dc20950380a927ed1a02
Mikyo King <mikyo@arize.com>
2024-06-10 14:13:36 -0600

    feat(datasets): version history modal (#3444)

---
+++ app/src/pages/dataset/DatasetHistoryDialog.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { graphql, useLazyLoadQuery } from "react-relay";
+import { css } from "@emotion/react";
+
+import { Dialog } from "@arizeai/components";
+
+import { DatasetHistoryDialogQuery } from "./__generated__/DatasetHistoryDialogQuery.graphql";
+import { DatasetHistoryTable } from "./DatasetHistoryTable";
+
+export function DatasetHistoryDialog(props: { datasetId: string }) {
+  const { datasetId } = props;
+  const data = useLazyLoadQuery<DatasetHistoryDialogQuery>(
+    graphql`
+      query DatasetHistoryDialogQuery($datasetId: GlobalID!) {
+        dataset: node(id: $datasetId) {
+          ... on Dataset {
+            id
+            ...DatasetHistoryTable_versions
+          }
+        }
+      }
+    `,
+    {
+      datasetId,
+    }
+  );
+  return (
+    <Dialog size="L" title="Dataset History">
+      <div
+        css={css`
+          height: 500px;
+        `}
+      >
+        <DatasetHistoryTable dataset={data.dataset} />
+      </div>
+    </Dialog>
+  );
+}
---
+++ app/src/pages/dataset/__generated__/DatasetHistoryDialogQuery.graphql.ts
@@ -0,0 +1,233 @@
+/**
+ * @generated SignedSource<<3a5aeea28d9a5b831c4b4c47a1840ffe>>
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+// @ts-nocheck
+
+import { ConcreteRequest, Query } from 'relay-runtime';
+import { FragmentRefs } from "relay-runtime";
+export type DatasetHistoryDialogQuery$variables = {
+  datasetId: string;
+};
+export type DatasetHistoryDialogQuery$data = {
+  readonly dataset: {
+    readonly id?: string;
+    readonly " $fragmentSpreads": FragmentRefs<"DatasetHistoryTable_versions">;
+  };
+};
+export type DatasetHistoryDialogQuery = {
+  response: DatasetHistoryDialogQuery$data;
+  variables: DatasetHistoryDialogQuery$variables;
+};
+
+const node: ConcreteRequest = (function(){
+var v0 = [
+  {
+    "defaultValue": null,
+    "kind": "LocalArgument",
+    "name": "datasetId"
+  }
+],
+v1 = [
+  {
+    "kind": "Variable",
+    "name": "id",
+    "variableName": "datasetId"
+  }
+],
+v2 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "id",
+  "storageKey": null
+},
+v3 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "__typename",
+  "storageKey": null
+},
+v4 = [
+  {
+    "kind": "Literal",
+    "name": "first",
+    "value": 100
+  }
+];
+return {
+  "fragment": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "DatasetHistoryDialogQuery",
+    "selections": [
+      {
+        "alias": "dataset",
+        "args": (v1/*: any*/),
+        "concreteType": null,
+        "kind": "LinkedField",
+        "name": "node",
+        "plural": false,
+        "selections": [
+          {
+            "kind": "InlineFragment",
+            "selections": [
+              (v2/*: any*/),
+              {
+                "args": null,
+                "kind": "FragmentSpread",
+                "name": "DatasetHistoryTable_versions"
+              }
+            ],
+            "type": "Dataset",
+            "abstractKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ],
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Operation",
+    "name": "DatasetHistoryDialogQuery",
+    "selections": [
+      {
+        "alias": "dataset",
+        "args": (v1/*: any*/),
+        "concreteType": null,
+        "kind": "LinkedField",
+        "name": "node",
+        "plural": false,
+        "selections": [
+          (v3/*: any*/),
+          {
+            "kind": "TypeDiscriminator",
+            "abstractKey": "__isNode"
+          },
+          (v2/*: any*/),
+          {
+            "kind": "InlineFragment",
+            "selections": [
+              {
+                "alias": null,
+                "args": (v4/*: any*/),
+                "concreteType": "DatasetVersionConnection",
+                "kind": "LinkedField",
+                "name": "versions",
+                "plural": false,
+                "selections": [
+                  {
+                    "alias": null,
+                    "args": null,
+                    "concreteType": "DatasetVersionEdge",
+                    "kind": "LinkedField",
+                    "name": "edges",
+                    "plural": true,
+                    "selections": [
+                      {
+                        "alias": null,
+                        "args": null,
+                        "concreteType": "DatasetVersion",
+                        "kind": "LinkedField",
+                        "name": "node",
+                        "plural": false,
+                        "selections": [
+                          (v2/*: any*/),
+                          {
+                            "alias": null,
+                            "args": null,
+                            "kind": "ScalarField",
+                            "name": "description",
+                            "storageKey": null
+                          },
+                          {
+                            "alias": null,
+                            "args": null,
+                            "kind": "ScalarField",
+                            "name": "createdAt",
+                            "storageKey": null
+                          },
+                          (v3/*: any*/)
+                        ],
+                        "storageKey": null
+                      },
+                      {
+                        "alias": null,
+                        "args": null,
+                        "kind": "ScalarField",
+                        "name": "cursor",
+                        "storageKey": null
+                      }
+                    ],
+                    "storageKey": null
+                  },
+                  {
+                    "alias": null,
+                    "args": null,
+                    "concreteType": "PageInfo",
+                    "kind": "LinkedField",
+                    "name": "pageInfo",
+                    "plural": false,
+                    "selections": [
+                      {
+                        "alias": null,
+                        "args": null,
+                        "kind": "ScalarField",
+                        "name": "endCursor",
+                        "storageKey": null
+                      },
+                      {
+                        "alias": null,
+                        "args": null,
+                        "kind": "ScalarField",
+                        "name": "hasNextPage",
+                        "storageKey": null
+                      }
+                    ],
+                    "storageKey": null
+                  }
+                ],
+                "storageKey": "versions(first:100)"
+              },
+              {
+                "alias": null,
+                "args": (v4/*: any*/),
+                "filters": null,
+                "handle": "connection",
+                "key": "DatasetHistoryTable_versions",
+                "kind": "LinkedHandle",
+                "name": "versions"
+              }
+            ],
+            "type": "Dataset",
+            "abstractKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ]
+  },
+  "params": {
+    "cacheID": "df94b4e72c0187fd0fd1c8416af19354",
+    "id": null,
+    "metadata": {},
+    "name": "DatasetHistoryDialogQuery",
+    "operationKind": "query",
+    "text": "query DatasetHistoryDialogQuery(\n  $datasetId: GlobalID!\n) {\n  dataset: node(id: $datasetId) {\n    __typename\n    ... on Dataset {\n      id\n      ...DatasetHistoryTable_versions\n    }\n    __isNode: __typename\n    id\n  }\n}\n\nfragment DatasetHistoryTable_versions on Dataset {\n  versions(first: 100) {\n    edges {\n      node {\n        id\n        description\n        createdAt\n        __typename\n      }\n      cursor\n    }\n    pageInfo {\n      endCursor\n      hasNextPage\n    }\n  }\n  id\n}\n"
+  }
+};
+})();
+
+(node as any).hash = "7215c18bda690418c5ecbcb3131a288e";
+
+export default node;
