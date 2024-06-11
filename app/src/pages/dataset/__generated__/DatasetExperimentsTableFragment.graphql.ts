44cc8f582fa89c6cd2f60ff6a2c503a923633bb2
Mikyo King <mikyo@arize.com>
2024-06-10 18:08:06 -0600

    feat(experiments): experiments table (#3454)

---
+++ app/src/pages/dataset/__generated__/DatasetExperimentsTableFragment.graphql.ts
@@ -0,0 +1,201 @@
+/**
+ * @generated SignedSource<<75d490bb9b51eda771a7a004d1bb7b9a>>
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+// @ts-nocheck
+
+import { ReaderFragment, RefetchableFragment } from 'relay-runtime';
+import { FragmentRefs } from "relay-runtime";
+export type DatasetExperimentsTableFragment$data = {
+  readonly experiments: {
+    readonly edges: ReadonlyArray<{
+      readonly experiment: {
+        readonly createdAt: string;
+        readonly description: string | null;
+        readonly id: string;
+        readonly metadata: any;
+      };
+    }>;
+  };
+  readonly id: string;
+  readonly " $fragmentType": "DatasetExperimentsTableFragment";
+};
+export type DatasetExperimentsTableFragment$key = {
+  readonly " $data"?: DatasetExperimentsTableFragment$data;
+  readonly " $fragmentSpreads": FragmentRefs<"DatasetExperimentsTableFragment">;
+};
+
+const node: ReaderFragment = (function(){
+var v0 = [
+  "experiments"
+],
+v1 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "id",
+  "storageKey": null
+};
+return {
+  "argumentDefinitions": [
+    {
+      "defaultValue": null,
+      "kind": "LocalArgument",
+      "name": "after"
+    },
+    {
+      "defaultValue": 100,
+      "kind": "LocalArgument",
+      "name": "first"
+    }
+  ],
+  "kind": "Fragment",
+  "metadata": {
+    "connection": [
+      {
+        "count": "first",
+        "cursor": "after",
+        "direction": "forward",
+        "path": (v0/*: any*/)
+      }
+    ],
+    "refetch": {
+      "connection": {
+        "forward": {
+          "count": "first",
+          "cursor": "after"
+        },
+        "backward": null,
+        "path": (v0/*: any*/)
+      },
+      "fragmentPathInResult": [
+        "node"
+      ],
+      "operation": require('./DatasetExperimentsTableQuery.graphql'),
+      "identifierInfo": {
+        "identifierField": "id",
+        "identifierQueryVariableName": "id"
+      }
+    }
+  },
+  "name": "DatasetExperimentsTableFragment",
+  "selections": [
+    {
+      "alias": "experiments",
+      "args": null,
+      "concreteType": "ExperimentConnection",
+      "kind": "LinkedField",
+      "name": "__DatasetExperimentsTable_experiments_connection",
+      "plural": false,
+      "selections": [
+        {
+          "alias": null,
+          "args": null,
+          "concreteType": "ExperimentEdge",
+          "kind": "LinkedField",
+          "name": "edges",
+          "plural": true,
+          "selections": [
+            {
+              "alias": "experiment",
+              "args": null,
+              "concreteType": "Experiment",
+              "kind": "LinkedField",
+              "name": "node",
+              "plural": false,
+              "selections": [
+                (v1/*: any*/),
+                {
+                  "alias": null,
+                  "args": null,
+                  "kind": "ScalarField",
+                  "name": "description",
+                  "storageKey": null
+                },
+                {
+                  "alias": null,
+                  "args": null,
+                  "kind": "ScalarField",
+                  "name": "createdAt",
+                  "storageKey": null
+                },
+                {
+                  "alias": null,
+                  "args": null,
+                  "kind": "ScalarField",
+                  "name": "metadata",
+                  "storageKey": null
+                }
+              ],
+              "storageKey": null
+            },
+            {
+              "alias": null,
+              "args": null,
+              "kind": "ScalarField",
+              "name": "cursor",
+              "storageKey": null
+            },
+            {
+              "alias": null,
+              "args": null,
+              "concreteType": "Experiment",
+              "kind": "LinkedField",
+              "name": "node",
+              "plural": false,
+              "selections": [
+                {
+                  "alias": null,
+                  "args": null,
+                  "kind": "ScalarField",
+                  "name": "__typename",
+                  "storageKey": null
+                }
+              ],
+              "storageKey": null
+            }
+          ],
+          "storageKey": null
+        },
+        {
+          "alias": null,
+          "args": null,
+          "concreteType": "PageInfo",
+          "kind": "LinkedField",
+          "name": "pageInfo",
+          "plural": false,
+          "selections": [
+            {
+              "alias": null,
+              "args": null,
+              "kind": "ScalarField",
+              "name": "endCursor",
+              "storageKey": null
+            },
+            {
+              "alias": null,
+              "args": null,
+              "kind": "ScalarField",
+              "name": "hasNextPage",
+              "storageKey": null
+            }
+          ],
+          "storageKey": null
+        }
+      ],
+      "storageKey": null
+    },
+    (v1/*: any*/)
+  ],
+  "type": "Dataset",
+  "abstractKey": null
+};
+})();
+
+(node as any).hash = "ce88ea0f64db4ff5075dd9d33883ce3a";
+
+export default node;
