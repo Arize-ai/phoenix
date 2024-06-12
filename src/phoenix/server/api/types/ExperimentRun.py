8b691b7de012f3aa051d407719bfd98b4539d9a7
Xander Song <axiomofjoy@gmail.com>
2024-06-11 18:52:14 -0700

    feat: add annotations resolver on DatasetRun type (#3473)

--- src/phoenix/server/api/types/ExperimentRun.py
+++ src/phoenix/server/api/types/ExperimentRun.py
@@ -2,10 +2,23 @@ from datetime import datetime
 from typing import Optional
 
 import strawberry
-from strawberry.relay import Node, NodeID
+from sqlalchemy import select
+from strawberry import UNSET
+from strawberry.relay import Connection, Node, NodeID
 from strawberry.scalars import JSON
+from strawberry.types import Info
 
 from phoenix.db import models
+from phoenix.server.api.context import Context
+from phoenix.server.api.types.ExperimentRunAnnotation import (
+    ExperimentRunAnnotation,
+    to_gql_experiment_run_annotation,
+)
+from phoenix.server.api.types.pagination import (
+    ConnectionArgs,
+    CursorString,
+    connection_from_list,
+)
 
 
 @strawberry.type
@@ -16,6 +29,34 @@ class ExperimentRun(Node):
     start_time: datetime
     end_time: datetime
     error: Optional[str]
+
+    @strawberry.field
+    async def annotations(
+        self,
+        info: Info[Context, None],
+        first: Optional[int] = 50,
+        last: Optional[int] = UNSET,
+        after: Optional[CursorString] = UNSET,
+        before: Optional[CursorString] = UNSET,
+    ) -> Connection[ExperimentRunAnnotation]:
+        args = ConnectionArgs(
+            first=first,
+            after=after if isinstance(after, CursorString) else None,
+            last=last,
+            before=before if isinstance(before, CursorString) else None,
+        )
+        run_id = self.id_attr
+        async with info.context.db() as session:
+            annotations = (
+                await session.scalars(
+                    select(models.ExperimentAnnotation)
+                    .where(models.ExperimentAnnotation.experiment_run_id == run_id)
+                    .order_by(models.ExperimentAnnotation.id.desc())
+                )
+            ).all()
+        return connection_from_list(
+            [to_gql_experiment_run_annotation(annotation) for annotation in annotations], args
+        )
 
 
 def to_gql_experiment_run(run: models.ExperimentRun) -> ExperimentRun:
