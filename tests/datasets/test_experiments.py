11d90d7105e7bb0842fdbc6cc9ad01aa8a8108a5
Dustin Ngo <dustin@arize.com>
2024-06-12 18:33:02 -0400

    feat(experiments): Implement `run_experiment` (#3471)
    * Sketch out run_experiment
    
    * Make evals required
    
    * Correctly use example protocol
    
    * Set up full end-to-end clientside testing and write experiment test
    
    * Clean up type annotations
    
    * Ruff 🐶
    
    * Update type annotations for evals
    
    * Clean up experiment runner
    
    * Fix syntax error
    
    * Revert evals changes
    
    * Restore deleted space
    
    * Use independent rate limiters
    
    * Do not explicitly import `run_experiments` at the top level
    
    * Rename type annotations for domain-specificity

---
+++ tests/datasets/test_experiments.py
@@ -0,0 +1,51 @@
+from unittest.mock import patch
+
+import nest_asyncio
+from phoenix.datasets.experiments import run_experiment
+from phoenix.db import models
+from phoenix.server.api.types.node import from_global_id_with_expected_type
+from sqlalchemy import select
+from strawberry.relay import GlobalID
+
+
+async def test_run_experiment(session, sync_test_client, simple_dataset):
+    nest_asyncio.apply()
+
+    nonexistent_experiment = (await session.execute(select(models.Experiment))).scalar()
+    assert not nonexistent_experiment, "There should be no experiments in the database"
+
+    class TestExample:
+        def __init__(self, id, input):
+            self.id = id
+            self.input = input
+
+    class TestDataset:
+        id = str(GlobalID("Dataset", "0"))
+        version_id = str(GlobalID("DatasetVersion", "0"))
+
+        def examples(self):
+            example_gid = str(GlobalID("DatasetExample", "0"))
+            return [TestExample(id=example_gid, input="fancy input 1")]
+
+    with patch("phoenix.datasets.experiments._phoenix_client", return_value=sync_test_client):
+
+        def experiment_task(input):
+            return {"output": "doesn't matter, this is the output"}
+
+        experiment = run_experiment(TestDataset(), experiment_task)
+        experiment_id = from_global_id_with_expected_type(
+            GlobalID.from_id(experiment.id), "Experiment"
+        )
+        assert experiment_id
+
+    experiment = (await session.execute(select(models.Experiment))).scalar()
+    assert experiment, "An experiment was run"
+    assert experiment.dataset_id == 0
+    assert experiment.dataset_version_id == 0
+
+    experiment_run = (
+        await session.execute(
+            select(models.ExperimentRun).where(models.ExperimentRun.dataset_example_id == 0)
+        )
+    ).scalar()
+    assert experiment_run.output == {"output": "doesn't matter, this is the output"}
