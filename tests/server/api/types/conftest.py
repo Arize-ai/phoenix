0e15f7173ed6190ca71464923cf5eec110b8d03a
Roger Yang <roger.yang@arize.com>
2024-06-12 14:10:40 -0700

    clean up

--- tests/server/api/types/conftest.py
+++ tests/server/api/types/conftest.py
@@ -67,27 +67,23 @@ async def interlaced_experiments(session) -> List[int]:
             [{"name": f"{i}", "metadata_": {}} for i in range(3)],
         )
     )
-    dataset_version_ids = {
-        dataset_id: dataset_version_id
-        for dataset_id, dataset_version_id in await session.execute(
-            insert(models.DatasetVersion).returning(
-                models.DatasetVersion.dataset_id,
-                models.DatasetVersion.id,
-            ),
+    dataset_version_ids = list(
+        await session.scalars(
+            insert(models.DatasetVersion).returning(models.DatasetVersion.dataset_id),
             [{"dataset_id": dataset_id, "metadata_": {}} for dataset_id in dataset_ids],
         )
-    }
+    )
     return list(
         await session.scalars(
             insert(models.Experiment).returning(models.Experiment.id),
             [
                 {
                     "dataset_id": dataset_id,
-                    "dataset_version_id": dataset_version_ids[dataset_id],
+                    "dataset_version_id": dataset_version_ids[i],
                     "metadata_": {},
                 }
                 for _ in range(4)
-                for dataset_id in dataset_ids
+                for i, dataset_id in enumerate(dataset_ids)
             ],
         )
     )
