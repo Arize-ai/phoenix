c4d77f7cf9f0b6b1d7d0ca9545f6a5ee626e9139
Mikyo King <mikyo@arize.com>
2024-06-13 17:59:21 -0600

    feat(experiments): navigation to experiments view (#3509)
    * feat(experiments): experiment compare
    
    * add an experiment multi-select
    
    * feat(experiments): navigation to experiment comparison view
    
    * fix url params

--- app/src/pages/experiment/ExperimentComparePage.tsx
+++ app/src/pages/experiment/ExperimentComparePage.tsx
@@ -1,5 +1,38 @@
 import React from "react";
+import { useLoaderData, useSearchParams } from "react-router-dom";
+
+import { Flex, Heading, View } from "@arizeai/components";
+
+import { experimentCompareLoaderQuery$data } from "./__generated__/experimentCompareLoaderQuery.graphql";
+import { ExperimentMultiSelector } from "./ExperimentMultiSelector";
 
 export function ExperimentComparePage() {
-  return <main>Compare 1 or more experiments</main>;
+  const data = useLoaderData() as experimentCompareLoaderQuery$data;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const experimentIds = searchParams.getAll("experimentId");
+  return (
+    <main>
+      <View
+        padding="size-200"
+        borderBottomColor="dark"
+        borderBottomWidth="thin"
+      >
+        <Flex direction="column" gap="size-100">
+          <Heading level={1}>Compare Experiments</Heading>
+          <ExperimentMultiSelector
+            dataset={data.dataset}
+            selectedExperimentIds={experimentIds}
+            label="experiments"
+            onChange={(newExperimentIds) => {
+              searchParams.delete("experimentId");
+              newExperimentIds.forEach((id) => {
+                searchParams.append("experimentId", id);
+              });
+              setSearchParams(searchParams);
+            }}
+          />
+        </Flex>
+      </View>
+    </main>
+  );
 }
