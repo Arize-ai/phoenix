e9fad6d89db07de0592139c86d80aa914617e9e6
Alexander Song <axiomofjoy@gmail.com>
2024-06-17 13:56:42 -0700

    delete multiple experiments at once

--- src/phoenix/server/api/mutations/experiment_mutations.py
+++ src/phoenix/server/api/mutations/experiment_mutations.py
@@ -1,10 +1,13 @@
+from typing import List
+
 import strawberry
 from sqlalchemy import delete
+from strawberry.relay import GlobalID
 from strawberry.types import Info
 
 from phoenix.db import models
 from phoenix.server.api.context import Context
-from phoenix.server.api.input_types.DeleteExperimentInput import DeleteExperimentInput
+from phoenix.server.api.input_types.DeleteExperimentsInput import DeleteExperimentsInput
 from phoenix.server.api.mutations.auth import IsAuthenticated
 from phoenix.server.api.types.Experiment import Experiment, to_gql_experiment
 from phoenix.server.api.types.node import from_global_id_with_expected_type
@@ -12,25 +15,46 @@ from phoenix.server.api.types.node import from_global_id_with_expected_type
 
 @strawberry.type
 class ExperimentMutationPayload:
-    experiment: Experiment
+    experiments: List[Experiment]
 
 
 @strawberry.type
 class ExperimentMutationMixin:
     @strawberry.mutation(permission_classes=[IsAuthenticated])  # type: ignore
-    async def delete_experiment(
+    async def delete_experiments(
         self,
         info: Info[Context, None],
-        input: DeleteExperimentInput,
+        input: DeleteExperimentsInput,
     ) -> ExperimentMutationPayload:
-        experiment_id = from_global_id_with_expected_type(input.experiment_id, Experiment.__name__)
+        experiment_ids = [
+            from_global_id_with_expected_type(experiment_id, Experiment.__name__)
+            for experiment_id in input.experiment_ids
+        ]
         async with info.context.db() as session:
-            if (
-                experiment := await session.scalar(
-                    delete(models.Experiment)
-                    .where(models.Experiment.id == experiment_id)
-                    .returning(models.Experiment)
+            savepoint = await session.begin_nested()
+            experiments = {
+                experiment.id: experiment
+                async for experiment in (
+                    await session.stream_scalars(
+                        delete(models.Experiment)
+                        .where(models.Experiment.id.in_(experiment_ids))
+                        .returning(models.Experiment)
+                    )
                 )
-            ) is None:
-                raise ValueError(f"Unknown experiment: {input.experiment_id}")
-        return ExperimentMutationPayload(experiment=to_gql_experiment(experiment))
+            }
+            if unknown_experiment_ids := set(experiment_ids) - set(experiments.keys()):
+                await savepoint.rollback()
+                raise ValueError(
+                    "Unknown experiment IDs: "
+                    + str(
+                        [
+                            str(GlobalID(Experiment.__name__, str(experiment_id)))
+                            for experiment_id in unknown_experiment_ids
+                        ]
+                    )
+                )
+        return ExperimentMutationPayload(
+            experiments=[
+                to_gql_experiment(experiments[experiment_id]) for experiment_id in experiment_ids
+            ]
+        )
