8b37dad7d2318cc00867408e20482e2aa7059148
Roger Yang <80478925+RogerHYang@users.noreply.github.com>
2024-06-18 19:38:41 -0700

    refactor: define object types for experiments (#3568)

--- src/phoenix/server/api/routers/v1/experiment_runs.py
+++ src/phoenix/server/api/routers/v1/experiment_runs.py
@@ -6,30 +6,30 @@ from starlette.responses import JSONResponse, Response
 from starlette.status import HTTP_404_NOT_FOUND
 from strawberry.relay import GlobalID
 
+from phoenix.datasets.types import ExperimentResult, ExperimentRun
 from phoenix.db import models
 from phoenix.server.api.types.node import from_global_id_with_expected_type
+from phoenix.utilities.json_utils import jsonify
 
 
 async def create_experiment_run(request: Request) -> Response:
-    experiment_globalid = GlobalID.from_id(request.path_params["experiment_id"])
+    experiment_gid = GlobalID.from_id(request.path_params["experiment_id"])
     try:
-        experiment_id = from_global_id_with_expected_type(experiment_globalid, "Experiment")
+        experiment_id = from_global_id_with_expected_type(experiment_gid, "Experiment")
     except ValueError:
         return Response(
-            content=f"Experiment with ID {experiment_globalid} does not exist",
+            content=f"Experiment with ID {experiment_gid} does not exist",
             status_code=HTTP_404_NOT_FOUND,
         )
 
     payload = await request.json()
 
-    dataset_example_globalid = GlobalID.from_id(payload["dataset_example_id"])
+    example_gid = GlobalID.from_id(payload["dataset_example_id"])
     try:
-        dataset_example_id = from_global_id_with_expected_type(
-            dataset_example_globalid, "DatasetExample"
-        )
+        dataset_example_id = from_global_id_with_expected_type(example_gid, "DatasetExample")
     except ValueError:
         return Response(
-            content=f"DatasetExample with ID {dataset_example_globalid} does not exist",
+            content=f"DatasetExample with ID {example_gid} does not exist",
             status_code=HTTP_404_NOT_FOUND,
         )
 
@@ -41,7 +41,7 @@ async def create_experiment_run(request: Request) -> Response:
     error = payload.get("error")
 
     async with request.app.state.db() as session:
-        experiment_run = models.ExperimentRun(
+        exp_run = models.ExperimentRun(
             experiment_id=experiment_id,
             dataset_example_id=dataset_example_id,
             trace_id=trace_id,
@@ -51,31 +51,31 @@ async def create_experiment_run(request: Request) -> Response:
             end_time=datetime.fromisoformat(end_time),
             error=error,
         )
-        session.add(experiment_run)
+        session.add(exp_run)
         await session.flush()
 
-        run_globalid = GlobalID("ExperimentRun", str(experiment_run.id))
-        run_payload = {
-            "id": str(run_globalid),
-            "experiment_id": str(experiment_globalid),
-            "dataset_example_id": str(dataset_example_globalid),
-            "output": experiment_run.output,
-            "trace_id": experiment_run.trace_id,
-            "repetition_number": experiment_run.repetition_number,
-            "start_time": experiment_run.start_time.isoformat(),
-            "end_time": experiment_run.end_time.isoformat(),
-            "error": experiment_run.error,
-        }
-        return JSONResponse(content=run_payload, status_code=200)
+        run_gid = GlobalID("ExperimentRun", str(exp_run.id))
+        run_payload = ExperimentRun(
+            start_time=exp_run.start_time,
+            end_time=exp_run.end_time,
+            experiment_id=str(experiment_gid),
+            dataset_example_id=str(example_gid),
+            repetition_number=exp_run.repetition_number,
+            output=ExperimentResult(result=exp_run.output),
+            error=exp_run.error,
+            id=str(run_gid),
+            trace_id=exp_run.trace_id,
+        )
+        return JSONResponse(content=jsonify(run_payload), status_code=200)
 
 
 async def list_experiment_runs(request: Request) -> Response:
-    experiment_globalid = GlobalID.from_id(request.path_params["experiment_id"])
+    experiment_gid = GlobalID.from_id(request.path_params["experiment_id"])
     try:
-        experiment_id = from_global_id_with_expected_type(experiment_globalid, "Experiment")
+        experiment_id = from_global_id_with_expected_type(experiment_gid, "Experiment")
     except ValueError:
         return Response(
-            content=f"Experiment with ID {experiment_globalid} does not exist",
+            content=f"Experiment with ID {experiment_gid} does not exist",
             status_code=HTTP_404_NOT_FOUND,
         )
 
@@ -83,26 +83,26 @@ async def list_experiment_runs(request: Request) -> Response:
         experiment_runs = await session.execute(
             select(models.ExperimentRun)
             .where(models.ExperimentRun.experiment_id == experiment_id)
-            # order by dataset_exaple_id to be consistent with `list_dataset_examples`
+            # order by dataset_example_id to be consistent with `list_dataset_examples`
             .order_by(models.ExperimentRun.dataset_example_id.asc())
         )
         experiment_runs = experiment_runs.scalars().all()
         runs = []
-        for run in experiment_runs:
-            run_gid = GlobalID("ExperimentRun", str(run.id))
-            experiment_gid = GlobalID("Experiment", str(run.experiment_id))
-            example_gid = GlobalID("DatasetExample", str(run.dataset_example_id))
+        for exp_run in experiment_runs:
+            run_gid = GlobalID("ExperimentRun", str(exp_run.id))
+            experiment_gid = GlobalID("Experiment", str(exp_run.experiment_id))
+            example_gid = GlobalID("DatasetExample", str(exp_run.dataset_example_id))
             runs.append(
-                {
-                    "id": str(run_gid),
-                    "experiment_id": str(experiment_gid),
-                    "dataset_example_id": str(example_gid),
-                    "output": run.output,
-                    "trace_id": run.trace_id,
-                    "repetition_number": run.repetition_number,
-                    "start_time": run.start_time.isoformat(),
-                    "end_time": run.end_time.isoformat(),
-                    "error": run.error,
-                }
+                ExperimentRun(
+                    start_time=exp_run.start_time,
+                    end_time=exp_run.end_time,
+                    experiment_id=str(experiment_gid),
+                    dataset_example_id=str(example_gid),
+                    repetition_number=exp_run.repetition_number,
+                    output=ExperimentResult(result=exp_run.output),
+                    error=exp_run.error,
+                    id=str(run_gid),
+                    trace_id=exp_run.trace_id,
+                )
             )
-        return JSONResponse(content=runs, status_code=200)
+        return JSONResponse(content=jsonify(runs), status_code=200)
