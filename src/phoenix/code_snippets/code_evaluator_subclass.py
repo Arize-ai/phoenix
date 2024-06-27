044ceb25a11efc9a35b518e52661dbc7d8deeb7d
Alexander Song <axiomofjoy@gmail.com>
2024-06-27 01:23:13 -0700

    code evaluator subclasses

---
+++ src/phoenix/code_snippets/code_evaluator_subclass.py
@@ -0,0 +1,40 @@
+from types import MappingProxyType
+from typing import Any, Optional
+
+from phoenix.datasets.evaluators.base import CodeEvaluator
+from phoenix.datasets.types import (
+    EvaluationResult,
+    ExampleInput,
+    ExampleMetadata,
+    ExampleOutput,
+    TaskOutput,
+)
+
+
+class MyCodeEvaluator(CodeEvaluator):
+    # A name for the evaluator (if not provided, the class name will be used).
+    _name = "my-code-evaluator"
+
+    def evaluate(
+        self,
+        *,
+        output: Optional[TaskOutput] = None,
+        expected: Optional[ExampleOutput] = None,
+        metadata: ExampleMetadata = MappingProxyType({}),
+        input: ExampleInput = MappingProxyType({}),
+        **kwargs: Any,
+    ) -> EvaluationResult:
+        # Implement this method or the async method.
+        raise NotImplementedError("evaluate method not implemented")
+
+    async def async_evaluate(
+        self,
+        *,
+        output: Optional[TaskOutput] = None,
+        expected: Optional[ExampleOutput] = None,
+        metadata: ExampleMetadata = MappingProxyType({}),
+        input: ExampleInput = MappingProxyType({}),
+        **kwargs: Any,
+    ) -> EvaluationResult:
+        # Implement this method or the sync method.
+        raise NotImplementedError("async_evaluate method not implemented")
