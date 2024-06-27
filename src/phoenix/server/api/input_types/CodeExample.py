e8b89185bdb779087551a9cc053c6f45aeceddc6
Alexander Song <axiomofjoy@gmail.com>
2024-06-27 10:14:48 -0700

    tmp

--- src/phoenix/server/api/input_types/CodeExample.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-import strawberry
-
-
-@strawberry.enum
-class CodeExample(Enum):
-    CODE_EVALUATOR_SUBCLASS = "code_snippets/code_evaluator_subclass.py"
-    LLM_EVALUATOR_SUBCLASS = "code_snippets/llm_evaluator_subclass.py"
