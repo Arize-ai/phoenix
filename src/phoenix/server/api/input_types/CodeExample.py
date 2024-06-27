be6d50e21f198ccd87a326d60c53c21a2d8cc181
Alexander Song <axiomofjoy@gmail.com>
2024-06-27 10:12:58 -0700

    revert changes

--- src/phoenix/server/api/input_types/CodeExample.py
+++ src/phoenix/server/api/input_types/CodeExample.py
@@ -5,5 +5,5 @@ import strawberry
 
 @strawberry.enum
 class CodeExample(Enum):
-    CODE_EVALUATOR_SUBCLASS = "code_examples/code_evaluator_subclass.py"
-    LLM_EVALUATOR_SUBCLASS = "code_examples/llm_evaluator_subclass.py"
+    CODE_EVALUATOR_SUBCLASS = "code_snippets/code_evaluator_subclass.py"
+    LLM_EVALUATOR_SUBCLASS = "code_snippets/llm_evaluator_subclass.py"
