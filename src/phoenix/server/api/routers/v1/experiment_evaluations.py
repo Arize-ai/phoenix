4299f8c63fb79fc21914f08e4a1567491a46d344
Xander Song <axiomofjoy@gmail.com>
2024-06-26 21:30:48 -0700

    add upsert support (#3685)

--- src/phoenix/server/api/routers/v1/experiment_evaluations.py
+++ src/phoenix/server/api/routers/v1/experiment_evaluations.py
@@ -7,11 +7,13 @@ from strawberry.relay import GlobalID
 
 from phoenix.datasets.types import EvaluationResult, ExperimentEvaluationRun
 from phoenix.db import models
+from phoenix.db.helpers import SupportedSQLDialect
+from phoenix.db.insertion.helpers import OnConflict, insert_on_conflict
 from phoenix.server.api.types.node import from_global_id_with_expected_type
 from phoenix.utilities.json import jsonify
 
 
-async def create_experiment_evaluation(request: Request) -> Response:
+async def upsert_experiment_evaluation(request: Request) -> Response:
     payload = await request.json()
     experiment_run_gid = GlobalID.from_id(payload["experiment_run_id"])
     try:
@@ -32,7 +34,7 @@ async def create_experiment_evaluation(request: Request) -> Response:
     start_time = payload["start_time"]
     end_time = payload["end_time"]
     async with request.app.state.db() as session:
-        exp_eval_run = models.ExperimentRunAnnotation(
+        values = dict(
             experiment_run_id=experiment_run_id,
             name=name,
             annotator_kind=annotator_kind,
@@ -40,13 +42,27 @@ async def create_experiment_evaluation(request: Request) -> Response:
             score=score,
             explanation=explanation,
             error=error,
-            metadata_=metadata,
+            metadata_=metadata,  # `metadata_` must match database
             start_time=datetime.fromisoformat(start_time),
             end_time=datetime.fromisoformat(end_time),
             trace_id=payload.get("trace_id"),
         )
-        session.add(exp_eval_run)
-        await session.flush()
+        set_ = {
+            **{k: v for k, v in values.items() if k != "metadata_"},
+            "metadata": values["metadata_"],  # `metadata` must match database
+        }
+        dialect = SupportedSQLDialect(session.bind.dialect.name)
+        exp_eval_run = await session.scalar(
+            insert_on_conflict(
+                dialect=dialect,
+                table=models.ExperimentRunAnnotation,
+                values=values,
+                constraint="uq_experiment_run_annotations_experiment_run_id_name",
+                column_names=("experiment_run_id", "name"),
+                on_conflict=OnConflict.DO_UPDATE,
+                set_=set_,
+            ).returning(models.ExperimentRunAnnotation)
+        )
         evaluation_gid = GlobalID("ExperimentEvaluation", str(exp_eval_run.id))
         eval_payload = ExperimentEvaluationRun(
             id=str(evaluation_gid),
