eed531f221f5b368556b3abfbe2158024949dd73
Alexander Song <axiomofjoy@gmail.com>
2024-06-27 16:42:12 -0700

    unnest import

--- src/phoenix/datasets/evaluators/__init__.py
+++ src/phoenix/datasets/evaluators/__init__.py
@@ -1,4 +1,10 @@
-from phoenix.datasets.evaluators.code_evaluators import ContainsKeyword, JSONParsable
+from phoenix.datasets.evaluators.code_evaluators import (
+    ContainsAllKeywords,
+    ContainsAnyKeyword,
+    ContainsKeyword,
+    JSONParsable,
+    MatchesRegex,
+)
 from phoenix.datasets.evaluators.llm_evaluators import (
     CoherenceEvaluator,
     ConcisenessEvaluator,
@@ -10,8 +16,11 @@ from phoenix.datasets.evaluators.utils import create_evaluator
 
 __all__ = [
     "create_evaluator",
+    "ContainsAllKeywords",
+    "ContainsAnyKeyword",
     "ContainsKeyword",
     "JSONParsable",
+    "MatchesRegex",
     "CoherenceEvaluator",
     "ConcisenessEvaluator",
     "LLMCriteriaEvaluator",
