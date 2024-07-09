62294ab59e4df0787794c1f1b4e3eb9c8af65d5a
Alexander Song <axiomofjoy@gmail.com>
2024-07-08 22:05:31 -0700

    decorate datasets

--- src/phoenix/server/api/routers/v1/__init__.py
+++ src/phoenix/server/api/routers/v1/__init__.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 
 from . import (
-    datasets,
     evaluations,
     experiment_evaluations,
     experiment_runs,
@@ -10,8 +9,10 @@ from . import (
     traces,
 )
 from .dataset_examples import list_dataset_examples
+from .datasets import router as datasets_router
 
 router = APIRouter(prefix="/v1")
+router.include_router(datasets_router)
 
 V1_ROUTES = (
     ("/evaluations", evaluations.post_evaluations, ["POST"]),
@@ -19,15 +20,7 @@ V1_ROUTES = (
     ("/traces", traces.post_traces, ["POST"]),
     ("/spans", spans.query_spans_handler, ["POST"]),
     ("/spans", spans.get_spans_handler, ["GET"]),
-    ("/datasets/upload", datasets.post_datasets_upload, ["POST"]),
-    ("/datasets", datasets.list_datasets, ["GET"]),
-    ("/datasets/{id:str}", datasets.delete_dataset_by_id, ["DELETE"]),
-    ("/datasets/{id:str}", datasets.get_dataset_by_id, ["GET"]),
-    ("/datasets/{id:str}/csv", datasets.get_dataset_csv, ["GET"]),
-    ("/datasets/{id:str}/jsonl/openai_ft", datasets.get_dataset_jsonl_openai_ft, ["GET"]),
-    ("/datasets/{id:str}/jsonl/openai_evals", datasets.get_dataset_jsonl_openai_evals, ["GET"]),
     ("/datasets/{id:str}/examples", list_dataset_examples, ["GET"]),
-    ("/datasets/{id:str}/versions", datasets.get_dataset_versions, ["GET"]),
     ("/datasets/{dataset_id:str}/experiments", experiments.create_experiment, ["POST"]),
     ("/experiments/{experiment_id:str}", experiments.read_experiment, ["GET"]),
     ("/experiments/{experiment_id:str}/runs", experiment_runs.create_experiment_run, ["POST"]),
