4952b8d869118cb2c9b627ef03eac55f4a4c55de
Alexander Song <axiomofjoy@gmail.com>
2024-07-09 14:07:01 -0700

    generate openapi schema

--- src/phoenix/server/api/openapi/main.py
+++ src/phoenix/server/api/openapi/main.py
@@ -1,1 +1,6 @@
-# todo: implement
+from .schema import get_openapi_schema
+
+if __name__ == "__main__":
+    import yaml  # type: ignore
+
+    print(yaml.dump(get_openapi_schema(), indent=2))
--- src/phoenix/server/api/openapi/schema.py
+++ src/phoenix/server/api/openapi/schema.py
@@ -1,1 +1,16 @@
-# todo: implement
+from typing import Any, Dict
+
+from fastapi.openapi.utils import get_openapi
+
+from phoenix.server.api.routers.v1 import router as v1_router
+from phoenix.version import __version__
+
+
+def get_openapi_schema() -> Dict[str, Any]:
+    return get_openapi(
+        title="Arize-Phoenix API",
+        version=__version__,
+        openapi_version="3.1.0",
+        description="Schema for Arize-Phoenix REST API",
+        routes=v1_router.routes,
+    )
