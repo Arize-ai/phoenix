a45c9d152a826e5053c7cfdb3b05b2b5dfb93479
Alexander Song <axiomofjoy@gmail.com>
2024-07-13 10:46:07 -0700

    reduce number of tests being run

--- tests/server/api/openapi/test_schema.py
+++ tests/server/api/openapi/test_schema.py
@@ -1,6 +1,7 @@
 from phoenix.server.api.openapi.schema import get_openapi_schema
 
 
-def test_get_openapi_schema() -> None:
+def test_get_openapi_schema(pydantic_version) -> None:
+    assert pydantic_version == "v1"
     # ensures schema can be generated on both pydantic v1 and v2
     assert isinstance(get_openapi_schema(), dict)
