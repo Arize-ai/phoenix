6181d045ce00293fdab971a03811043b77231ffb
Parker Stafford <parker.stafford92@gmail.com>
2024-07-16 11:03:33 -0700

    add manual chunking

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 
+/**
+ * This is needed to support modulepreload in Vite when using a non HTML custom entrypoint (i.e., this file).
+ * We do this because our index.html is served by the Phoenix server
+ * @see https://vitejs.dev/config/build-options#build-modulepreload
+ */
 import "vite/modulepreload-polyfill";
 
 import App from "./App";
