6755903724efa47da925d5fe18f114a3e18f9f4b
Roger Yang <roger.yang@arize.com>
2024-07-22 15:09:49 -0700

    clean up

--- src/phoenix/db/bulk_inserter.py
+++ src/phoenix/db/bulk_inserter.py
@@ -107,9 +107,8 @@ class BulkInserter:
     async def __aexit__(self, *args: Any) -> None:
         self._running = False
         if self._task:
-            await self._task
+            self._task.cancel()
             self._task = None
-        self._operations = None
 
     def _enqueue_operation(self, operation: DataManipulation) -> None:
         cast("Queue[DataManipulation]", self._operations).put_nowait(operation)
