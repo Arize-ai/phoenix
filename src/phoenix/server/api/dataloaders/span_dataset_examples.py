65fbbf4628cd1a9e6aef8bbd6410f84c2db807c8
Dustin Ngo <dustin@arize.com>
2024-07-25 16:56:17 -0400

    Improve names and resolve missing type annotations

--- src/phoenix/server/api/dataloaders/span_dataset_examples.py
+++ src/phoenix/server/api/dataloaders/span_dataset_examples.py
@@ -1,6 +1,7 @@
 from typing import (
     AsyncContextManager,
     Callable,
+    Dict,
     List,
 )
 
@@ -22,14 +23,16 @@ class SpanDatasetExamplesDataLoader(DataLoader[Key, Result]):
         self._db = db
 
     async def _load_fn(self, keys: List[Key]) -> List[Result]:
-        span_ids = keys
+        span_rowids = keys
         async with self._db() as session:
-            dataset_examples = {span_id: [] for span_id in span_ids}
-            async for span_id, dataset_example in await session.stream(
+            dataset_examples: Dict[Key, List[models.DatasetExample]] = {
+                span_rowid: [] for span_rowid in span_rowids
+            }
+            async for span_rowid, dataset_example in await session.stream(
                 select(models.Span.id, models.DatasetExample)
                 .select_from(models.Span)
                 .join(models.DatasetExample, models.DatasetExample.span_rowid == models.Span.id)
-                .where(models.Span.id.in_(span_ids))
+                .where(models.Span.id.in_(span_rowids))
             ):
-                dataset_examples[span_id].append(dataset_example)
-        return [dataset_examples.get(span_id, []) for span_id in span_ids]
+                dataset_examples[span_rowid].append(dataset_example)
+        return [dataset_examples.get(span_rowid, []) for span_rowid in span_rowids]
