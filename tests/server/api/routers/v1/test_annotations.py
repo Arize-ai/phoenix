42100e338e78c2913f6ea4c31948cdc2963893aa
Parker Stafford <parker.stafford92@gmail.com>
2024-08-09 11:10:21 -0700

    update tests

--- tests/server/api/routers/v1/test_annotations.py
+++ tests/server/api/routers/v1/test_annotations.py
@@ -308,7 +308,7 @@ class TestSendingAnnotationsBeforeSpans:
     ) -> Callable[[bool, float], Awaitable[None]]:
         async def _(exist: bool, score_offset: float = 0) -> None:
             expected = mean_score + score_offset
-            for summaries, names in ((anno_summaries, anno_names), (eval_summaries, eval_names)):
+            for summaries, names in ((anno_summaries, anno_names), (_, eval_names)):
                 for summary in summaries:
                     mean_scores = await self._mean_scores(
                         httpx_client, summary, project_names, *names
@@ -431,12 +431,6 @@ _Evals: TypeAlias = Union[SpanEvaluations, TraceEvaluations, DocumentEvaluations
 _Summary: TypeAlias = Literal[
     "spanAnnotationSummary(annotation",
     "traceAnnotationSummary(annotation",
-    "spanEvaluationSummary(evaluation",
-    "traceEvaluationSummary(evaluation",
-]
-eval_summaries: List[_Summary] = [
-    "spanEvaluationSummary(evaluation",
-    "traceEvaluationSummary(evaluation",
 ]
 anno_summaries: List[_Summary] = [
     "spanAnnotationSummary(annotation",
