ad0a98f4f375278ee57f00d6859ad1efb54fab89
Roger Yang <roger.yang@arize.com>
2024-08-20 16:34:09 -0700

    clean up

--- integration_tests/server/conftest.py
+++ integration_tests/server/conftest.py
@@ -1,15 +1,9 @@
 import os
-import sys
 import tempfile
-from contextlib import ExitStack, contextmanager
-from queue import SimpleQueue
-from subprocess import PIPE, STDOUT
-from threading import Thread
-from time import sleep, time
-from typing import Callable, ContextManager, Iterator, List
+from contextlib import ExitStack
+from typing import Iterator, List
 from unittest import mock
 from urllib.parse import urljoin
-from urllib.request import urlopen
 
 import pytest
 from _pytest.monkeypatch import MonkeyPatch
@@ -30,7 +24,6 @@ from phoenix.config import (
     get_env_host,
 )
 from portpicker import pick_unused_port  # type: ignore[import-untyped]
-from psutil import STATUS_ZOMBIE, Popen
 
 
 @pytest.fixture(autouse=True)
@@ -50,56 +43,11 @@ def set_env_var(monkeypatch: Iterator[MonkeyPatch]) -> Iterator[None]:
 
 
 @pytest.fixture
-def app(base_url: str) -> Callable[[], ContextManager[None]]:
-    @contextmanager
-    def _() -> Iterator[None]:
-        with launch(base_url):
-            yield
-
-    return _
-
-
-@contextmanager
-def launch(base_url: str) -> Iterator[None]:
-    command = f"{sys.executable} -m phoenix.server.main --no-ui serve"
-    process = Popen(command.split(), stdout=PIPE, stderr=STDOUT, text=True, env=os.environ)
-    log: "SimpleQueue[str]" = SimpleQueue()
-    Thread(target=capture_stdout, args=(process, log), daemon=True).start()
-    t = 60
-    time_limit = time() + t
-    timed_out = False
-    url = urljoin(base_url, "healthz")
-    while not timed_out and is_alive(process):
-        sleep(0.1)
-        try:
-            urlopen(url)
-            break
-        except BaseException:
-            timed_out = time() > time_limit
-    try:
-        if timed_out:
-            raise TimeoutError(f"Server did not start within {t} seconds.")
-        assert is_alive(process)
-        yield
-        process.terminate()
-        process.wait(10)
-    finally:
-        logs = []
-        while not log.empty():
-            # For unknown reasons, this hangs if we try to print immediately
-            # after `get()`, so we collect the lines and print them later.
-            logs.append(log.get())
-        for line in logs:
-            print(line, end="")
-
-
-@pytest.fixture
 def tracers(
-    base_url: str,
     project_name: str,
     fake: Faker,
 ) -> List[Tracer]:
-    http_endpoint = urljoin(base_url, "v1/traces")
+    http_endpoint = urljoin(get_base_url(), "v1/traces")
     grpc_endpoint = f"http://{get_env_host()}:{get_env_grpc_port()}"
     tracers = []
     resource = Resource({ResourceAttributes.PROJECT_NAME: project_name})
@@ -111,11 +59,6 @@ def tracers(
 
 
 @pytest.fixture
-def base_url() -> str:
-    return get_base_url()
-
-
-@pytest.fixture
 def fake() -> Faker:
     return Faker()
 
@@ -123,12 +66,3 @@ def fake() -> Faker:
 @pytest.fixture
 def project_name(fake: Faker) -> str:
     return fake.pystr()
-
-
-def is_alive(process: Popen) -> bool:
-    return process.is_running() and process.status() != STATUS_ZOMBIE
-
-
-def capture_stdout(process: Popen, log: "SimpleQueue[str]") -> None:
-    while True:
-        log.put(process.stdout.readline())
--- integration_tests/server/test_launch_app.py
+++ integration_tests/server/test_launch_app.py
@@ -1,37 +1,44 @@
 import json
-from time import sleep
-from typing import Callable, ContextManager, List
+import os
+import sys
+from contextlib import contextmanager
+from queue import SimpleQueue
+from subprocess import PIPE, STDOUT
+from threading import Thread
+from time import sleep, time
+from typing import Any, Dict, Iterator, List, Optional
 from urllib.parse import urljoin
 from urllib.request import Request, urlopen
 
 import pytest
 from faker import Faker
 from opentelemetry.trace import Tracer
+from phoenix.config import get_base_url
+from psutil import STATUS_ZOMBIE, Popen
 
 
 @pytest.fixture
-def req(base_url: str) -> Request:
+def req() -> Request:
     query = dict(query="query{projects{edges{node{name spans{edges{node{name}}}}}}}")
     return Request(
         method="POST",
-        url=urljoin(base_url, "graphql"),
+        url=urljoin(get_base_url(), "graphql"),
         data=json.dumps(query).encode("utf-8"),
         headers={"Content-Type": "application/json"},
     )
 
 
 def test_launch_app(
-    app: Callable[[], ContextManager[None]],
     tracers: List[Tracer],
     project_name: str,
-    base_url: str,
     req: Request,
     fake: Faker,
 ) -> None:
     cycles = 2
     span_names: List[str] = []
     for _ in range(cycles):
-        with app():
+        response_dict: Optional[Dict[str, Any]] = None
+        with launch():
             for tracer in tracers:
                 span_names.append(fake.pystr())
                 tracer.start_span(span_names[-1]).end()
@@ -39,7 +46,6 @@ def test_launch_app(
             response = urlopen(req)
             response_dict = json.loads(response.read().decode("utf-8"))
             assert response_dict
-            print(f"{response_dict=}")
             assert not response_dict.get("errors")
             assert sorted(
                 span["node"]["name"]
@@ -47,3 +53,47 @@ def test_launch_app(
                 for span in project["node"]["spans"]["edges"]
                 if project["node"]["name"] == project_name
             ) == sorted(span_names)
+        print(f"{response_dict=}")
+
+
+@contextmanager
+def launch() -> Iterator[None]:
+    command = f"{sys.executable} -m phoenix.server.main --no-ui serve"
+    process = Popen(command.split(), stdout=PIPE, stderr=STDOUT, text=True, env=os.environ)
+    log: "SimpleQueue[str]" = SimpleQueue()
+    Thread(target=capture_stdout, args=(process, log), daemon=True).start()
+    t = 60
+    time_limit = time() + t
+    timed_out = False
+    url = urljoin(get_base_url(), "healthz")
+    while not timed_out and is_alive(process):
+        sleep(0.1)
+        try:
+            urlopen(url)
+            break
+        except BaseException:
+            timed_out = time() > time_limit
+    try:
+        if timed_out:
+            raise TimeoutError(f"Server did not start within {t} seconds.")
+        assert is_alive(process)
+        yield
+        process.terminate()
+        process.wait(10)
+    finally:
+        logs = []
+        while not log.empty():
+            # For unknown reasons, this hangs if we try to print immediately
+            # after `get()`, so we collect the lines and print them later.
+            logs.append(log.get())
+        for line in logs:
+            print(line, end="")
+
+
+def is_alive(process: Popen) -> bool:
+    return process.is_running() and process.status() != STATUS_ZOMBIE
+
+
+def capture_stdout(process: Popen, log: "SimpleQueue[str]") -> None:
+    while True:
+        log.put(process.stdout.readline())
