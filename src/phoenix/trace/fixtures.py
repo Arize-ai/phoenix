9f545107f111d6083df69ae601c4f96ca7cce03a
Chris Park <cjpark@berkeley.edu>
2024-08-20 11:41:32 -0700

    docs: add LLM fixtures for demo dataset (fine-tuning dataset), fix demo notebook (#4286)
    * wip
    
    * wip
    
    * wip
    
    * traces/evals
    
    * updated and fixed
    
    * fix names
    
    * fixtures
    
    * transfer fixtures outsiide of git
    
    * expose new demo data as trace dataset fixture
    
    * remove unnecessary notebook
    
    * remove json files and add to .gitignore
    
    * rename directory/files, fix .gitignore
    
    * rename
    
    * fix notebook and fixtures with renamed files
    
    * missed a section
    
    * post-review changes
    
    * demo llm fixtures, fix notebook
    
    * fix comments

--- src/phoenix/trace/fixtures.py
+++ src/phoenix/trace/fixtures.py
@@ -85,7 +85,7 @@ class TracesFixture:
 demo_llama_index_rag_fixture = TracesFixture(
     name="demo_llama_index_rag",
     description="Traces and evaluations of a RAG chatbot using LlamaIndex.",
-    file_name="demo_llama_index_rag.parquet",
+    file_name="demo_llama_index_rag_traces.parquet",
     evaluation_fixtures=(
         EvaluationFixture(
             evaluation_name="Q&A Correctness",
@@ -100,6 +100,12 @@ demo_llama_index_rag_fixture = TracesFixture(
             file_name="demo_llama_index_rag_doc_relevance_eval.parquet",
         ),
     ),
+)
+
+demo_llama_index_rag_llm_fixture = TracesFixture(
+    name="demo_llama_index_rag_llm",
+    description="LLM traces for RAG chatbot using LlamaIndex.",
+    file_name="demo_llama_index_llm_all_spans.parquet",
 )
 
 llama_index_rag_fixture = TracesFixture(
@@ -196,6 +202,7 @@ random_fixture = TracesFixture(
 
 TRACES_FIXTURES: List[TracesFixture] = [
     demo_llama_index_rag_fixture,
+    demo_llama_index_rag_llm_fixture,
     llama_index_rag_fixture,
     llama_index_rag_fixture_with_davinci,
     langchain_rag_stuff_document_chain_fixture,
