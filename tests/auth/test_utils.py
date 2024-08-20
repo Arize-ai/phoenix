6bae89448e8930fa337f8b076a2f934c13247030
Alexander Song <axiomofjoy@gmail.com>
2024-08-19 20:07:40 -0700

    ensure compatibility with lower versions of python

--- tests/auth/test_utils.py
+++ tests/auth/test_utils.py
@@ -21,8 +21,11 @@ from phoenix.auth.utils import compute_password_hash
 async def test_compute_password_hash_produces_same_hash_iff_password_and_salt_are_the_same(
     password0: str, password1: str, salt0: str, salt1: str
 ) -> None:
+    loop = asyncio.get_running_loop()
     password0_salt0_hash0, password0_salt0_hash1, password1_salt0_hash0, password0_salt1_hash0 = [
-        await asyncio.to_thread(lambda: compute_password_hash(password, salt))
+        await loop.run_in_executor(
+            executor=None, func=lambda: compute_password_hash(password, salt)
+        )
         for password, salt in (
             (password0, salt0),
             (password0, salt0),
