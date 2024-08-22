31aeb6e8e94ce71178bbe43bcd1957470c678511
Mikyo King <mikyo@arize.com>
2024-08-21 18:10:34 -0600

    feat(auth): minimal login page

--- app/src/Routes.tsx
+++ app/src/Routes.tsx
@@ -24,6 +24,7 @@ import {
   ExperimentsPage,
   homeLoader,
   Layout,
+  LoginPage,
   ModelPage,
   ModelRoot,
   projectLoader,
@@ -37,110 +38,114 @@ import {
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Layout />} errorElement={<ErrorElement />}>
-      <Route index loader={homeLoader} />
-      <Route
-        path="/model"
-        handle={{ crumb: () => "model" }}
-        element={<ModelRoot />}
-      >
-        <Route index element={<ModelPage />} />
-        <Route element={<ModelPage />}>
-          <Route path="dimensions">
-            <Route
-              path=":dimensionId"
-              element={<DimensionPage />}
-              loader={dimensionLoader}
-            />
-          </Route>
-        </Route>
-        <Route path="embeddings">
-          <Route
-            path=":embeddingDimensionId"
-            element={<EmbeddingPage />}
-            loader={embeddingLoader}
-            handle={{
-              // `crumb` is your own abstraction, we decided
-              // to make this one a function so we can pass
-              // the data from the loader to it so that our
-              // breadcrumb is made up of dynamic content
-              crumb: (data: embeddingLoaderQuery$data) => data.embedding.name,
-            }}
-          />
-        </Route>
-      </Route>
-      <Route
-        path="/projects"
-        handle={{ crumb: () => "projects" }}
-        element={<ProjectsRoot />}
-      >
-        <Route index element={<ProjectsPage />} />
+    <Route path="/" errorElement={<ErrorElement />}>
+      <Route path="/login" element={<LoginPage />} />
+      <Route element={<Layout />}>
+        <Route index loader={homeLoader} />
+
         <Route
-          path=":projectId"
-          element={<TracingRoot />}
-          loader={projectLoader}
-          handle={{
-            crumb: (data: projectLoaderQuery$data) => data.project.name,
-          }}
+          path="/model"
+          handle={{ crumb: () => "model" }}
+          element={<ModelRoot />}
         >
-          <Route index element={<ProjectPage />} />
-          <Route element={<ProjectPage />}>
-            <Route path="traces/:traceId" element={<TracePage />} />
-          </Route>
-        </Route>
-      </Route>
-      <Route path="/datasets" handle={{ crumb: () => "datasets" }}>
-        <Route index element={<DatasetsPage />} />
-        <Route
-          path=":datasetId"
-          loader={datasetLoader}
-          handle={{
-            crumb: (data: datasetLoaderQuery$data) => data.dataset.name,
-          }}
-        >
-          <Route element={<DatasetPage />} loader={datasetLoader}>
-            <Route
-              index
-              element={<ExperimentsPage />}
-              loader={experimentsLoader}
-            />
-            <Route
-              path="experiments"
-              element={<ExperimentsPage />}
-              loader={experimentsLoader}
-            />
-            <Route
-              path="examples"
-              element={<ExamplesPage />}
-              loader={examplesLoader}
-            >
-              <Route path=":exampleId" element={<ExamplePage />} />
+          <Route index element={<ModelPage />} />
+          <Route element={<ModelPage />}>
+            <Route path="dimensions">
+              <Route
+                path=":dimensionId"
+                element={<DimensionPage />}
+                loader={dimensionLoader}
+              />
             </Route>
           </Route>
-          <Route
-            path="compare"
-            handle={{
-              crumb: () => "compare",
-            }}
-            loader={experimentCompareLoader}
-            element={<ExperimentComparePage />}
-          />
+          <Route path="embeddings">
+            <Route
+              path=":embeddingDimensionId"
+              element={<EmbeddingPage />}
+              loader={embeddingLoader}
+              handle={{
+                // `crumb` is your own abstraction, we decided
+                // to make this one a function so we can pass
+                // the data from the loader to it so that our
+                // breadcrumb is made up of dynamic content
+                crumb: (data: embeddingLoaderQuery$data) => data.embedding.name,
+              }}
+            />
+          </Route>
         </Route>
+        <Route
+          path="/projects"
+          handle={{ crumb: () => "projects" }}
+          element={<ProjectsRoot />}
+        >
+          <Route index element={<ProjectsPage />} />
+          <Route
+            path=":projectId"
+            element={<TracingRoot />}
+            loader={projectLoader}
+            handle={{
+              crumb: (data: projectLoaderQuery$data) => data.project.name,
+            }}
+          >
+            <Route index element={<ProjectPage />} />
+            <Route element={<ProjectPage />}>
+              <Route path="traces/:traceId" element={<TracePage />} />
+            </Route>
+          </Route>
+        </Route>
+        <Route path="/datasets" handle={{ crumb: () => "datasets" }}>
+          <Route index element={<DatasetsPage />} />
+          <Route
+            path=":datasetId"
+            loader={datasetLoader}
+            handle={{
+              crumb: (data: datasetLoaderQuery$data) => data.dataset.name,
+            }}
+          >
+            <Route element={<DatasetPage />} loader={datasetLoader}>
+              <Route
+                index
+                element={<ExperimentsPage />}
+                loader={experimentsLoader}
+              />
+              <Route
+                path="experiments"
+                element={<ExperimentsPage />}
+                loader={experimentsLoader}
+              />
+              <Route
+                path="examples"
+                element={<ExamplesPage />}
+                loader={examplesLoader}
+              >
+                <Route path=":exampleId" element={<ExamplePage />} />
+              </Route>
+            </Route>
+            <Route
+              path="compare"
+              handle={{
+                crumb: () => "compare",
+              }}
+              loader={experimentCompareLoader}
+              element={<ExperimentComparePage />}
+            />
+          </Route>
+        </Route>
+        <Route
+          path="/apis"
+          element={<APIsPage />}
+          handle={{
+            crumb: () => "APIs",
+          }}
+        />
+        <Route
+          path="/settings"
+          element={<SettingsPage />}
+          handle={{
+            crumb: () => "Settings",
+          }}
+        />
       </Route>
-      <Route
-        path="/apis"
-        element={<APIsPage />}
-        handle={{
-          crumb: () => "APIs",
-        }}
-      />
-      <Route
-        path="/settings"
-        element={<SettingsPage />}
-        handle={{
-          crumb: () => "Settings",
-        }}
-      />
     </Route>
   ),
   {
---
+++ app/src/pages/login/LoginPage.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { css } from "@emotion/react";
+
+import {
+  Button,
+  Flex,
+  Form,
+  Heading,
+  TextField,
+  View,
+} from "@arizeai/components";
+
+import { Logo } from "@phoenix/components/nav/Logo";
+
+export function LoginPage() {
+  return (
+    <main
+      css={css`
+        padding-top: var(--ac-global-dimension-size-2000);
+        width: 100%;
+      `}
+    >
+      <View
+        borderColor="dark"
+        borderWidth="thin"
+        width="size-5000"
+        padding="size-400"
+        backgroundColor="dark"
+        marginStart="auto"
+        marginEnd="auto"
+        borderRadius="medium"
+      >
+        <Flex direction="column" gap="size-200" alignItems="center">
+          <View paddingBottom="size-400">
+            <Logo size={120} />
+          </View>
+        </Flex>
+        <Form action="/login" method="post">
+          <TextField
+            label="Email"
+            name="email"
+            isRequired
+            type="email"
+            placeholder="your email address"
+          />
+          <TextField
+            label="Password"
+            name="password"
+            type="password"
+            isRequired
+            placeholder="your password"
+          />
+          <div
+            css={css`
+              margin-top: var(--ac-global-dimension-size-400);
+              margin-bottom: var(--ac-global-dimension-size-50);
+              button {
+                width: 100%;
+              }
+            `}
+          >
+            <Button variant="primary" type="submit">
+              Login
+            </Button>
+          </div>
+        </Form>
+      </View>
+    </main>
+  );
+}
