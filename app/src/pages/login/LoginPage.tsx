1f2fb8ac75e6e82cb68a48ef12bc0bd328eac0f2
Mikyo King <mikyo@arize.com>
2024-08-22 17:42:47 -0600

    cleanup

--- app/src/pages/login/LoginPage.tsx
+++ app/src/pages/login/LoginPage.tsx
@@ -12,11 +12,16 @@ export function LoginPage() {
         padding-top: 200px;
         width: 100%;
         height: 100vh;
+        overflow: hidden;
         background: radial-gradient(
           90% 60% at 50% 30%,
           rgba(5, 145, 193, 0.4) 0%,
           transparent 100%
         );
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        box-sizing: border-box;
       `}
     >
       <View
@@ -64,6 +69,31 @@ export function LoginPage() {
           </div>
         </Form>
       </View>
+      <footer
+        css={css`
+          display: flex;
+          justify-content: center;
+          padding: var(--ac-global-dimension-size-400);
+          gap: var(--ac-global-dimension-size-200);
+          a {
+            color: var(--ac-global-text-color-700);
+            transition: color 0.2s ease-in-out;
+            text-decoration: none;
+            &:hover {
+              color: var(--ac-global-text-color-900);
+              text-decoration: underline;
+            }
+          }
+        `}
+      >
+        <a href="https://docs.arize.com/phoenix">Documentation</a>|
+        <a href="https://join.slack.com/t/arize-ai/shared_invite/zt-1px8dcmlf-fmThhDFD_V_48oU7ALan4Q">
+          Community
+        </a>
+        |<a href="https://twitter.com/ArizePhoenix">Social</a>|
+        <a href="https://github.com/Arize-ai/phoenix">GitHub</a>|
+        <a href="https://github.com/Arize-ai/phoenix/releases">{`arize-phoenix v${window.Config.platformVersion}`}</a>
+      </footer>
     </main>
   );
 }
