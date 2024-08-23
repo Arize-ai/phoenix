3b2f1bcaa35b62eb06d93860d536bb3ba54167d6
Alexander Song <axiomofjoy@gmail.com>
2024-08-22 18:00:34 -0700

    simplify

--- src/phoenix/server/api/mutations/auth_mutations.py
+++ src/phoenix/server/api/mutations/auth_mutations.py
@@ -28,32 +28,22 @@ class AuthMutationMixin:
         info: Info[Context, None],
         input: LoginMutationInput,
     ) -> None:
-        # This is written to avoid an information disclosure vulnerability. In
-        # the event of a failed login, the stacktrace should not reveal whether
-        # login failed because of a non-existent email or an incorrect password.
-        # This is a workaround until we improve our error logging to prevent
-        # sensitive stacktraces from being logged.
-        # https://github.com/Arize-ai/phoenix/issues/4335
-        login_failed = False
         async with info.context.db() as session:
             if (
                 user := await session.scalar(
                     select(models.User).where(models.User.email == input.email)
                 )
             ) is None or (password_hash := user.password_hash) is None:
-                login_failed = True
-        if not login_failed:
-            secret = info.context.get_secret()
-            loop = asyncio.get_running_loop()
-            if not await loop.run_in_executor(
-                executor=None,
-                func=lambda: is_valid_password(
-                    password=input.password, salt=secret, password_hash=password_hash
-                ),
-            ):
-                login_failed = True
-        if login_failed:
-            raise ValueError("login failed")
+                raise ValueError
+        secret = info.context.get_secret()
+        loop = asyncio.get_running_loop()
+        if not await loop.run_in_executor(
+            executor=None,
+            func=lambda: is_valid_password(
+                password=input.password, salt=secret, password_hash=password_hash
+            ),
+        ):
+            raise ValueError
         response = info.context.get_response()
         response.set_cookie(
             key=PHOENIX_ACCESS_TOKEN_COOKIE_NAME,
