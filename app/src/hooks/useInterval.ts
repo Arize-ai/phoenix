26132c41a690e4bd219a723a422bf1b1f2f3f779
Mikyo King <mikyo@arize.com>
2024-08-27 18:50:30 -0600

    Update app/src/hooks/useInterval.ts

--- app/src/hooks/useInterval.ts
+++ app/src/hooks/useInterval.ts
@@ -5,7 +5,7 @@ type Callback = () => void;
 /**
  * Custom hook to use setInterval with React hooks
  * @param callback
- * @param {number | null} delay - if set to undefined or null, no interval will be set
+ * @param {number | null} delay - if set to null, no interval will be set
  */
 export function useInterval(callback: Callback, delay: number | null) {
   const savedCallback = useRef<Callback | null>(null);
