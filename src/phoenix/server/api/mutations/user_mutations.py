c8fddfc09e035e2211796ff4fdedfe194576caee
Mikyo King <mikyo@arize.com>
2024-08-29 13:44:17 -0600

    remove user role not used

--- src/phoenix/server/api/mutations/user_mutations.py
+++ src/phoenix/server/api/mutations/user_mutations.py
@@ -12,7 +12,6 @@ from phoenix.server.api.context import Context
 from phoenix.server.api.input_types.UserRoleInput import UserRoleInput
 from phoenix.server.api.mutations.auth import HasSecret, IsAdmin, IsAuthenticated, IsNotReadOnly
 from phoenix.server.api.types.User import User
-from phoenix.server.api.types.UserRole import UserRole
 
 
 @strawberry.input
