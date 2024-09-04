08b518c79ce2597a31d94ffa23dcfa8e5b198df0
Parker Stafford <parker.stafford92@gmail.com>
2024-09-04 10:38:06 -0700

    fix(auth): add back user api keys table

--- app/src/pages/settings/UserAPIKeysTable.tsx
+++ app/src/pages/settings/UserAPIKeysTable.tsx
@@ -1,5 +1,5 @@
-import React, { startTransition, useCallback, useMemo } from "react";
-import { graphql, useMutation, useRefetchableFragment } from "react-relay";
+import React, { startTransition, useMemo } from "react";
+import { graphql, useRefetchableFragment } from "react-relay";
 import {
   ColumnDef,
   flexRender,
@@ -13,25 +13,24 @@ import { TextCell } from "@phoenix/components/table";
 import { tableCSS } from "@phoenix/components/table/styles";
 import { TableEmpty } from "@phoenix/components/table/TableEmpty";
 import { TimestampCell } from "@phoenix/components/table/TimestampCell";
-import { useNotifySuccess } from "@phoenix/contexts";
 
-import { SystemAPIKeysTableFragment$key } from "./__generated__/SystemAPIKeysTableFragment.graphql";
-import { SystemAPIKeysTableQuery } from "./__generated__/SystemAPIKeysTableQuery.graphql";
+import { UserAPIKeysTableFragment$key } from "./__generated__/UserAPIKeysTableFragment.graphql";
+import { UserAPIKeysTableQuery } from "./__generated__/UserAPIKeysTableQuery.graphql";
 import { DeleteAPIKeyButton } from "./DeleteAPIKeyButton";
 
-export function SystemAPIKeysTable({
+export function UserAPIKeysTable({
   query,
 }: {
-  query: SystemAPIKeysTableFragment$key;
+  query: UserAPIKeysTableFragment$key;
 }) {
   const [data, refetch] = useRefetchableFragment<
-    SystemAPIKeysTableQuery,
-    SystemAPIKeysTableFragment$key
+    UserAPIKeysTableQuery,
+    UserAPIKeysTableFragment$key
   >(
     graphql`
-      fragment SystemAPIKeysTableFragment on Query
-      @refetchable(queryName: "SystemAPIKeysTableQuery") {
-        systemApiKeys {
+      fragment UserAPIKeysTableFragment on Query
+      @refetchable(queryName: "UserAPIKeysTableQuery") {
+        userApiKeys {
           id
           name
           description
@@ -43,46 +42,8 @@ export function SystemAPIKeysTable({
     query
   );
 
-  const notifySuccess = useNotifySuccess();
-  const [commit] = useMutation(graphql`
-    mutation SystemAPIKeysTableDeleteAPIKeyMutation(
-      $input: DeleteApiKeyInput!
-    ) {
-      deleteSystemApiKey(input: $input) {
-        __typename
-        id
-      }
-    }
-  `);
-  const handleDelete = useCallback(
-    (id: string) => {
-      commit({
-        variables: {
-          input: {
-            id,
-          },
-        },
-        onCompleted: () => {
-          notifySuccess({
-            title: "System key deleted",
-            message: "The system key has been deleted and is no longer active.",
-          });
-          startTransition(() => {
-            refetch(
-              {},
-              {
-                fetchPolicy: "network-only",
-              }
-            );
-          });
-        },
-      });
-    },
-    [commit, notifySuccess, refetch]
-  );
-
   const tableData = useMemo(() => {
-    return [...data.systemApiKeys];
+    return [...data.userApiKeys];
   }, [data]);
 
   type TableRow = (typeof tableData)[number];
@@ -107,16 +68,25 @@ export function SystemAPIKeysTable({
         accessorKey: "expiresAt",
         cell: TimestampCell,
       },
+      // TODO(parker): Do not render this column for non admins once https://github.com/Arize-ai/phoenix/issues/4454 is done
       {
         header: "",
         accessorKey: "id",
         size: 10,
-        cell: ({ row }) => {
+        cell: () => {
           return (
             <Flex direction="row" justifyContent="end" width="100%">
               <DeleteAPIKeyButton
                 handleDelete={() => {
-                  handleDelete(row.original.id);
+                  // TODO(parker): implement handle delete when https://github.com/Arize-ai/phoenix/issues/4059 is done
+                  startTransition(() => {
+                    refetch(
+                      {},
+                      {
+                        fetchPolicy: "network-only",
+                      }
+                    );
+                  });
                 }}
               />
             </Flex>
@@ -128,7 +98,7 @@ export function SystemAPIKeysTable({
       },
     ];
     return cols;
-  }, [handleDelete]);
+  }, [refetch]);
   const table = useReactTable<TableRow>({
     columns,
     data: tableData,
