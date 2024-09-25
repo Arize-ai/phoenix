501c5c148b93bacd04350678b6d0ac95749637f6
Parker Stafford <parker.stafford92@gmail.com>
2024-09-25 13:34:18 -0700

    remove comment

--- app/playwright.config.ts
+++ app/playwright.config.ts
@@ -19,8 +19,6 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  // workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
