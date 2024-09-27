300de770a79777d23154796e1644503e5e0e5c88
Mikyo King <mikyo@arize.com>
2024-09-26 18:54:23 -0700

    feat: Playground UI prototype (#4778)
    * docs: initial playground PRD
    
    * feat: playground prototype
    
    * add rudamentary messages UI
    
    * WIP
    
    * routing
    
    * add a playground button
    
    * cleanup
    
    * cleanup routes
    
    * Update app/src/pages/playground/PlaygroundTemplate.tsx
    
    * Update app/src/pages/playground/spanPlaygroundPageLoader.ts
    
    * cleanup store

---
+++ app/src/pages/playground/PlaygroundInstance.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
+import { css } from "@emotion/react";
+
+import {
+  compactResizeHandleCSS,
+  resizeHandleCSS,
+} from "@phoenix/components/resize";
+import { usePlaygroundContext } from "@phoenix/contexts/PlaygroundContext";
+
+import { PlaygroundInput } from "./PlaygroundInput";
+import { PlaygroundOutput } from "./PlaygroundOutput";
+import { PlaygroundTemplate } from "./PlaygroundTemplate";
+import { PlaygroundTools } from "./PlaygroundTools";
+import { PlaygroundInstanceProps } from "./types";
+
+const panelContentCSS = css`
+  padding: var(--ac-global-dimension-size-200);
+  overflow: auto;
+  display: flex;
+  flex-direction: column;
+  gap: var(--ac-global-dimension-size-200);
+`;
+
+export function PlaygroundInstance(props: PlaygroundInstanceProps) {
+  const numInstances = usePlaygroundContext((state) => state.instances.length);
+  const isSingleInstance = numInstances == 1;
+  return (
+    <PanelGroup direction={isSingleInstance ? "horizontal" : "vertical"}>
+      <Panel defaultSize={50} order={1} css={panelContentCSS}>
+        <PlaygroundTemplate {...props} />
+        <PlaygroundTools />
+      </Panel>
+      <PanelResizeHandle
+        css={isSingleInstance ? resizeHandleCSS : compactResizeHandleCSS}
+      />
+      <Panel defaultSize={50} order={2} css={panelContentCSS}>
+        <PlaygroundInput />
+        <PlaygroundOutput />
+      </Panel>
+    </PanelGroup>
+  );
+}
---
+++ app/src/pages/playground/SpanPlaygroundPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+// import { useLoaderData } from "react-router";
+import { Playground } from "./Playground";
+
+export function SpanPlaygroundPage() {
+  // const data = useLoaderData();
+
+  return <Playground />;
+}
