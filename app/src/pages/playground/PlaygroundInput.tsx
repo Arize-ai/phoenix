5c1008f09da9cecdeec8d653a1e37a6c79a9a207
Mikyo King <mikyo@arize.com>
2024-09-26 17:12:31 -0700

    feat: playground prototype

--- app/src/pages/playground/PlaygroundInput.tsx
+++ app/src/pages/playground/PlaygroundInput.tsx
@@ -2,9 +2,16 @@ import React from "react";
 
 import { Card } from "@arizeai/components";
 
+import { PlaygroundInputTypeTypeRadioGroup } from "./PlaygroundInputModeRadioGroup";
+
 export function PlaygroundInput() {
   return (
-    <Card title="Input" collapsible variant="compact">
+    <Card
+      title="Input"
+      collapsible
+      variant="compact"
+      extra={<PlaygroundInputTypeTypeRadioGroup />}
+    >
       Input goes here
     </Card>
   );
--- app/src/pages/playground/PlaygroundTemplate.tsx
+++ app/src/pages/playground/PlaygroundTemplate.tsx
@@ -1,18 +1,66 @@
 import React from "react";
 
-import { Card } from "@arizeai/components";
+import { Button, Card, Icon, Icons } from "@arizeai/components";
 
 import { usePlaygroundContext } from "@phoenix/contexts/PlaygroundContext";
 
-export function PlaygroundTemplate() {
-  const operationType = usePlaygroundContext((state) => state.operationType);
+import { NUM_MAX_PLAYGROUND_INSTANCES } from "./constants";
+import { PlaygroundInstanceProps } from "./types";
+
+interface PlaygroundTemplateProps extends PlaygroundInstanceProps {}
+
+export function PlaygroundTemplate(props: PlaygroundTemplateProps) {
+  const id = props.playgroundInstanceId;
+  // TODO: remove the hard coding of the first instance
+  const instances = usePlaygroundContext((state) => state.instances);
+  const playground = instances.find((instance) => instance.id === id);
+  if (!playground) {
+    throw new Error(`Playground instance ${id} not found`);
+  }
+  const { template } = playground;
+
   return (
-    <Card title="Template" collapsible variant="compact">
-      {operationType === "chat" ? (
-        <div>Chat Template goes here</div>
-      ) : (
-        <div>Completion Template goes here</div>
-      )}
+    <Card
+      title="Template"
+      collapsible
+      variant="compact"
+      extra={
+        instances.length >= NUM_MAX_PLAYGROUND_INSTANCES ? (
+          <DeleteButton {...props} />
+        ) : (
+          <CompareButton />
+        )
+      }
+    >
+      {template.__type === "chat" ? "Chat Template" : "Completion Template"}
     </Card>
+  );
+}
+
+function CompareButton() {
+  const addInstance = usePlaygroundContext((state) => state.addInstance);
+  return (
+    <Button
+      variant="default"
+      size="compact"
+      icon={<Icon svg={<Icons.ArrowCompareOutline />} />}
+      onClick={() => {
+        addInstance();
+      }}
+    />
+  );
+}
+
+function DeleteButton(props: PlaygroundInstanceProps) {
+  const deleteInstance = usePlaygroundContext((state) => state.deleteInstance);
+  return (
+    <Button
+      variant="default"
+      size="compact"
+      icon={<Icon svg={<Icons.TrashOutline />} />}
+      onClick={() => {
+        deleteInstance(props.playgroundInstanceId);
+      }}
+    />
   );
 }
