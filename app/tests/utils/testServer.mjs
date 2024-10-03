b3796bcafb32965e7cf5eb4daf29918ee3153690
Roger Yang <roger.yang@arize.com>
2024-10-02 20:35:05 -0700

    clean up

--- app/tests/utils/testServer.mjs
+++ app/tests/utils/testServer.mjs
@@ -10,19 +10,25 @@ import process from "process";
 const appPrefix = "phoenix";
 const tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), appPrefix));
 
+const args = process.argv.slice(2);
+args.forEach((arg) => {
+  if (arg === "--use-in-memory-sqlite") {
+    process.env["PHOENIX_SQL_DATABASE_URL"] = "sqlite:///:memory:";
+    console.log(
+      "PHOENIX_SQL_DATABASE_URL: ",
+      process.env["PHOENIX_SQL_DATABASE_URL"]
+    );
+  }
+});
+
 // Set environment variables for the server
 process.env["PHOENIX_WORKING_DIR"] = tmpDir;
 process.env["PHOENIX_ENABLE_AUTH"] = "True";
 process.env["PHOENIX_SECRET"] = crypto.randomBytes(32).toString("hex");
-process.env["PHOENIX_SQL_DATABASE_URL"] = "sqlite:///:memory:";
 
 console.log("Phoenix test server starting...");
 console.log("PHOENIX_SECRET: ", process.env["PHOENIX_SECRET"]);
 console.log("PHOENIX_WORKING_DIR: ", process.env["PHOENIX_WORKING_DIR"]);
-console.log(
-  "PHOENIX_SQL_DATABASE_URL: ",
-  process.env["PHOENIX_SQL_DATABASE_URL"]
-);
 
 // Execute the server
 // TODO: Might have to force install the phoenix server
