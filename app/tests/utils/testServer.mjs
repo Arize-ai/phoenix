ed901f6c0d0aabef85e98bf17d30676815e837af
Roger Yang <roger.yang@arize.com>
2024-10-02 19:31:50 -0700

    ci: in-memory sqlite for playwright

--- app/tests/utils/testServer.mjs
+++ app/tests/utils/testServer.mjs
@@ -14,10 +14,15 @@ const tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), appPrefix));
 process.env["PHOENIX_WORKING_DIR"] = tmpDir;
 process.env["PHOENIX_ENABLE_AUTH"] = "True";
 process.env["PHOENIX_SECRET"] = crypto.randomBytes(32).toString("hex");
+process.env["PHOENIX_SQL_DATABASE_URL"] = "sqlite:///:memory:";
 
 console.log("Phoenix test server starting...");
 console.log("PHOENIX_SECRET: ", process.env["PHOENIX_SECRET"]);
 console.log("PHOENIX_WORKING_DIR: ", process.env["PHOENIX_WORKING_DIR"]);
+console.log(
+  "PHOENIX_SQL_DATABASE_URL: ",
+  process.env["PHOENIX_SQL_DATABASE_URL"]
+);
 
 // Execute the server
 // TODO: Might have to force install the phoenix server
