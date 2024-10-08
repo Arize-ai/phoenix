6091c19bf186b830eb52a313a2a8e0cd87f30b3f
Parker Stafford <52351508+Parker-Stafford@users.noreply.github.com>
2024-10-08 15:51:37 -0700

    feat(playground): parse span attribute `llm.input_messages` for playground span replay (#4906)
    * begin building zod schemas for llm attributes
    
    * feat(playground): parse span input messages
    
    * move to utils, update role
    
    * update initial instance id
    
    * update id generation, add todo, add test files
    
    * add tests
    
    * memoize
    
    * move jest-canvas-mock to dev dependencies
    
    * update span not found error message
    
    * parse roles as strings and corece to ChatMessageRole
    
    * update ChatRoleMap comment
    
    * fix typos
    
    * update prop type to be InitialPlaygroundState
    
    * update naming
    
    * fix naming conflict

---
+++ app/src/pages/playground/__tests__/fixtures.ts
@@ -0,0 +1,40 @@
+import { PlaygroundSpan } from "../spanPlaygroundPageLoader";
+
+export const basePlaygroundSpan: PlaygroundSpan = {
+  __typename: "Span",
+  context: {
+    spanId: "test",
+  },
+  attributes: "",
+};
+export const spanAttributesWithInputMessages = {
+  llm: {
+    output_messages: [
+      {
+        message: {
+          content: "This is an AI Answer",
+          role: "assistant",
+        },
+      },
+    ],
+    model_name: "gpt-3.5-turbo",
+    token_count: { completion: 9.0, prompt: 1881.0, total: 1890.0 },
+    input_messages: [
+      {
+        message: {
+          content: "You are a chatbot",
+          role: "system",
+        },
+      },
+      {
+        message: {
+          content: "Anser me the following question. Are you sentient?",
+          role: "user",
+        },
+      },
+    ],
+    invocation_parameters:
+      '{"context_window": 16384, "num_output": -1, "is_chat_model": true, "is_function_calling_model": true, "model_name": "gpt-3.5-turbo"}',
+  },
+  openinference: { span: { kind: "LLM" } },
+} as const;
