0f0676116742ab5eb9c70412fb4c281d28893f1f
Mikyo King <mikyo@arize.com>
2024-10-08 18:16:44 -0600

    refactor(playground): move run, test input mode selection the top bar

--- app/src/pages/playground/Playground.tsx
+++ app/src/pages/playground/Playground.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 
-import { Button, Flex, Heading, View } from "@arizeai/components";
+import { Button, Flex, Heading, Icon, Icons, View } from "@arizeai/components";
 
 import { resizeHandleCSS } from "@phoenix/components/resize";
 import {
@@ -10,8 +10,9 @@ import {
 } from "@phoenix/contexts/PlaygroundContext";
 import { InitialPlaygroundState } from "@phoenix/store";
 
+import { PlaygroundInputTypeTypeRadioGroup } from "./PlaygroundInputModeRadioGroup";
 import { PlaygroundInstance } from "./PlaygroundInstance";
-import { PlaygroundOperationTypeRadioGroup } from "./PlaygroundOperationTypeRadioGroup";
+import { PlaygroundRunButton } from "./PlaygroundRunButton";
 
 export function Playground(props: InitialPlaygroundState) {
   return (
@@ -21,14 +22,19 @@ export function Playground(props: InitialPlaygroundState) {
         borderBottomWidth="thin"
         padding="size-200"
       >
-        <Flex direction="row" justifyContent="space-between">
-          <View>
-            <Flex direction="row" gap="size-200" alignItems="center">
-              <Heading level={1}>Playground</Heading>
-              <PlaygroundOperationTypeRadioGroup />
-            </Flex>
-          </View>
-          <Button variant="default">API Keys</Button>
+        <Flex
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Heading level={1}>Playground</Heading>
+          <Flex direction="row" gap="size-100" alignItems="center">
+            <PlaygroundInputTypeTypeRadioGroup />
+            <Button variant="default" size="compact">
+              API Keys
+            </Button>
+            <PlaygroundRunButton />
+          </Flex>
         </Flex>
       </View>
       <PlaygroundInstances />
