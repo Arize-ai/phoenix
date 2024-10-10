240bfe8277f94b812ceb4a4ede46b88a85aa273b
Mikyo King <mikyo@arize.com>
2024-10-10 17:19:56 -0600

    feat(playground): allow up to 4 instances, distinguish alphabetically (#4951)
    * WIP
    
    * feat(playground): allow up to 4 playground instances, distinguish alphabetically

--- app/src/pages/playground/PlaygroundTools.tsx
+++ app/src/pages/playground/PlaygroundTools.tsx
@@ -2,9 +2,24 @@ import React from "react";
 
 import { Card } from "@arizeai/components";
 
-export function PlaygroundTools() {
+import { usePlaygroundContext } from "@phoenix/contexts/PlaygroundContext";
+
+import { TitleWithAlphabeticIndex } from "./TitleWithAlphabeticIndex";
+import { PlaygroundInstanceProps } from "./types";
+
+interface PlaygroundToolsProps extends PlaygroundInstanceProps {}
+export function PlaygroundTools(props: PlaygroundToolsProps) {
+  const index = usePlaygroundContext((state) =>
+    state.instances.findIndex(
+      (instance) => instance.id === props.playgroundInstanceId
+    )
+  );
   return (
-    <Card title="Tools" collapsible variant="compact">
+    <Card
+      title={<TitleWithAlphabeticIndex index={index} title="Tools" />}
+      collapsible
+      variant="compact"
+    >
       Tools go here
     </Card>
   );
