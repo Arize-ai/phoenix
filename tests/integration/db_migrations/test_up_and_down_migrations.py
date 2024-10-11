4f6f36f13f07dfaf0eea2a841fbb2d4b0add1a03
Roger Yang <roger.yang@arize.com>
2024-10-11 08:22:30 -0700

    fix error matching for postgresql

--- tests/integration/db_migrations/test_up_and_down_migrations.py
+++ tests/integration/db_migrations/test_up_and_down_migrations.py
@@ -10,7 +10,7 @@ def test_up_and_down_migrations(
 ) -> None:
     stmt = text("SELECT version_num FROM alembic_version")
     with engine.connect() as conn:
-        with pytest.raises(BaseException, match="no such table: alembic_version"):
+        with pytest.raises(BaseException, match="alembic_version"):
             conn.execute(stmt)
     with engine.connect() as conn:
         alembic_config.attributes["connection"] = conn
