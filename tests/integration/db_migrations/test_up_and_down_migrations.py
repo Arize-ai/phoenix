32e396242dc6f0cb9f25308bd45f3201b4690791
Roger Yang <roger.yang@arize.com>
2024-10-11 08:19:39 -0700

    clean up

--- tests/integration/db_migrations/test_up_and_down_migrations.py
+++ tests/integration/db_migrations/test_up_and_down_migrations.py
@@ -1,3 +1,4 @@
+import pytest
 from alembic import command
 from alembic.config import Config
 from sqlalchemy import Engine, text
@@ -7,16 +8,18 @@ def test_up_and_down_migrations(
     alembic_config: Config,
     engine: Engine,
 ) -> None:
+    stmt = text("SELECT version_num FROM alembic_version")
+    with engine.connect() as conn:
+        with pytest.raises(BaseException, match="no such table: alembic_version"):
+            conn.execute(stmt)
     with engine.connect() as conn:
         alembic_config.attributes["connection"] = conn
         command.upgrade(alembic_config, "head")
-    stmt = text("SELECT version_num FROM alembic_version")
     with engine.connect() as conn:
-        version_num = conn.execute(stmt).fetchone()
+        version_num = conn.execute(stmt).first()
         assert version_num == ("cd164e83824f",)
     with engine.connect() as conn:
         alembic_config.attributes["connection"] = conn
         command.downgrade(alembic_config, "base")
     with engine.connect() as conn:
-        version_num = conn.execute(stmt).fetchone()
-        assert version_num is None
+        assert conn.execute(stmt).first() is None
