bac88e8c0d0893581776172fab37096ba9f2db29
Mikyo King <mikyo@arize.com>
2024-10-14 11:05:47 -0600

    final changes

--- app/src/pages/playground/Playground.tsx
+++ app/src/pages/playground/Playground.tsx
@@ -84,127 +84,110 @@ function AddPromptButton() {
   );
 }
 
+const playgroundPromptPanelContentCSS = css`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  overflow: hidden;
+  .ac-accordion {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    overflow: hidden;
+    flex: 1 1 auto;
+    .ac-accordion-item {
+      height: 100%;
+      overflow: hidden;
+      flex: 1 1 auto;
+      .ac-accordion-itemContent {
+        height: 100%;
+        overflow: hidden;
+        flex: 1 1 auto;
+        & > * {
+          height: 100%;
+          flex: 1 1 auto;
+          overflow: auto;
+          box-sizing: border-box;
+          // Fix padding issue with flexbox
+          padding-bottom: 57px !important;
+        }
+      }
+    }
+  }
+`;
+
+const playgroundInputOutputPanelContentCSS = css`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  overflow: scroll;
+`;
+
 function PlaygroundContent() {
   const instances = usePlaygroundContext((state) => state.instances);
   const numInstances = instances.length;
   const isSingleInstance = numInstances === 1;
 
-  if (isSingleInstance) {
-    return <SingleInstancePlayground />;
-  } else {
-    return <MultiInstancePlayground />;
-  }
-}
-
-function SingleInstancePlayground() {
-  const instances = usePlaygroundContext((state) => state.instances);
-  const instanceId = instances[0].id;
   return (
-    <PanelGroup direction="horizontal" autoSaveId="playground-single">
-      <Panel defaultSize={50}>
-        <div
-          css={css`
-            display: flex;
-            flex-direction: column;
-            height: 100%;
-            overflow: hidden;
-            .ac-accordion {
-              display: flex;
-              flex-direction: column;
-              height: 100%;
-              overflow: hidden;
-              flex: 1 1 auto;
-              .ac-accordion-item {
-                height: 100%;
-                overflow: hidden;
-                flex: 1 1 auto;
-                .ac-accordion-itemContent {
-                  height: 100%;
-                  overflow: hidden;
-                  flex: 1 1 auto;
-                  & > * {
-                    height: 100%;
-                    flex: 1 1 auto;
-                    overflow: auto;
-                    box-sizing: border-box;
-                    // Fix padding issue with flexbox
-                    padding-bottom: 57px !important;
-                  }
-                }
-              }
-            }
-          `}
-        >
+    <PanelGroup
+      direction={isSingleInstance ? "horizontal" : "vertical"}
+      autoSaveId={
+        isSingleInstance ? "playground-horizontal" : "playground-vertical"
+      }
+    >
+      <Panel>
+        <div css={playgroundPromptPanelContentCSS}>
           <Accordion>
             <AccordionItem
               title="Prompts"
               id="prompts"
               extra={<AddPromptButton />}
             >
-              <View padding="size-200" height="100%">
-                <PlaygroundTemplate playgroundInstanceId={instanceId} />
+              <View height="100%" padding="size-200">
+                <Flex direction="row" gap="size-100">
+                  {instances.map((instance, i) => (
+                    <div
+                      key={i}
+                      css={css`
+                        flex: 1 1 0px;
+                      `}
+                    >
+                      <PlaygroundTemplate
+                        key={i}
+                        playgroundInstanceId={instance.id}
+                      />
+                    </div>
+                  ))}
+                </Flex>
               </View>
             </AccordionItem>
           </Accordion>
         </div>
       </Panel>
       <PanelResizeHandle css={resizeHandleCSS} />
-      <Panel defaultSize={50}>
-        <Accordion>
-          <AccordionItem title="Inputs" id="input">
-            <View padding="size-200">Input goes here</View>
-          </AccordionItem>
-          <AccordionItem title="Output" id="output">
-            <View padding="size-200" height="100%">
-              <PlaygroundOutput playgroundInstanceId={instanceId} />
-            </View>
-          </AccordionItem>
-        </Accordion>
+      <Panel>
+        <div css={playgroundInputOutputPanelContentCSS}>
+          <Accordion>
+            <AccordionItem title="Inputs" id="input">
+              <View padding="size-200">Inputs go here</View>
+            </AccordionItem>
+            <AccordionItem title="Output" id="output">
+              <View padding="size-200" height="100%">
+                <Flex direction="row" gap="size-200">
+                  {instances.map((instance, i) => (
+                    <View key={i} flex="1 1 0px">
+                      <PlaygroundOutput
+                        key={i}
+                        playgroundInstanceId={instance.id}
+                      />
+                    </View>
+                  ))}
+                </Flex>
+              </View>
+            </AccordionItem>
+          </Accordion>
+        </div>
       </Panel>
     </PanelGroup>
-  );
-}
-
-function MultiInstancePlayground() {
-  const instances = usePlaygroundContext((state) => state.instances);
-  return (
-    <Accordion>
-      <AccordionItem title="Prompts" id="prompts" extra={<AddPromptButton />}>
-        <View height="100%" padding="size-200">
-          <Flex direction="row" gap="size-100">
-            {instances.map((instance, i) => (
-              <div
-                key={i}
-                css={css`
-                  flex: 1 1 0px;
-                  max-height: 500px;
-                  overflow-y: auto;
-                  box-sizing: border-box;
-                `}
-              >
-                <PlaygroundTemplate
-                  key={i}
-                  playgroundInstanceId={instance.id}
-                />
-              </div>
-            ))}
-          </Flex>
-        </View>
-      </AccordionItem>
-      <AccordionItem title="Inputs" id="input">
-        <View padding="size-200">Inputs go here</View>
-      </AccordionItem>
-      <AccordionItem title="Output" id="output">
-        <View padding="size-200" height="100%">
-          <Flex direction="row" gap="size-200">
-            {instances.map((instance, i) => (
-              <View key={i} flex="1 1 0px">
-                <PlaygroundOutput key={i} playgroundInstanceId={instance.id} />
-              </View>
-            ))}
-          </Flex>
-        </View>
-      </AccordionItem>
-    </Accordion>
   );
 }
