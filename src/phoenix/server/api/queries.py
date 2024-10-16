1e3e7724be721d9856f46b262213a4f1c7a09e01
Mikyo King <mikyo@arize.com>
2024-10-16 09:55:23 -0600

    Update src/phoenix/server/api/queries.py
    Co-authored-by: Xander Song <axiomofjoy@gmail.com>

--- src/phoenix/server/api/queries.py
+++ src/phoenix/server/api/queries.py
@@ -130,29 +130,7 @@ class Query:
             "gpt-3.5-turbo-1106",
             "gpt-3.5-turbo-instruct",
         ]
-        azure_openai_models = [
-            "o1-preview",
-            "o1-preview-2024-09-12",
-            "o1-mini",
-            "o1-mini-2024-09-12",
-            "gpt-4o",
-            "gpt-4o-2024-08-06",
-            "gpt-4o-2024-05-13",
-            "chatgpt-4o-latest",
-            "gpt-4o-mini",
-            "gpt-4o-mini-2024-07-18",
-            "gpt-4-turbo",
-            "gpt-4-turbo-2024-04-09",
-            "gpt-4-turbo-preview",
-            "gpt-4-0125-preview",
-            "gpt-4-1106-preview",
-            "gpt-4",
-            "gpt-4-0613",
-            "gpt-3.5-turbo-0125",
-            "gpt-3.5-turbo",
-            "gpt-3.5-turbo-1106",
-            "gpt-3.5-turbo-instruct",
-        ]
+        azure_openai_models = []
         anthropic_models = [
             "claude-3-5-sonnet-20240620",
             "claude-3-opus-20240229",
