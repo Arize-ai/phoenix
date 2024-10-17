9934df447f6ac16a645980a8cbb44c5eefdcb185
Alexander Song <axiomofjoy@gmail.com>
2024-10-16 21:49:46 -0700

    refactor into subclasses

--- src/phoenix/utilities/template_formatters.py
+++ src/phoenix/utilities/template_formatters.py
@@ -1,39 +1,45 @@
 import re
+from abc import ABC, abstractmethod
 from string import Formatter
-from typing import Any, Literal, Set
-
-from typing_extensions import assert_never
-
-PATTERN = re.compile(r"{{\s*(\w+)\s*}}")
+from typing import Any, Iterable, Set
 
 
-class TemplateFormatter:
-    def __init__(self, template: str, template_language: Literal["mustache", "f-string"]) -> None:
-        self._template = template
-        self._template_language = template_language
-
+class TemplateFormatter(ABC):
+    @abstractmethod
     def parse(self, template: str) -> Set[str]:
-        if self._template_language == "mustache":
-            return set(match for match in re.findall(PATTERN, template))
-        if self._template_language == "f-string":
-            return set(
-                field_name for _, field_name, _, _ in Formatter().parse(template) if field_name
-            )
-        assert_never(self._template_language)
+        raise NotImplementedError
 
-    def format(self, **kwargs: Any) -> str:
-        template_variable_names = self.parse(self._template)
+    def format(self, template: str, **kwargs: Any) -> str:
+        template_variable_names = self.parse(template)
         if missing_template_variables := template_variable_names - set(kwargs.keys()):
             raise ValueError(f"Missing template variables: {', '.join(missing_template_variables)}")
-        if self._template_language == "mustache":
-            template = self._template
-            for template_variable_name in template_variable_names:
-                template = re.sub(
-                    rf"{{{{\s*{template_variable_name}\s*}}}}",
-                    kwargs[template_variable_name],
-                    template,
-                )
-            return template
-        if self._template_language == "f-string":
-            return self._template.format(**kwargs)
-        assert_never(self._template_language)
+        return self._format(template, template_variable_names, **kwargs)
+
+    @abstractmethod
+    def _format(self, template: str, variable_names: Iterable[str], **kwargs: Any) -> str:
+        raise NotImplementedError
+
+
+class FStringTemplateFormatter(TemplateFormatter):
+    def parse(self, template: str) -> Set[str]:
+        return set(field_name for _, field_name, _, _ in Formatter().parse(template) if field_name)
+
+    def _format(self, template: str, variable_names: Iterable[str], **kwargs: Any) -> str:
+        return template.format(**kwargs)
+
+
+class MustacheTemplateFormatter(TemplateFormatter):
+    PATTERN = re.compile(r"{{\s*(\w+)\s*}}")
+
+    def parse(self, template: str) -> Set[str]:
+        return set(match for match in re.findall(self.PATTERN, template))
+
+    def _format(self, template: str, variable_names: Iterable[str], **kwargs: Any) -> str:
+        variables = kwargs
+        for variable_name in variable_names:
+            template = re.sub(
+                pattern=rf"{{{{\s*{variable_name}\s*}}}}",
+                repl=variables[variable_name],
+                string=template,
+            )
+        return template
--- tests/utilities/test_template_formatters.py
+++ tests/utilities/test_template_formatters.py
@@ -2,68 +2,88 @@ from typing import Any, Dict
 
 import pytest
 
-from phoenix.utilities.template_formatters import TemplateFormatter
+from phoenix.utilities.template_formatters import (
+    FStringTemplateFormatter,
+    MustacheTemplateFormatter,
+    TemplateFormatter,
+)
 
 
 @pytest.mark.parametrize(
-    "template_language, template, variables, expected_prompt",
+    "formatter_cls, template, variables, expected_prompt",
     (
         pytest.param(
-            "mustache",
+            MustacheTemplateFormatter,
             "{{ hello }}",
             {"hello": "world"},
             "world",
             id="mustache-whitespace-both-sides",
         ),
         pytest.param(
-            "mustache", "{{hello}}", {"hello": "world"}, "world", id="nmustache-o-whitespace"
+            MustacheTemplateFormatter,
+            "{{hello}}",
+            {"hello": "world"},
+            "world",
+            id="nmustache-o-whitespace",
         ),
         pytest.param(
-            "mustache", "{{  hello}}", {"hello": "world"}, "world", id="mustache-whitespace-left"
+            MustacheTemplateFormatter,
+            "{{  hello}}",
+            {"hello": "world"},
+            "world",
+            id="mustache-whitespace-left",
         ),
         pytest.param(
-            "mustache", "{{hello }}", {"hello": "world"}, "world", id="mustache-whitespace-right"
+            MustacheTemplateFormatter,
+            "{{hello }}",
+            {"hello": "world"},
+            "world",
+            id="mustache-whitespace-right",
         ),
         pytest.param(
-            "mustache",
+            MustacheTemplateFormatter,
             "{{ hello }}, {{ world }}",
             {"hello": "1", "world": "2"},
             "1, 2",
             id="mustache-multiple-variables",
         ),
         pytest.param(
-            "mustache",
+            MustacheTemplateFormatter,
             "{{ hello }} + {{hello}} = {{ world }}",
             {"hello": "1", "world": "2"},
             "1 + 1 = 2",
             id="mustache-duplicate-variables",
         ),
         pytest.param(
-            "mustache",
+            MustacheTemplateFormatter,
             "{{ hello }}, {{ world }}",
             {"hello": "world", "world": "hello"},
             "world, hello",
             id="mustache-replaced-value-is-variable-name",
         ),
         pytest.param(
-            "f-string", "{hello}", {"hello": "world"}, "world", id="f-string-single-variable"
+            FStringTemplateFormatter,
+            "{hello}",
+            {"hello": "world"},
+            "world",
+            id="f-string-single-variable",
         ),
         pytest.param(
-            "f-string",
+            FStringTemplateFormatter,
             "{hello}, {world}",
             {"hello": "1", "world": "2"},
             "1, 2",
             id="f-string-multiple-variables",
         ),
         pytest.param(
-            "f-string",
+            FStringTemplateFormatter,
             "{hello} + {hello} = {world}",
             {"hello": "1", "world": "2"},
             "1 + 1 = 2",
             id="f-string-duplicate-variables",
         ),
         pytest.param(
-            "f-string",
+            FStringTemplateFormatter,
             "{hello}, {world}",
             {"hello": "world", "world": "hello"},
             "world, hello",
@@ -72,31 +92,31 @@ from phoenix.utilities.template_formatters import TemplateFormatter
     ),
 )
 def test_template_formatter(
-    template_language: str, template: str, variables: Dict[str, Any], expected_prompt: str
+    formatter_cls: TemplateFormatter, template: str, variables: Dict[str, Any], expected_prompt: str
 ) -> None:
-    formatter = TemplateFormatter(template, template_language)
-    prompt = formatter.format(**variables)
+    formatter = formatter_cls()
+    prompt = formatter.format(template, **variables)
     assert prompt == expected_prompt
 
 
 @pytest.mark.parametrize(
-    "template_language, template",
+    "formatter_cls, template",
     (
         pytest.param(
-            "mustache",
+            MustacheTemplateFormatter,
             "{{ hello }}",
             id="mustache-missing-template-variables",
         ),
         pytest.param(
-            "f-string",
+            FStringTemplateFormatter,
             "{hello}",
             id="f-string-missing-template-variable",
         ),
     ),
 )
 def test_template_formatter_raises_expected_error_on_missing_variables(
-    template_language: str, template: str
+    formatter_cls: TemplateFormatter, template: str
 ) -> None:
-    formatter = TemplateFormatter(template, template_language)
+    formatter = formatter_cls()
     with pytest.raises(ValueError, match="Missing template variables"):
-        formatter.format()
+        formatter.format(template)
