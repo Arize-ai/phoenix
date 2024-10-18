3aa19355c0e8cc57b53f5eee5daab8911138e367
Parker Stafford <parker.stafford92@gmail.com>
2024-10-18 10:39:43 -0700

    get tool cals working for non tool roles

---
+++ app/src/pages/playground/ToolToggleButton.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+import {
+  Button,
+  Icon,
+  Icons,
+  Tooltip,
+  TooltipTrigger,
+} from "@arizeai/components";
+
+import { ToolOutline } from "./ToolOutline";
+
+export function ToolToggleButton({
+  hasTools,
+  onClick,
+}: {
+  hasTools: boolean;
+  onClick: () => void;
+}) {
+  return (
+    <div>
+      <TooltipTrigger delay={0} offset={5}>
+        <Button
+          icon={
+            hasTools ? (
+              <Icon svg={<Icons.MinusOutline />} />
+            ) : (
+              <Icon svg={<ToolOutline />} />
+            )
+          }
+          aria-label={hasTools ? "Remove tool calls" : "Add tool calls"}
+          title={hasTools ? "Remove tool calls" : "Add tool calls"}
+          size="compact"
+          variant="default"
+          onClick={onClick}
+        />
+        <Tooltip>{hasTools ? "Remove tool calls" : "Add tool calls"}</Tooltip>
+      </TooltipTrigger>
+    </div>
+  );
+}
