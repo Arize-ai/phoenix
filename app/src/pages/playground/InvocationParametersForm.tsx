403ee75c169dd846c9335c4864fec151cfe5d1e4
Tony Powell <apowell@arize.com>
2024-10-18 13:50:33 -0400

    Generate invocation parameters form based on provider/model schema

--- app/src/pages/playground/InvocationParametersForm.tsx
+++ app/src/pages/playground/InvocationParametersForm.tsx
@@ -1,8 +1,153 @@
 import React from "react";
+import { z } from "zod";
 
 import { Flex, Slider, TextField } from "@arizeai/components";
 
 import { ModelConfig } from "@phoenix/store";
+import { schemaForType } from "@phoenix/typeUtils";
+
+import { InvocationParameters } from "./__generated__/PlaygroundOutputSubscription.graphql";
+
+/**
+ * Model invocation parameters schema in zod.
+ *
+ * Includes all keys besides toolChoice
+ */
+const invocationParameterSchema = schemaForType<InvocationParameters>()(
+  z.object({
+    temperature: z.number().optional(),
+    topP: z.number().optional(),
+    maxTokens: z.number().optional(),
+    stop: z.array(z.string()).optional(),
+    seed: z.number().optional(),
+    maxCompletionTokens: z.number().optional(),
+  })
+);
+
+type InvocationParametersSchema = z.infer<typeof invocationParameterSchema>;
+
+/**
+ * Default set of invocation parameters for all providers and models.
+ */
+const baseInvocationParameterSchema = invocationParameterSchema.omit({
+  maxCompletionTokens: true,
+});
+
+type BaseInvocationParameters = z.infer<typeof baseInvocationParameterSchema>;
+
+/**
+ * Invocation parameters for O1 models.
+ */
+const o1BaseInvocationParameterSchema = baseInvocationParameterSchema
+  .extend({
+    maxCompletionTokens: z.number().optional(),
+  })
+  .omit({ maxTokens: true });
+
+/**
+ * Provider schemas for all models and optionally for a specific model.
+ */
+const providerSchemas = {
+  OPENAI: {
+    default: baseInvocationParameterSchema,
+    "o1-preview": o1BaseInvocationParameterSchema,
+    "o1-preview-2024-09-12": o1BaseInvocationParameterSchema,
+    "o1-mini": o1BaseInvocationParameterSchema,
+    "o1-mini-2024-09-12": o1BaseInvocationParameterSchema,
+  },
+  AZURE_OPENAI: {
+    default: baseInvocationParameterSchema,
+  },
+  ANTHROPIC: {
+    default: baseInvocationParameterSchema,
+  },
+} satisfies Record<
+  ModelProvider,
+  Record<string, z.ZodType<BaseInvocationParameters>>
+>;
+
+/**
+ * Form field for a single invocation parameter.
+ *
+ * TODO(apowell): Should this be generic over the schema field data type? There
+ * probably aren't enough fields for that to be worthwhile at the moment.
+ */
+const FormField = ({
+  field,
+  value,
+  onChange,
+}: {
+  field: keyof InvocationParametersSchema;
+  value: InvocationParametersSchema[keyof InvocationParametersSchema];
+  onChange: (
+    value: InvocationParametersSchema[keyof InvocationParametersSchema]
+  ) => void;
+}) => {
+  switch (field) {
+    case "temperature":
+      if (typeof value !== "number" && value !== undefined) return null;
+      return (
+        <Slider
+          label="Temperature"
+          value={value}
+          step={0.1}
+          minValue={0}
+          maxValue={2}
+          onChange={(value) => onChange(value)}
+        />
+      );
+    case "topP":
+      if (typeof value !== "number" && value !== undefined) return null;
+      return (
+        <Slider
+          label="Top P"
+          value={value}
+          step={0.1}
+          minValue={0}
+          maxValue={1}
+          onChange={(value) => onChange(value)}
+        />
+      );
+    case "maxCompletionTokens":
+      return (
+        <TextField
+          label="Max Completion Tokens"
+          value={value?.toString() || ""}
+          type="number"
+          onChange={(value) => onChange(Number(value))}
+        />
+      );
+    case "maxTokens":
+      return (
+        <TextField
+          label="Max Tokens"
+          value={value?.toString() || ""}
+          type="number"
+          onChange={(value) => onChange(Number(value))}
+        />
+      );
+    case "stop":
+      if (!Array.isArray(value) && value !== undefined) return null;
+      return (
+        <TextField
+          label="Stop"
+          defaultValue={value?.join(", ") || ""}
+          onChange={(value) => onChange(value.split(/, */g))}
+        />
+      );
+    case "seed":
+      return (
+        <TextField
+          label="Seed"
+          value={value?.toString() || ""}
+          type="number"
+          onChange={(value) => onChange(Number(value))}
+        />
+      );
+    default:
+      return null;
+  }
+};
 
 export type InvocationParametersChangeHandler = <
   T extends keyof ModelConfig["invocationParameters"],
@@ -21,56 +166,33 @@ export const InvocationParametersForm = ({
   onChange,
 }: InvocationParametersFormProps) => {
   const { invocationParameters, provider, modelName } = model;
+  // Get the schema for the incoming provider and model combination.
+  const schema =
+    providerSchemas?.[provider]?.[
+      (modelName || "default") as keyof (typeof providerSchemas)[ModelProvider]
+    ] ?? providerSchemas[provider].default;
+
+  // TODO(apowell): Should we instead fail open here and display all inputs?
+  const valid = schema.safeParse(invocationParameters);
+  if (!valid.success) {
+    return null;
+  }
+  // Generate form fields for all invocation parameters, constrained by the schema.
+  const parameters = valid.data;
+  const fieldsForSchema = Object.keys(schema.shape).map((field) => {
+    const fieldKey = field as keyof typeof parameters;
+    return (
+      <FormField
+        key={fieldKey}
+        field={fieldKey}
+        value={parameters[fieldKey]}
+        onChange={(value) => onChange(fieldKey, value)}
+      />
+    );
+  });
   return (
     <Flex direction="column" gap="size-200">
-      <Slider
-        label="Temperature"
-        value={invocationParameters.temperature || 0}
-        step={0.1}
-        minValue={0}
-        maxValue={2}
-        onChange={(value) => onChange("temperature", value)}
-      />
-      <Slider
-        label="Top P"
-        value={invocationParameters.topP || 0}
-        step={0.1}
-        minValue={0}
-        maxValue={1}
-        onChange={(value) => onChange("topP", value)}
-      />
-      {/* TODO(apowell): there's likely a better way. Do we maintain a map of provider:model to form field overrides? */}
-      {modelName?.startsWith("o1") && provider === "OPENAI" ? (
-        <TextField
-          label="Max Completion Tokens"
-          value={invocationParameters.maxCompletionTokens?.toString() || ""}
-          type="number"
-          onChange={(value) => onChange("maxCompletionTokens", Number(value))}
-          description="The maximum number of tokens to generate"
-        />
-      ) : (
-        <TextField
-          label="Max Tokens"
-          value={invocationParameters.maxTokens?.toString() || ""}
-          type="number"
-          onChange={(value) => onChange("maxTokens", Number(value))}
-          description="The maximum number of tokens to generate"
-        />
-      )}
-      {/* TODO(apowell): Combobox / Tag input? Bespoke add/remove menu? */}
-      <TextField
-        label="Stop"
-        defaultValue={invocationParameters.stop?.join(", ") || ""}
-        onChange={(value) => onChange("stop", value.split(/, */g))}
-        description="Comma-separated list of tokens to stop on"
-      />
-      <TextField
-        label="Seed"
-        value={invocationParameters.seed?.toString() || ""}
-        type="number"
-        onChange={(value) => onChange("seed", Number(value))}
-        description="Any integer that can be reused to produce semi-deterministic outputs"
-      />
+      {fieldsForSchema}
     </Flex>
   );
 };
