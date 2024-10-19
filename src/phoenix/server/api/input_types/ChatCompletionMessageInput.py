3d76f84c294c8cde6ef0884e3188dff9897eb031
Parker Stafford <parker.stafford92@gmail.com>
2024-10-18 17:30:58 -0700

    plumb through tool calls to server

--- src/phoenix/server/api/input_types/ChatCompletionMessageInput.py
+++ src/phoenix/server/api/input_types/ChatCompletionMessageInput.py
@@ -1,4 +1,7 @@
+from typing import List, Optional
+
 import strawberry
+from strawberry import UNSET
 from strawberry.scalars import JSON
 
 from phoenix.server.api.types.ChatCompletionMessageRole import ChatCompletionMessageRole
@@ -8,5 +11,9 @@ from phoenix.server.api.types.ChatCompletionMessageRole import ChatCompletionMes
 class ChatCompletionMessageInput:
     role: ChatCompletionMessageRole
     content: JSON = strawberry.field(
-        description="The content of the message as JSON to support text and tools",
+        description="The content of the message as JSON to support various kinds of text"
     )
+    tool_calls: Optional[List[JSON]] = UNSET
+    """The tool calls that were made in the message"""
+    tool_call_id: Optional[str] = UNSET
+    """The ID of the tool call that was made in a prior message"""
