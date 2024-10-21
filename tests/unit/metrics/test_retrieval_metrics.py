989ea2c86a20f1ce05e3943b61214583a4e5515b
Roger Yang <roger.yang@arize.com>
2024-10-21 12:44:04 -0700

    fix unit/metrics/test_retrieval_metrics.py

--- tests/unit/metrics/test_retrieval_metrics.py
+++ tests/unit/metrics/test_retrieval_metrics.py
@@ -2,7 +2,7 @@ from typing import List, Optional
 
 import numpy as np
 import pytest
-from sklearn.metrics import ndcg_score
+from sklearn.metrics import ndcg_score  # type: ignore[import-untyped]
 
 from phoenix.metrics.retrieval_metrics import RetrievalMetrics
 
