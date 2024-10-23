1e829fe4d41e89d9c29f3ba0f74c0aa3b5e6407e
Xander Song <axiomofjoy@gmail.com>
2024-10-22 19:07:39 -0700

    test(playground): add api test for anthropic (#5159)

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -109,6 +109,13 @@ def openai_api_key(monkeypatch: pytest.MonkeyPatch) -> str:
     return api_key
 
 
+@pytest.fixture
+def anthropic_api_key(monkeypatch: pytest.MonkeyPatch) -> str:
+    api_key = "sk-0123456789"
+    monkeypatch.setenv("ANTHROPIC_API_KEY", api_key)
+    return api_key
+
+
 postgresql_connection = factories.postgresql("postgresql_proc")
 
 
