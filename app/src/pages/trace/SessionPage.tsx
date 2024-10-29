aed81f870ff451273f546bd4eee29a45498b65fb
Parker Stafford <52351508+Parker-Stafford@users.noreply.github.com>
2024-10-29 15:50:17 -0700

    feat(sesions): add session details page (#5225)
    * feat(sesions): add session details page
    
    * remove unused import, add error boundary
    
    * pull user from root span

--- app/src/pages/trace/SessionPage.tsx
+++ app/src/pages/trace/SessionPage.tsx
@@ -1,14 +1,18 @@
 import React from "react";
-import { useNavigate, useParams } from "react-router";
+import { useLoaderData, useNavigate, useParams } from "react-router";
 
 import { Dialog, DialogContainer } from "@arizeai/components";
 
+import { ErrorBoundary } from "@phoenix/components/ErrorBoundary";
+
+import { sessionLoaderQuery$data } from "./__generated__/sessionLoaderQuery.graphql";
 import { SessionDetails } from "./SessionDetails";
 
 /**
  * A component that shows the details of a session
  */
 export function SessionPage() {
+  const loaderData = useLoaderData() as sessionLoaderQuery$data;
   const { sessionId, projectId } = useParams();
   const navigate = useNavigate();
   return (
@@ -17,8 +21,13 @@ export function SessionPage() {
       isDismissable
       onDismiss={() => navigate(`/projects/${projectId}`)}
     >
-      <Dialog size="fullscreen" title="Session Details">
-        <SessionDetails sessionId={sessionId as string} />
+      <Dialog
+        size="fullscreen"
+        title={`Session ID: ${loaderData.session?.sessionId ?? "--"}`}
+      >
+        <ErrorBoundary>
+          <SessionDetails sessionId={sessionId as string} />
+        </ErrorBoundary>
       </Dialog>
     </DialogContainer>
   );
