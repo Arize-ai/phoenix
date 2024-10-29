b8f38fc09452d97e93dcd748a3f08ebe2299a9f8
Roger Yang <80478925+RogerHYang@users.noreply.github.com>
2024-10-29 15:44:51 -0700

    refactor(sessions): add dataloaders for session queries (#5222)

--- tests/unit/server/api/types/test_Trace.py
+++ tests/unit/server/api/types/test_Trace.py
@@ -1,22 +1,23 @@
-from typing import Any
+from typing import Any, NamedTuple
 
 import httpx
 import pytest
 from strawberry.relay import GlobalID
-from typing_extensions import TypeAlias
 
 from phoenix.db import models
 from phoenix.server.api.types.ProjectSession import ProjectSession
+from phoenix.server.api.types.Span import Span
 from phoenix.server.api.types.Trace import Trace
 from phoenix.server.types import DbSessionFactory
 
-from ...._helpers import _add_project, _add_project_session, _add_trace, _node
+from ...._helpers import _add_project, _add_project_session, _add_span, _add_trace, _node
 
-_Data: TypeAlias = tuple[
-    list[models.Trace],
-    list[models.ProjectSession],
-    list[models.Project],
-]
+
+class _Data(NamedTuple):
+    spans: list[models.Span]
+    traces: list[models.Trace]
+    project_sessions: list[models.ProjectSession]
+    projects: list[models.Project]
 
 
 class TestTrace:
@@ -36,21 +37,45 @@ class TestTrace:
     @pytest.fixture
     async def _data(self, db: DbSessionFactory) -> _Data:
         traces = []
+        spans = []
         async with db() as session:
             project = await _add_project(session)
             project_session = await _add_project_session(session, project)
             traces.append(await _add_trace(session, project))
             traces.append(await _add_trace(session, project, project_session))
-        return traces, [project_session], [project]
+            spans.append(await _add_span(session, traces[-1]))
+            spans.append(await _add_span(session, traces[-1], parent_span=spans[-1]))
+        return _Data(
+            spans=spans,
+            traces=traces,
+            project_sessions=[project_session],
+            projects=[project],
+        )
 
     async def test_session(
         self,
         _data: _Data,
         httpx_client: httpx.AsyncClient,
     ) -> None:
-        traces = _data[0]
-        project_session = _data[1][0]
-        assert await self._node("session{id}", traces[0], httpx_client) is None
-        assert await self._node("session{id}", traces[1], httpx_client) == {
-            "id": str(GlobalID(ProjectSession.__name__, str(project_session.id)))
+        traces = _data.traces
+        project_session = _data.project_sessions[0]
+        field = "session{id sessionId}"
+        assert await self._node(field, traces[0], httpx_client) is None
+        assert await self._node(field, traces[1], httpx_client) == {
+            "id": str(GlobalID(ProjectSession.__name__, str(project_session.id))),
+            "sessionId": project_session.session_id,
+        }
+
+    async def test_root_span(
+        self,
+        _data: _Data,
+        httpx_client: httpx.AsyncClient,
+    ) -> None:
+        traces = _data.traces
+        span = _data.spans[0]
+        field = "rootSpan{id name}"
+        assert await self._node(field, traces[0], httpx_client) is None
+        assert await self._node(field, traces[1], httpx_client) == {
+            "id": str(GlobalID(Span.__name__, str(span.id))),
+            "name": span.name,
         }
