144ec459614bbceb3f917a227983d6d46adfcb31
Parker Stafford <parker.stafford92@gmail.com>
2024-10-30 14:53:54 -0700

    revert dataloader changes for project span and trace latency

--- src/phoenix/server/api/types/Project.py
+++ src/phoenix/server/api/types/Project.py
@@ -128,7 +128,13 @@ class Project(Node):
         time_range: Optional[TimeRange] = UNSET,
     ) -> Optional[float]:
         return await info.context.data_loaders.latency_ms_quantile.load(
-            ("trace", self.id_attr, time_range, None, probability, None),
+            (
+                "trace",
+                self.id_attr,
+                time_range,
+                None,
+                probability,
+            ),
         )
 
     @strawberry.field
@@ -140,7 +146,13 @@ class Project(Node):
         filter_condition: Optional[str] = UNSET,
     ) -> Optional[float]:
         return await info.context.data_loaders.latency_ms_quantile.load(
-            ("span", self.id_attr, time_range, filter_condition, probability, None),
+            (
+                "span",
+                self.id_attr,
+                time_range,
+                filter_condition,
+                probability,
+            ),
         )
 
     @strawberry.field
