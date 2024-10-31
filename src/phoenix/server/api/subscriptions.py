394c8610e4ca6075813f27a99b6a7e6723048ce6
Dustin Ngo <dustin@arize.com>
2024-10-31 13:25:11 -0400

    Update tests for new InvocationParameter spec

--- src/phoenix/server/api/subscriptions.py
+++ src/phoenix/server/api/subscriptions.py
@@ -147,7 +147,7 @@ class GenerativeModelInput:
 class ChatCompletionInput:
     messages: list[ChatCompletionMessageInput]
     model: GenerativeModelInput
-    invocation_parameters: list[InvocationParameterInput]
+    invocation_parameters: list[InvocationParameterInput] = strawberry.field(default_factory=list)
     tools: Optional[list[JSONScalarType]] = UNSET
     template: Optional[TemplateOptions] = UNSET
     api_key: Optional[str] = strawberry.field(default=None)
