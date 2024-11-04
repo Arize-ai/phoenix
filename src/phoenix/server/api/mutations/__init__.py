ee8b295c5c678e50d2849d6100c8e45938781992
Parker Stafford <parker.stafford92@gmail.com>
2024-11-04 14:13:17 -0800

    update imports

--- src/phoenix/server/api/mutations/__init__.py
+++ src/phoenix/server/api/mutations/__init__.py
@@ -1,12 +1,12 @@
 import strawberry
 
 from phoenix.server.api.mutations.api_key_mutations import ApiKeyMutationMixin
+from phoenix.server.api.mutations.chat_mutations import (
+    ChatCompletionMutationMixin,
+)
 from phoenix.server.api.mutations.dataset_mutations import DatasetMutationMixin
 from phoenix.server.api.mutations.experiment_mutations import ExperimentMutationMixin
 from phoenix.server.api.mutations.export_events_mutations import ExportEventsMutationMixin
-from phoenix.server.api.mutations.playground_chat_mutations import (
-    PlaygroundChatCompletionMutationMixin,
-)
 from phoenix.server.api.mutations.project_mutations import ProjectMutationMixin
 from phoenix.server.api.mutations.span_annotations_mutations import SpanAnnotationMutationMixin
 from phoenix.server.api.mutations.trace_annotations_mutations import TraceAnnotationMutationMixin
@@ -23,6 +23,6 @@ class Mutation(
     SpanAnnotationMutationMixin,
     TraceAnnotationMutationMixin,
     UserMutationMixin,
-    PlaygroundChatCompletionMutationMixin,
+    ChatCompletionMutationMixin,
 ):
     pass
