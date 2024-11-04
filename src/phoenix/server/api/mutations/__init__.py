348fd4c17a65401f0c475c2c401e354a33a26102
Dustin Ngo <dustin@arize.com>
2024-11-04 17:28:09 -0500

    feat: Add chat completion mutation (#5255)
    * Add chat completion mutation
    
    * Clarify mutation name
    
    * Refactor output types
    
    * Improve payload output type
    
    * address feedback
    
    * update imports
    
    ---------
    
    Co-authored-by: Parker Stafford <parker.stafford92@gmail.com>

--- src/phoenix/server/api/mutations/__init__.py
+++ src/phoenix/server/api/mutations/__init__.py
@@ -1,6 +1,9 @@
 import strawberry
 
 from phoenix.server.api.mutations.api_key_mutations import ApiKeyMutationMixin
+from phoenix.server.api.mutations.chat_mutations import (
+    ChatCompletionMutationMixin,
+)
 from phoenix.server.api.mutations.dataset_mutations import DatasetMutationMixin
 from phoenix.server.api.mutations.experiment_mutations import ExperimentMutationMixin
 from phoenix.server.api.mutations.export_events_mutations import ExportEventsMutationMixin
@@ -20,5 +23,6 @@ class Mutation(
     SpanAnnotationMutationMixin,
     TraceAnnotationMutationMixin,
     UserMutationMixin,
+    ChatCompletionMutationMixin,
 ):
     pass
