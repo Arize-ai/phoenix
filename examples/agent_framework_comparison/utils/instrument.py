4a0fc7857d09a7cdc7f20a32ff624dfe8a1bd733
JohnGilhuly <jgilhuly@arize.com>
2024-11-11 17:53:02 -0800

    docs: add multi agent example projects to agent comparison repo (#5322)
    * Update requirements.txt
    
    * Add example db
    
    * Add crewai example
    
    * Add autogen example
    
    * Update instrument.py
    
    * Update requirements.txt

--- examples/agent_framework_comparison/utils/instrument.py
+++ examples/agent_framework_comparison/utils/instrument.py
@@ -4,7 +4,7 @@ from enum import Enum
 from openinference.instrumentation.langchain import LangChainInstrumentor
 from openinference.instrumentation.llama_index import LlamaIndexInstrumentor
 from openinference.instrumentation.openai import OpenAIInstrumentor
-
+from openinference.instrumentation.litellm import LiteLLMInstrumentor
 from phoenix.otel import register
 
 
@@ -12,6 +12,8 @@ class Framework(Enum):
     LLAMA_INDEX = "llama_index"
     LANGGRAPH = "langgraph"
     CODE_BASED = "code_based"
+    CREWAI = "crewai"
+    AUTOGEN = "autogen"
 
 
 def instrument(project_name="code-based-agent", framework=Framework.CODE_BASED):
@@ -26,5 +28,9 @@ def instrument(project_name="code-based-agent", framework=Framework.CODE_BASED):
         LlamaIndexInstrumentor().instrument(tracer_provider=tracer_provider)
     elif framework == Framework.LANGGRAPH:
         LangChainInstrumentor().instrument(tracer_provider=tracer_provider)
+    elif framework == Framework.CREWAI:
+        LiteLLMInstrumentor().instrument(tracer_provider=tracer_provider)
+    elif framework == Framework.AUTOGEN:
+        OpenAIInstrumentor().instrument(tracer_provider=tracer_provider)
     else:
         OpenAIInstrumentor().instrument(tracer_provider=tracer_provider)
