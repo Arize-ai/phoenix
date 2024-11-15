a5c0e7be7547dc65ccc0c6e4c46e252fab6bdd14
Alexander Song <axiomofjoy@gmail.com>
2024-11-15 01:41:01 -0800

    raise value errors

--- src/phoenix/config.py
+++ src/phoenix/config.py
@@ -561,10 +561,17 @@ def get_env_host() -> str:
 def get_env_host_root_path() -> str:
     if (host_root_path := os.getenv(ENV_PHOENIX_HOST_ROOT_PATH)) is None:
         return HOST_ROOT_PATH
-    host_root_path = host_root_path.strip("/")
-    if not host_root_path:
-        return HOST_ROOT_PATH
-    return "/" + host_root_path
+    if not host_root_path.startswith("/"):
+        raise ValueError(
+            f"Invalid value for environment variable {ENV_PHOENIX_HOST_ROOT_PATH}: "
+            f"{host_root_path}. Value must start with '/'"
+        )
+    if host_root_path.endswith("/"):
+        raise ValueError(
+            f"Invalid value for environment variable {ENV_PHOENIX_HOST_ROOT_PATH}: "
+            f"{host_root_path}. Value cannot end with '/'"
+        )
+    return host_root_path
 
 
 def get_env_collector_endpoint() -> Optional[str]:
