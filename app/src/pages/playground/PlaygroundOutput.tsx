93c7952360c815c4e39b53f884cc82c119dc4473
Parker Stafford <parker.stafford92@gmail.com>
2024-11-25 09:48:45 -0800

    remove logs

--- app/src/pages/playground/PlaygroundOutput.tsx
+++ app/src/pages/playground/PlaygroundOutput.tsx
@@ -292,7 +292,6 @@ export function PlaygroundOutput(props: PlaygroundOutputProps) {
         },
       });
       if (errors) {
-        console.log("test--", errors);
         notifyError({
           title: "Chat completion failed",
           message: errors[0].message,
@@ -359,7 +358,6 @@ export function PlaygroundOutput(props: PlaygroundOutputProps) {
             markPlaygroundInstanceComplete(props.playgroundInstanceId);
           },
           onError: (error) => {
-            console.log("test--", error);
             setLoading(false);
             markPlaygroundInstanceComplete(props.playgroundInstanceId);
             const errorMessages =
@@ -388,7 +386,6 @@ export function PlaygroundOutput(props: PlaygroundOutputProps) {
       onError(error) {
         setLoading(false);
         markPlaygroundInstanceComplete(props.playgroundInstanceId);
-        console.log("test--", error);
         const errorMessages = getErrorMessagesFromRelayMutationError(error);
         if (errorMessages != null && errorMessages.length > 0) {
           notifyError({
