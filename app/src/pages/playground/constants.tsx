315c73206ed000a3dee6e10a8d6b62d630a34d5e
Tony Powell <apowell@arize.com>
2024-11-26 16:46:58 -0500

    Fix type import

--- app/src/pages/playground/constants.tsx
+++ app/src/pages/playground/constants.tsx
@@ -1,4 +1,4 @@
-import { CanonicalParameterName } from "./__generated__/InvocationParametersFormFieldsQuery.graphql";
+import { CanonicalParameterName } from "./__generated__/ModelSupportedParamsFetcherQuery.graphql";
 
 export const NUM_MAX_PLAYGROUND_INSTANCES = 4;
 
