547394217c1e8dfd3677279f88f676b12b9bf743
Mikyo King <mikyo@arize.com>
2024-12-03 12:10:16 -0700

    feat(sessions): getting started guide (#5592)
    * feat(sessions): getting started guide
    
    * minimum working sessions example
    
    * add python getting started guide
    
    * add typescript guide
    
    * add tutorials
    
    * cleanup
    
    * fix format

--- app/src/pages/project/SessionsTableEmpty.tsx
+++ app/src/pages/project/SessionsTableEmpty.tsx
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { ReactNode, useState } from "react";
 import { css } from "@emotion/react";
 
-import { Flex } from "@arizeai/components";
+import {
+  Button,
+  Dialog,
+  DialogContainer,
+  Flex,
+  Icon,
+  Icons,
+  View,
+} from "@arizeai/components";
 
+import { CodeLanguage, CodeLanguageRadioGroup } from "@phoenix/components/code";
+
+import { PythonSessionsGuide } from "./PythonSessionsGuide";
+import { TypeScriptSessionsGuide } from "./TypeScriptSessionsGuide";
+
+function SetupSessionsDialog() {
+  const [language, setLanguage] = useState<CodeLanguage>("Python");
+  return (
+    <Dialog title="Setup Sessions for this Project" size="L">
+      <View padding="size-400" overflow="auto">
+        <View paddingBottom="size-100">
+          <CodeLanguageRadioGroup language={language} onChange={setLanguage} />
+        </View>
+        {language === "Python" ? (
+          <PythonSessionsGuide />
+        ) : (
+          <TypeScriptSessionsGuide />
+        )}
+      </View>
+    </Dialog>
+  );
+}
 export function SessionsTableEmpty() {
+  const [dialog, setDialog] = useState<ReactNode | null>(null);
+
+  const onGettingStartedClick = () => {
+    setDialog(<SetupSessionsDialog />);
+  };
+
   return (
     <tbody className="is-empty">
       <tr>
@@ -16,9 +52,23 @@ export function SessionsTableEmpty() {
         >
           <Flex direction="column" gap="size-200" alignItems="center">
             No sessions found for this project
+            <Button
+              variant="default"
+              icon={<Icon svg={<Icons.PlayCircleOutline />} />}
+              onClick={onGettingStartedClick}
+            >
+              Setup Sessions
+            </Button>
           </Flex>
         </td>
       </tr>
+      <DialogContainer
+        onDismiss={() => setDialog(null)}
+        isDismissable
+        type="slideOver"
+      >
+        {dialog}
+      </DialogContainer>
     </tbody>
   );
 }
