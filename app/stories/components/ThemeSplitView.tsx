91e6f40c9d084f59b432934c861d6a09f9291e85
Parker Stafford <parker.stafford92@gmail.com>
2024-12-04 21:19:49 -0800

    fix type

--- app/stories/components/ThemeSplitView.tsx
+++ app/stories/components/ThemeSplitView.tsx
@@ -6,9 +6,7 @@ export function ThemeSplitView({ children }: PropsWithChildren) {
   return (
     <>
       <ThemeToggleWrap key="dark">{children}</ThemeToggleWrap>
-      <ThemeToggleWrap defaultLight key="light">
-        {children}
-      </ThemeToggleWrap>
+      <ThemeToggleWrap key="light">{children}</ThemeToggleWrap>
     </>
   );
 }
