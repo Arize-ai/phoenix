c67ba679fba470001bb0f8d1602e385ed9f5a01b
Mikyo King <mikyo@arize.com>
2024-12-12 11:32:16 -0700

    feat(js): run experiment

---
+++ js/packages/phoenix-client/src/client.ts
@@ -0,0 +1,61 @@
+import createOpenApiClient, { ClientOptions } from "openapi-fetch";
+import type {
+  paths as oapiPathsV1,
+  components as oapiComponentsV1,
+  operations as oapiOperationsV1,
+} from "./__generated__/api/v1.d.ts";
+import {
+  defaultGetEnvironmentOptions,
+  makeDefaultClientOptions,
+} from "./config";
+
+type pathsV1 = oapiPathsV1;
+type componentsV1 = oapiComponentsV1;
+type operationsV1 = oapiOperationsV1;
+
+export type Types = {
+  V1: {
+    paths: pathsV1;
+    components: componentsV1;
+    operations: operationsV1;
+  };
+};
+
+/**
+ * Merge all configuration options according to priority:
+ * defaults < environment < explicit options
+ *
+ * Headers are simply replaced, not merged.
+ */
+export const getMergedOptions = ({
+  options = {},
+  getEnvironmentOptions = defaultGetEnvironmentOptions,
+}: {
+  options?: Partial<ClientOptions>;
+  getEnvironmentOptions?: () => Partial<ClientOptions>;
+} = {}): ClientOptions => {
+  const defaultOptions = makeDefaultClientOptions();
+  const environmentOptions = getEnvironmentOptions();
+  return {
+    ...defaultOptions,
+    ...environmentOptions,
+    ...options,
+  };
+};
+
+/**
+ * Create a Phoenix client.
+ *
+ * @param configuration - The configuration to use for the client.
+ * @param configuration.options - The options to use for the client's OpenAPI Fetch wrapper.
+ * @returns The Phoenix client.
+ */
+export const createClient = (
+  config: {
+    options?: Partial<ClientOptions>;
+    getEnvironmentOptions?: () => Partial<ClientOptions>;
+  } = {},
+) => {
+  const mergedOptions = getMergedOptions(config);
+  return createOpenApiClient<pathsV1>(mergedOptions);
+};
---
+++ js/packages/phoenix-client/src/experiments/runExperiment.ts
@@ -0,0 +1,10 @@
+// import { Client } from "../client";
+import { Task } from "../types/experiments";
+
+export type RunExperimentParams = {
+  task: Task;
+};
+
+export function runExperiment({ task }: RunExperimentParams) {
+  // TODO fill this out
+}
