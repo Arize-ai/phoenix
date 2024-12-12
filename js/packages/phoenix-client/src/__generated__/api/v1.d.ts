c3aba48c47fd9fccc4657dda5075b83f0ca8f276
Mikyo King <mikyo@arize.com>
2024-12-12 11:16:37 -0700

    feat(js): initial directories (#5671)
    * feat(js): initial directories
    
    * chore(ts): scaffold phoenix ts packages (#5701)
    
    * chore(ts): scaffold phoenix ts packages
    
    * add prettier and eslint and ci
    
    * fix(ts): remove explicit prebuild command
    
    * chore(js): add rudamentary types for experiments
    
    * add types
    
    * fix
    
    * Update js/packages/phoenix-client/src/types/experiments.ts
    
    Co-authored-by: Parker Stafford <52351508+Parker-Stafford@users.noreply.github.com>
    
    * feat(js): Produce ESM build artifacts
    
    * Delete unused package stubs
    
    * Bump baseline esm config target and configure aliasing
    
    * Add config tests
    
    Had to break out config merging logic so that it was testable
    
    ---------
    
    Co-authored-by: Parker Stafford <52351508+Parker-Stafford@users.noreply.github.com>
    Co-authored-by: Parker Stafford <parker.stafford92@gmail.com>
    Co-authored-by: Tony Powell <apowell@arize.com>

--- js/packages/phoenix-client/src/__generated__/api/v1.d.ts
+++ js/packages/phoenix-client/src/__generated__/api/v1.d.ts
@@ -193,6 +193,24 @@ export interface paths {
         patch?: never;
         trace?: never;
     };
+    "/v1/evaluations": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        /** Get span, trace, or document evaluations from a project */
+        get: operations["getEvaluations"];
+        put?: never;
+        /** Add span, trace, or document evaluations */
+        post: operations["addEvaluations"];
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
 }
 export type webhooks = Record<string, never>;
 export interface components {
@@ -1125,6 +1143,109 @@ export interface operations {
                 };
                 content: {
                     "application/json": components["schemas"]["HTTPValidationError"];
+                };
+            };
+        };
+    };
+    getEvaluations: {
+        parameters: {
+            query?: {
+                /** @description The name of the project to get evaluations from (if omitted, evaluations will be drawn from the `default` project) */
+                project_name?: string | null;
+            };
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        requestBody?: never;
+        responses: {
+            /** @description Successful Response */
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "application/json": unknown;
+                };
+            };
+            /** @description Forbidden */
+            403: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "text/plain": string;
+                };
+            };
+            /** @description Not Found */
+            404: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "text/plain": string;
+                };
+            };
+            /** @description Validation Error */
+            422: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "application/json": components["schemas"]["HTTPValidationError"];
+                };
+            };
+        };
+    };
+    addEvaluations: {
+        parameters: {
+            query?: never;
+            header?: {
+                "content-type"?: string | null;
+                "content-encoding"?: string | null;
+            };
+            path?: never;
+            cookie?: never;
+        };
+        requestBody: {
+            content: {
+                "application/x-protobuf": string;
+                "application/x-pandas-arrow": string;
+            };
+        };
+        responses: {
+            /** @description Successful Response */
+            204: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content?: never;
+            };
+            /** @description Forbidden */
+            403: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "text/plain": string;
+                };
+            };
+            /** @description Unsupported content type, only gzipped protobuf and pandas-arrow are supported */
+            415: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "text/plain": string;
+                };
+            };
+            /** @description Unprocessable Entity */
+            422: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "text/plain": string;
                 };
             };
         };
---
+++ js/packages/phoenix-client/src/types/annotations.ts
@@ -0,0 +1,1 @@
+export type AnnotatorKind = "HUMAN" | "LLM";
