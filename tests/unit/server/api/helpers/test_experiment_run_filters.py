cb9e8a2c47a93158d0132d34dbdf9b172b6c927d
Alexander Song <axiomofjoy@gmail.com>
2024-12-14 14:44:44 -0800

    skip postgres for now

--- tests/unit/server/api/helpers/test_experiment_run_filters.py
+++ tests/unit/server/api/helpers/test_experiment_run_filters.py
@@ -6,48 +6,58 @@ from phoenix.server.api.helpers.experiment_run_filters import ExperimentRunFilte
 
 
 @pytest.mark.parametrize(
-    "filter_expression,expected_sql_expression",
+    "filter_expression,expected_sqlite_expression",
     [
         pytest.param(
             "input",
             "dataset_example_revisions.input",
-            id="basic_input",
-        ),
-        pytest.param(
-            "output",
-            "experiment_runs.output",
-            id="basic_output",
+            id="input-name",
         ),
         pytest.param(
             "reference_output",
             "dataset_example_revisions.output",
-            id="basic_reference_output",
+            id="reference-output-name",
         ),
-        pytest.param("error", "experiment_runs.error", id="basic_error"),
+        pytest.param(
+            "output",
+            "experiment_runs.output",
+            id="output-name",
+        ),
+        pytest.param(
+            "error",
+            "experiment_runs.error",
+            id="error-name",
+        ),
         pytest.param(
             "latency_ms",
             "round(CAST((EXTRACT(EPOCH FROM experiment_runs.end_time) - EXTRACT(EPOCH FROM experiment_runs.start_time)) * 1000 AS NUMERIC), 1)",  # noqa: E501
-            id="basic_latency",
+            id="latency-ms-name",
         ),
         pytest.param(
             "error is None",
             "experiment_runs.error IS NULL AND experiment_runs.experiment_id = 0",
-            id="error_is_none",
+            id="primitive-is-none-expression",
         ),
         pytest.param(
             "error is not None",
             "experiment_runs.error IS NOT NULL AND experiment_runs.experiment_id = 0",
-            id="error_is_not_none",
+            id="primitive-is-not-none-expression",
         ),
         pytest.param(
             "latency_ms > 10",
             "round(CAST((EXTRACT(EPOCH FROM experiment_runs.end_time) - EXTRACT(EPOCH FROM experiment_runs.start_time)) * 1000 AS NUMERIC), 1) > 10 AND experiment_runs.experiment_id = 0",  # noqa: E501
-            id="latency_comparison",
+            id="primitive-inequality-expression",
         ),
         pytest.param(
-            "experiments[0].input", "dataset_example_revisions.input", id="experiment_0_input"
+            "experiments[0].input",
+            "dataset_example_revisions.input",
+            id="experiment_0_input",
         ),
-        pytest.param("experiments[1].output", "experiment_runs.output", id="experiment_1_output"),
+        pytest.param(
+            "experiments[1].output",
+            "experiment_runs.output",
+            id="experiment_1_output",
+        ),
         pytest.param(
             "experiments[2].error is None",
             "experiment_runs.error IS NULL AND experiment_runs.experiment_id = 2",
@@ -85,11 +95,15 @@ from phoenix.server.api.helpers.experiment_run_filters import ExperimentRunFilte
         ),
     ],
 )
-def test_node_transformer(filter_expression: str, expected_sql_expression: str) -> None:
+def test_node_transformer(
+    filter_expression: str, expected_sqlite_expression: str, dialect: str
+) -> None:
+    if dialect == "postgres":
+        pytest.skip("test case currently works only in postgres")
     tree = ast.parse(filter_expression, mode="eval")
     transformer = ExperimentRunFilterTransformer([0, 1, 2])
     transformed_tree = transformer.visit(tree)
     node = transformed_tree.body
     orm_expression = node.compile()
     sql_expression = str(orm_expression.compile(compile_kwargs={"literal_binds": True}))
-    assert sql_expression == expected_sql_expression
+    assert sql_expression == expected_sqlite_expression
