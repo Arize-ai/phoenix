1d614e31541b9a6c6dfab8cbc54fd4e6d1a209d2
Alexander Song <axiomofjoy@gmail.com>
2024-12-14 17:08:00 -0800

    support for an arbitrary number of boolean operands

--- tests/unit/server/api/helpers/test_experiment_run_filters.py
+++ tests/unit/server/api/helpers/test_experiment_run_filters.py
@@ -61,107 +61,132 @@ from phoenix.server.api.helpers.experiment_run_filters import ExperimentRunFilte
         pytest.param(
             "error is None",
             "experiment_runs.error IS NULL AND experiment_runs.experiment_id = 0",
-            id="primitive-is-none-expression",
+            id="is-none",
         ),
         pytest.param(
             "error is not None",
             "experiment_runs.error IS NOT NULL AND experiment_runs.experiment_id = 0",
-            id="primitive-is-not-none-expression",
+            id="is-not-none",
         ),
         pytest.param(
             "latency_ms > 1000",
             "round(CAST((EXTRACT(EPOCH FROM experiment_runs.end_time) - EXTRACT(EPOCH FROM experiment_runs.start_time)) * 1000 AS NUMERIC), 1) > 1000 AND experiment_runs.experiment_id = 0",  # noqa: E501
-            id="primitive-gt-expression",
+            id="gt",
         ),
         pytest.param(
             "1000 < latency_ms",
             "round(CAST((EXTRACT(EPOCH FROM experiment_runs.end_time) - EXTRACT(EPOCH FROM experiment_runs.start_time)) * 1000 AS NUMERIC), 1) > 1000 AND experiment_runs.experiment_id = 0",  # noqa: E501
-            id="primitive-gt-expression-reversed",
+            id="gt-reversed",
         ),
         pytest.param(
             "latency_ms >= 1000",
             "round(CAST((EXTRACT(EPOCH FROM experiment_runs.end_time) - EXTRACT(EPOCH FROM experiment_runs.start_time)) * 1000 AS NUMERIC), 1) >= 1000 AND experiment_runs.experiment_id = 0",  # noqa: E501
-            id="primitive-gte-expression",
+            id="gte",
         ),
         pytest.param(
             "1000 <= latency_ms",
             "round(CAST((EXTRACT(EPOCH FROM experiment_runs.end_time) - EXTRACT(EPOCH FROM experiment_runs.start_time)) * 1000 AS NUMERIC), 1) >= 1000 AND experiment_runs.experiment_id = 0",  # noqa: E501
-            id="primitive-gte-expression-reversed",
+            id="gte-reversed",
         ),
         pytest.param(
             "latency_ms < 1000",
             "round(CAST((EXTRACT(EPOCH FROM experiment_runs.end_time) - EXTRACT(EPOCH FROM experiment_runs.start_time)) * 1000 AS NUMERIC), 1) < 1000 AND experiment_runs.experiment_id = 0",  # noqa: E501
-            id="primitive-lt-expression",
+            id="lt",
         ),
         pytest.param(
             "1000 > latency_ms",
             "round(CAST((EXTRACT(EPOCH FROM experiment_runs.end_time) - EXTRACT(EPOCH FROM experiment_runs.start_time)) * 1000 AS NUMERIC), 1) < 1000 AND experiment_runs.experiment_id = 0",  # noqa: E501
-            id="primitive-lt-expression-reversed",
+            id="lt-reversed",
         ),
         pytest.param(
             "latency_ms <= 1000",
             "round(CAST((EXTRACT(EPOCH FROM experiment_runs.end_time) - EXTRACT(EPOCH FROM experiment_runs.start_time)) * 1000 AS NUMERIC), 1) <= 1000 AND experiment_runs.experiment_id = 0",  # noqa: E501
-            id="primitive-lte-expression",
+            id="lte",
         ),
         pytest.param(
             "1000 >= latency_ms",
             "round(CAST((EXTRACT(EPOCH FROM experiment_runs.end_time) - EXTRACT(EPOCH FROM experiment_runs.start_time)) * 1000 AS NUMERIC), 1) <= 1000 AND experiment_runs.experiment_id = 0",  # noqa: E501
-            id="primitive-lte-expression-reversed",
+            id="lte-reversed",
         ),
         pytest.param(
             "latency_ms == 1000",
             "round(CAST((EXTRACT(EPOCH FROM experiment_runs.end_time) - EXTRACT(EPOCH FROM experiment_runs.start_time)) * 1000 AS NUMERIC), 1) = 1000 AND experiment_runs.experiment_id = 0",  # noqa: E501
-            id="primitive-eq-expression",
+            id="eq",
         ),
         pytest.param(
             "1000 == latency_ms",
             "round(CAST((EXTRACT(EPOCH FROM experiment_runs.end_time) - EXTRACT(EPOCH FROM experiment_runs.start_time)) * 1000 AS NUMERIC), 1) = 1000 AND experiment_runs.experiment_id = 0",  # noqa: E501
-            id="primitive-eq-expression-reversed",
+            id="eq-reversed",
         ),
         pytest.param(
             "latency_ms != 1000",
             "round(CAST((EXTRACT(EPOCH FROM experiment_runs.end_time) - EXTRACT(EPOCH FROM experiment_runs.start_time)) * 1000 AS NUMERIC), 1) != 1000 AND experiment_runs.experiment_id = 0",  # noqa: E501
-            id="primitive-ne-expression",
+            id="ne",
         ),
         pytest.param(
             "1000 != latency_ms",
             "round(CAST((EXTRACT(EPOCH FROM experiment_runs.end_time) - EXTRACT(EPOCH FROM experiment_runs.start_time)) * 1000 AS NUMERIC), 1) != 1000 AND experiment_runs.experiment_id = 0",  # noqa: E501
-            id="primitive-ne-expression-reversed",
+            id="ne-reversed",
         ),
         pytest.param(
             "experiments[2].error is None",
             "experiment_runs.error IS NULL AND experiment_runs.experiment_id = 2",
-            id="experiment_2_error_is_none",
+            id="experiment-error-is-none",
         ),
         pytest.param(
-            "experiments[0].error is not None",
-            "experiment_runs.error IS NOT NULL AND experiment_runs.experiment_id = 0",
-            id="experiment_0_error_is_not_none",
+            "experiments[1].error is not None",
+            "experiment_runs.error IS NOT NULL AND experiment_runs.experiment_id = 1",
+            id="experiment-error-is-not-none",
         ),
         pytest.param(
             "experiments[1].latency_ms > 10",
             "round(CAST((EXTRACT(EPOCH FROM experiment_runs.end_time) - EXTRACT(EPOCH FROM experiment_runs.start_time)) * 1000 AS NUMERIC), 1) > 10 AND experiment_runs.experiment_id = 1",  # noqa: E501
-            id="experiment_1_latency_comparison",
+            id="experiment-latency-gt",
         ),
         pytest.param(
             "experiments[0].evals['Hallucination'].score > 0.5",
             "experiment_run_annotations.score > 0.5 AND experiment_runs.experiment_id = 0 AND experiment_run_annotations.name = 'Hallucination'",  # noqa: E501
-            id="experiment_0_hallucination_score",
+            id="experiment-hallucination-score-gt",
         ),
         pytest.param(
             "experiments[0].evals['Hallucination'].label == 'hallucinated'",
             "experiment_run_annotations.label = 'hallucinated' AND experiment_runs.experiment_id = 0 AND experiment_run_annotations.name = 'Hallucination'",  # noqa: E501
-            id="experiment_0_hallucination_label",
+            id="experiment-hallucination-label-eq",
+        ),
+        pytest.param(
+            "not experiments[0].evals['Hallucination'].label == 'hallucinated'",
+            "NOT (experiment_run_annotations.label = 'hallucinated' AND experiment_runs.experiment_id = 0 AND experiment_run_annotations.name = 'Hallucination')",  # noqa: E501
+            id="negation",
+        ),
+        pytest.param(
+            "experiments[0].evals['Hallucination'].score > 0.5 and latency_ms > 1000",
+            "experiment_run_annotations.score > 0.5 AND experiment_runs.experiment_id = 0 AND experiment_run_annotations.name = 'Hallucination' AND round(CAST((EXTRACT(EPOCH FROM experiment_runs.end_time) - EXTRACT(EPOCH FROM experiment_runs.start_time)) * 1000 AS NUMERIC), 1) > 1000 AND experiment_runs.experiment_id = 0",  # noqa: E501
+            id="conjunction",
+        ),
+        pytest.param(
+            "experiments[0].evals['Hallucination'].score > 0.5 and latency_ms > 1000 and experiments[1].error is None",  # noqa: E501
+            "experiment_run_annotations.score > 0.5 AND experiment_runs.experiment_id = 0 AND experiment_run_annotations.name = 'Hallucination' AND round(CAST((EXTRACT(EPOCH FROM experiment_runs.end_time) - EXTRACT(EPOCH FROM experiment_runs.start_time)) * 1000 AS NUMERIC), 1) > 1000 AND experiment_runs.experiment_id = 0 AND experiment_runs.error IS NULL AND experiment_runs.experiment_id = 1",  # noqa: E501
+            id="conjunction-of-three",
         ),
         pytest.param(
             "experiments[0].evals['Hallucination'].score > 0.5 or latency_ms > 1000",
             "experiment_run_annotations.score > 0.5 AND experiment_runs.experiment_id = 0 AND experiment_run_annotations.name = 'Hallucination' OR round(CAST((EXTRACT(EPOCH FROM experiment_runs.end_time) - EXTRACT(EPOCH FROM experiment_runs.start_time)) * 1000 AS NUMERIC), 1) > 1000 AND experiment_runs.experiment_id = 0",  # noqa: E501
-            id="complex_or_condition",
+            id="disjunction",
+        ),
+        pytest.param(
+            "experiments[0].evals['Hallucination'].score > 0.5 or latency_ms > 1000 or experiments[1].error is None",  # noqa: E501
+            "experiment_run_annotations.score > 0.5 AND experiment_runs.experiment_id = 0 AND experiment_run_annotations.name = 'Hallucination' OR round(CAST((EXTRACT(EPOCH FROM experiment_runs.end_time) - EXTRACT(EPOCH FROM experiment_runs.start_time)) * 1000 AS NUMERIC), 1) > 1000 AND experiment_runs.experiment_id = 0 OR experiment_runs.error IS NULL AND experiment_runs.experiment_id = 1",  # noqa: E501
+            id="disjunction-of-three",
+        ),
+        pytest.param(
+            "experiments[0].evals['Hallucination'].score > 0.5 or latency_ms > 1000 and experiments[1].error is None",  # noqa: E501
+            "experiment_run_annotations.score > 0.5 AND experiment_runs.experiment_id = 0 AND experiment_run_annotations.name = 'Hallucination' OR round(CAST((EXTRACT(EPOCH FROM experiment_runs.end_time) - EXTRACT(EPOCH FROM experiment_runs.start_time)) * 1000 AS NUMERIC), 1) > 1000 AND experiment_runs.experiment_id = 0 AND experiment_runs.error IS NULL AND experiment_runs.experiment_id = 1",  # noqa: E501
+            id="mixed-conjunction-and-disjunction-without-parentheses",
         ),
         pytest.param(
             "not (experiments[0].evals['Hallucination'].score > 0.5 or latency_ms > 1000)",
             "NOT (experiment_run_annotations.score > 0.5 AND experiment_runs.experiment_id = 0 AND experiment_run_annotations.name = 'Hallucination' OR round(CAST((EXTRACT(EPOCH FROM experiment_runs.end_time) - EXTRACT(EPOCH FROM experiment_runs.start_time)) * 1000 AS NUMERIC), 1) > 1000 AND experiment_runs.experiment_id = 0)",  # noqa: E501
-            id="complex_not_condition",
+            id="complex-negation",
         ),
     ],
 )
