cfa9b1daa0739d43b1e31700517429d9996e2047
Mikyo King <mikyo@arize.com>
2024-12-16 10:25:22 -0800

    feat(promts): skeleton UI for promts (#5726)
    * wip
    
    * WIP
    
    * WIP
    
    * add the prompt type
    
    * add initial listing
    
    * feat: temporary prompt
    
    * final changes
    
    * add aria role

---
+++ app/src/pages/prompt/__generated__/promptLoaderQuery.graphql.ts
@@ -0,0 +1,112 @@
+/**
+ * @generated SignedSource<<d4e02cae69ff9b96b268be86c66b40ee>>
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+// @ts-nocheck
+
+import { ConcreteRequest, Query } from 'relay-runtime';
+export type promptLoaderQuery$variables = {
+  id: string;
+};
+export type promptLoaderQuery$data = {
+  readonly prompt: {
+    readonly __typename: string;
+    readonly id: string;
+    readonly name?: string;
+  };
+};
+export type promptLoaderQuery = {
+  response: promptLoaderQuery$data;
+  variables: promptLoaderQuery$variables;
+};
+
+const node: ConcreteRequest = (function(){
+var v0 = [
+  {
+    "defaultValue": null,
+    "kind": "LocalArgument",
+    "name": "id"
+  }
+],
+v1 = [
+  {
+    "alias": "prompt",
+    "args": [
+      {
+        "kind": "Variable",
+        "name": "id",
+        "variableName": "id"
+      }
+    ],
+    "concreteType": null,
+    "kind": "LinkedField",
+    "name": "node",
+    "plural": false,
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "kind": "ScalarField",
+        "name": "__typename",
+        "storageKey": null
+      },
+      {
+        "alias": null,
+        "args": null,
+        "kind": "ScalarField",
+        "name": "id",
+        "storageKey": null
+      },
+      {
+        "kind": "InlineFragment",
+        "selections": [
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "name",
+            "storageKey": null
+          }
+        ],
+        "type": "Prompt",
+        "abstractKey": null
+      }
+    ],
+    "storageKey": null
+  }
+];
+return {
+  "fragment": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "promptLoaderQuery",
+    "selections": (v1/*: any*/),
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Operation",
+    "name": "promptLoaderQuery",
+    "selections": (v1/*: any*/)
+  },
+  "params": {
+    "cacheID": "a08f26b93468d63c0a76176105703d87",
+    "id": null,
+    "metadata": {},
+    "name": "promptLoaderQuery",
+    "operationKind": "query",
+    "text": "query promptLoaderQuery(\n  $id: GlobalID!\n) {\n  prompt: node(id: $id) {\n    __typename\n    id\n    ... on Prompt {\n      name\n    }\n  }\n}\n"
+  }
+};
+})();
+
+(node as any).hash = "2be55248f208b8502c9abc178078a721";
+
+export default node;
