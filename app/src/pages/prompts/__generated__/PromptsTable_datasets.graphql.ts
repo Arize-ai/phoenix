68fb57d0434e19cea75ca5f650a6b8e22e30e170
Mikyo King <mikyo@arize.com>
2024-12-16 10:25:22 -0800

    feat(promts): skeleton UI for promts (#5726)
    * wip
    
    * WIP
    
    * WIP
    
    * add the prompt type
    
    * add initial listing
    
    * feat: temporary prompt
    
    * final changes
    
    * add aria role

---
+++ app/src/pages/prompts/__generated__/PromptsTable_datasets.graphql.ts
@@ -0,0 +1,183 @@
+/**
+ * @generated SignedSource<<38dc69229eba228c19dba71256351314>>
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+// @ts-nocheck
+
+import { ReaderFragment, RefetchableFragment } from 'relay-runtime';
+import { FragmentRefs } from "relay-runtime";
+export type PromptsTable_datasets$data = {
+  readonly prompts: {
+    readonly edges: ReadonlyArray<{
+      readonly node: {
+        readonly createdAt: string;
+        readonly description: string | null;
+        readonly id: string;
+        readonly name: string;
+      };
+    }>;
+  };
+  readonly " $fragmentType": "PromptsTable_datasets";
+};
+export type PromptsTable_datasets$key = {
+  readonly " $data"?: PromptsTable_datasets$data;
+  readonly " $fragmentSpreads": FragmentRefs<"PromptsTable_datasets">;
+};
+
+import PromptsTablePromptsQuery_graphql from './PromptsTablePromptsQuery.graphql';
+
+const node: ReaderFragment = (function(){
+var v0 = [
+  "prompts"
+];
+return {
+  "argumentDefinitions": [
+    {
+      "defaultValue": null,
+      "kind": "LocalArgument",
+      "name": "after"
+    },
+    {
+      "defaultValue": 100,
+      "kind": "LocalArgument",
+      "name": "first"
+    }
+  ],
+  "kind": "Fragment",
+  "metadata": {
+    "connection": [
+      {
+        "count": "first",
+        "cursor": "after",
+        "direction": "forward",
+        "path": (v0/*: any*/)
+      }
+    ],
+    "refetch": {
+      "connection": {
+        "forward": {
+          "count": "first",
+          "cursor": "after"
+        },
+        "backward": null,
+        "path": (v0/*: any*/)
+      },
+      "fragmentPathInResult": [],
+      "operation": PromptsTablePromptsQuery_graphql
+    }
+  },
+  "name": "PromptsTable_datasets",
+  "selections": [
+    {
+      "alias": "prompts",
+      "args": null,
+      "concreteType": "PromptConnection",
+      "kind": "LinkedField",
+      "name": "__PromptsTable_prompts_connection",
+      "plural": false,
+      "selections": [
+        {
+          "alias": null,
+          "args": null,
+          "concreteType": "PromptEdge",
+          "kind": "LinkedField",
+          "name": "edges",
+          "plural": true,
+          "selections": [
+            {
+              "alias": null,
+              "args": null,
+              "concreteType": "Prompt",
+              "kind": "LinkedField",
+              "name": "node",
+              "plural": false,
+              "selections": [
+                {
+                  "alias": null,
+                  "args": null,
+                  "kind": "ScalarField",
+                  "name": "id",
+                  "storageKey": null
+                },
+                {
+                  "alias": null,
+                  "args": null,
+                  "kind": "ScalarField",
+                  "name": "name",
+                  "storageKey": null
+                },
+                {
+                  "alias": null,
+                  "args": null,
+                  "kind": "ScalarField",
+                  "name": "description",
+                  "storageKey": null
+                },
+                {
+                  "alias": null,
+                  "args": null,
+                  "kind": "ScalarField",
+                  "name": "createdAt",
+                  "storageKey": null
+                },
+                {
+                  "alias": null,
+                  "args": null,
+                  "kind": "ScalarField",
+                  "name": "__typename",
+                  "storageKey": null
+                }
+              ],
+              "storageKey": null
+            },
+            {
+              "alias": null,
+              "args": null,
+              "kind": "ScalarField",
+              "name": "cursor",
+              "storageKey": null
+            }
+          ],
+          "storageKey": null
+        },
+        {
+          "alias": null,
+          "args": null,
+          "concreteType": "PageInfo",
+          "kind": "LinkedField",
+          "name": "pageInfo",
+          "plural": false,
+          "selections": [
+            {
+              "alias": null,
+              "args": null,
+              "kind": "ScalarField",
+              "name": "endCursor",
+              "storageKey": null
+            },
+            {
+              "alias": null,
+              "args": null,
+              "kind": "ScalarField",
+              "name": "hasNextPage",
+              "storageKey": null
+            }
+          ],
+          "storageKey": null
+        }
+      ],
+      "storageKey": null
+    }
+  ],
+  "type": "Query",
+  "abstractKey": null
+};
+})();
+
+(node as any).hash = "1e8e93aca29806102e8ad65cd86a74f1";
+
+export default node;
