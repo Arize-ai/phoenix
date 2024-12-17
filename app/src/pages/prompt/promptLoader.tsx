f30c34da7000f6596654dfddd53110e4c06588f1
Mikyo King <mikyo@arize.com>
2024-12-17 12:24:01 -0800

    feat(prompts): rudimentary prompt details page #5741 (#5752)
    * cleanup
    
    * feat(prompts): rudimentary prompt details page #5741
    
    * Update app/src/pages/prompt/PromptTabContent.tsx
    
    Co-authored-by: Anthony Powell <apowell@arize.com>
    
    ---------
    
    Co-authored-by: Anthony Powell <apowell@arize.com>

--- app/src/pages/prompt/promptLoader.tsx
+++ app/src/pages/prompt/promptLoader.tsx
@@ -19,6 +19,7 @@ export async function promptLoader(args: LoaderFunctionArgs) {
           id
           ... on Prompt {
             name
+            ...PromptTabContent__main
           }
         }
       }
