e13813250d5051881c2143c117e6c4bebd977442
Mikyo King <mikyo@arize.com>
2024-12-17 12:24:01 -0800

    feat(prompts): rudimentary prompt details page #5741 (#5752)
    * cleanup
    
    * feat(prompts): rudimentary prompt details page #5741
    
    * Update app/src/pages/prompt/PromptTabContent.tsx
    
    Co-authored-by: Anthony Powell <apowell@arize.com>
    
    ---------
    
    Co-authored-by: Anthony Powell <apowell@arize.com>

--- app/src/pages/prompt/PromptPage.tsx
+++ app/src/pages/prompt/PromptPage.tsx
@@ -1,22 +1,67 @@
 import React from "react";
 import { useLoaderData, useNavigate } from "react-router";
+import { css } from "@emotion/react";
 
-import { Button, Flex, Heading, Icon, Icons, View } from "@arizeai/components";
+import {
+  Button,
+  Counter,
+  Flex,
+  Heading,
+  Icon,
+  Icons,
+  TabPane,
+  Tabs,
+  View,
+} from "@arizeai/components";
 
 import { promptLoaderQuery$data } from "./__generated__/promptLoaderQuery.graphql";
+import { PromptTabContent } from "./PromptTabContent";
+
+const mainCSS = css`
+  flex: 1 1 auto;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+  .ac-tabs {
+    flex: 1 1 auto;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    div[role="tablist"] {
+      flex: none;
+    }
+    .ac-tabs__pane-container {
+      flex: 1 1 auto;
+      display: flex;
+      flex-direction: column;
+      overflow: hidden;
+      div[role="tabpanel"]:not([hidden]) {
+        flex: 1 1 auto;
+        display: flex;
+        flex-direction: column;
+        overflow: hidden;
+      }
+    }
+  }
+`;
 
 export function PromptPage() {
   const loaderData = useLoaderData() as promptLoaderQuery$data;
   const navigate = useNavigate();
   return (
-    <Flex direction="column" height="100%">
+    <main css={mainCSS}>
       <View
-        padding="size-200"
-        borderBottomWidth="thin"
-        borderBottomColor="dark"
+        paddingStart="size-200"
+        paddingEnd="size-200"
+        paddingTop="size-100"
+        paddingBottom="size-100"
         flex="none"
       >
-        <Flex direction="row" justifyContent="space-between">
+        <Flex
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
           <Heading level={1}>{loaderData.prompt.name}</Heading>
           <Button
             variant="default"
@@ -30,6 +75,14 @@ export function PromptPage() {
           </Button>
         </Flex>
       </View>
-    </Flex>
+      <Tabs>
+        <TabPane name={"Prompt"}>
+          <PromptTabContent prompt={loaderData.prompt} />
+        </TabPane>
+        <TabPane name={"Versions"} extra={<Counter>0</Counter>}>
+          Versions
+        </TabPane>
+      </Tabs>
+    </main>
   );
 }
