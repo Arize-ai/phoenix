f599292f60c7c6494f259778c2cf39aed9abe42c
Anthony Powell <apowell@arize.com>
2024-12-17 10:46:36 -0500

    feat(prompts): Add prompt combobox to playground page + deeplink to prompt specific playground (#5748)
    * feat(prompts): Add prompt selection combobox to playground instances
    
    * Implement prompt/:id/playground route and add link from prompt page
    
    * Rename prompt combobox components for clarity

--- app/src/pages/prompt/PromptPage.tsx
+++ app/src/pages/prompt/PromptPage.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { useLoaderData } from "react-router";
+import { useLoaderData, useNavigate } from "react-router";
 
-import { Flex, Heading, View } from "@arizeai/components";
+import { Button, Flex, Heading, Icon, Icons, View } from "@arizeai/components";
 
 import { promptLoaderQuery$data } from "./__generated__/promptLoaderQuery.graphql";
 
 export function PromptPage() {
   const loaderData = useLoaderData() as promptLoaderQuery$data;
+  const navigate = useNavigate();
   return (
     <Flex direction="column" height="100%">
       <View
@@ -17,6 +18,16 @@ export function PromptPage() {
       >
         <Flex direction="row" justifyContent="space-between">
           <Heading level={1}>{loaderData.prompt.name}</Heading>
+          <Button
+            variant="default"
+            size="compact"
+            icon={<Icon svg={<Icons.Edit2Outline />} />}
+            onClick={() => {
+              navigate(`/prompts/${loaderData.prompt.id}/playground`);
+            }}
+          >
+            Edit in Playground
+          </Button>
         </Flex>
       </View>
     </Flex>
