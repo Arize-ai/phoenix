75ec0ea58497584a9bc3ea898c53c4309488e0b6
Anthony Powell <apowell@arize.com>
2024-12-18 14:06:42 -0500

    feat(prompts): Scaffold prompt versions tab / list (#5766)
    * refactor(prompts): URL based routing and loader sharing for prompts
    
    - /prompts/:prompId has been converted to a layout component
    - sub-routes of promptId share loader data with promptLoader
    - prompt detail page tabs perform navigations
    
    * feat(prompts): Add query fragment to prompt versions sub-route
    
    * feat(prompts): Implement routing, loaders, and fragments for prompt version list
    
    * Add PromptVersion to node query
    
    * fix imports
    
    * Ensure all unset fields are set
    
    * fix comma
    
    * Distinguish prompt version details from one another
    
    * Move truncate component
    
    * Consolidate prompt tab nav logic
    
    ---------
    
    Co-authored-by: Dustin Ngo <dustin@arize.com>

--- app/src/pages/prompt/PromptLayout.tsx
+++ app/src/pages/prompt/PromptLayout.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { useLoaderData, useNavigate } from "react-router";
+import { useFragment } from "react-relay";
+import { Outlet, useLocation, useNavigate } from "react-router";
+import { graphql } from "relay-runtime";
 import { css } from "@emotion/react";
 
 import {
@@ -14,8 +16,8 @@ import {
   View,
 } from "@arizeai/components";
 
-import { promptLoaderQuery$data } from "./__generated__/promptLoaderQuery.graphql";
-import { PromptTabContent } from "./PromptTabContent";
+import { PromptLayout__main$key } from "./__generated__/PromptLayout__main.graphql";
+import { usePromptIdLoader } from "./usePromptIdLoader";
 
 const mainCSS = css`
   flex: 1 1 auto;
@@ -45,9 +47,30 @@ const mainCSS = css`
   }
 `;
 
-export function PromptPage() {
-  const loaderData = useLoaderData() as promptLoaderQuery$data;
+export function PromptLayout() {
+  const loaderData = usePromptIdLoader();
   const navigate = useNavigate();
+  const { pathname } = useLocation();
+  let tabIndex = 0;
+  if (pathname.includes("versions")) {
+    tabIndex = 1;
+  }
+
+  const data = useFragment<PromptLayout__main$key>(
+    graphql`
+      fragment PromptLayout__main on Prompt {
+        promptVersions {
+          edges {
+            node {
+              id
+            }
+          }
+        }
+      }
+    `,
+    loaderData.prompt
+  );
+
   return (
     <main css={mainCSS}>
       <View
@@ -75,12 +98,26 @@ export function PromptPage() {
           </Button>
         </Flex>
       </View>
-      <Tabs>
+      <Tabs
+        index={tabIndex}
+        onChange={(index) => {
+          let url: string;
+          if (index === 1) {
+            url = `/prompts/${loaderData.prompt.id}/versions`;
+          } else {
+            url = `/prompts/${loaderData.prompt.id}`;
+          }
+          navigate(url);
+        }}
+      >
         <TabPane name={"Prompt"}>
-          <PromptTabContent prompt={loaderData.prompt} />
+          <Outlet />
         </TabPane>
-        <TabPane name={"Versions"} extra={<Counter>0</Counter>}>
-          Versions
+        <TabPane
+          name={"Versions"}
+          extra={<Counter>{data.promptVersions.edges.length}</Counter>}
+        >
+          <Outlet />
         </TabPane>
       </Tabs>
     </main>
--- app/src/pages/prompt/index.tsx
+++ app/src/pages/prompt/index.tsx
@@ -1,2 +1,2 @@
 export * from "./promptLoader";
-export * from "./PromptPage";
+export * from "./PromptLayout";
