2ae52d8454e4504da07583c8f71efdb7efc22d10
Roger Yang <roger.yang@arize.com>
2024-12-20 15:10:16 -0800

    clean up

--- tests/integration/db_migrations/test_autoincrement_primary_keys.py
+++ tests/integration/db_migrations/test_autoincrement_primary_keys.py
@@ -2,10 +2,7 @@ from secrets import token_hex
 
 from alembic.config import Config
 from phoenix.db import models
-from sqlalchemy import (
-    Engine,
-    select,
-)
+from sqlalchemy import Engine, select
 from sqlalchemy.orm import sessionmaker
 
 from . import _up
@@ -33,10 +30,12 @@ def test_prompt_versions(
         session.add(prompt_version)
     assert (id_ := prompt_version.id) is not None
     with db.begin() as session:
+        assert session.scalar(select(models.PromptVersion.id).filter_by(id=id_)) is not None
+    with db.begin() as session:
         session.delete(prompt_version)
     with db.begin() as session:
         assert session.scalar(select(models.PromptVersion.id).filter_by(id=id_)) is None
     with db.begin() as session:
-        prompt_version = models.PromptVersion(**values)
-        session.add(prompt_version)
-    assert prompt_version.id > id_
+        new_prompt_version = models.PromptVersion(**values)
+        session.add(new_prompt_version)
+    assert new_prompt_version.id > id_
