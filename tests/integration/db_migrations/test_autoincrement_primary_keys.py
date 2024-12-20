b9081f3f331fef28e4208237ecbb4806fa8669db
Roger Yang <80478925+RogerHYang@users.noreply.github.com>
2024-12-20 15:20:50 -0800

    fix(prompts): add sqlite_autoincrement for primary key on prompt_versions table (#5810)

---
+++ tests/integration/db_migrations/test_autoincrement_primary_keys.py
@@ -0,0 +1,43 @@
+from secrets import token_hex
+
+from alembic.config import Config
+from phoenix.db import models
+from sqlalchemy import Engine, select
+from sqlalchemy.orm import sessionmaker
+
+from . import _up
+
+
+def test_prompt_versions(
+    _engine: Engine,
+    _alembic_config: Config,
+) -> None:
+    _up(_engine, _alembic_config, "bc8fea3c2bc8")
+    db = sessionmaker(bind=_engine, expire_on_commit=False)
+    with db.begin() as session:
+        prompt = models.Prompt(name=token_hex(16))
+        session.add(prompt)
+    values = dict(
+        prompt_id=prompt.id,
+        template={},
+        template_type="chat",
+        template_format="mustache",
+        model_provider=token_hex(16),
+        model_name=token_hex(16),
+    )
+    with db.begin() as session:
+        prompt_version = models.PromptVersion(**values)
+        assert prompt_version.id is None
+        session.add(prompt_version)
+    assert (id_ := prompt_version.id) is not None
+    with db.begin() as session:
+        assert session.scalar(select(models.PromptVersion.id).filter_by(id=id_)) is not None
+    with db.begin() as session:
+        session.delete(prompt_version)
+    with db.begin() as session:
+        assert session.scalar(select(models.PromptVersion.id).filter_by(id=id_)) is None
+    with db.begin() as session:
+        new_prompt_version = models.PromptVersion(**values)
+        assert new_prompt_version.id is None
+        session.add(new_prompt_version)
+    assert new_prompt_version.id > id_
