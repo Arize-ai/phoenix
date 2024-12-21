540ef3f639bcfccc6b04ce8b502e10ab5db26f96
Mikyo King <mikyo@arize.com>
2024-12-20 17:15:43 -0700

    feat(tracing): pretty print JSON strings for structured data outputs

---
+++ app/src/components/utility/PrettyText.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { css } from "@emotion/react";
+
+import { JSONBlock } from "@phoenix/components/code";
+import { usePrettyText } from "@phoenix/hooks/usePrettyText";
+import { assertUnreachable } from "@phoenix/typeUtils";
+
+export function PrettyText({ children }: { children: string }) {
+  const { text, textType } = usePrettyText(children);
+  if (textType === "string") {
+    return (
+      <pre
+        css={css`
+          white-space: pre-wrap;
+          text-wrap: wrap;
+          margin: 0;
+        `}
+      >
+        {text}
+      </pre>
+    );
+  }
+  if (textType === "json") {
+    return <JSONBlock value={text} />;
+  }
+  assertUnreachable(textType);
+}
