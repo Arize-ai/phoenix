586334a1143c7338d53c38c08c96b15c1cea292d
Mikyo King <mikyo@arize.com>
2025-01-02 19:31:54 -0700

    Fix generic

--- app/src/components/tag/TagList.tsx
+++ app/src/components/tag/TagList.tsx
@@ -9,13 +9,17 @@ const tagListCSS = css`
   gap: var(--ac-global-dimension-size-50);
   height: 28px;
 `;
-function TagList<T extends Record<string, unknown>>(
+function TagList<T extends object>(
   props: TagListProps<T>,
   ref: Ref<HTMLDivElement>
 ) {
   return <AriaTagList {...props} ref={ref} css={tagListCSS} />;
 }
 
-const _TagList = forwardRef(TagList);
+// Use forwardRef with the generic type to ensure the type propagates
+const _TagList = forwardRef(TagList) as <T extends object>(
+  props: TagListProps<T> & { ref?: Ref<HTMLDivElement> }
+) => JSX.Element;
+
 export { _TagList as TagList };
 export type { TagListProps };
