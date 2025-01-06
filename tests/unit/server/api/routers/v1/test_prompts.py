84ed085b4c742c20c0dc9c118ab5341f4c489361
Roger Yang <roger.yang@arize.com>
2025-01-06 13:18:50 -0800

    clean up

--- tests/unit/server/api/routers/v1/test_prompts.py
+++ tests/unit/server/api/routers/v1/test_prompts.py
@@ -17,9 +17,6 @@ class TestPrompts:
         db: DbSessionFactory,
     ) -> None:
         prompt_name = token_hex(16)
-        tag_name = token_hex(16)
-        model_provider = token_hex(16)
-        model_name = token_hex(16)
         prompt_versions = []
 
         async with db() as session:
@@ -34,13 +31,14 @@ class TestPrompts:
                         template_format="MUSTACHE",
                         template={"messages": [{"role": "USER", "content": "hi"}]},
                         invocation_parameters={},
-                        model_provider=model_provider,
-                        model_name=model_name,
+                        model_provider=token_hex(16),
+                        model_name=token_hex(16),
                     )
                 )
             session.add_all(prompt_versions)
 
         prompt_version = prompt_versions[1]
+        tag_name = token_hex(16)
         async with db() as session:
             prompt_version_tag = models.PromptVersionTag(
                 prompt_id=prompt.id,
@@ -49,7 +47,8 @@ class TestPrompts:
             )
             session.add(prompt_version_tag)
 
-        for prompts_identifier in GlobalID(Prompt.__name__, str(prompt.id)), prompt_name:
+        prompt_id = str(GlobalID(Prompt.__name__, str(prompt.id)))
+        for prompts_identifier in prompt_id, prompt_name:
             response = await httpx_client.get(
                 f"/v1/prompts/{prompts_identifier}/tags/{tag_name}",
             )
@@ -57,9 +56,18 @@ class TestPrompts:
             assert isinstance((data := response.json()["data"]), dict)
             assert (
                 from_global_id_with_expected_type(
-                    GlobalID.from_id(data["id"]), PromptVersion.__name__
+                    GlobalID.from_id(data.pop("id")),
+                    PromptVersion.__name__,
                 )
                 == prompt_version.id
             )
-            assert prompt_version.model_name == model_name
-            assert prompt_version.model_provider == model_provider
+            assert data.pop("description") == (prompt_version.description or "")
+            assert data.pop("invocation_parameters") == prompt_version.invocation_parameters
+            assert data.pop("model_name") == prompt_version.model_name
+            assert data.pop("model_provider") == prompt_version.model_provider
+            assert data.pop("output_schema") == prompt_version.output_schema
+            assert data.pop("template") == prompt_version.template
+            assert data.pop("template_format") == prompt_version.template_format
+            assert data.pop("template_type") == prompt_version.template_type
+            assert data.pop("tools") == prompt_version.tools
+            assert not data
