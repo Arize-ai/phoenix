0c8953a1450244f09ed6c5aa646645c194bdeee5
Anthony Powell <apowell@arize.com>
2025-01-06 19:24:44 -0500

    feat(prompts): Create new prompt and prompt versions from Playground (#5914)
    * feat(prompts): Create prompt versions from playground
    
    - Create new prompt + prompt version
    
    * Add new prompt versions on update
    
    * Add aria-label props to TagGroup
    
    without these we spam the console with many warnings
    
    * Revalidate prompt versions after mutations
    
    * Prevent stale prompt combobox list
    
    * Add dialog postfix to upsertPrompt component
    
    * Move template format out of prompt to instance conversion
    
    * Hide combobox description when error is visible
    
    * Clarify selected prompt id props

--- app/src/pages/playground/SavePromptForm.tsx
+++ app/src/pages/playground/SavePromptForm.tsx
@@ -1,99 +1,176 @@
-import React from "react";
+import React, { useCallback, useRef, useState } from "react";
 import { Controller, useForm } from "react-hook-form";
+import { graphql, useLazyLoadQuery } from "react-relay";
 
-import { Form, TextArea, TextField } from "@arizeai/components";
+import { Form, TextArea } from "@arizeai/components";
 
 import { Button, Flex, View } from "@phoenix/components";
+import { SavePromptFormQuery } from "@phoenix/pages/playground/__generated__/SavePromptFormQuery.graphql";
+import { PromptComboBox } from "@phoenix/pages/playground/PromptComboBox";
+
 export type SavePromptSubmitHandler = (params: SavePromptFormParams) => void;
 
 export type SavePromptFormParams = {
+  promptId?: string;
   name: string;
   description?: string;
 };
 
 export function SavePromptForm({
-  onSubmit,
+  onCreate,
+  onUpdate,
   isSubmitting = false,
-  submitButtonText = "Save",
+  defaultSelectedPromptId,
 }: {
-  onSubmit: SavePromptSubmitHandler;
+  onCreate: SavePromptSubmitHandler;
+  onUpdate: SavePromptSubmitHandler;
   isSubmitting?: boolean;
-  submitButtonText?: string;
+  defaultSelectedPromptId?: string;
 }) {
+  const flexContainer = useRef<HTMLDivElement>(null);
+  const prompts = useLazyLoadQuery<SavePromptFormQuery>(
+    graphql`
+      query SavePromptFormQuery {
+        prompts {
+          edges {
+            prompt: node {
+              id
+              name
+            }
+          }
+        }
+      }
+    `,
+    {},
+    { fetchPolicy: "network-only" }
+  );
+  const [selectedPromptId, setSelectedPromptId] = useState<string | null>(
+    defaultSelectedPromptId ?? null
+  );
+  const selectedPrompt = prompts?.prompts?.edges?.find(
+    (edge) => edge?.prompt?.id === selectedPromptId
+  );
+  const [promptInputValue, setPromptInputValue] = useState<string>(
+    selectedPrompt?.prompt?.name ?? ""
+  );
+
+  const mode: "create" | "update" = selectedPrompt ? "update" : "create";
+  const submitButtonText =
+    mode === "create" ? "Create Prompt" : "Update Prompt";
   const {
     control,
     handleSubmit,
-    formState: { isDirty },
+    formState: { isDirty, isValid, errors },
   } = useForm<SavePromptFormParams>({
+    values: {
+      promptId: selectedPromptId ?? undefined,
+      name:
+        mode === "update" && selectedPrompt
+          ? selectedPrompt?.prompt?.name
+          : promptInputValue,
+    },
     defaultValues: {
-      name: "Prompt " + new Date().toISOString(),
       description: "",
+    },
+    reValidateMode: "onChange",
+    resetOptions: {
+      keepDefaultValues: true,
     },
   });
 
+  const onSubmit = useCallback(
+    (params: SavePromptFormParams) => {
+      if (mode === "create") {
+        onCreate(params);
+      } else {
+        onUpdate(params);
+      }
+    },
+    [onCreate, onUpdate, mode]
+  );
+
   return (
-    <Form>
-      <View padding="size-200">
+    <Flex direction="column" gap="size-200" ref={flexContainer}>
+      <View padding="size-200" paddingBottom={0}>
         <Controller
           name="name"
           control={control}
           rules={{
-            required: "Prompt name is required",
+            required: {
+              message: "Prompt is required",
+              value: true,
+            },
           }}
-          render={({
-            field: { onChange, onBlur, value },
-            fieldState: { invalid, error },
-          }) => (
-            <TextField
-              label="Prompt Name"
-              description="The name of your saved prompt"
-              errorMessage={error?.message}
-              validationState={invalid ? "invalid" : "valid"}
-              onChange={onChange}
+          render={({ field: { onBlur, onChange } }) => (
+            <PromptComboBox
+              label="Prompt"
+              description="The prompt to update, or prompt name to create"
+              placeholder="Select a prompt, or enter a new prompt name"
+              isRequired
               onBlur={onBlur}
-              value={value}
-            />
-          )}
-        />
-        <Controller
-          name="description"
-          control={control}
-          render={({
-            field: { onChange, onBlur, value },
-            fieldState: { invalid, error },
-          }) => (
-            <TextArea
-              label="Description"
-              description="A description of your prompt (optional)"
-              isRequired={false}
-              height={100}
-              errorMessage={error?.message}
-              validationState={invalid ? "invalid" : "valid"}
-              onChange={onChange}
-              onBlur={onBlur}
-              value={value}
+              defaultInputValue={promptInputValue}
+              onInputChange={setPromptInputValue}
+              // this seems... not great. not sure how else to get a stable element reference that doesn't use a ref
+              // https://react-spectrum.adobe.com/react-aria/Popover.html#props
+              // eslint-disable-next-line react-compiler/react-compiler
+              container={flexContainer.current ?? undefined}
+              errorMessage={errors.name?.message}
+              allowsCustomValue
+              onChange={(promptId) => {
+                onChange(promptId);
+                setSelectedPromptId(promptId);
+              }}
+              promptId={selectedPromptId}
             />
           )}
         />
       </View>
-      <View
-        paddingEnd="size-200"
-        paddingTop="size-100"
-        paddingBottom="size-100"
-        borderTopColor="light"
-        borderTopWidth="thin"
-      >
-        <Flex direction="row" justifyContent="end">
-          <Button
-            variant={isDirty ? "primary" : "default"}
-            size="S"
-            onPress={() => handleSubmit(onSubmit)()}
-            isDisabled={isSubmitting}
-          >
-            {submitButtonText}
-          </Button>
-        </Flex>
-      </View>
-    </Form>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <View padding="size-200" paddingTop={0}>
+          <Controller
+            name="description"
+            control={control}
+            render={({
+              field: { onChange, onBlur, value },
+              fieldState: { invalid, error },
+            }) => (
+              <TextArea
+                label="Description"
+                description={
+                  mode === "create"
+                    ? "A description of your prompt (optional)"
+                    : "A description of your changes to the prompt (optional)"
+                }
+                isRequired={false}
+                height={100}
+                errorMessage={error?.message}
+                validationState={invalid ? "invalid" : "valid"}
+                onChange={onChange}
+                onBlur={onBlur}
+                value={value}
+              />
+            )}
+          />
+        </View>
+        <View
+          paddingEnd="size-200"
+          paddingTop="size-100"
+          paddingBottom="size-100"
+          borderTopColor="light"
+          borderTopWidth="thin"
+        >
+          <Flex direction="row" justifyContent="end">
+            <Button
+              variant={isDirty ? "primary" : "default"}
+              size="S"
+              isDisabled={isSubmitting || !isValid}
+              type="submit"
+            >
+              {submitButtonText}
+            </Button>
+          </Flex>
+        </View>
+      </Form>
+    </Flex>
   );
 }
