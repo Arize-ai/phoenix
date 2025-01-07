be75d582fd8644724ec193a79d20aa43914d808f
Dustin Ngo <dustin@arize.com>
2025-01-07 10:30:23 -0500

    feat: Add SetPromptVersionTag mutation (#5912)
    * Add create and patch Version Tag mutations
    
    * Unify prompt version tag mutations
    
    * Update gql schema
    
    * Update tags field
    
    * Remove docstring
    
    * Update tags field on prompt
    
    * Use insert on conflict helper

--- src/phoenix/server/api/types/PromptVersionTag.py
+++ src/phoenix/server/api/types/PromptVersionTag.py
@@ -1,11 +1,26 @@
 from typing import Optional
 
 import strawberry
-from strawberry.relay import Node, NodeID
+from strawberry.relay import GlobalID, Node, NodeID
+
+from phoenix.db import models
 
 
 @strawberry.type
 class PromptVersionTag(Node):
     id_attr: NodeID[int]
+    prompt_version_id: GlobalID
     name: str
     description: Optional[str] = None
+
+
+def to_gql_prompt_version_tag(prompt_version_tag: models.PromptVersionTag) -> PromptVersionTag:
+    from phoenix.server.api.types.PromptVersion import PromptVersion
+
+    version_gid = GlobalID(PromptVersion.__name__, str(prompt_version_tag.prompt_version_id))
+    return PromptVersionTag(
+        id_attr=prompt_version_tag.id,
+        prompt_version_id=version_gid,
+        name=prompt_version_tag.name,
+        description=prompt_version_tag.description,
+    )
