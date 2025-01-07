12bbb3a53ac74cf1f913e5063381be3a2434f8f7
Dustin Ngo <dustin@arize.com>
2025-01-07 16:59:34 -0500

    feat: Add delete Prompt and delete PromptVersionTag mutations (#5935)
    * Implement delete mutations for Prompt and PromptVersionTag
    
    * Update graphql schema

--- src/phoenix/server/api/mutations/prompt_version_tag_mutations.py
+++ src/phoenix/server/api/mutations/prompt_version_tag_mutations.py
@@ -1,29 +1,61 @@
+from typing import Optional
+
 import strawberry
-from sqlalchemy import select
+from sqlalchemy import delete, select
 from sqlalchemy.exc import IntegrityError as PostgreSQLIntegrityError
 from sqlean.dbapi2 import IntegrityError as SQLiteIntegrityError  # type: ignore[import-untyped]
+from strawberry.relay import GlobalID
 from strawberry.types import Info
 
 from phoenix.db import models
 from phoenix.db.helpers import SupportedSQLDialect
 from phoenix.db.insertion.helpers import insert_on_conflict
 from phoenix.server.api.context import Context
-from phoenix.server.api.exceptions import BadRequest, Conflict
-from phoenix.server.api.input_types.PromptVersionTagInput import SetPromptVersionTagInput
+from phoenix.server.api.exceptions import BadRequest, Conflict, NotFound
 from phoenix.server.api.queries import Query
 from phoenix.server.api.types.node import from_global_id_with_expected_type
 from phoenix.server.api.types.PromptVersion import PromptVersion
 from phoenix.server.api.types.PromptVersionTag import PromptVersionTag, to_gql_prompt_version_tag
 
 
+@strawberry.input
+class DeletePromptVersionTagInput:
+    prompt_version_tag_id: GlobalID
+
+
+@strawberry.input
+class SetPromptVersionTagInput:
+    prompt_version_id: GlobalID
+    name: str
+    description: Optional[str] = None
+
+
 @strawberry.type
 class PromptVersionTagMutationPayload:
-    prompt_version_tag: PromptVersionTag
+    prompt_version_tag: Optional[PromptVersionTag]
     query: Query
 
 
 @strawberry.type
 class PromptVersionTagMutationMixin:
+    @strawberry.mutation
+    async def delete_prompt_version_tag(
+        self, info: Info[Context, None], input: DeletePromptVersionTagInput
+    ) -> PromptVersionTagMutationPayload:
+        async with info.context.db() as session:
+            prompt_version_tag_id = from_global_id_with_expected_type(
+                input.prompt_version_tag_id, PromptVersionTag.__name__
+            )
+            stmt = delete(models.PromptVersionTag).where(
+                models.PromptVersionTag.id == prompt_version_tag_id
+            )
+            result = await session.execute(stmt)
+            if result.rowcount == 0:
+                raise NotFound(f"PromptVersionTag with ID {input.prompt_version_tag_id} not found")
+
+            await session.commit()
+            return PromptVersionTagMutationPayload(prompt_version_tag=None, query=Query())
+
     @strawberry.mutation
     async def set_prompt_version_tag(
         self, info: Info[Context, None], input: SetPromptVersionTagInput
