493199b55cb56174a835647a311b97141c444733
Anthony Powell <apowell@arize.com>
2025-01-06 19:24:44 -0500

    feat(prompts): Create new prompt and prompt versions from Playground (#5914)
    * feat(prompts): Create prompt versions from playground
    
    - Create new prompt + prompt version
    
    * Add new prompt versions on update
    
    * Add aria-label props to TagGroup
    
    without these we spam the console with many warnings
    
    * Revalidate prompt versions after mutations
    
    * Prevent stale prompt combobox list
    
    * Add dialog postfix to upsertPrompt component
    
    * Move template format out of prompt to instance conversion
    
    * Hide combobox description when error is visible
    
    * Clarify selected prompt id props

---
+++ app/src/pages/playground/__generated__/UpsertPromptFromTemplateDialogCreateMutation.graphql.ts
@@ -0,0 +1,129 @@
+/**
+ * @generated SignedSource<<bbe52578863d934988bed1b0f90758f0>>
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+// @ts-nocheck
+
+import { ConcreteRequest, Mutation } from 'relay-runtime';
+export type PromptMessageRole = "AI" | "SYSTEM" | "TOOL" | "USER";
+export type PromptTemplateFormat = "FSTRING" | "MUSTACHE" | "NONE";
+export type PromptTemplateType = "CHAT" | "STRING";
+export type CreateChatPromptInput = {
+  description?: string | null;
+  name: string;
+  promptVersion: ChatPromptVersionInput;
+};
+export type ChatPromptVersionInput = {
+  description?: string | null;
+  invocationParameters?: any;
+  modelName: string;
+  modelProvider: string;
+  outputSchema?: JSONSchemaInput | null;
+  template: PromptChatTemplateInput;
+  templateFormat: PromptTemplateFormat;
+  templateType: PromptTemplateType;
+  tools?: ReadonlyArray<ToolDefinitionInput>;
+};
+export type PromptChatTemplateInput = {
+  messages: ReadonlyArray<TextPromptMessageInput>;
+};
+export type TextPromptMessageInput = {
+  content: string;
+  role: PromptMessageRole;
+};
+export type ToolDefinitionInput = {
+  definition: any;
+};
+export type JSONSchemaInput = {
+  definition: any;
+};
+export type UpsertPromptFromTemplateDialogCreateMutation$variables = {
+  input: CreateChatPromptInput;
+};
+export type UpsertPromptFromTemplateDialogCreateMutation$data = {
+  readonly createChatPrompt: {
+    readonly id: string;
+    readonly name: string;
+  };
+};
+export type UpsertPromptFromTemplateDialogCreateMutation = {
+  response: UpsertPromptFromTemplateDialogCreateMutation$data;
+  variables: UpsertPromptFromTemplateDialogCreateMutation$variables;
+};
+
+const node: ConcreteRequest = (function(){
+var v0 = [
+  {
+    "defaultValue": null,
+    "kind": "LocalArgument",
+    "name": "input"
+  }
+],
+v1 = [
+  {
+    "alias": null,
+    "args": [
+      {
+        "kind": "Variable",
+        "name": "input",
+        "variableName": "input"
+      }
+    ],
+    "concreteType": "Prompt",
+    "kind": "LinkedField",
+    "name": "createChatPrompt",
+    "plural": false,
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "kind": "ScalarField",
+        "name": "id",
+        "storageKey": null
+      },
+      {
+        "alias": null,
+        "args": null,
+        "kind": "ScalarField",
+        "name": "name",
+        "storageKey": null
+      }
+    ],
+    "storageKey": null
+  }
+];
+return {
+  "fragment": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "UpsertPromptFromTemplateDialogCreateMutation",
+    "selections": (v1/*: any*/),
+    "type": "Mutation",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Operation",
+    "name": "UpsertPromptFromTemplateDialogCreateMutation",
+    "selections": (v1/*: any*/)
+  },
+  "params": {
+    "cacheID": "73d9d27e0df3bc27e4af4dd3d77f41fb",
+    "id": null,
+    "metadata": {},
+    "name": "UpsertPromptFromTemplateDialogCreateMutation",
+    "operationKind": "mutation",
+    "text": "mutation UpsertPromptFromTemplateDialogCreateMutation(\n  $input: CreateChatPromptInput!\n) {\n  createChatPrompt(input: $input) {\n    id\n    name\n  }\n}\n"
+  }
+};
+})();
+
+(node as any).hash = "fa52cba6c2bfaa6298a5360253dc9bc5";
+
+export default node;
