453645ba0680af22e4a8bfa206a5e70c930f7fc5
Mikyo King <mikyo@arize.com>
2025-01-09 15:47:26 -0700

    chore(components): make combobox consistent (#5979)

--- app/stories/Gallery.stories.tsx
+++ app/stories/Gallery.stories.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { Meta, StoryFn } from "@storybook/react";
 
 import { Item, Picker } from "@arizeai/components";
 
 import {
   Button,
+  ComboBox,
+  ComboBoxItem,
   Disclosure,
   DisclosureGroup,
   DisclosurePanel,
@@ -19,11 +21,6 @@ import {
   TextField,
   View,
 } from "@phoenix/components";
-import {
-  ComboBox,
-  ComboBoxItem,
-  ComboBoxProps,
-} from "@phoenix/components/combobox/ComboBox";
 
 import { ThemeWrapper } from "./components/ThemeWrapper";
 
@@ -37,155 +34,175 @@ const meta: Meta = {
 
 export default meta;
 
-const Template: StoryFn<ComboBoxProps<object>> = () => (
-  <ThemeWrapper>
-    <View
-      borderWidth="thin"
-      borderColor="dark"
-      padding="size-200"
-      borderRadius="medium"
-      marginTop="size-800"
-    >
-      <Flex direction="row" gap="size-200" alignItems="center">
-        <ComboBox label="Ice cream flavor" description={"pick a flavor"}>
-          <ComboBoxItem textValue="Chocolate" key={"chocolate"}>
-            Chocolate
-          </ComboBoxItem>
-          <ComboBoxItem textValue="Mint" key={"mint"}>
-            Mint
-          </ComboBoxItem>
-          <ComboBoxItem textValue="Strawberry" key={"strawberry"}>
-            Strawberry
-          </ComboBoxItem>
-          <ComboBoxItem textValue="Vanilla" key={"vanilla"}>
-            Vanilla
-          </ComboBoxItem>
-        </ComboBox>
-        <View minWidth="270px">
-          <TagGroup selectionMode="multiple">
-            <Label>Categories</Label>
-            <TagList>
-              <Tag>News</Tag>
-              <Tag>Travel</Tag>
-              <Tag>Gaming</Tag>
-              <Tag>Shopping</Tag>
-            </TagList>
-            <Text slot="description">Choose your own category</Text>
-          </TagGroup>
-        </View>
-        <Picker label="Toppings" size="compact" description={"pick a flavor"}>
-          <Item key="chocolate">Chocolate</Item>
-          <Item key="mint">Mint</Item>
-          <Item key="strawberry">Strawberry</Item>
-          <Item key="vanilla">Vanilla</Item>
-        </Picker>
-        <TextField size="M">
-          <Label>Label</Label>
-          <Input type="text" />
-          <Text slot="description">some text</Text>
-        </TextField>
-        <Button size="S">Button</Button>
-      </Flex>
-      <Flex direction="row" gap="size-200" alignItems="center">
-        <ComboBox
-          label="Ice cream flavor"
-          description={"pick a flavor"}
-          size="L"
+const Template: StoryFn = () => {
+  const [direction, setDirection] = useState<"row" | "column">("row");
+  return (
+    <ThemeWrapper>
+      <View
+        padding="size-200"
+        borderWidth="thin"
+        borderColor="dark"
+        borderRadius="medium"
+      >
+        <Button
+          size="S"
+          onPress={() => setDirection(direction === "row" ? "column" : "row")}
         >
-          <ComboBoxItem textValue="Chocolate" key={"chocolate"}>
-            Chocolate
-          </ComboBoxItem>
-          <ComboBoxItem textValue="Mint" key={"mint"}>
-            Mint
-          </ComboBoxItem>
-          <ComboBoxItem textValue="Strawberry" key={"strawberry"}>
-            Strawberry
-          </ComboBoxItem>
-          <ComboBoxItem textValue="Vanilla" key={"vanilla"}>
-            Vanilla
-          </ComboBoxItem>
-        </ComboBox>
-        <Picker label="Toppings" size="default" description={"pick a flavor"}>
-          <Item key="chocolate">Chocolate</Item>
-          <Item key="mint">Mint</Item>
-          <Item key="strawberry">Strawberry</Item>
-          <Item key="vanilla">Vanilla</Item>
-        </Picker>
-        <TextField size="L">
-          <Label>Label</Label>
-          <Input type="text" />
-          <Text slot="description">some description</Text>
-        </TextField>
-        <Button size="M">Button</Button>
-      </Flex>
-    </View>
-    <View
-      padding="size-200"
-      borderWidth="thin"
-      borderColor="dark"
-      borderRadius="medium"
-      marginTop="size-200"
-    >
-      <Flex direction="column" gap="size-200">
-        <ComboBox label="Ice cream flavor">
-          <ComboBoxItem textValue="Chocolate" key={"chocolate"}>
-            Chocolate
-          </ComboBoxItem>
-          <ComboBoxItem textValue="Mint" key={"mint"}>
-            Mint
-          </ComboBoxItem>
-          <ComboBoxItem textValue="Strawberry" key={"strawberry"}>
-            Strawberry
-          </ComboBoxItem>
-          <ComboBoxItem textValue="Vanilla" key={"vanilla"}>
-            Vanilla
-          </ComboBoxItem>
-        </ComboBox>
-        <Picker label="Toppings" size="compact">
-          <Item key="chocolate">Chocolate</Item>
-          <Item key="mint">Mint</Item>
-          <Item key="strawberry">Strawberry</Item>
-          <Item key="vanilla">Vanilla</Item>
-        </Picker>
-        <ComboBox label="Ice cream flavor" size="L">
-          <ComboBoxItem textValue="Chocolate" key={"chocolate"}>
-            Chocolate
-          </ComboBoxItem>
-          <ComboBoxItem textValue="Mint" key={"mint"}>
-            Mint
-          </ComboBoxItem>
-          <ComboBoxItem textValue="Strawberry" key={"strawberry"}>
-            Strawberry
-          </ComboBoxItem>
-          <ComboBoxItem textValue="Vanilla" key={"vanilla"}>
-            Vanilla
-          </ComboBoxItem>
-        </ComboBox>
-        <Picker label="Toppings" size="default">
-          <Item key="chocolate">Chocolate</Item>
-          <Item key="mint">Mint</Item>
-          <Item key="strawberry">Strawberry</Item>
-          <Item key="vanilla">Vanilla</Item>
-        </Picker>
-      </Flex>
-    </View>
-    <View
-      padding="size-200"
-      borderWidth="thin"
-      borderColor="dark"
-      borderRadius="medium"
-      marginTop="size-200"
-    >
-      <DisclosureGroup>
-        <Disclosure id="content">
-          <DisclosureTrigger>Nutrition Facts</DisclosureTrigger>
-          <DisclosurePanel>
-            <Text>Ice cream is sooooo good for you!</Text>
-          </DisclosurePanel>
-        </Disclosure>
-      </DisclosureGroup>
-    </View>
-  </ThemeWrapper>
-);
+          {`Toggle Direction: ${direction}`}
+        </Button>
+      </View>
+      <View
+        borderWidth="thin"
+        borderColor="dark"
+        padding="size-200"
+        borderRadius="medium"
+        marginTop="size-200"
+      >
+        <Flex
+          direction={direction}
+          gap="size-200"
+          alignItems={direction === "row" ? "center" : "start"}
+        >
+          <ComboBox label="Ice cream flavor" description={"pick a flavor"}>
+            <ComboBoxItem textValue="Chocolate" key={"chocolate"}>
+              Chocolate
+            </ComboBoxItem>
+            <ComboBoxItem textValue="Mint" key={"mint"}>
+              Mint
+            </ComboBoxItem>
+            <ComboBoxItem textValue="Strawberry" key={"strawberry"}>
+              Strawberry
+            </ComboBoxItem>
+            <ComboBoxItem textValue="Vanilla" key={"vanilla"}>
+              Vanilla
+            </ComboBoxItem>
+          </ComboBox>
+          <Picker label="Toppings" size="compact" description={"pick a flavor"}>
+            <Item key="chocolate">Chocolate</Item>
+            <Item key="mint">Mint</Item>
+            <Item key="strawberry">Strawberry</Item>
+            <Item key="vanilla">Vanilla</Item>
+          </Picker>
+          <View minWidth="270px">
+            <TagGroup selectionMode="multiple">
+              <Label>Categories</Label>
+              <TagList>
+                <Tag>News</Tag>
+                <Tag>Travel</Tag>
+                <Tag>Gaming</Tag>
+                <Tag>Shopping</Tag>
+              </TagList>
+              <Text slot="description">Choose your own category</Text>
+            </TagGroup>
+          </View>
+          <TextField size="M">
+            <Label>Label</Label>
+            <Input type="text" />
+            <Text slot="description">some text</Text>
+          </TextField>
+          <Button size="S">Button</Button>
+        </Flex>
+        <Flex direction="row" gap="size-200" alignItems="center">
+          <ComboBox
+            label="Ice cream flavor"
+            description={"pick a flavor"}
+            size="L"
+          >
+            <ComboBoxItem textValue="Chocolate" key={"chocolate"}>
+              Chocolate
+            </ComboBoxItem>
+            <ComboBoxItem textValue="Mint" key={"mint"}>
+              Mint
+            </ComboBoxItem>
+            <ComboBoxItem textValue="Strawberry" key={"strawberry"}>
+              Strawberry
+            </ComboBoxItem>
+            <ComboBoxItem textValue="Vanilla" key={"vanilla"}>
+              Vanilla
+            </ComboBoxItem>
+          </ComboBox>
+          <Picker label="Toppings" size="default" description={"pick a flavor"}>
+            <Item key="chocolate">Chocolate</Item>
+            <Item key="mint">Mint</Item>
+            <Item key="strawberry">Strawberry</Item>
+            <Item key="vanilla">Vanilla</Item>
+          </Picker>
+          <TextField size="L">
+            <Label>Label</Label>
+            <Input type="text" />
+            <Text slot="description">some description</Text>
+          </TextField>
+          <Button size="M">Button</Button>
+        </Flex>
+      </View>
+      <View
+        padding="size-200"
+        borderWidth="thin"
+        borderColor="dark"
+        borderRadius="medium"
+        marginTop="size-200"
+      >
+        <Flex direction="column" gap="size-200">
+          <ComboBox label="Ice cream flavor">
+            <ComboBoxItem textValue="Chocolate" key={"chocolate"}>
+              Chocolate
+            </ComboBoxItem>
+            <ComboBoxItem textValue="Mint" key={"mint"}>
+              Mint
+            </ComboBoxItem>
+            <ComboBoxItem textValue="Strawberry" key={"strawberry"}>
+              Strawberry
+            </ComboBoxItem>
+            <ComboBoxItem textValue="Vanilla" key={"vanilla"}>
+              Vanilla
+            </ComboBoxItem>
+          </ComboBox>
+          <Picker label="Toppings" size="compact">
+            <Item key="chocolate">Chocolate</Item>
+            <Item key="mint">Mint</Item>
+            <Item key="strawberry">Strawberry</Item>
+            <Item key="vanilla">Vanilla</Item>
+          </Picker>
+          <ComboBox label="Ice cream flavor" size="L">
+            <ComboBoxItem textValue="Chocolate" key={"chocolate"}>
+              Chocolate
+            </ComboBoxItem>
+            <ComboBoxItem textValue="Mint" key={"mint"}>
+              Mint
+            </ComboBoxItem>
+            <ComboBoxItem textValue="Strawberry" key={"strawberry"}>
+              Strawberry
+            </ComboBoxItem>
+            <ComboBoxItem textValue="Vanilla" key={"vanilla"}>
+              Vanilla
+            </ComboBoxItem>
+          </ComboBox>
+          <Picker label="Toppings" size="default">
+            <Item key="chocolate">Chocolate</Item>
+            <Item key="mint">Mint</Item>
+            <Item key="strawberry">Strawberry</Item>
+            <Item key="vanilla">Vanilla</Item>
+          </Picker>
+        </Flex>
+      </View>
+      <View
+        padding="size-200"
+        borderWidth="thin"
+        borderColor="dark"
+        borderRadius="medium"
+        marginTop="size-200"
+      >
+        <DisclosureGroup>
+          <Disclosure id="content">
+            <DisclosureTrigger>Nutrition Facts</DisclosureTrigger>
+            <DisclosurePanel>
+              <Text>Ice cream is sooooo good for you!</Text>
+            </DisclosurePanel>
+          </Disclosure>
+        </DisclosureGroup>
+      </View>
+    </ThemeWrapper>
+  );
+};
 
 export const Default = Template.bind({});
