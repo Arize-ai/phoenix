dfef01d23e3ec62d2b4ccf9a616b859783d4998c
Mikyo King <mikyo@arize.com>
2025-01-13 21:32:58 -0700

    refactor(components): use storybook builtins for theming (#6028)
    * refactor(components): use storybook builtins for theming
    
    * reinstall"
    
    * cleanup
    
    * add descriptions

--- app/stories/Colors.stories.tsx
+++ app/stories/Colors.stories.tsx
@@ -3,7 +3,6 @@ import { Meta, StoryFn } from "@storybook/react";
 
 import { Flex, Text, View } from "@phoenix/components";
 
-import { ThemeWrapper } from "./components/ThemeWrapper";
 import { GLOBAL_COLORS } from "./constants/colorConstants";
 
 const meta: Meta = {
@@ -13,21 +12,19 @@ const meta: Meta = {
 export default meta;
 
 const Template: StoryFn = () => (
-  <ThemeWrapper>
-    <Flex direction="row" wrap>
-      {GLOBAL_COLORS.map((color) => (
-        <View
-          backgroundColor={color}
-          key={color}
-          height={50}
-          width={100}
-          padding="size-50"
-        >
-          <Text size="XS">{color}</Text>
-        </View>
-      ))}
-    </Flex>
-  </ThemeWrapper>
+  <Flex direction="row" wrap>
+    {GLOBAL_COLORS.map((color) => (
+      <View
+        backgroundColor={color}
+        key={color}
+        height={50}
+        width={100}
+        padding="size-50"
+      >
+        <Text size="XS">{color}</Text>
+      </View>
+    ))}
+  </Flex>
 );
 
 export const Default = Template.bind({});
--- app/stories/Popover.stories.tsx
+++ app/stories/Popover.stories.tsx
@@ -5,44 +5,33 @@ import {
   Button,
   Dialog,
   DialogTrigger,
+  DialogTriggerProps,
   Popover,
   PopoverArrow,
   PopoverProps,
   View,
 } from "@phoenix/components";
 
-import { ThemeWrapper } from "./components/ThemeWrapper";
-
 const meta: Meta = {
   title: "Popover",
   component: Popover,
   parameters: {
-    controls: { expanded: true },
-  },
-  argTypes: {
-    selectionMode: {
-      options: ["none", "single", "multiple"],
-      control: {
-        type: "radio",
-      },
-    },
+    layout: "centered",
   },
 };
 
 export default meta;
 
-const Template: StoryFn<PopoverProps> = (args) => (
-  <ThemeWrapper>
-    <DialogTrigger isOpen>
-      <Button>Settings</Button>
-      <Popover {...args}>
-        <PopoverArrow />
-        <Dialog>
-          <View padding="size-100">Dialog Content goes here</View>
-        </Dialog>
-      </Popover>
-    </DialogTrigger>
-  </ThemeWrapper>
+const Template: StoryFn<DialogTriggerProps> = (args) => (
+  <DialogTrigger>
+    <Button>Settings</Button>
+    <Popover {...args}>
+      <PopoverArrow />
+      <Dialog>
+        <View padding="size-100">Dialog Content goes here</View>
+      </Dialog>
+    </Popover>
+  </DialogTrigger>
 );
 
 export const Default = Template.bind({});
