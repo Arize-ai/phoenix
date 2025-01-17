b335a71af0d00443341bcb2defbea4881ecd4837
Dustin Ngo <dustin@arize.com>
2025-01-17 16:22:35 +0900

    Add prompt label node and mutations

---
+++ src/phoenix/server/api/types/PromptLabel.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+import strawberry
+from strawberry.relay import Node, NodeID
+
+from phoenix.db import models
+from phoenix.server.api.types.Identifier import Identifier
+
+
+@strawberry.type
+class PromptLabel(Node):
+    id_attr: NodeID[int]
+    name: Identifier
+    description: Optional[str] = None
+
+
+def to_gql_prompt_label(label_orm: models.PromptLabel) -> PromptLabel:
+    return PromptLabel(
+        id_attr=label_orm.id,
+        name=Identifier(label_orm.name),
+        description=label_orm.description,
+    )
