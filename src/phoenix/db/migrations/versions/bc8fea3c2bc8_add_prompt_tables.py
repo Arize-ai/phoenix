d1eda1d3778f6d334fab438805ba6fedf7abefd8
Dustin Ngo <dustin@arize.com>
2025-01-24 13:28:51 +0900

    feat: PromptLabel gql interface (#6100)
    * Add prompt label node and mutations
    
    * Add PromptLabel to node interface
    
    * Add prompts resolver on PromptLabel node
    
    * Add prompt label mutations mixin and build gql schema
    
    * Add prompt labels query
    
    * Delete needless comment
    
    * Address feedback
    
    * Build gql schema

--- src/phoenix/db/migrations/versions/bc8fea3c2bc8_add_prompt_tables.py
+++ src/phoenix/db/migrations/versions/bc8fea3c2bc8_add_prompt_tables.py
@@ -98,6 +98,10 @@ def upgrade() -> None:
             nullable=False,
             index=True,
         ),
+        sa.UniqueConstraint(
+            "prompt_label_id",
+            "prompt_id",
+        ),
     )
 
     op.create_table(
