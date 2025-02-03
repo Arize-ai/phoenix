51598fcdc26ee0632c70510f610f19255943abb3
Xander Song <axiomofjoy@gmail.com>
2025-02-03 12:22:15 -0800

    fix(prompts): output schema (#6194)

--- src/phoenix/server/api/types/OutputSchema.py
+++ src/phoenix/server/api/types/OutputSchema.py
@@ -1,15 +1,9 @@
 import strawberry
 from strawberry.scalars import JSON
 
-from phoenix.server.api.helpers.prompts.models import PromptOutputSchema
-
 
 @strawberry.type
 class OutputSchema:
     """A JSON schema definition used to guide an LLM's output"""
 
     definition: JSON
-
-
-def to_gql_output_schema_from_pydantic(pydantic_output_schema: PromptOutputSchema) -> OutputSchema:
-    return OutputSchema(**pydantic_output_schema.model_dump())
