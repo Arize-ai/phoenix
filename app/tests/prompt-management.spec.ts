62c1ca9ae3dadb101631872df0996adb6d88e953
Mikyo King <mikyo@arize.com>
2025-02-12 17:39:59 -0700

    cleanup

---
+++ app/tests/prompt-management.spec.ts
@@ -0,0 +1,69 @@
+import { expect, test } from "@playwright/test";
+import { randomUUID } from "crypto";
+
+test.beforeEach(async ({ page }) => {
+  page.goto(`/login`);
+
+  await page.getByLabel("Email").fill("admin@localhost");
+  await page.getByLabel("Password").fill("admin123");
+  await page.getByRole("button", { name: "Log In", exact: true }).click();
+  await page.waitForURL("**/projects");
+});
+
+test("can create a prompt", async ({ page }) => {
+  await page.goto("/prompts");
+  await page.getByRole("link", { name: "Create Prompt" }).click();
+  await page.getByText("You are a chatbot").fill("You are a helpful assistant");
+  await await page.getByRole("button", { name: "Save" }).click();
+  await page.getByPlaceholder("Select or enter new prompt").click();
+  const promptName = `chatbot-${randomUUID()}`;
+  await page.getByPlaceholder("Select or enter new prompt").fill(promptName);
+  await page.getByLabel("Prompt Description").click();
+  await page.getByLabel("Prompt Description").fill("very kind chatbot");
+  await page.getByRole("button", { name: "Create Prompt" }).click();
+  await page.getByRole("button", { name: "View Prompt" }).click();
+
+  // Check if the prompt
+  await expect(page.getByRole("heading", { name: promptName })).toBeVisible();
+  await expect(
+    page.getByText("You are a helpful assistant").first()
+  ).toBeVisible();
+});
+
+test("can edit a prompt", async ({ page }) => {
+  await page.goto("/playground");
+  await page.getByRole("button", { name: "Save" }).click();
+  await page.getByPlaceholder("Select or enter new prompt").click();
+  const promptName = `chatbot-${randomUUID()}`;
+  await page.getByPlaceholder("Select or enter new prompt").fill(promptName);
+  await page.getByLabel("Prompt Description").click();
+  await page.getByLabel("Prompt Description").fill("very kind chatbot");
+  await page.getByRole("button", { name: "Create Prompt" }).click();
+  await page.getByRole("button", { name: "View Prompt" }).click();
+
+  // Go to the prompt listing
+  await page.goto("/prompts");
+  await page.getByRole("link", { name: promptName }).click();
+
+  await page.getByRole("button", { name: "Edit in Playground" }).click();
+
+  // Ensure that the prompt is loaded into the playground page
+  await page.waitForURL("**/playground");
+
+  // Edit the prompt
+  await page.getByText("You are a chatbot").first().click(); // Not sure why there are multiple but need to call .first()
+  await page.getByText("You are a chatbot").fill("You are an angry chatbot");
+  await page.getByRole("button", { name: "Save" }).click();
+
+  // Save the prompt
+  await page.getByLabel("Change Description").fill("very angry chatbot");
+  await page.getByRole("button", { name: "Update Prompt" }).click();
+
+  await page.getByRole("button", { name: "View Prompt" }).click();
+
+  // Check if the prompt was updated
+  await expect(page.getByRole("heading", { name: promptName })).toBeVisible();
+  await expect(
+    page.getByText("You are an angry chatbot").first()
+  ).toBeVisible();
+});
