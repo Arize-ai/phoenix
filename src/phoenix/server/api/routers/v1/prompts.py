960e2f5745cbaaee7446952333eb1212e6dac8f0
Roger Yang <roger.yang@arize.com>
2025-02-19 15:11:31 -0800

    clean up

--- src/phoenix/server/api/routers/v1/prompts.py
+++ src/phoenix/server/api/routers/v1/prompts.py
@@ -298,7 +298,7 @@ async def create_prompt(
     request: Request,
     request_body: CreatePromptRequestBody,
 ) -> CreatePromptResponseBody:
-    if request_body.version.template_type != "CHAT":
+    if request_body.version.template_type is not PromptTemplateType.CHAT:
         raise HTTPException(
             HTTP_422_UNPROCESSABLE_ENTITY,
             "Only CHAT template type is supported for prompts",
