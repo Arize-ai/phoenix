53ee04444c658003f796424ebea2fa0c42c24e48
Tony Powell <apowell@arize.com>
2025-02-25 14:27:20 -0500

    Consolidate tab logic

--- app/src/pages/TracingRoot.tsx
+++ app/src/pages/TracingRoot.tsx
@@ -1,20 +1,18 @@
 import React from "react";
-import { useLocation, useParams } from "react-router";
+import { useParams } from "react-router";
 
 import { TracingProvider } from "@phoenix/contexts/TracingContext";
+import { useProjectRootPath } from "@phoenix/hooks/useProjectRootPath";
 
 export function TracingRoot({ children }: React.PropsWithChildren) {
   const { projectId } = useParams();
   if (!projectId) {
     throw new Error("projectId is required");
   }
-  // extract the path segment after the projectId, no matter how many segments there are
-  const pathSegments = useLocation().pathname.split("/").slice(1);
-  // find the first path segment after projectId
-  const tableId = pathSegments.slice(pathSegments.indexOf(projectId) + 1)[0];
+  const { tab } = useProjectRootPath();
 
   return (
-    <TracingProvider projectId={projectId} tableId={tableId}>
+    <TracingProvider projectId={projectId} tableId={tab}>
       {children}
     </TracingProvider>
   );
