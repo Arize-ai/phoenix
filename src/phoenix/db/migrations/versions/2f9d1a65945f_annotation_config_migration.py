326821806774e079d79646c322fa91e6b75b70cb
Dustin Ngo <dustin@arize.com>
2025-02-26 14:51:39 -0500

    Always use batch_alter_table

--- src/phoenix/db/migrations/versions/2f9d1a65945f_annotation_config_migration.py
+++ src/phoenix/db/migrations/versions/2f9d1a65945f_annotation_config_migration.py
@@ -18,62 +18,30 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    dialect = op.get_bind().dialect.name
-    if op.get_bind().dialect.name == "sqlite":
-        with op.batch_alter_table("span_annotations", recreate="always") as batch_op:
-            batch_op.drop_constraint("uq_span_annotations_name_span_rowid", type_="unique")
-        with op.batch_alter_table("trace_annotations", recreate="always") as batch_op:
-            batch_op.drop_constraint("uq_trace_annotations_name_trace_rowid", type_="unique")
-        with op.batch_alter_table("document_annotations", recreate="always") as batch_op:
-            batch_op.drop_constraint(
-                "uq_document_annotations_name_span_rowid_document_position",
-                type_="unique",
-            )
-    elif op.get_bind().dialect.name == "postgresql":
-        op.drop_constraint("uq_span_annotations_name_span_rowid", "span_annotations", type_="unique")
-        op.drop_constraint("uq_trace_annotations_name_trace_rowid", "trace_annotations", type_="unique")
-        op.drop_constraint(
+    with op.batch_alter_table("span_annotations", recreate="always") as batch_op:
+        batch_op.drop_constraint("uq_span_annotations_name_span_rowid", type_="unique")
+    with op.batch_alter_table("trace_annotations", recreate="always") as batch_op:
+        batch_op.drop_constraint("uq_trace_annotations_name_trace_rowid", type_="unique")
+    with op.batch_alter_table("document_annotations", recreate="always") as batch_op:
+        batch_op.drop_constraint(
             "uq_document_annotations_name_span_rowid_document_position",
-            "document_annotations",
             type_="unique",
         )
-    else:
-        raise ValueError(f"Unsupported dialect: {dialect}")
 
 
 def downgrade() -> None:
-    dialect = op.get_bind().dialect.name
-    if dialect == "sqlite":
-        with op.batch_alter_table("span_annotations", recreate="always") as batch_op:
-            batch_op.create_unique_constraint(
-                "uq_span_annotations_name_span_rowid",
-                ["name", "span_rowid"],
-            )
-        with op.batch_alter_table("trace_annotations", recreate="always") as batch_op:
-            batch_op.create_unique_constraint(
-                "uq_trace_annotations_name_trace_rowid",
-                ["name", "trace_rowid"],
-            )
-        with op.batch_alter_table("document_annotations", recreate="always") as batch_op:
-            batch_op.create_unique_constraint(
-                "uq_document_annotations_name_span_rowid_document_position",
-                ["name", "span_rowid", "document_position"],
-            )
-    elif dialect == "postgresql":
-        op.create_unique_constraint(
+    with op.batch_alter_table("span_annotations", recreate="always") as batch_op:
+        batch_op.create_unique_constraint(
             "uq_span_annotations_name_span_rowid",
-            "span_annotations",
             ["name", "span_rowid"],
         )
-        op.create_unique_constraint(
+    with op.batch_alter_table("trace_annotations", recreate="always") as batch_op:
+        batch_op.create_unique_constraint(
             "uq_trace_annotations_name_trace_rowid",
-            "trace_annotations",
             ["name", "trace_rowid"],
         )
-        op.create_unique_constraint(
+    with op.batch_alter_table("document_annotations", recreate="always") as batch_op:
+        batch_op.create_unique_constraint(
             "uq_document_annotations_name_span_rowid_document_position",
-            "document_annotations",
             ["name", "span_rowid", "document_position"],
         )
-    else:
-        raise ValueError(f"Unsupported dialect: {dialect}")
