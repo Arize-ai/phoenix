74414918051bc42ad024d06456663257d61e870e
Dustin Ngo <dustin@arize.com>
2025-02-27 09:55:59 -0500

    Use auto recreate

--- src/phoenix/db/migrations/versions/2f9d1a65945f_annotation_config_migration.py
+++ src/phoenix/db/migrations/versions/2f9d1a65945f_annotation_config_migration.py
@@ -18,11 +18,11 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    with op.batch_alter_table("span_annotations", recreate="always") as batch_op:
+    with op.batch_alter_table("span_annotations", recreate="auto") as batch_op:
         batch_op.drop_constraint("uq_span_annotations_name_span_rowid", type_="unique")
-    with op.batch_alter_table("trace_annotations", recreate="always") as batch_op:
+    with op.batch_alter_table("trace_annotations", recreate="auto") as batch_op:
         batch_op.drop_constraint("uq_trace_annotations_name_trace_rowid", type_="unique")
-    with op.batch_alter_table("document_annotations", recreate="always") as batch_op:
+    with op.batch_alter_table("document_annotations", recreate="auto") as batch_op:
         batch_op.drop_constraint(
             "uq_document_annotations_name_span_rowid_document_position",
             type_="unique",
@@ -30,17 +30,17 @@ def upgrade() -> None:
 
 
 def downgrade() -> None:
-    with op.batch_alter_table("span_annotations", recreate="always") as batch_op:
+    with op.batch_alter_table("span_annotations", recreate="auto") as batch_op:
         batch_op.create_unique_constraint(
             "uq_span_annotations_name_span_rowid",
             ["name", "span_rowid"],
         )
-    with op.batch_alter_table("trace_annotations", recreate="always") as batch_op:
+    with op.batch_alter_table("trace_annotations", recreate="auto") as batch_op:
         batch_op.create_unique_constraint(
             "uq_trace_annotations_name_trace_rowid",
             ["name", "trace_rowid"],
         )
-    with op.batch_alter_table("document_annotations", recreate="always") as batch_op:
+    with op.batch_alter_table("document_annotations", recreate="auto") as batch_op:
         batch_op.create_unique_constraint(
             "uq_document_annotations_name_span_rowid_document_position",
             ["name", "span_rowid", "document_position"],
