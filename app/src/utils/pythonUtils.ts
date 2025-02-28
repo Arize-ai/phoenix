fe8b0b094469d6c68d59bfb3983c18f1215d79e1
Anthony Powell <apowell@arize.com>
2025-02-28 18:12:03 -0500

    feat: Implement quick filtering support for metadata cells in trace table (#6623)
    * feat: Implement quick filtering support for metadata cells in trace table
    
    Add metadata renderer to span table
    
    Memoize trace table
    
    Tweak metadata label styles
    
    Respond to large amounts of metadata on span
    
    role button for a11y
    
    feat: Implement quick filtering support for metadata cells in trace table
    
    Add metadata renderer to span table
    
    Memoize trace table
    
    Tweak metadata label styles
    
    Respond to large amounts of metadata on span
    
    cleanup
    
    cleanup
    
    * fix booleans
    
    * fix booleans
    
    * cleanup
    
    * cleanup
    
    ---------
    
    Co-authored-by: Mikyo King <mikyo@arize.com>

---
+++ app/src/utils/pythonUtils.ts
@@ -0,0 +1,21 @@
+/**
+ * A function that converts it to a python friendly string
+ */
+function sanitizePythonStr(value: string) {
+  return value.replaceAll("\n", "\\n").replaceAll('"', '\\"');
+}
+
+/**
+ * A function that converts a javascript primitive to a python primitive string
+ * @param val - The value to convert
+ * @returns The python primitive string
+ */
+export function toPythonPrimitiveStr(val: string | number | boolean): string {
+  if (typeof val === "boolean") {
+    return val ? "True" : "False";
+  }
+  if (typeof val === "number") {
+    return val.toString();
+  }
+  return `"${sanitizePythonStr(val)}"`;
+}
