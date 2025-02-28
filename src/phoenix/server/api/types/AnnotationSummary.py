8b08126136b0526402db17e0bae794c23f185a17
Dustin Ngo <dustin@arize.com>
2025-02-28 13:54:10 -0500

    feat: Allow multiple annotations per span with the same name (#6573)
    * Drop unique constraint migration
    
    * Update /span_annotations REST route
    
    * Update bulk inserters
    
    * Update data loader to aggregate label fractions per span first
    
    * Use correct kwarg name
    
    * Use sqlite compatible migration
    
    * Always use batch_alter_table
    
    * Ensure that spans with an annotation aggregate labels properly
    
    * Use auto recreate
    
    * Re-apply SpanFilter
    
    * Add migration to integration test
    
    * Update OpenAPI schema
    
    * Update traces router
    
    * Update helper test
    
    * Cast to floats before doing arithmetic
    
    * Add type annotations
    
    * Rebuild js client
    
    * Use more robust typing
    
    * Use a weighted avg for scores
    - optimize query to not use cartesian product
    - will scale very poorly as the number of labels increases
    
    * Use properly per-span aggregated scores
    
    * Cast to float before returning LabelFraction
    
    * Add tests
    
    * Remove score column from base stmt
    
    * Use column indexing
    
    * Improve clarity for type checker
    
    * Fix unit test type checks

--- src/phoenix/server/api/types/AnnotationSummary.py
+++ src/phoenix/server/api/types/AnnotationSummary.py
@@ -23,28 +23,26 @@ class AnnotationSummary:
 
     @strawberry.field
     def labels(self) -> list[str]:
-        return self.df.label.dropna().tolist()
+        unique_labels = self.df["label"].dropna().unique()
+        return [str(label) for label in unique_labels]
 
     @strawberry.field
     def label_fractions(self) -> list[LabelFraction]:
-        if not (n := self.df.label_count.sum()):
-            return []
         return [
             LabelFraction(
-                label=cast(str, row.label),
-                fraction=row.label_count / n,
+                label=row.label,
+                fraction=float(row.avg_label_fraction),
             )
-            for row in self.df.loc[
-                self.df.label.notna(),
-                ["label", "label_count"],
-            ].itertuples()
+            for row in self.df.itertuples()
+            if row.label is not None
         ]
 
     @strawberry.field
     def mean_score(self) -> Optional[float]:
-        if not (n := self.df.score_count.sum()):
+        avg_scores = self.df["avg_score"].dropna()
+        if avg_scores.empty:
             return None
-        return cast(float, self.df.score_sum.sum() / n)
+        return float(avg_scores.mean())  # all avg_scores should be the same
 
     @strawberry.field
     def score_count(self) -> int:
