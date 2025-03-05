273743902da049be8ec7c79f5f492807579b3fb0
Dustin Ngo <dustin@arize.com>
2025-03-05 13:32:47 -0500

    feat: Add thinking budget invocation parameter (#6670)
    * Add thinking budget invocation parameter
    
    * New client for 3.7 sonnet
    
    * Ruff 🐶
    
    * Use structured invocation parameter
    
    * No need to redefine chat completion create
    
    * Build gql schema
    
    * Build relay
    
    * Add versioned sonnet 3.7

--- app/src/pages/playground/__generated__/PlaygroundOutputMutation.graphql.ts
+++ app/src/pages/playground/__generated__/PlaygroundOutputMutation.graphql.ts
@@ -1,5 +1,5 @@
 /**
- * @generated SignedSource<<0beff0ab66850522d06b75508c6fa9e5>>
+ * @generated SignedSource<<24fed74cf7acb36741331d28662373a1>>
  * @lightSyntaxTransform
  * @nogrep
  */
@@ -9,7 +9,7 @@
 // @ts-nocheck
 
 import { ConcreteRequest } from 'relay-runtime';
-export type CanonicalParameterName = "MAX_COMPLETION_TOKENS" | "RANDOM_SEED" | "REASONING_EFFORT" | "RESPONSE_FORMAT" | "STOP_SEQUENCES" | "TEMPERATURE" | "TOOL_CHOICE" | "TOP_P";
+export type CanonicalParameterName = "ANTHROPIC_EXTENDED_THINKING" | "MAX_COMPLETION_TOKENS" | "RANDOM_SEED" | "REASONING_EFFORT" | "RESPONSE_FORMAT" | "STOP_SEQUENCES" | "TEMPERATURE" | "TOOL_CHOICE" | "TOP_P";
 export type ChatCompletionMessageRole = "AI" | "SYSTEM" | "TOOL" | "USER";
 export type GenerativeProviderKey = "ANTHROPIC" | "AZURE_OPENAI" | "GOOGLE" | "OPENAI";
 export type PromptTemplateFormat = "F_STRING" | "MUSTACHE" | "NONE";
