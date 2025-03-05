bd6fdb9bde48525af52e13418249afb7534ce917
Xander Song <axiomofjoy@gmail.com>
2025-03-05 12:16:24 -0800

    feat: experiment json downloads (#6642)

---
+++ app/src/pages/experiments/DownloadExperimentActionMenu.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import { ActionMenu, Item } from "@arizeai/components";
+
+import { Icon, Icons } from "@phoenix/components";
+import { StopPropagation } from "@phoenix/components/StopPropagation";
+import { prependBasename } from "@phoenix/utils/routingUtils";
+interface DownloadExperimentActionMenuProps {
+  experimentId: string;
+}
+
+export function DownloadExperimentActionMenu({
+  experimentId,
+}: DownloadExperimentActionMenuProps) {
+  return (
+    <StopPropagation>
+      <ActionMenu
+        buttonSize="compact"
+        align="end"
+        icon={<Icon svg={<Icons.DownloadOutline />} />}
+        onAction={(action) => {
+          if (action === "json") {
+            window.open(
+              prependBasename(`/v1/experiments/${experimentId}/json`),
+              "_blank"
+            );
+          }
+        }}
+      >
+        <Item key="json">Download JSON</Item>
+      </ActionMenu>
+    </StopPropagation>
+  );
+}
