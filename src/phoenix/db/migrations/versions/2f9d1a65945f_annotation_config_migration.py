66913daa38e757cf27b1edaf55419ae4316992a4
Dustin Ngo <dustin@arize.com>
2025-03-19 17:06:59 -0400

    feat: Annotation Configurations (#6495)
    Co-authored-by: Alexander Song <axiomofjoy@gmail.com>

--- src/phoenix/db/migrations/versions/2f9d1a65945f_annotation_config_migration.py
+++ src/phoenix/db/migrations/versions/2f9d1a65945f_annotation_config_migration.py
@@ -8,6 +8,7 @@ Create Date: 2025-02-06 10:17:15.726197
 
 from typing import Sequence, Union
 
+import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
@@ -28,8 +29,115 @@ def upgrade() -> None:
             type_="unique",
         )
 
+    op.create_table(
+        "annotation_configs",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("name", sa.String, nullable=False, unique=True),
+        sa.Column(
+            "annotation_type",
+            sa.String,
+            sa.CheckConstraint(
+                "annotation_type IN ('CATEGORICAL', 'CONTINUOUS', 'FREEFORM')",
+                name="valid_annotation_type",
+            ),
+            nullable=False,
+        ),
+        sa.Column("description", sa.String, nullable=True),
+    )
+
+    op.create_table(
+        "continuous_annotation_configs",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column(
+            "annotation_config_id",
+            sa.Integer,
+            sa.ForeignKey("annotation_configs.id", ondelete="CASCADE"),
+            nullable=False,
+            index=True,
+        ),
+        sa.Column(
+            "optimization_direction",
+            sa.String,
+            sa.CheckConstraint(
+                "optimization_direction IN ('MINIMIZE', 'MAXIMIZE')",
+                name="valid_optimization_direction",
+            ),
+            nullable=False,
+        ),
+        sa.Column("lower_bound", sa.Float, nullable=True),
+        sa.Column("upper_bound", sa.Float, nullable=True),
+    )
+
+    op.create_table(
+        "categorical_annotation_configs",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column(
+            "annotation_config_id",
+            sa.Integer,
+            sa.ForeignKey("annotation_configs.id", ondelete="CASCADE"),
+            nullable=False,
+            index=True,
+        ),
+        sa.Column(
+            "optimization_direction",
+            sa.String,
+            sa.CheckConstraint(
+                "optimization_direction IN ('MINIMIZE', 'MAXIMIZE')",
+                name="valid_optimization_direction",
+            ),
+            nullable=False,
+        ),
+    )
+
+    op.create_table(
+        "categorical_annotation_values",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column(
+            "categorical_annotation_config_id",
+            sa.Integer,
+            sa.ForeignKey("categorical_annotation_configs.id", ondelete="CASCADE"),
+            nullable=False,
+            index=True,
+        ),
+        sa.Column("label", sa.String, nullable=False),
+        sa.Column("score", sa.Float, nullable=True),
+        sa.UniqueConstraint(
+            "categorical_annotation_config_id",
+            "label",
+        ),
+    )
+
+    op.create_table(
+        "project_annotation_configs",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column(
+            "project_id",
+            sa.Integer,
+            sa.ForeignKey("projects.id", ondelete="CASCADE"),
+            nullable=False,
+            index=True,
+        ),
+        sa.Column(
+            "annotation_config_id",
+            sa.Integer,
+            sa.ForeignKey("annotation_configs.id", ondelete="CASCADE"),
+            nullable=False,
+            index=True,
+        ),
+        sa.UniqueConstraint(
+            "project_id",
+            "annotation_config_id",
+        ),
+    )
+
 
 def downgrade() -> None:
+    op.drop_table("project_annotation_configs")
+    op.drop_table("categorical_annotation_values")
+    op.drop_table("categorical_annotation_configs")
+    op.drop_table("continuous_annotation_configs")
+    op.drop_table("annotation_configs")
+
     with op.batch_alter_table("span_annotations", recreate="auto") as batch_op:
         batch_op.create_unique_constraint(
             "uq_span_annotations_name_span_rowid",
