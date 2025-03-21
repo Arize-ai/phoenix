03127a91e9d077734a8dd0842bdee03f51a48580
Mikyo King <mikyo@arize.com>
2025-03-20 18:17:07 -0600

    feat(projects): config tab

--- app/src/Routes.tsx
+++ app/src/Routes.tsx
@@ -11,6 +11,7 @@ import { embeddingLoaderQuery$data } from "./pages/embedding/__generated__/embed
 import { Layout } from "./pages/Layout";
 import { spanPlaygroundPageLoaderQuery$data } from "./pages/playground/__generated__/spanPlaygroundPageLoaderQuery.graphql";
 import { projectLoaderQuery$data } from "./pages/project/__generated__/projectLoaderQuery.graphql";
+import { ProjectConfigPage } from "./pages/project/ProjectConfigPage";
 import { promptLoaderQuery$data } from "./pages/prompt/__generated__/promptLoaderQuery.graphql";
 import { promptConfigLoader } from "./pages/prompt/promptConfigLoader";
 import { PromptIndexPage } from "./pages/prompt/PromptIndexPage";
@@ -153,6 +154,7 @@ const router = createBrowserRouter(
                     loader={sessionLoader}
                   />
                 </Route>
+                <Route path="config" element={<ProjectConfigPage />} />
               </Route>
             </Route>
           </Route>
