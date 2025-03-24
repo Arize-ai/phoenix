e1e0cef776ef6e1f61360d937783394f5beff96f
Anthony Powell <apowell@arize.com>
2025-03-24 16:35:32 -0400

    feat(annotations): Annotation Config UI (#6856)
    * feat(annotations): Annotation Config UI
    
    * Fix style merging in AnnotationLabel
    
    * Handle large values
    
    * Rename card
    
    * Persist annotation config via gql
    
    * Convert annotation config popover to dialog
    
    * Implement annotation selection toolbar + deletion
    
    * Remove bad lint rule
    
    * Replace dialog extra buttons with cancel button
    
    * styling changes
    
    * clean up annotation label
    
    * Remove hover state from annotation label unless clickable

--- app/src/Routes.tsx
+++ app/src/Routes.tsx
@@ -9,6 +9,8 @@ import { RouterProvider } from "react-router/dom";
 
 import { SettingsAIProvidersPage } from "@phoenix/pages/settings/SettingsAIProvidersPage";
 import { settingsAIProvidersPageLoader } from "@phoenix/pages/settings/settingsAIProvidersPageLoader";
+import { SettingsAnnotationsPage } from "@phoenix/pages/settings/SettingsAnnotationsPage";
+import { settingsAnnotationsPageLoader } from "@phoenix/pages/settings/settingsAnnotationsPageLoader";
 import { SettingsGeneralPage } from "@phoenix/pages/settings/SettingsGeneralPage";
 
 import { datasetLoaderQuery$data } from "./pages/dataset/__generated__/datasetLoaderQuery.graphql";
@@ -318,6 +320,14 @@ const router = createBrowserRouter(
               element={<SettingsAIProvidersPage />}
               handle={{
                 crumb: () => "providers",
+              }}
+            />
+            <Route
+              path="annotations"
+              loader={settingsAnnotationsPageLoader}
+              element={<SettingsAnnotationsPage />}
+              handle={{
+                crumb: () => "annotations",
               }}
             />
           </Route>
