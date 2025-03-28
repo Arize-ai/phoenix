56cf71cea34c406e5d85d24aacf7cb9a23417d36
Tony Powell <apowell@arize.com>
2025-03-28 10:47:54 -0400

    Use panels for annotation edits

--- app/src/pages/trace/SpanAside.tsx
+++ app/src/pages/trace/SpanAside.tsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, {
+  forwardRef,
+  PropsWithChildren,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from "react";
 import { graphql, useRefetchableFragment } from "react-relay";
+import {
+  ImperativePanelHandle,
+  Panel,
+  PanelGroup,
+  PanelResizeHandle,
+} from "react-resizable-panels";
 import { css } from "@emotion/react";
 
-import {
-  Disclosure,
-  DisclosureGroup,
-  DisclosurePanel,
-  DisclosureTrigger,
-  Flex,
-  View,
-} from "@phoenix/components";
+import { Flex, Icon, Icons, View } from "@phoenix/components";
 import { AnnotationLabel } from "@phoenix/components/annotation";
 import { SpanAnnotationsEditor } from "@phoenix/components/trace/SpanAnnotationsEditor";
 
@@ -58,9 +63,9 @@ export function SpanAside(props: { span: SpanAside_span$key }) {
   const hasAnnotations = annotations.length > 0;
 
   return (
-    <Flex direction="column" flex="1 1 auto" height="100%">
+    <PanelGroup direction="vertical">
       {hasAnnotations && (
-        <Flex flexShrink={1}>
+        <Panel order={1}>
           <View padding="size-200">
             <ul css={annotationListCSS}>
               {annotations.map((annotation) => (
@@ -73,49 +78,84 @@ export function SpanAside(props: { span: SpanAside_span$key }) {
               ))}
             </ul>
           </View>
-        </Flex>
+        </Panel>
       )}
-      <DisclosureGroup
-        css={css`
-          display: flex;
-          flex-direction: column;
-          flex: 1 1 auto;
-          height: 1px;
-
-          .ac-disclosure {
-            flex: 1 1 auto;
-            display: flex;
-            flex-direction: column;
-            height: 1px;
-          }
-
-          .ac-disclosure-panel {
-            flex: 1 1 auto;
-            display: flex;
-            flex-direction: column;
-            height: 1px;
-          }
-        `}
-      >
-        <Disclosure
-          id="edit-annotations"
-          css={css`
-            border-top: 1px solid var(--ac-global-border-color-default);
-
-            .ac-disclosure-trigger {
-              width: 100%;
-            }
-          `}
-        >
-          <DisclosureTrigger>Edit annotations</DisclosureTrigger>
-          <DisclosurePanel>
-            <SpanAnnotationsEditor
-              projectId={data.project.id}
-              spanNodeId={data.id}
-            />
-          </DisclosurePanel>
-        </Disclosure>
-      </DisclosureGroup>
-    </Flex>
+      <TitledPanel title="Edit annotations">
+        <SpanAnnotationsEditor
+          projectId={data.project.id}
+          spanNodeId={data.id}
+        />
+      </TitledPanel>
+    </PanelGroup>
   );
 }
+
+const TitledPanel = forwardRef<
+  ImperativePanelHandle | null,
+  PropsWithChildren<{ title: React.ReactNode }>
+>(({ children, title }, ref) => {
+  const panelRef = useRef<ImperativePanelHandle | null>(null);
+  useImperativeHandle<
+    ImperativePanelHandle | null,
+    ImperativePanelHandle | null
+  >(ref, () => panelRef.current);
+  const [collapsed, setCollapsed] = useState(false);
+
+  return (
+    <>
+      <PanelResizeHandle
+        css={css`
+          padding-top: var(--ac-global-dimension-size-50);
+          &:not([data-resize-handle-state="drag"]) + [data-panel] {
+            transition: flex 0.2s ease-in-out;
+          }
+          &:focus,
+          &:focus-visible,
+          &:active {
+            outline: 1px solid var(--ac-global-border-color-default);
+          }
+        `}
+        onClick={() => {
+          const panel = panelRef.current;
+          if (panel?.getSize() === 0) {
+            panel?.expand();
+          } else {
+            panel?.collapse();
+          }
+        }}
+      >
+        <Flex
+          alignItems="center"
+          css={css`
+            font-weight: var(--px-font-weight-heavy);
+            font-size: var(--ac-global-font-size-s);
+          `}
+        >
+          <Icon
+            data-collapsed={collapsed}
+            svg={<Icons.ChevronDown />}
+            css={css`
+              font-size: var(--ac-global-font-size-xl);
+              transition: transform 0.2s ease-in-out;
+              &[data-collapsed="true"] {
+                transform: rotate(270deg);
+              }
+            `}
+          />
+          {title}
+        </Flex>
+      </PanelResizeHandle>
+      <Panel
+        order={2}
+        ref={panelRef}
+        collapsible
+        onCollapse={() => setCollapsed(true)}
+        onExpand={() => setCollapsed(false)}
+      >
+        {children}
+      </Panel>
+    </>
+  );
+});
+
+TitledPanel.displayName = "TitledPanel";
