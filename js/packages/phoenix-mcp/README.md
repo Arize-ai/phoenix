6a3f670d58b62c69b00c0f9587b147f1c1f4be8b
Mikyo King <mikyo@arize.com>
2025-04-03 18:08:08 -0600

    feat(mcp-server) initial commit

---
+++ js/packages/phoenix-mcp/README.md
@@ -0,0 +1,48 @@
+# Phoenix MCP Server
+
+A MCP server for Arize Phoenix.
+
+## Installation
+
+This package is installed using PNPM:
+
+```bash
+pnpm install @arize/phoenix-mcp
+```
+
+## Development
+
+### Building
+
+To build the project:
+
+```bash
+pnpm build
+```
+
+### Development Mode
+
+To run in development mode:
+
+```bash
+pnpm dev
+```
+
+### Debugging
+
+To run the debugger:
+
+```bash
+npx @modelcontextprotocol/inspector node ./build/index.js
+```
+
+## Environment Variables
+
+The server requires the following environment variables:
+
+- `PHOENIX_API_KEY`: Your Phoenix API key
+- `PHOENIX_BASE_URL`: The base URL for Phoenix
+
+## License
+
+This project is licensed under the Elv2 license.
---
+++ js/packages/phoenix-mcp/src/tools/promptTools.ts
@@ -0,0 +1,88 @@
+import { PhoenixClient } from "@arizeai/phoenix-client";
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import z from "zod";
+
+export const initializePromptTools = ({
+  client,
+  server,
+}: {
+  client: PhoenixClient;
+  server: McpServer;
+}) => {
+  server.tool(
+    "list-prompts",
+    "Get a list of all the prompts",
+    {
+      limit: z.number().min(1).max(100).default(100),
+    },
+    async ({ limit }) => {
+      const response = await client.GET("/v1/prompts", {
+        params: {
+          query: {
+            limit,
+          },
+        },
+      });
+      return {
+        content: [
+          {
+            type: "text",
+            text: JSON.stringify(response.data?.data, null, 2),
+          },
+        ],
+      };
+    }
+  );
+
+  server.tool(
+    "get-latest-prompt",
+    "Get the latest prompt",
+    { prompt_identifier: z.string() },
+    async ({ prompt_identifier }) => {
+      const response = await client.GET(
+        "/v1/prompts/{prompt_identifier}/latest",
+        {
+          params: {
+            path: {
+              prompt_identifier,
+            },
+          },
+        }
+      );
+      return {
+        content: [
+          {
+            type: "text",
+            text: JSON.stringify(response.data, null, 2),
+          },
+        ],
+      };
+    }
+  );
+
+  server.tool(
+    "get-prompt-by-identifier",
+    "Get a prompt's latest version by its identifier",
+    { prompt_identifier: z.string() },
+    async ({ prompt_identifier }) => {
+      const response = await client.GET(
+        "/v1/prompts/{prompt_identifier}/latest",
+        {
+          params: {
+            path: {
+              prompt_identifier,
+            },
+          },
+        }
+      );
+      return {
+        content: [
+          {
+            type: "text",
+            text: JSON.stringify(response.data, null, 2),
+          },
+        ],
+      };
+    }
+  );
+};
