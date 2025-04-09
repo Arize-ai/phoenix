325f0acdc565778e4dfc9c6586917e45bf341038
Tony Powell <apowell@arize.com>
2025-04-09 09:18:16 -0400

    Use number field instead of slider for continuous input

--- app/src/components/annotation/ContinuousAnnotationInput.tsx
+++ app/src/components/annotation/ContinuousAnnotationInput.tsx
@@ -1,34 +1,43 @@
 import React, { useLayoutEffect, useRef } from "react";
 
-import { Slider, SliderNumberField, SliderProps } from "@phoenix/components";
+import {
+  Input,
+  Label,
+  NumberField,
+  NumberFieldProps,
+  Text,
+} from "@phoenix/components";
 import { useAnnotationFocus } from "@phoenix/components/annotation/AnnotationFocusContext";
 import { AnnotationConfigContinuous } from "@phoenix/pages/settings/types";
 
 type ContinuousAnnotationInputProps = {
   annotationConfig: AnnotationConfigContinuous;
-} & SliderProps<number>;
+} & NumberFieldProps;
 
 export const ContinuousAnnotationInput = ({
   annotationConfig,
   ...props
 }: ContinuousAnnotationInputProps) => {
-  const sliderRef = useRef<HTMLDivElement>(null);
+  const numberFieldRef = useRef<HTMLDivElement>(null);
   const { register } = useAnnotationFocus();
   useLayoutEffect(() => {
-    register(sliderRef);
+    register(numberFieldRef);
   }, [register]);
   // step should be 1 if the min and max end in .0, .1 otherwise
   const step = (annotationConfig?.lowerBound ?? 0) % 1 === 0 ? 1 : 0.1;
   return (
-    <Slider
+    <NumberField
       {...props}
-      ref={sliderRef}
-      label={annotationConfig.name}
+      ref={numberFieldRef}
       minValue={annotationConfig?.lowerBound ?? 0}
       maxValue={annotationConfig?.upperBound ?? 1}
       step={step}
     >
-      <SliderNumberField />
-    </Slider>
+      <Label>{annotationConfig.name}</Label>
+      <Input />
+      <Text slot="description">
+        {annotationConfig.lowerBound} - {annotationConfig.upperBound}
+      </Text>
+    </NumberField>
   );
 };
