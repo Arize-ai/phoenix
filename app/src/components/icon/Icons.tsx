e39a49a0f01e4189e7621c0ef22f6071ddf16acc
Anthony Powell <apowell@arize.com>
2025-04-09 10:36:40 -0400

    feat(annotations): Implement annotation config form elements (#7063)
    Additionally:
    
    * Add react-aria package and FocusScope
    
    * Use dark mode in storybook if OS is set to dark mode

--- app/src/components/icon/Icons.tsx
+++ app/src/components/icon/Icons.tsx
@@ -1870,6 +1870,24 @@ export const Refresh = () => (
   </svg>
 );
 
+export const Rotate = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    style={{ fill: "none" }}
+  >
+    <path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8" />
+    <path d="M3 3v5h5" />
+  </svg>
+);
+
 export const RowCollapseOutline = () => (
   <svg
     width="24"
