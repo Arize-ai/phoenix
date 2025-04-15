2683be45386fbdd1812fe798c23ce30c834dd083
Mikyo King <mikyo@arize.com>
2025-04-14 18:25:14 -0600

    rudimentary dialogue

--- app/src/pages/settings/SettingsDataPage.tsx
+++ app/src/pages/settings/SettingsDataPage.tsx
@@ -4,6 +4,18 @@ import invariant from "tiny-invariant";
 
 import { Card } from "@arizeai/components";
 
+import {
+  Button,
+  Dialog,
+  DialogTrigger,
+  Flex,
+  Heading,
+  Icon,
+  Icons,
+  Modal,
+  View,
+} from "@phoenix/components";
+
 import { RetentionPoliciesTable } from "./RetentionPoliciesTable";
 import { settingsDataPageLoader } from "./settingsDataPageLoader";
 
@@ -16,6 +28,26 @@ export function SettingsDataPage() {
       title="Retention Policies"
       bodyStyle={{ padding: 0 }}
       variant="compact"
+      extra={
+        <DialogTrigger>
+          <Button size="S" leadingVisual={<Icon svg={<Icons.PlusOutline />} />}>
+            New Policy
+          </Button>
+          <Modal>
+            <Dialog>
+              <Heading slot="title">New Retention Policy</Heading>
+              <View padding="size-100" borderTopWidth="thin" borderColor="dark">
+                <Flex direction="row" justifyContent="end" gap="size-100">
+                  <Button size="S" slot="close">
+                    Cancel
+                  </Button>
+                  <Button size="S">Create</Button>
+                </Flex>
+              </View>
+            </Dialog>
+          </Modal>
+        </DialogTrigger>
+      }
     >
       <RetentionPoliciesTable query={loaderData} />
     </Card>
