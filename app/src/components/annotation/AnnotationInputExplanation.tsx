368c2578e1c849ab751289580722af34ceb5d722
Anthony Powell <apowell@arize.com>
2025-04-18 12:52:28 -0400

    feat(annotations): Annotate spans based on project annotation configs (#7174)

--- app/src/components/annotation/AnnotationInputExplanation.tsx
+++ app/src/components/annotation/AnnotationInputExplanation.tsx
@@ -1,50 +1,77 @@
 import React from "react";
 import { FocusScope } from "react-aria";
-import { Label, TextArea } from "react-aria-components";
-
-import { Tooltip, TooltipTrigger, TriggerWrap } from "@arizeai/components";
+import { css } from "@emotion/react";
 
 import {
   Button,
   Dialog,
   DialogTrigger,
   Flex,
+  Input,
+  Label,
   Popover,
   PopoverArrow,
   Text,
   TextField,
   View,
 } from "@phoenix/components";
-import { Icon, Icons } from "@phoenix/components/icon";
+import { Annotation } from "@phoenix/components/annotation";
+import { EXPLANATION_LABEL_WIDTH } from "@phoenix/components/annotation/constants";
 
 export const AnnotationInputExplanation = ({
-  explanation,
+  annotation,
   onSubmit,
+  containerRef,
 }: {
-  explanation?: string;
+  annotation?: Annotation;
   onSubmit?: (explanation: string) => void;
+  containerRef?: HTMLDivElement;
 }) => {
+  const fieldName = annotation?.name
+    ? `${annotation.name}.explanation`
+    : "explanation";
   return (
     <DialogTrigger>
-      <TooltipTrigger placement="top">
-        <TriggerWrap>
-          <Button variant="quiet" type="button">
-            <Icon svg={<Icons.Edit2Outline />} />
-          </Button>
-        </TriggerWrap>
-        <Tooltip>Explain this score</Tooltip>
-      </TooltipTrigger>
-      <Popover placement="bottom end">
+      <Button
+        excludeFromTabOrder
+        type="button"
+        isDisabled={!annotation?.id}
+        className="annotation-input-explanation"
+        css={css`
+          position: absolute;
+          top: 0;
+          right: 4px;
+          width: ${EXPLANATION_LABEL_WIDTH};
+          background: none;
+          border: none;
+          padding: 0 !important;
+          line-height: unset;
+          color: var(--ac-global-link-color);
+          &:disabled {
+            cursor: default;
+            opacity: var(--ac-opacity-disabled);
+            color: var(--ac-global-text-color-900);
+          }
+          &:hover:not(:disabled) {
+            text-decoration: underline;
+            cursor: pointer;
+            background: none;
+          }
+        `}
+      >
+        Explain
+      </Button>
+      <Popover placement="bottom end" UNSTABLE_portalContainer={containerRef}>
         <PopoverArrow />
         <Dialog>
           {({ close }) => (
-            <FocusScope autoFocus restoreFocus>
+            <FocusScope autoFocus contain restoreFocus>
               <View padding="size-100">
                 <form
                   onSubmit={(e) => {
                     e.preventDefault();
                     const formData = new FormData(e.target as HTMLFormElement);
-                    const explanation = formData.get("explanation");
+                    const explanation = formData.get(fieldName);
                     if (typeof explanation === "string") {
                       onSubmit?.(explanation);
                     }
@@ -52,14 +79,20 @@ export const AnnotationInputExplanation = ({
                   }}
                 >
                   <Flex direction="column" gap="size-100">
-                    <TextField name="explanation" defaultValue={explanation}>
+                    <TextField
+                      name={fieldName}
+                      defaultValue={annotation?.explanation ?? ""}
+                      css={{
+                        minWidth: "300px",
+                      }}
+                    >
                       <Label>Explanation</Label>
-                      <TextArea rows={2} />
+                      <Input />
                       <Text slot="description">
                         Why did you give this score?
                       </Text>
                     </TextField>
-                    <Button variant="primary" type="submit">
+                    <Button type="submit" variant="primary">
                       Save
                     </Button>
                   </Flex>
