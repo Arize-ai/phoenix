4a3f0a91709557f393d7295ab519f86dc5d3d9c0
Roger Yang <roger.yang@arize.com>
2025-04-22 15:17:45 -0700

    clean up

--- tests/integration/_helpers.py
+++ tests/integration/_helpers.py
@@ -1258,7 +1258,7 @@ class _OIDCServer:
                     "subject_types_supported": ["public"],
                     "id_token_signing_alg_values_supported": ["HS256"],
                     "scopes_supported": ["openid", "profile", "email"],
-                    "token_endpoint_auth_methods_supported": ["client_secret_post"],
+                    "token_endpoint_auth_methods_supported": ["client_secret_basic"],
                     "claims_supported": [
                         "sub",
                         "iss",
