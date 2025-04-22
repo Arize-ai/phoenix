93bd8a3f6c303d90c16c52284ef2fff62e53528f
Xander Song <axiomofjoy@gmail.com>
2025-04-22 15:59:38 -0700

    fix(annotations): validate values for categorical annotation config (#7235)

--- src/phoenix/db/types/annotation_configs.py
+++ src/phoenix/db/types/annotation_configs.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import Annotated, Literal, Optional, Union
 
-from pydantic import Field, RootModel
+from pydantic import AfterValidator, Field, RootModel
 from typing_extensions import TypeAlias
 
 from .db_models import DBBaseModel
@@ -27,10 +27,32 @@ class CategoricalAnnotationValue(DBBaseModel):
     score: Optional[float] = None
 
 
+def is_non_empty(values: list[CategoricalAnnotationValue]) -> list[CategoricalAnnotationValue]:
+    if not values:
+        raise ValueError("Values must be non-empty")
+    return values
+
+
+def has_unique_labels(values: list[CategoricalAnnotationValue]) -> list[CategoricalAnnotationValue]:
+    labels = set()
+    for value in values:
+        label = value.label
+        if label in labels:
+            raise ValueError(
+                f'Values for categorical annotation config has duplicate label: "{label}"'
+            )
+        labels.add(label)
+    return values
+
+
 class CategoricalAnnotationConfig(_BaseAnnotationConfig):
     type: Literal[AnnotationType.CATEGORICAL.value]  # type: ignore[name-defined]
     optimization_direction: OptimizationDirection
-    values: list[CategoricalAnnotationValue]
+    values: Annotated[
+        list[CategoricalAnnotationValue],
+        AfterValidator(is_non_empty),
+        AfterValidator(has_unique_labels),
+    ]
 
 
 class ContinuousAnnotationConfig(_BaseAnnotationConfig):
