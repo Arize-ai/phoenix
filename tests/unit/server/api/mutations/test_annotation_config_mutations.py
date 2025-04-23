d9c108f25d87e1578ec24d1069ade8047a1f7346
Alexander Song <axiomofjoy@gmail.com>
2025-04-22 22:13:06 -0700

    continuous

--- tests/unit/server/api/mutations/test_annotation_config_mutations.py
+++ tests/unit/server/api/mutations/test_annotation_config_mutations.py
@@ -29,6 +29,14 @@ class TestAnnotationConfigMutations:
         }
     }
 
+    mutation CreateContinuousAnnotationConfig($input: CreateAnnotationConfigInput!) {
+        createAnnotationConfig(input: $input) {
+            annotationConfig {
+                ...ContinuousAnnotationConfigFields
+            }
+        }
+    }
+
     mutation UpdateCategoricalAnnotationConfig($input: UpdateAnnotationConfigInput!) {
         updateAnnotationConfig(input: $input) {
             annotationConfig {
@@ -37,11 +45,24 @@ class TestAnnotationConfigMutations:
         }
     }
 
+    mutation UpdateContinuousAnnotationConfig($input: UpdateAnnotationConfigInput!) {
+        updateAnnotationConfig(input: $input) {
+            annotationConfig {
+                ...ContinuousAnnotationConfigFields
+            }
+        }
+    }
+
     query ListAnnotationConfigs {
         annotationConfigs(first: 10) {
             edges {
                 node {
-                    ...CategoricalAnnotationConfigFields
+                    ... on CategoricalAnnotationConfig {
+                        ...CategoricalAnnotationConfigFields
+                    }
+                    ... on ContinuousAnnotationConfig {
+                        ...ContinuousAnnotationConfigFields
+                    }
                 }
             }
         }
@@ -53,7 +74,12 @@ class TestAnnotationConfigMutations:
                 annotationConfigs {
                     edges {
                         node {
-                            ...CategoricalAnnotationConfigFields
+                            ... on CategoricalAnnotationConfig {
+                                ...CategoricalAnnotationConfigFields
+                            }
+                            ... on ContinuousAnnotationConfig {
+                                ...ContinuousAnnotationConfigFields
+                            }
                         }
                     }
                 }
@@ -67,7 +93,12 @@ class TestAnnotationConfigMutations:
                 annotationConfigs {
                     edges {
                         node {
-                            ...CategoricalAnnotationConfigFields
+                            ... on CategoricalAnnotationConfig {
+                                ...CategoricalAnnotationConfigFields
+                            }
+                            ... on ContinuousAnnotationConfig {
+                                ...ContinuousAnnotationConfigFields
+                            }
                         }
                     }
                 }
@@ -82,7 +113,12 @@ class TestAnnotationConfigMutations:
     mutation DeleteAnnotationConfigs($input: DeleteAnnotationConfigsInput!) {
         deleteAnnotationConfigs(input: $input) {
             annotationConfigs {
-                ...CategoricalAnnotationConfigFields
+                ... on CategoricalAnnotationConfig {
+                    ...CategoricalAnnotationConfigFields
+                }
+                ... on ContinuousAnnotationConfig {
+                    ...ContinuousAnnotationConfigFields
+                }
             }
         }
     }
@@ -97,6 +133,16 @@ class TestAnnotationConfigMutations:
             label
             score
         }
+    }
+
+    fragment ContinuousAnnotationConfigFields on ContinuousAnnotationConfig {
+        id
+        name
+        annotationType
+        optimizationDirection
+        description
+        lowerBound
+        upperBound
     }
     """
 
@@ -192,6 +238,167 @@ class TestAnnotationConfigMutations:
             "annotationType": "CATEGORICAL",
             "optimizationDirection": "MINIMIZE",
             "values": [{"label": "Excellent", "score": 1.0}, {"label": "Poor", "score": 0.0}],
+        }
+        assert updated_config == expected_config
+
+        # Add annotation config to project
+        project_id = str(GlobalID("Project", str(project.id)))
+        add_to_project_input = {
+            "input": [
+                {
+                    "projectId": project_id,
+                    "annotationConfigId": config_id,
+                }
+            ]
+        }
+        add_response = await gql_client.execute(
+            query=self.QUERY,
+            variables=add_to_project_input,
+            operation_name="AddAnnotationConfigToProject",
+        )
+        assert not add_response.errors
+        assert (data := add_response.data) is not None
+        project_configs = data["addAnnotationConfigToProject"]["project"]["annotationConfigs"][
+            "edges"
+        ]
+        assert len(project_configs) == 1
+        assert project_configs[0]["node"]["id"] == config_id
+
+        # Remove annotation config from project
+        remove_from_project_input = {
+            "input": [
+                {
+                    "projectId": project_id,
+                    "annotationConfigId": config_id,
+                }
+            ]
+        }
+        remove_response = await gql_client.execute(
+            query=self.QUERY,
+            variables=remove_from_project_input,
+            operation_name="RemoveAnnotationConfigFromProject",
+        )
+        assert not remove_response.errors
+        assert (data := remove_response.data) is not None
+        project_configs = data["removeAnnotationConfigFromProject"]["project"]["annotationConfigs"][
+            "edges"
+        ]
+        assert len(project_configs) == 0
+        associations = data["removeAnnotationConfigFromProject"][
+            "projectAnnotationConfigAssociations"
+        ]
+        assert len(associations) == 1
+        assert associations[0]["projectId"] == project_id
+        assert associations[0]["annotationConfigId"] == config_id
+
+        # Delete the annotation config
+        delete_input = {
+            "input": {
+                "ids": [config_id],
+            }
+        }
+        delete_response = await gql_client.execute(
+            query=self.QUERY,
+            variables=delete_input,
+            operation_name="DeleteAnnotationConfigs",
+        )
+        assert not delete_response.errors
+        assert (data := delete_response.data) is not None
+        deleted_configs = data["deleteAnnotationConfigs"]["annotationConfigs"]
+        assert len(deleted_configs) == 1
+        assert deleted_configs[0] == expected_config
+
+        # Verify the config is deleted by listing
+        list_response = await gql_client.execute(
+            query=self.QUERY,
+            operation_name="ListAnnotationConfigs",
+        )
+        assert not list_response.errors
+        assert (data := list_response.data) is not None
+        configs = data["annotationConfigs"]["edges"]
+        assert len(configs) == 0
+
+    async def test_continuous_annotation_config_crud_operations(
+        self,
+        gql_client: AsyncGraphQLClient,
+        project: models.Project,
+    ) -> None:
+        # Create a continuous annotation config
+        create_input = {
+            "input": {
+                "annotationConfig": {
+                    "continuous": {
+                        "name": "Test Continuous Config",
+                        "description": "Test description",
+                        "optimizationDirection": "MAXIMIZE",
+                        "lowerBound": 0.0,
+                        "upperBound": 1.0,
+                    }
+                }
+            }
+        }
+        create_response = await gql_client.execute(
+            query=self.QUERY,
+            variables=create_input,
+            operation_name="CreateContinuousAnnotationConfig",
+        )
+        assert not create_response.errors
+        assert (data := create_response.data) is not None
+        created_config = data["createAnnotationConfig"]["annotationConfig"]
+        config_id = created_config["id"]
+        expected_config = {
+            "name": "Test Continuous Config",
+            "id": config_id,
+            "description": "Test description",
+            "annotationType": "CONTINUOUS",
+            "optimizationDirection": "MAXIMIZE",
+            "lowerBound": 0.0,
+            "upperBound": 1.0,
+        }
+        assert created_config == expected_config
+
+        # List annotation configs
+        list_response = await gql_client.execute(
+            query=self.QUERY,
+            operation_name="ListAnnotationConfigs",
+        )
+        assert not list_response.errors
+        assert (data := list_response.data) is not None
+        configs = data["annotationConfigs"]["edges"]
+        assert len(configs) == 1
+        assert configs[0]["node"] == created_config
+
+        # Update the annotation config
+        update_input = {
+            "input": {
+                "id": config_id,
+                "annotationConfig": {
+                    "continuous": {
+                        "name": "Updated Continuous Config",
+                        "description": "Updated description",
+                        "optimizationDirection": "MINIMIZE",
+                        "lowerBound": -1.0,
+                        "upperBound": 2.0,
+                    }
+                },
+            }
+        }
+        update_response = await gql_client.execute(
+            query=self.QUERY,
+            variables=update_input,
+            operation_name="UpdateContinuousAnnotationConfig",
+        )
+        assert not update_response.errors
+        assert (data := update_response.data) is not None
+        updated_config = data["updateAnnotationConfig"]["annotationConfig"]
+        expected_config = {
+            "name": "Updated Continuous Config",
+            "id": config_id,
+            "description": "Updated description",
+            "annotationType": "CONTINUOUS",
+            "optimizationDirection": "MINIMIZE",
+            "lowerBound": -1.0,
+            "upperBound": 2.0,
         }
         assert updated_config == expected_config
 
