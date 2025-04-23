2f27a9a7473e95e7b8da4ca1d0788c14ad21899b
Dustin Ngo <dustin@arize.com>
2025-04-23 15:30:13 -0400

    Create span comment mutation

--- src/phoenix/server/api/input_types/CreateSpanAnnotationInput.py
+++ src/phoenix/server/api/input_types/CreateSpanAnnotationInput.py
@@ -19,3 +19,9 @@ class CreateSpanAnnotationInput:
     metadata: JSON = strawberry.field(default_factory=dict)
     identifier: Optional[str] = None
     source: AnnotationSource
+
+
+@strawberry.input
+class CreateSpanCommentInput:
+    span_id: GlobalID
+    comment: str
