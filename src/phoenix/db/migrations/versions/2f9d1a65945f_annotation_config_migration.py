a8bbe3c49ee1626fb5fe061ec517a09a62f9e37b
Xander Song <axiomofjoy@gmail.com>
2025-04-25 15:59:05 -0700

    consolidate annotation migrations (#7282)

--- src/phoenix/db/migrations/versions/2f9d1a65945f_annotation_config_migration.py
+++ src/phoenix/db/migrations/versions/2f9d1a65945f_annotation_config_migration.py
@@ -189,9 +189,95 @@ def upgrade() -> None:
             "annotation_config_id",
         ),
     )
+    op.create_index(
+        "ix_span_annotations_identifier", "span_annotations", ["identifier"], unique=False
+    )
+    op.create_index(
+        "uq_span_annotations_span_rowid_name_null_identifier",
+        "span_annotations",
+        ["span_rowid", "name"],
+        unique=True,
+        postgresql_where=sa.column("identifier").is_(None),
+        sqlite_where=sa.column("identifier").is_(None),
+    )
+    op.create_index(
+        "uq_span_annotations_span_rowid_name_identifier_not_null",
+        "span_annotations",
+        ["span_rowid", "name", "identifier"],
+        unique=True,
+        postgresql_where=sa.column("identifier").isnot(None),
+        sqlite_where=sa.column("identifier").isnot(None),
+    )
+
+    op.create_index(
+        "ix_trace_annotations_identifier", "trace_annotations", ["identifier"], unique=False
+    )
+    op.create_index(
+        "uq_trace_annotations_trace_rowid_name_null_identifier",
+        "trace_annotations",
+        ["trace_rowid", "name"],
+        unique=True,
+        postgresql_where=sa.column("identifier").is_(None),
+        sqlite_where=sa.column("identifier").is_(None),
+    )
+    op.create_index(
+        "uq_trace_annotations_trace_rowid_name_identifier_not_null",
+        "trace_annotations",
+        ["trace_rowid", "name", "identifier"],
+        unique=True,
+        postgresql_where=sa.column("identifier").isnot(None),
+        sqlite_where=sa.column("identifier").isnot(None),
+    )
+
+    op.create_index(
+        "ix_document_annotations_identifier",
+        "document_annotations",
+        ["identifier"],
+        unique=False,
+    )
+    op.create_index(
+        "uq_document_annotations_span_rowid_name_null_identifier",
+        "document_annotations",
+        ["span_rowid", "name"],
+        unique=True,
+        postgresql_where=sa.column("identifier").is_(None),
+        sqlite_where=sa.column("identifier").is_(None),
+    )
+    op.create_index(
+        "uq_document_annotations_span_rowid_name_identifier_not_null",
+        "document_annotations",
+        ["span_rowid", "name", "identifier"],
+        unique=True,
+        postgresql_where=sa.column("identifier").isnot(None),
+        sqlite_where=sa.column("identifier").isnot(None),
+    )
 
 
 def downgrade() -> None:
+    op.drop_index(
+        "uq_document_annotations_span_rowid_name_identifier_not_null",
+        table_name="document_annotations",
+    )
+    op.drop_index(
+        "uq_document_annotations_span_rowid_name_null_identifier", table_name="document_annotations"
+    )
+    op.drop_index("ix_document_annotations_identifier", table_name="document_annotations")
+
+    op.drop_index(
+        "uq_trace_annotations_trace_rowid_name_identifier_not_null", table_name="trace_annotations"
+    )
+    op.drop_index(
+        "uq_trace_annotations_trace_rowid_name_null_identifier", table_name="trace_annotations"
+    )
+    op.drop_index("ix_trace_annotations_identifier", table_name="trace_annotations")
+
+    op.drop_index(
+        "uq_span_annotations_span_rowid_name_identifier_not_null", table_name="span_annotations"
+    )
+    op.drop_index(
+        "uq_span_annotations_span_rowid_name_null_identifier", table_name="span_annotations"
+    )
+    op.drop_index("ix_span_annotations_identifier", table_name="span_annotations")
     op.drop_table("project_annotation_configs")
     op.drop_table("annotation_configs")
 
