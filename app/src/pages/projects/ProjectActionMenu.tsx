78d05a3be0d45deab7f21938e5508a47607b0918
Anthony Powell <apowell@arize.com>
2025-04-30 13:08:53 -0400

    feat: Add "copy name" button to project menu (#7358)

--- app/src/pages/projects/ProjectActionMenu.tsx
+++ app/src/pages/projects/ProjectActionMenu.tsx
@@ -13,7 +13,9 @@ import { Button, Flex, Icon, Icons, Text, View } from "@phoenix/components";
 import { ProjectActionMenuClearMutation } from "./__generated__/ProjectActionMenuClearMutation.graphql";
 import { ProjectActionMenuDeleteMutation } from "./__generated__/ProjectActionMenuDeleteMutation.graphql";
 import { RemoveProjectDataForm } from "./RemoveProjectDataForm";
+
 enum ProjectAction {
+  COPY_NAME = "copyName",
   DELETE = "deleteProject",
   CLEAR = "clearProject",
   REMOVE_DATA = "removeProjectData",
@@ -169,6 +171,10 @@ export function ProjectActionMenu({
         align="end"
         onAction={(action) => {
           switch (action as ProjectAction) {
+            case ProjectAction.COPY_NAME: {
+              navigator.clipboard.writeText(projectName);
+              return;
+            }
             case ProjectAction.DELETE: {
               return onDelete();
             }
@@ -182,6 +188,17 @@ export function ProjectActionMenu({
         }}
         disabledKeys={canDelete ? [] : [ProjectAction.DELETE]}
       >
+        <Item key={ProjectAction.COPY_NAME} textValue="Copy Name">
+          <Flex
+            direction={"row"}
+            gap="size-75"
+            justifyContent={"start"}
+            alignItems={"center"}
+          >
+            <Icon svg={<Icons.ClipboardCopy />} />
+            <Text>Copy Name</Text>
+          </Flex>
+        </Item>
         <Item key={ProjectAction.CLEAR} textValue="Clear All Traces">
           <Flex
             direction={"row"}
