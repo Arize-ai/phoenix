991bf85948a440334f602df6f3ea463870d9dbc0
Mikyo King <mikyo@arize.com>
2025-05-01 08:52:34 -0700

    final push

--- app/src/components/icon/Icons.tsx
+++ app/src/components/icon/Icons.tsx
@@ -1870,6 +1870,32 @@ export const Refresh = () => (
   </svg>
 );
 
+// @src: lucide
+export const Rocket = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path
+      d="M4.5 16.5c-1.5 1.26-2 5-2 5s3.74-.5 5-2c.71-.84.7-2.13-.09-2.91a2.18 2.18 0 0 0-2.91-.09z"
+      fill="none"
+    />
+    <path
+      d="m12 15-3-3a22 22 0 0 1 2-3.95A12.88 12.88 0 0 1 22 2c0 2.72-.78 7.5-6 11a22.35 22.35 0 0 1-4 2z"
+      fill="none"
+    />
+    <path d="M9 12H4s.55-3.03 2-4c1.62-1.08 5 0 5 0" fill="none" />
+    <path d="M12 15v5s3.03-.55 4-2c1.08-1.62 0-5 0-5" fill="none" />
+  </svg>
+);
+
 export const RowCollapseOutline = () => (
   <svg
     width="24"
