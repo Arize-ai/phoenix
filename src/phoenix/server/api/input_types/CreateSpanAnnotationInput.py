2616ed989b7cd73014113438b3b5e71c7c5b7fbb
Xander Song <axiomofjoy@gmail.com>
2025-05-07 13:12:08 -0700

    fix(annotations): make identifier type non-optional in rest and graphql apis (#7459)

--- src/phoenix/server/api/input_types/CreateSpanAnnotationInput.py
+++ src/phoenix/server/api/input_types/CreateSpanAnnotationInput.py
@@ -4,7 +4,6 @@ import strawberry
 from strawberry.relay import GlobalID
 from strawberry.scalars import JSON
 
-from phoenix.server.api.exceptions import BadRequest
 from phoenix.server.api.types.AnnotationSource import AnnotationSource
 from phoenix.server.api.types.AnnotatorKind import AnnotatorKind
 
@@ -18,12 +17,8 @@ class CreateSpanAnnotationInput:
     score: Optional[float] = None
     explanation: Optional[str] = None
     metadata: JSON = strawberry.field(default_factory=dict)
-    identifier: Optional[str] = None
     source: AnnotationSource
-
-    def __post_init__(self) -> None:
-        if self.identifier == "":
-            raise BadRequest("Identifier must be a non-empty string or null")
+    identifier: Optional[str] = strawberry.UNSET
 
 
 @strawberry.input
--- src/phoenix/server/api/input_types/PatchAnnotationInput.py
+++ src/phoenix/server/api/input_types/PatchAnnotationInput.py
@@ -5,7 +5,6 @@ from strawberry import UNSET
 from strawberry.relay import GlobalID
 from strawberry.scalars import JSON
 
-from phoenix.server.api.exceptions import BadRequest
 from phoenix.server.api.types.AnnotatorKind import AnnotatorKind
 
 
@@ -19,7 +18,3 @@ class PatchAnnotationInput:
     explanation: Optional[str] = UNSET
     metadata: Optional[JSON] = UNSET
     identifier: Optional[str] = UNSET
-
-    def __post_init__(self) -> None:
-        if self.identifier == "":
-            raise BadRequest("Identifier must be a non-empty string or null")
