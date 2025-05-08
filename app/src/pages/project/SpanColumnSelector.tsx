f5ea84030d9d465ea34106b26759c76c15e2b9fb
Mikyo King <mikyo@arize.com>
2025-05-08 12:50:22 -0600

    refactor: change more places to say annotations (#7479)
    * refactor: change more places to say annotations
    
    * cleanup

--- app/src/pages/project/SpanColumnSelector.tsx
+++ app/src/pages/project/SpanColumnSelector.tsx
@@ -166,7 +166,7 @@ function EvaluationColumnSelector({
     });
   }, [data.spanAnnotationNames, annotationColumnVisibility]);
 
-  const onToggleEvaluations = useCallback(() => {
+  const onToggleAnnotations = useCallback(() => {
     const newVisibilityState = data.spanAnnotationNames.reduce((acc, name) => {
       return { ...acc, [name]: !allVisible };
     }, {});
@@ -185,11 +185,11 @@ function EvaluationColumnSelector({
           <label>
             <input
               type="checkbox"
-              name={"toggle-evaluations-all"}
+              name={"toggle-annotations-all"}
               checked={allVisible}
-              onChange={onToggleEvaluations}
+              onChange={onToggleAnnotations}
             />
-            feedback
+            annotations
           </label>
         </div>
       </View>
