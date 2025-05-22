74789833e858499368eafe61fdae7b41e0d244a3
Ge Li <77590974+GeLi2001@users.noreply.github.com>
2025-05-21 19:21:07 -0700

    fix: increase prompt fetch limit to 200 and update component to be scrollable (#7666)
    * increase prompt fetch limit to 100 and update component to be scrollable
    
    * prettier run
    
    * small update
    
    * nix combobox styling
    
    * increase to 200
    
    * pnpm run build

--- app/src/pages/playground/PromptComboBox.tsx
+++ app/src/pages/playground/PromptComboBox.tsx
@@ -25,7 +25,7 @@ export function PromptComboBox({
   const data = useLazyLoadQuery<PromptComboBoxQuery>(
     graphql`
       query PromptComboBoxQuery {
-        prompts {
+        prompts(first: 200) {
           edges {
             prompt: node {
               __typename
