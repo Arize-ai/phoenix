3424da17be22d7d7e938f6613220daa496f0f4fc
Dustin Ngo <dustin@arize.com>
2025-05-28 15:27:38 -0400

    Revert unintended change to add_span_processor

--- packages/phoenix-otel/src/phoenix/otel/otel.py
+++ packages/phoenix-otel/src/phoenix/otel/otel.py
@@ -185,13 +185,17 @@ class TracerProvider(_TracerProvider):
         if verbose:
             print(self._tracing_details())
 
-    def add_span_processor(self, *args: Any, **kwargs: Any) -> None:
+    def add_span_processor(
+        self, *args: Any, replace_default_processor: bool = True, **kwargs: Any
+    ) -> None:
         """
         Registers a new `SpanProcessor` for this `TracerProvider`.
 
         If this `TracerProvider` has a default processor, it will be removed.
         """
 
+        if replace_default_processor:
+            self._default_processor = False
         if self._default_processor:
             self._active_span_processor.shutdown()
             self._active_span_processor._span_processors = tuple()  # remove default processors
