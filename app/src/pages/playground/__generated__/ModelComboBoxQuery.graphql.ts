1980f65bfca874a94be049b562142ff9c85ed0db
Mikyo King <mikyo@arize.com>
2025-06-05 17:09:38 -0600

    feat(cost): add graphql model resolver (#7947)
    * feat(cost): add graphql model resolver
    
    * ruff
    
    * add missing
    
    * relay rebuild
    
    * cleanup

--- app/src/pages/playground/__generated__/ModelComboBoxQuery.graphql.ts
+++ app/src/pages/playground/__generated__/ModelComboBoxQuery.graphql.ts
@@ -14,7 +14,7 @@ export type ModelComboBoxQuery$variables = {
   providerKey: GenerativeProviderKey;
 };
 export type ModelComboBoxQuery$data = {
-  readonly models: ReadonlyArray<{
+  readonly playgroundModels: ReadonlyArray<{
     readonly name: string;
   }>;
 };
@@ -47,9 +47,9 @@ v1 = [
         "name": "input"
       }
     ],
-    "concreteType": "GenerativeModel",
+    "concreteType": "PlaygroundModel",
     "kind": "LinkedField",
-    "name": "models",
+    "name": "playgroundModels",
     "plural": true,
     "selections": [
       {
@@ -81,16 +81,16 @@ return {
     "selections": (v1/*: any*/)
   },
   "params": {
-    "cacheID": "a3417a3b2ae9ad193a5fd68335cfd660",
+    "cacheID": "67fe753e1bc2ede814f616699bf7f3bb",
     "id": null,
     "metadata": {},
     "name": "ModelComboBoxQuery",
     "operationKind": "query",
-    "text": "query ModelComboBoxQuery(\n  $providerKey: GenerativeProviderKey!\n) {\n  models(input: {providerKey: $providerKey}) {\n    name\n  }\n}\n"
+    "text": "query ModelComboBoxQuery(\n  $providerKey: GenerativeProviderKey!\n) {\n  playgroundModels(input: {providerKey: $providerKey}) {\n    name\n  }\n}\n"
   }
 };
 })();
 
-(node as any).hash = "fc5563c6dca10fdeda807cd7061e275a";
+(node as any).hash = "1e812293e1547b594275780290b294be";
 
 export default node;
