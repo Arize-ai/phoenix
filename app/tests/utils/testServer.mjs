cbc588497ded8e848b60a660e59ed63aa7e260eb
Tony Powell <apowell@arize.com>
2025-06-10 18:45:54 -0400

    with openai

--- app/tests/utils/testServer.mjs
+++ app/tests/utils/testServer.mjs
@@ -22,7 +22,7 @@ console.log("PHOENIX_WORKING_DIR: ", process.env["PHOENIX_WORKING_DIR"]);
 
 // Execute the server
 const childProcess = exec(
-  "uv tool run --with=arize-phoenix-evals@../packages/phoenix-evals --with=arize-phoenix-otel@../packages/phoenix-otel --project=.. arize-phoenix serve"
+  "uv tool run --with=openai --with=arize-phoenix-evals@../packages/phoenix-evals --with=arize-phoenix-otel@../packages/phoenix-otel --project=.. arize-phoenix serve"
 );
 
 childProcess.stdout?.pipe(process.stdout);
