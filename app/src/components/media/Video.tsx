6bea7127a94f0008b4c1f4e584f3b114611b88f6
Tony Powell <apowell@arize.com>
2025-06-10 15:23:14 -0400

    temporarily disable video content

--- app/src/components/media/Video.tsx
+++ app/src/components/media/Video.tsx
@@ -61,6 +61,11 @@ export const Video: React.FC<VideoProps> = ({
   preload = "none",
   className,
 }) => {
+  // disable video to debug playwright tests
+  // eslint-disable-next-line no-constant-condition
+  if (true) {
+    return null;
+  }
   return (
     <video
       css={videoCSS}
