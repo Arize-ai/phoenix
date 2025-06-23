954d5696f11e49c2c991693e84f89f026dadd739
Tony Powell <apowell@arize.com>
2025-06-23 19:42:17 -0400

    Refactor SegmentChart to plain HTML

--- app/src/components/chart/SegmentChart.tsx
+++ app/src/components/chart/SegmentChart.tsx
@@ -1,8 +1,21 @@
-import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
+import { css } from "@emotion/react";
 
 import { getChartColor, useChartColors } from "./colors";
 
-const DEFAULT_RADIUS: [number, number, number, number] = [8, 8, 8, 8];
+const chartContainerCSS = css`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  overflow: hidden;
+  border-radius: 8px;
+  gap: 4px;
+`;
+
+const chartSegmentCSS = css`
+  height: 100%;
+  flex-shrink: 0;
+  flex-grow: 0;
+`;
 
 export type SegmentChartProps = {
   /**
@@ -43,62 +56,23 @@ export const SegmentChart = ({
   // this is useful for cases where the total value is not known ahead of time
   const totalValue =
     _totalValue ?? segments.reduce((acc, segment) => acc + segment.value, 0);
-  // Transform the segments into a format that recharts can use
-  const data = [
-    segments.reduce(
-      (acc, segment) => {
-        acc[segment.name] = segment.value / totalValue;
-        return acc;
-      },
-      {} as Record<string, number>
-    ),
-  ];
-  // map the segment names to their colors
-  const colorByNameMap = segments.reduce(
-    (acc, segment, index) => {
-      acc[segment.name] = segment.color || getChartColor(index, colors);
-      return acc;
-    },
-    {} as Record<string, string>
-  );
 
   return (
-    <div style={{ width: "100%", height }}>
-      <ResponsiveContainer width="100%" height={height}>
-        <BarChart
-          style={{ display: "flex" }}
-          margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
-          data={data}
-          barSize="100%"
-          layout="vertical"
-        >
-          {/* recharts has a weird bug where you can not render a vertical bar chart without x and y axes */}
-          {/* so we hide them and use the dataKey to set the x and y axes */}
-          <XAxis type="number" hide />
-          <YAxis type="category" hide />
-          {Object.keys(data[0]).map((name, index, arr) => {
-            const first = index === 0;
-            const last = index === arr.length - 1;
-            // round the corners of terminal bars, but not the center bars
-            const radius: [number, number, number, number] = first
-              ? [8, 0, 0, 8]
-              : last
-                ? [0, 8, 8, 0]
-                : [0, 0, 0, 0];
-            return (
-              <Bar
-                key={name}
-                dataKey={name}
-                fill={colorByNameMap[name]}
-                stackId="a"
-                isAnimationActive={false}
-                // if there is only one segment, we want to round all corners
-                radius={arr.length > 1 ? radius : DEFAULT_RADIUS}
-              />
-            );
-          })}
-        </BarChart>
-      </ResponsiveContainer>
+    <div style={{ height: `${height}px` }} css={chartContainerCSS}>
+      {segments.map((segment, index) => {
+        const percentage = (segment.value / totalValue) * 100;
+        const color = segment.color || getChartColor(index, colors);
+        return (
+          <div
+            key={segment.name}
+            css={chartSegmentCSS}
+            style={{
+              width: `${percentage}%`,
+              backgroundColor: color,
+            }}
+          />
+        );
+      })}
     </div>
   );
 };
