5431a203def2fd3c79af5df18ab4ee11cd9bef37
Xander Song <axiomofjoy@gmail.com>
2025-06-24 16:38:36 -0700

    fix(cost): disable sorting on trace cost (#8287)
    * fix(cost): disable sorting on trace total cost
    
    * fix(cost): add model cost

--- app/src/pages/project/TracesTable.tsx
+++ app/src/pages/project/TracesTable.tsx
@@ -667,6 +667,7 @@ export function TracesTable(props: TracesTableProps) {
         header: "total cost",
         minSize: 80,
         accessorKey: "cumulativeCostSummary.total.cost",
+        enableSorting: false,
         cell: ({ row, getValue }) => {
           const value = getValue();
           if (value === null || typeof value !== "number") {
