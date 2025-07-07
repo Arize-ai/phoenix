85b4456140eeae771bbb0b2222033564a1215c2a
Roger Yang <roger.yang@arize.com>
2025-07-07 09:58:46 -0700

    use FAILED_PRECONDITION

--- src/phoenix/server/app.py
+++ src/phoenix/server/app.py
@@ -855,7 +855,7 @@ class DbDiskUsageInterceptor(AsyncServerInterceptor):
             and self._db.should_not_insert_or_update
         ):
             await context.abort(
-                grpc.StatusCode.RESOURCE_EXHAUSTED,
+                grpc.StatusCode.FAILED_PRECONDITION,
                 "Database disk usage threshold exceeded",
             )
         return await method(request_or_iterator, context)
