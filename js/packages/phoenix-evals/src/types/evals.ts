c429fdcc9e886ff7927621cdbc8191b863f07cc9
Mikyo King <mikyo@arize.com>
2025-07-07 22:30:16 -0600

    add types

---
+++ js/packages/phoenix-evals/src/types/evals.ts
@@ -0,0 +1,35 @@
+/**
+ * The arguments for an evaluation
+ */
+export interface EvaluationArgs<OutputType, InputType> {
+  output: OutputType;
+  expected?: OutputType;
+  input: InputType;
+}
+
+/**
+ * The result of an evaluation
+ */
+export interface EvaluationResult {
+  /**
+   * The score of the evaluation.
+   * e.x. 0.95
+   */
+  score: number;
+  /**
+   * The label of the evaluation.
+   * e.x. "correct"
+   */
+  label: string;
+  /**
+   * The explanation of the evaluation.
+   * e.x. "The model correctly identified the sentiment of the text."
+   */
+  explanation: string;
+}
+
+/**
+ * The result of a classification evaluation
+ */
+export interface ClassificationEvaluationResult
+  extends Omit<EvaluationResult, "score"> {}
