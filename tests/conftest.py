9d210241cd92be099b179748fdb6a51dacb93469
Roger Yang <80478925+RogerHYang@users.noreply.github.com>
2025-07-09 12:42:12 -0700

    ci: use in-memory sqlite for unit tests (#8493)

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,11 @@ from _pytest.config.argparsing import Parser
 
 def pytest_addoption(parser: Parser) -> None:
     parser.addoption(
+        "--sqlite-on-disk",
+        action="store_true",
+        help="Run tests using file-based SQLite",
+    )
+    parser.addoption(
         "--run-postgres",
         action="store_true",
         help="Run tests that require Postgres",
