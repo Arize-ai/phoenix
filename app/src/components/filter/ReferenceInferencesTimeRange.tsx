02210949c4b4440b64ccf675aad60f571158f8e0
Yasmine Frigui <yfrigui@arize.com>
2025-07-10 11:37:00 -0400

    migrate embeddings page tooltips

--- app/src/components/filter/ReferenceInferencesTimeRange.tsx
+++ app/src/components/filter/ReferenceInferencesTimeRange.tsx
@@ -1,14 +1,16 @@
 import { timeFormat } from "d3-time-format";
 import { css } from "@emotion/react";
 
-import {
-  FieldColorDesignation,
-  Tooltip,
-  TooltipTrigger,
-  TriggerWrap,
-} from "@arizeai/components";
+import { FieldColorDesignation } from "@arizeai/components";
 
-import { Input, Label, TextField } from "@phoenix/components";
+import {
+  Input,
+  Label,
+  TextField,
+  Tooltip,
+  TooltipArrow,
+  TooltipTrigger,
+} from "@phoenix/components";
 import { useInferences } from "@phoenix/contexts";
 
 const timeFormatter = timeFormat("%x %X");
@@ -36,19 +38,20 @@ export function ReferenceInferencesTimeRange({
     >
       <FieldColorDesignation color={"designationPurple"}>
         <TooltipTrigger>
-          <TriggerWrap>
-            <TextField
-              isReadOnly
-              aria-label={"reference inferences time range"}
-              value={`${timeFormatter(timeRange.start)} - ${timeFormatter(
-                timeRange.end
-              )}`}
-            >
-              <Label>{`${nameAbbr} inferences`}</Label>
-              <Input />
-            </TextField>
-          </TriggerWrap>
-          <Tooltip>The static time range of the reference inferences</Tooltip>
+          <TextField
+            isReadOnly
+            aria-label={"reference inferences time range"}
+            value={`${timeFormatter(timeRange.start)} - ${timeFormatter(
+              timeRange.end
+            )}`}
+          >
+            <Label>{`${nameAbbr} inferences`}</Label>
+            <Input />
+          </TextField>
+          <Tooltip>
+            <TooltipArrow />
+            The static time range of the reference inferences
+          </Tooltip>
         </TooltipTrigger>
       </FieldColorDesignation>
     </div>
