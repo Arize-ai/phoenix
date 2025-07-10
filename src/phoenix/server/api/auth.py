66e1e4ef1714efc90db9f80d377857f9658a071a
Roger Yang <roger.yang@arize.com>
2025-07-09 22:18:02 -0700

    clean up

--- src/phoenix/server/api/auth.py
+++ src/phoenix/server/api/auth.py
@@ -20,7 +20,7 @@ class IsNotReadOnly(Authorization):
         return not info.context.read_only
 
 
-class IsLocked(BasePermission):
+class IsLocked(Authorization):
     """
     Disables mutations and subscriptions that create or update data but allows
     queries and delete mutations.
