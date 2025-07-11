5eaad801919e40f7eda030327cae8e0b79bc1828
Mikyo King <mikyo@arize.com>
2025-07-11 16:11:31 -0600

    wip

--- app/.storybook/preview.tsx
+++ app/.storybook/preview.tsx
@@ -1,8 +1,29 @@
 import type { Preview } from "@storybook/react";
-import { Provider } from "@arizeai/components";
+import { Provider, ProviderTheme } from "@arizeai/components";
 import { GlobalStyles } from "../src/GlobalStyles";
 import { ThemeProvider } from "../src/contexts";
 import { MemoryRouter } from "react-router";
+import React, { PropsWithChildren } from "react";
+
+/**
+ * A Component that renders a background for the story based on the theme
+ * @returns
+ */
+function StoryBackground() {
+  return (
+    <div
+      data-testid="story-background"
+      style={{
+        backgroundColor: "var(--ac-global-background-color-default)",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        position: "relative",
+      }}
+    />
+  );
+}
 const preview: Preview = {
   parameters: {
     controls: {
@@ -22,7 +43,7 @@ const preview: Preview = {
         title: "Theme",
         icon: "paintbrush",
         // Array of plain string values or MenuItem shape (see below)
-        items: ["light", "dark"],
+        items: ["light", "dark", "all"],
         // Change title based on selected value
         dynamicTitle: true,
       },
@@ -36,16 +57,42 @@ const preview: Preview = {
   decorators: [
     // 👇 Defining the decorator in the preview file applies it to all stories
     (Story, { parameters, globals }) => {
-      const theme = globals.theme || "light";
+      let themes: ProviderTheme[] = ["light"];
+      if (globals.theme === "all") {
+        themes = ["light", "dark"];
+      } else {
+        themes = [globals.theme];
+      }
+
+      const contents = themes.map((theme) => (
+        <div
+          key={theme}
+          style={{ width: "100%", height: "100%", position: "absolute" }}
+        >
+          <Provider theme={theme} mountGlobalStyles={false}>
+            <ThemeProvider theme={theme}>
+              <MemoryRouter initialEntries={["/"]}>
+                <GlobalStyles />
+                <StoryBackground />
+                <Story />
+              </MemoryRouter>
+            </ThemeProvider>
+          </Provider>
+        </div>
+      ));
       return (
-        <Provider theme={theme} mountGlobalStyles={false}>
-          <ThemeProvider theme={theme}>
-            <MemoryRouter initialEntries={["/"]}>
-              <GlobalStyles />
-              <Story />
-            </MemoryRouter>
-          </ThemeProvider>
-        </Provider>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            gap: "2rem",
+            width: "100%",
+            height: "100%",
+            minHeight: globals.theme === "all" ? "100vh" : "auto",
+          }}
+        >
+          {contents}
+        </div>
       );
     },
   ],
