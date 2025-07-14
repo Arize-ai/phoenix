414a9e4e4ee50985a371cb5ba77a4d363e96257d
Mikyo King <mikyo@arize.com>
2025-07-14 14:23:25 -0600

    feat(storybook): split mode (#8575)
    * wip
    
    * feat(storybook): add all theme mode for split view
    
    * remove div

--- app/.storybook/preview.tsx
+++ app/.storybook/preview.tsx
@@ -1,8 +1,33 @@
 import type { Preview } from "@storybook/react";
-import { Provider } from "@arizeai/components";
+import { Provider, ProviderTheme } from "@arizeai/components";
 import { GlobalStyles } from "../src/GlobalStyles";
 import { ThemeProvider } from "../src/contexts";
 import { MemoryRouter } from "react-router";
+import React, { PropsWithChildren } from "react";
+
+/**
+ * A Component that renders a background for the story based on the theme
+ * @returns
+ */
+function StoryBackground({
+  children,
+  padding,
+}: {
+  children: React.ReactNode;
+  padding?: string;
+}) {
+  return (
+    <div
+      data-testid="story-background"
+      style={{
+        backgroundColor: "var(--ac-global-background-color-default)",
+        padding: padding ?? "0",
+      }}
+    >
+      {children}
+    </div>
+  );
+}
 const preview: Preview = {
   parameters: {
     controls: {
@@ -22,7 +47,7 @@ const preview: Preview = {
         title: "Theme",
         icon: "paintbrush",
         // Array of plain string values or MenuItem shape (see below)
-        items: ["light", "dark"],
+        items: ["light", "dark", "all"],
         // Change title based on selected value
         dynamicTitle: true,
       },
@@ -36,16 +61,51 @@ const preview: Preview = {
   decorators: [
     // 👇 Defining the decorator in the preview file applies it to all stories
     (Story, { parameters, globals }) => {
-      const theme = globals.theme || "light";
-      return (
+      let themes: ProviderTheme[] = ["light"];
+      if (globals.theme === "all") {
+        themes = ["light", "dark"];
+      } else {
+        themes = [globals.theme];
+      }
+
+      const numThemes = themes.length;
+
+      const contents = themes.map((theme) => (
         <Provider theme={theme} mountGlobalStyles={false}>
           <ThemeProvider theme={theme}>
             <MemoryRouter initialEntries={["/"]}>
               <GlobalStyles />
-              <Story />
+              <StoryBackground
+                padding={
+                  // Add padding to the story entries if we have multiple themes so that we can see a background color
+                  numThemes < 1 ? "0" : "var(--ac-global-dimension-size-500)"
+                }
+              >
+                <Story />
+              </StoryBackground>
             </MemoryRouter>
           </ThemeProvider>
         </Provider>
+      ));
+
+      // If we only have one theme, we can just return the story
+      if (numThemes === 1) {
+        return contents[0];
+      }
+
+      return (
+        <ul
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            width: "100%",
+            height: "100%",
+          }}
+        >
+          {contents.map((content, index) => (
+            <li key={index}>{content}</li>
+          ))}
+        </ul>
       );
     },
   ],
