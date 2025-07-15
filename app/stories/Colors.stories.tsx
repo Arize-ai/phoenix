a8031f557fe889e996a603d19f16a52d33fc581b
Rick Steele <rsteele@arize.com>
2025-07-15 10:25:53 -0700

    Add: enhanced docs for colors

--- app/stories/Colors.stories.tsx
+++ app/stories/Colors.stories.tsx
@@ -1,3 +1,4 @@
+import { Canvas, Title } from "@storybook/blocks";
 import { Meta, StoryFn } from "@storybook/react";
 
 import { Flex, View } from "@phoenix/components";
@@ -15,6 +16,40 @@ import {
 
 const meta: Meta = {
   title: "Colors",
+  parameters: {
+    docs: {
+      page: () => (
+        <>
+          <Title />
+          <Canvas of={Default} />
+
+          <h2>Categorical Chart Colors</h2>
+          <p>
+            Use categorical chart colors when you want to represent discrete
+            segments. The categorical colors are ordered intentionally and meant
+            to be used in sequence, defaulting to start with colors closer to
+            our brand, but with room to expand to up to 12 discrete categories.
+          </p>
+          <Canvas of={CategorialChart} />
+
+          <h2>Semantic Chart Colors</h2>
+          <p>
+            Use semantic chart colors when you want express an opinion about an
+            outcome, labelelling it specifically as positive, negative, or
+            neutral.
+          </p>
+          <h4>Mixing semantic and categorical colors.</h4>
+          <p>
+            Semantic colors purposefully do not overlap with categorical colors,
+            allowing them to be used side by side. For example, you could show
+            muliple types of tool call using categorical colors, while using the
+            semantic-negative color to indicate calls that failed.
+          </p>
+          <Canvas of={SemanticChart} />
+        </>
+      ),
+    },
+  },
 };
 
 export default meta;
