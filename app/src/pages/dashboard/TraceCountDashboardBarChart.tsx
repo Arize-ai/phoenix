8bec6ae973adb86d72d4a00f6b6cdfb1a0e1241a
Mikyo King <mikyo@arize.com>
2025-07-15 15:12:57 -0600

    move constant times

--- app/src/pages/dashboard/TraceCountDashboardBarChart.tsx
+++ app/src/pages/dashboard/TraceCountDashboardBarChart.tsx
@@ -2,18 +2,19 @@ import { useMemo } from "react";
 import { graphql, useLazyLoadQuery } from "react-relay";
 
 import { useTimeRange } from "@phoenix/components/datetime";
+import {
+  ONE_DAY_SEC,
+  ONE_HOUR_SEC,
+  ONE_MONTH_SEC,
+  ONE_WEEK_SEC,
+  ONE_YEAR_SEC,
+} from "@phoenix/constants/timeConstants";
 
 import type {
   TimeBinScale,
   TraceCountDashboardBarChartQuery,
 } from "./__generated__/TraceCountDashboardBarChartQuery.graphql";
 import { DashboardBarChart } from "./DashboardBarChart";
-
-const YEAR = 60 * 60 * 24 * 365;
-const MONTH = 60 * 60 * 24 * 30;
-const WEEK = 60 * 60 * 24 * 7;
-const DAY = 60 * 60 * 24;
-const HOUR = 60 * 60;
 
 export function TraceCountDashboardBarChart({
   projectId,
@@ -32,15 +33,15 @@ export function TraceCountDashboardBarChart({
     if (startTime) {
       const endTime = timeRange.end || new Date();
       const duration = (endTime.getTime() - startTime.getTime()) / 1000; // in seconds
-      if (duration > 5 * YEAR) {
+      if (duration > 5 * ONE_YEAR_SEC) {
         scale = "YEAR";
-      } else if (duration > 5 * MONTH) {
+      } else if (duration > 5 * ONE_MONTH_SEC) {
         scale = "MONTH";
-      } else if (duration > 5 * WEEK) {
+      } else if (duration > 5 * ONE_WEEK_SEC) {
         scale = "WEEK";
-      } else if (duration > 5 * DAY) {
+      } else if (duration > 5 * ONE_DAY_SEC) {
         scale = "DAY";
-      } else if (duration > 5 * HOUR) {
+      } else if (duration > 5 * ONE_HOUR_SEC) {
         scale = "HOUR";
       } else {
         scale = "MINUTE";
