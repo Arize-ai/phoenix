4200b5d435d780fd9bad3db5fd79677c5fa271c2
Alexander Song <axiomofjoy@gmail.com>
2025-07-15 16:06:42 -0700

    nix value

--- src/phoenix/server/api/types/Project.py
+++ src/phoenix/server/api/types/Project.py
@@ -820,7 +820,7 @@ class Project(Node):
             utc_offset_minutes=utc_offset_minutes,
         ):
             if timestamp not in data:
-                data[timestamp] = TimeSeriesDataPoint(timestamp=timestamp, value=0.0)
+                data[timestamp] = TimeSeriesDataPoint(timestamp=timestamp)
         return TraceCountTimeSeries(data=sorted(data.values(), key=lambda x: x.timestamp))
 
 
