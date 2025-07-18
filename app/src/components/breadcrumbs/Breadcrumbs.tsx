eab7c6c3d9ddd4ac7dc2b713de5814dda4dd617a
Donny Li <geli2001@berkeley.edu>
2025-07-18 10:58:00 -0700

    fix to react-area-components

--- app/src/components/breadcrumbs/Breadcrumbs.tsx
+++ app/src/components/breadcrumbs/Breadcrumbs.tsx
@@ -1,67 +1,43 @@
-import React, { createContext, forwardRef, ReactNode, useContext } from "react";
-import { useBreadcrumbItem, useBreadcrumbs } from "react-aria";
-
-// Context to share breadcrumb state
-interface BreadcrumbsContextValue {
-  onAction?: (index: string | number) => void;
-}
-
-const BreadcrumbsContext = createContext<BreadcrumbsContextValue>({});
+import React from "react";
+import {
+  Breadcrumb as AriaBreadcrumb,
+  Breadcrumbs as AriaBreadcrumbs,
+  Link,
+} from "react-aria-components";
 
 export interface BreadcrumbsProps {
-  children: ReactNode;
+  children: React.ReactNode;
   onAction?: (index: string | number) => void;
 }
 
 export interface BreadcrumbProps {
-  children: ReactNode;
+  children: React.ReactNode;
   href?: string;
   onPress?: () => void;
   isDisabled?: boolean;
   isCurrent?: boolean;
 }
 
-export const Breadcrumbs = forwardRef<HTMLElement, BreadcrumbsProps>(
-  function Breadcrumbs({ children, onAction, ...props }, ref) {
-    const { navProps } = useBreadcrumbs(props);
+export const Breadcrumbs = ({ children, ...props }: BreadcrumbsProps) => {
+  return <AriaBreadcrumbs {...props}>{children}</AriaBreadcrumbs>;
+};
 
-    return (
-      <BreadcrumbsContext.Provider value={{ onAction }}>
-        <nav {...navProps} ref={ref}>
-          <ol>{children}</ol>
-        </nav>
-      </BreadcrumbsContext.Provider>
-    );
-  }
-);
-
-export const Breadcrumb = forwardRef<HTMLLIElement, BreadcrumbProps>(
-  function Breadcrumb(
-    { children, href, onPress, isDisabled, isCurrent, ...props },
-    ref
-  ) {
-    const { onAction } = useContext(BreadcrumbsContext);
-    const { itemProps } = useBreadcrumbItem(
-      {
-        children,
-        onPress: onPress || (() => href && onAction?.(href)),
-        isDisabled,
-        isCurrent,
-        ...props,
-      },
-      ref as React.RefObject<HTMLElement>
-    );
-
-    return (
-      <li ref={ref}>
-        {href && !isCurrent ? (
-          <a href={href} {...itemProps}>
-            {children}
-          </a>
-        ) : (
-          <span {...itemProps}>{children}</span>
-        )}
-      </li>
-    );
-  }
-);
+export const Breadcrumb = ({
+  children,
+  href,
+  isDisabled,
+  isCurrent,
+  ...props
+}: BreadcrumbProps) => {
+  return (
+    <AriaBreadcrumb {...props}>
+      {href && !isCurrent ? (
+        <Link href={href} isDisabled={isDisabled}>
+          {children}
+        </Link>
+      ) : (
+        <span>{children}</span>
+      )}
+    </AriaBreadcrumb>
+  );
+};
