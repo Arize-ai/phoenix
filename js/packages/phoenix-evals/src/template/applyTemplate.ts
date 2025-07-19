9d4c84b40e9d6ef18c6b12b64f4e635d8dd27f77
Mikyo King <mikyo@arize.com>
2025-07-19 13:11:29 -0600

    fix mustache

--- js/packages/phoenix-evals/src/template/applyTemplate.ts
+++ js/packages/phoenix-evals/src/template/applyTemplate.ts
@@ -1,5 +1,5 @@
 import { Template } from "../types/templating";
-import { render } from "mustache";
+import Mustache from "mustache";
 
 /**
  * A function that applies a set of variables to a template (e.g. a prompt)
@@ -10,5 +10,5 @@ export function formatTemplate(args: {
   variables: Record<string, unknown>;
 }) {
   const { template, variables } = args;
-  return render(template, variables);
+  return Mustache.render(template, variables);
 }
