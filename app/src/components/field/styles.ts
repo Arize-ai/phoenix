6ba948cb874f29c4d0bc4ccab687c65c78843406
Mikyo King <mikyo@arize.com>
2025-07-21 16:10:25 -0600

    cleanup testfield (#8702)

--- app/src/components/field/styles.ts
+++ app/src/components/field/styles.ts
@@ -85,32 +85,17 @@ export const textFieldCSS = css`
   display: flex;
   flex-direction: column;
   width: 100%;
-  --icon-alignment-offset: 22%;
-  --icon-size: var(--ac-global-font-size-s);
-
-  &:has(label) {
-    --icon-alignment-offset: 52%;
-  }
 
   &[data-size="S"] {
     --textfield-input-height: 30px;
     --textfield-vertical-padding: 6px;
     --textfield-horizontal-padding: 6px;
-    --icon-size: var(--ac-global-font-size-s);
-    --icon-alignment-offset: 22%;
-    &:has(label) {
-      --icon-alignment-offset: 52%;
-    }
   }
   &[data-size="M"] {
     --textfield-input-height: 38px;
     --textfield-vertical-padding: 10px;
     --textfield-horizontal-padding: var(--ac-global-dimension-static-size-200);
     --icon-size: var(--ac-global-font-size-l);
-    --icon-alignment-offset: 24%;
-    &:has(label) {
-      --icon-alignment-offset: 52%;
-    }
   }
 
   &:has(.ac-icon-wrap) {
@@ -147,14 +132,5 @@ export const textFieldCSS = css`
   .react-aria-Input {
     /* TODO: remove this sizing */
     height: var(--textfield-input-height);
-  }
-
-  .ac-icon-wrap {
-    font-size: var(--icon-size);
-    position: absolute;
-    right: var(--textfield-horizontal-padding);
-    top: var(--icon-alignment-offset);
-    transform: translateY(calc(var(--icon-alignment-offset) * -1));
-    color: var(--ac-global-color-info);
   }
 `;
