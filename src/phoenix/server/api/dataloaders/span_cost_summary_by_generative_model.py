ba7c4f8adc40cc268e8156d7ebdb1cb06a34d9fc
Alexander Song <axiomofjoy@gmail.com>
2025-07-22 14:56:07 -0700

    restore dataloaders

--- src/phoenix/server/api/dataloaders/span_cost_summary_by_generative_model.py
+++ src/phoenix/server/api/dataloaders/span_cost_summary_by_generative_model.py
@@ -1,5 +1,4 @@
 from collections import defaultdict
-from typing import Optional
 
 from sqlalchemy import func, select
 from sqlalchemy.sql.functions import coalesce
@@ -11,8 +10,7 @@ from phoenix.server.api.dataloaders.types import CostBreakdown, SpanCostSummary
 from phoenix.server.types import DbSessionFactory
 
 GenerativeModelId: TypeAlias = int
-ProjectId: TypeAlias = int
-Key: TypeAlias = tuple[GenerativeModelId, Optional[ProjectId]]
+Key: TypeAlias = GenerativeModelId
 Result: TypeAlias = SpanCostSummary
 
 
@@ -22,11 +20,10 @@ class SpanCostSummaryByGenerativeModelDataLoader(DataLoader[Key, Result]):
         self._db = db
 
     async def _load_fn(self, keys: list[Key]) -> list[Result]:
-        model_ids = [key[0] for key in keys]
+        pk = models.SpanCost.model_id
         stmt = (
             select(
-                models.SpanCost.model_id,
-                models.Trace.project_rowid,
+                pk,
                 coalesce(func.sum(models.SpanCost.prompt_cost), 0).label("prompt_cost"),
                 coalesce(func.sum(models.SpanCost.completion_cost), 0).label("completion_cost"),
                 coalesce(func.sum(models.SpanCost.total_cost), 0).label("total_cost"),
@@ -34,41 +31,25 @@ class SpanCostSummaryByGenerativeModelDataLoader(DataLoader[Key, Result]):
                 coalesce(func.sum(models.SpanCost.completion_tokens), 0).label("completion_tokens"),
                 coalesce(func.sum(models.SpanCost.total_tokens), 0).label("total_tokens"),
             )
-            .join(models.Trace, models.Trace.id == models.SpanCost.trace_rowid)
-            .where(models.SpanCost.model_id.in_(model_ids))
-            .group_by(models.SpanCost.model_id, models.Trace.project_rowid)
+            .where(pk.in_(keys))
+            .group_by(pk)
         )
-        data: dict[GenerativeModelId, dict[ProjectId, Result]] = {}
-
+        results: defaultdict[Key, Result] = defaultdict(SpanCostSummary)
         async with self._db() as session:
+            data = await session.stream(stmt)
             async for (
-                model_id,
-                project_id,
+                id_,
                 prompt_cost,
                 completion_cost,
                 total_cost,
                 prompt_tokens,
                 completion_tokens,
                 total_tokens,
-            ) in await session.stream(stmt):
+            ) in data:
                 summary = SpanCostSummary(
                     prompt=CostBreakdown(tokens=prompt_tokens, cost=prompt_cost),
                     completion=CostBreakdown(tokens=completion_tokens, cost=completion_cost),
                     total=CostBreakdown(tokens=total_tokens, cost=total_cost),
                 )
-                if model_id not in data:
-                    data[model_id] = {}
-                data[model_id][project_id] = summary
-
-        results: defaultdict[Key, Result] = defaultdict(SpanCostSummary)
-        for model_id, project_id in keys:
-            if model_id not in data:
-                continue
-            if project_id is None:
-                print(data[model_id])
-                results[(model_id, project_id)] = sum(
-                    data[model_id].values(), start=SpanCostSummary()
-                )
-            else:
-                results[(model_id, project_id)] = data[model_id][project_id]
-        return [results.get(key, SpanCostSummary()) for key in keys]
+                results[id_] = summary
+        return list(map(results.__getitem__, keys))
--- src/phoenix/server/api/dataloaders/types.py
+++ src/phoenix/server/api/dataloaders/types.py
@@ -14,24 +14,12 @@ class CostBreakdown:
             return self.cost / self.tokens
         return None
 
-    def __add__(self, other: "CostBreakdown") -> "CostBreakdown":
-        tokens = (self.tokens or 0) + (other.tokens or 0)
-        cost = (self.cost or 0) + (other.cost or 0)
-        return CostBreakdown(tokens=tokens, cost=cost)
-
 
 @dataclass(frozen=True)
 class SpanCostSummary:
     prompt: CostBreakdown = field(default_factory=CostBreakdown)
     completion: CostBreakdown = field(default_factory=CostBreakdown)
     total: CostBreakdown = field(default_factory=CostBreakdown)
-
-    def __add__(self, other: "SpanCostSummary") -> "SpanCostSummary":
-        return SpanCostSummary(
-            prompt=self.prompt + other.prompt,
-            completion=self.completion + other.completion,
-            total=self.total + other.total,
-        )
 
 
 @dataclass(frozen=True)
