e82cde3d26e580b7023a96bb37024f3be1bfe1f0
Dustin Ngo <dustin@arize.com>
2025-07-24 15:49:09 -0400

    Reorder imports

--- packages/phoenix-evals/src/phoenix/evals/__init__.py
+++ packages/phoenix-evals/src/phoenix/evals/__init__.py
@@ -6,6 +6,62 @@ from importlib.metadata import version
 from types import ModuleType
 from typing import Any, Dict, List, Optional, Set
 
+from .classify import llm_classify, run_evals
+from .default_templates import (
+    CODE_FUNCTIONALITY_PROMPT_RAILS_MAP,
+    CODE_FUNCTIONALITY_PROMPT_TEMPLATE,
+    CODE_READABILITY_PROMPT_RAILS_MAP,
+    CODE_READABILITY_PROMPT_TEMPLATE,
+    HALLUCINATION_PROMPT_RAILS_MAP,
+    HALLUCINATION_PROMPT_TEMPLATE,
+    HUMAN_VS_AI_PROMPT_RAILS_MAP,
+    HUMAN_VS_AI_PROMPT_TEMPLATE,
+    QA_PROMPT_RAILS_MAP,
+    QA_PROMPT_TEMPLATE,
+    RAG_RELEVANCY_PROMPT_RAILS_MAP,
+    RAG_RELEVANCY_PROMPT_TEMPLATE,
+    REFERENCE_LINK_CORRECTNESS_PROMPT_RAILS_MAP,
+    REFERENCE_LINK_CORRECTNESS_PROMPT_TEMPLATE,
+    SQL_GEN_EVAL_PROMPT_RAILS_MAP,
+    SQL_GEN_EVAL_PROMPT_TEMPLATE,
+    TOOL_CALLING_PROMPT_RAILS_MAP,
+    TOOL_CALLING_PROMPT_TEMPLATE,
+    TOXICITY_PROMPT_RAILS_MAP,
+    TOXICITY_PROMPT_TEMPLATE,
+    USER_FRUSTRATION_PROMPT_RAILS_MAP,
+    USER_FRUSTRATION_PROMPT_TEMPLATE,
+)
+from .evaluators import (
+    HallucinationEvaluator,
+    LLMEvaluator,
+    QAEvaluator,
+    RelevanceEvaluator,
+    SQLEvaluator,
+    SummarizationEvaluator,
+    ToxicityEvaluator,
+)
+from .generate import llm_generate
+from .models import (
+    AnthropicModel,
+    BedrockModel,
+    GeminiModel,
+    LiteLLMModel,
+    MistralAIModel,
+    OpenAIModel,
+    VertexAIModel,
+)
+from .retrievals import compute_precisions_at_k
+from .span_templates import (
+    HALLUCINATION_SPAN_PROMPT_TEMPLATE,
+    QA_SPAN_PROMPT_TEMPLATE,
+    TOOL_CALLING_SPAN_PROMPT_TEMPLATE,
+)
+from .templates import (
+    ClassificationTemplate,
+    PromptTemplate,
+)
+from .utils import NOT_PARSABLE, download_benchmark_dataset
+
 EXPERIMENTAL_MODULES: Dict[str, Dict[str, Any]] = {
     "phoenix.evals.llm": {
         "warning_message": (
@@ -109,62 +165,7 @@ class ExperimentalModuleLoader(Loader):
 
 sys.meta_path.insert(0, ExperimentalModuleFinder())
 
-from . import llm  # noqa: F401
-from .classify import llm_classify, run_evals
-from .default_templates import (
-    CODE_FUNCTIONALITY_PROMPT_RAILS_MAP,
-    CODE_FUNCTIONALITY_PROMPT_TEMPLATE,
-    CODE_READABILITY_PROMPT_RAILS_MAP,
-    CODE_READABILITY_PROMPT_TEMPLATE,
-    HALLUCINATION_PROMPT_RAILS_MAP,
-    HALLUCINATION_PROMPT_TEMPLATE,
-    HUMAN_VS_AI_PROMPT_RAILS_MAP,
-    HUMAN_VS_AI_PROMPT_TEMPLATE,
-    QA_PROMPT_RAILS_MAP,
-    QA_PROMPT_TEMPLATE,
-    RAG_RELEVANCY_PROMPT_RAILS_MAP,
-    RAG_RELEVANCY_PROMPT_TEMPLATE,
-    REFERENCE_LINK_CORRECTNESS_PROMPT_RAILS_MAP,
-    REFERENCE_LINK_CORRECTNESS_PROMPT_TEMPLATE,
-    SQL_GEN_EVAL_PROMPT_RAILS_MAP,
-    SQL_GEN_EVAL_PROMPT_TEMPLATE,
-    TOOL_CALLING_PROMPT_RAILS_MAP,
-    TOOL_CALLING_PROMPT_TEMPLATE,
-    TOXICITY_PROMPT_RAILS_MAP,
-    TOXICITY_PROMPT_TEMPLATE,
-    USER_FRUSTRATION_PROMPT_RAILS_MAP,
-    USER_FRUSTRATION_PROMPT_TEMPLATE,
-)
-from .evaluators import (
-    HallucinationEvaluator,
-    LLMEvaluator,
-    QAEvaluator,
-    RelevanceEvaluator,
-    SQLEvaluator,
-    SummarizationEvaluator,
-    ToxicityEvaluator,
-)
-from .generate import llm_generate
-from .models import (
-    AnthropicModel,
-    BedrockModel,
-    GeminiModel,
-    LiteLLMModel,
-    MistralAIModel,
-    OpenAIModel,
-    VertexAIModel,
-)
-from .retrievals import compute_precisions_at_k
-from .span_templates import (
-    HALLUCINATION_SPAN_PROMPT_TEMPLATE,
-    QA_SPAN_PROMPT_TEMPLATE,
-    TOOL_CALLING_SPAN_PROMPT_TEMPLATE,
-)
-from .templates import (
-    ClassificationTemplate,
-    PromptTemplate,
-)
-from .utils import NOT_PARSABLE, download_benchmark_dataset
+from . import llm  # noqa: E402
 
 __version__ = version("arize-phoenix-evals")
 
