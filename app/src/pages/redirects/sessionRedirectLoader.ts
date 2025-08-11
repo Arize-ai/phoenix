0348f9135a526fd14e04583d40cd5bdb8f88f7c0
Alexander Song <axiomofjoy@gmail.com>
2025-08-11 14:56:21 -0400

    refactor(redirections): rename session otel id to session id

--- app/src/pages/redirects/sessionRedirectLoader.ts
+++ app/src/pages/redirects/sessionRedirectLoader.ts
@@ -6,24 +6,24 @@ import RelayEnvironment from "@phoenix/RelayEnvironment";
 import { sessionRedirectLoaderQuery } from "./__generated__/sessionRedirectLoaderQuery.graphql";
 
 export async function sessionRedirectLoader({ params }: LoaderFunctionArgs) {
-  const { session_otel_id } = params;
+  const { session_id: sessionId } = params;
 
-  if (!session_otel_id) {
+  if (!sessionId) {
     throw new Error("Session redirect requires a session ID");
   }
 
   const response = await fetchQuery<sessionRedirectLoaderQuery>(
     RelayEnvironment,
     graphql`
-      query sessionRedirectLoaderQuery($sessionOtelId: String!) {
-        session: getProjectSessionByOtelId(sessionId: $sessionOtelId) {
+      query sessionRedirectLoaderQuery($sessionId: String!) {
+        session: getProjectSessionById(sessionId: $sessionId) {
           projectId
           id
         }
       }
     `,
     {
-      sessionOtelId: session_otel_id,
+      sessionId,
     }
   ).toPromise();
 
@@ -31,6 +31,6 @@ export async function sessionRedirectLoader({ params }: LoaderFunctionArgs) {
     const { projectId, id } = response.session;
     return redirect(`/projects/${projectId}/sessions/${id}`);
   } else {
-    throw new Error(`Session with id "${session_otel_id}" not found`);
+    throw new Error(`Session with id "${sessionId}" not found`);
   }
 }
