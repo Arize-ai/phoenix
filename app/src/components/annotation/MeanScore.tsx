d4000dfe61c0effe4fc4a0b27f90964677ed7e51
Mikyo King <mikyo@arize.com>
2025-08-11 15:00:21 -0600

    feat(design): add geist mono for values

--- app/src/components/annotation/MeanScore.tsx
+++ app/src/components/annotation/MeanScore.tsx
@@ -12,12 +12,16 @@ export const MeanScore = ({
   fallback?: React.ReactNode;
 } & Omit<TextProps, "children">) => {
   if (value == null || typeof value !== "number" || isNaN(value)) {
-    return <Text {...props}>{fallback}</Text>;
+    return (
+      <Text {...props} fontFamily="mono">
+        {fallback}
+      </Text>
+    );
   }
   return (
     <Text {...props}>
       <span aria-label="mean score">μ&nbsp;</span>
-      {`${formatFloat(value)}`}
+      <span className="font-mono">{`${formatFloat(value)}`}</span>
     </Text>
   );
 };
