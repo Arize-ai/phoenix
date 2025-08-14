cf3f8b9cb2e0612460870eb30ac5b16a0b877289
Mikyo King <mikyo@arize.com>
2025-08-14 14:52:39 -0600

    feat(projects): transfer traces (#9050)
    * feat(projects): transfer traces
    
    * feat(projects): transfer traces
    
    * WIP
    
    * add autocomplete
    
    * cleanup

--- app/src/components/icon/Icons.tsx
+++ app/src/components/icon/Icons.tsx
@@ -548,6 +548,22 @@ export const Column = () => (
   </svg>
 );
 
+export const CornerUpRightOutline = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <g data-name="Layer 2">
+      <g data-name="corner-up-right">
+        <rect
+          width="24"
+          height="24"
+          transform="rotate(-90 12 12)"
+          opacity="0"
+        />
+        <path d="M19.78 10.38l-4-5a1 1 0 0 0-1.56 1.24l2.7 3.38H8a3 3 0 0 0-3 3v5a1 1 0 0 0 2 0v-5a1 1 0 0 1 1-1h8.92l-2.7 3.38a1 1 0 0 0 .16 1.4A1 1 0 0 0 15 17a1 1 0 0 0 .78-.38l4-5a1 1 0 0 0 0-1.24z" />
+      </g>
+    </g>
+  </svg>
+);
+
 export const Commit = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
