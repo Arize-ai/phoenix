7151cd5c0e4234461f6a6c8bb5069a16565c47e2
Jonathan <jonathanbeltran35@gmail.com>
2025-08-27 11:54:48 -0400

    feat: Experiment migration (#9279)

---
+++ src/phoenix/db/migrations/versions/d0690a79ea51_users_on_experiments.py
@@ -0,0 +1,34 @@
+"""users_on_experiments
+
+Revision ID: d0690a79ea51
+Revises: 0df286449799
+Create Date: 2025-08-26 19:12:47.849806
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "d0690a79ea51"
+down_revision: Union[str, None] = "0df286449799"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    with op.batch_alter_table("experiments") as batch_op:
+        batch_op.add_column(
+            sa.Column(
+                "user_id",
+                sa.Integer,
+                sa.ForeignKey("users.id", ondelete="SET NULL"),
+                nullable=True,
+            ),
+        )
+
+def downgrade() -> None:
+    with op.batch_alter_table("experiments") as batch_op:
+        batch_op.drop_column("user_id") 
