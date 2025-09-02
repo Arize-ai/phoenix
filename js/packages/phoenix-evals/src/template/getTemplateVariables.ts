abc186c225a19c8e4155cb7373699c2deef675dc
Mikyo King <mikyo@arize.com>
2025-09-02 11:17:11 -0600

    feat: add template variable parsing

---
+++ js/packages/phoenix-evals/src/template/getTemplateVariables.ts
@@ -0,0 +1,23 @@
+import { Template } from "../types/templating";
+import Mustache from "mustache";
+
+type GetTemplateVariableArgs = {
+  template: Template;
+};
+/**
+ * Parse out the template variables of a prompt
+ * @param {GetTemplateVariableArgs} args
+ * @returns {string[]} a list of prompt template variables
+ */
+export function getTemplateVariables(args: GetTemplateVariableArgs) {
+  const { template } = args;
+  const templateSpans = Mustache.parse(template);
+  return templateSpans.entries().reduce((acc, entry) => {
+    const [, templateSpan] = entry;
+    const [spanType, value] = templateSpan;
+    if (spanType === "name") {
+      acc = [...acc, value];
+    }
+    return acc;
+  }, [] as string[]);
+}
