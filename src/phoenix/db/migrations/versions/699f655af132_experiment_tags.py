fb66874c03f1dd4525c7ffcd8254783e4dd902b0
Jonathan Beltran <jonathanbeltran35@gmail.com>
2025-09-05 13:20:16 -0400

    feat: experiment-tags on experiment and datasets

---
+++ src/phoenix/db/migrations/versions/699f655af132_experiment_tags.py
@@ -0,0 +1,45 @@
+"""experiment_tags
+
+Revision ID: 699f655af132
+Revises: d0690a79ea51
+Create Date: 2025-09-05 13:14:22.676233
+
+"""
+
+from typing import Sequence, Union
+
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = "699f655af132"
+down_revision: Union[str, None] = "d0690a79ea51"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "experiment_tags",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column(
+            "experiment_id",
+            sa.Integer,
+            sa.ForeignKey("experiments.id", ondelete="CASCADE"),
+            nullable=False,
+            index=True,
+        ),
+        sa.Column(
+            "dataset_id",
+            sa.Integer,
+            sa.ForeignKey("datasets.id", ondelete="CASCADE"),
+            nullable=False,
+            index=True,
+        ),
+        sa.Column("name", sa.String, nullable=False),
+        sa.UniqueConstraint("name", "dataset_id"),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("experiment_tags")
