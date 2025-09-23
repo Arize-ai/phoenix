914cf640409936d378b6b9b959573b5858017ac5
Jonathan <jonathanbeltran35@gmail.com>
2025-09-23 13:58:00 -0400

    feat: Dataset Splits Add Dialog UI (#9601)

--- app/src/contexts/FeatureFlagsContext.tsx
+++ app/src/contexts/FeatureFlagsContext.tsx
@@ -12,7 +12,7 @@ import {
   DialogTitleExtra,
 } from "@phoenix/components/dialog";
 
-type FeatureFlag = "evaluators";
+type FeatureFlag = "evaluators" | "datasetSplitsUI";
 export type FeatureFlagsContextType = {
   featureFlags: Record<FeatureFlag, boolean>;
   setFeatureFlags: (featureFlags: Record<FeatureFlag, boolean>) => void;
@@ -22,6 +22,7 @@ export const LOCAL_STORAGE_FEATURE_FLAGS_KEY = "arize-phoenix-feature-flags";
 
 const DEFAULT_FEATURE_FLAGS: Record<FeatureFlag, boolean> = {
   evaluators: false,
+  datasetSplitsUI: false,
 };
 
 function getFeatureFlags(): Record<FeatureFlag, boolean> {
