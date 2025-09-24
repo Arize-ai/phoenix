fd51edec1be9fb77456b02749bc4df40a4b60d13
Jonathan Beltran <jonathanbeltran35@gmail.com>
2025-09-24 15:24:51 -0400

    rename Split Form to Dataset Split Form

--- app/src/components/dataset/NewDatasetSplitDialog.tsx
+++ app/src/components/dataset/NewDatasetSplitDialog.tsx
@@ -2,9 +2,9 @@ import { graphql, useMutation } from "react-relay";
 
 import { Dialog, Modal } from "@phoenix/components";
 import {
-  NewSplitForm,
-  type SplitParams,
-} from "@phoenix/components/datasetSplit/NewSplitForm";
+  type DatasetSplitParams,
+  NewDatasetSplitForm,
+} from "@phoenix/components/datasetSplit/NewDatasetSplitForm";
 import {
   DialogCloseButton,
   DialogContent,
@@ -40,8 +40,9 @@ export function NewDatasetSplitDialog(props: NewDatasetSplitDialogProps) {
       }
     `);
 
-  const onSubmit = (params: SplitParams) => {
+  const onSubmit = (params: DatasetSplitParams) => {
     const trimmed = params.name.trim();
+    // TODO: Validate params
     if (!trimmed) return;
     commit({
       variables: {
@@ -79,7 +80,7 @@ export function NewDatasetSplitDialog(props: NewDatasetSplitDialogProps) {
               <DialogCloseButton />
             </DialogTitleExtra>
           </DialogHeader>
-          <NewSplitForm onSubmit={onSubmit} isSubmitting={isInFlight} />
+          <NewDatasetSplitForm onSubmit={onSubmit} isSubmitting={isInFlight} />
         </DialogContent>
       </Dialog>
     </Modal>
