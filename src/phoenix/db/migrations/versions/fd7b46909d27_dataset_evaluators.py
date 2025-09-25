053f73cfc84c83c69b0167e839a556ba335f23e2
Mikyo King <mikyo@arize.com>
2025-09-25 15:55:36 -0600

    fix the revision

--- src/phoenix/db/migrations/versions/fd7b46909d27_dataset_evaluators.py
+++ src/phoenix/db/migrations/versions/fd7b46909d27_dataset_evaluators.py
@@ -1,7 +1,7 @@
 """dataset_evaluators
 
 Revision ID: fd7b46909d27
-Revises: 58228d933c91
+Revises:  deb2c81c0bb2
 Create Date: 2025-09-22 11:36:40.216192
 
 """
@@ -16,7 +16,7 @@ from sqlalchemy.ext.compiler import compiles
 
 # revision identifiers, used by Alembic.
 revision: str = "fd7b46909d27"
-down_revision: Union[str, None] = "58228d933c91"
+down_revision: Union[str, None] = "deb2c81c0bb2"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
