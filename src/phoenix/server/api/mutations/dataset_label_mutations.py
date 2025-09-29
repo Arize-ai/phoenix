41bdb094e88c233e555aab8f3bcd16c436e8a9f7
Donny Li <geli2001@berkeley.edu>
2025-09-29 14:20:19 -0700

    wip

--- src/phoenix/server/api/mutations/dataset_label_mutations.py
+++ src/phoenix/server/api/mutations/dataset_label_mutations.py
@@ -9,7 +9,7 @@ from strawberry.types import Info
 from phoenix.db import models
 from phoenix.server.api.auth import IsLocked, IsNotReadOnly
 from phoenix.server.api.context import Context
-from phoenix.server.api.exceptions import NotFound
+from phoenix.server.api.exceptions import BadRequest, NotFound
 from phoenix.server.api.types.DatasetLabel import DatasetLabel, to_gql_dataset_label
 from phoenix.server.api.types.node import from_global_id_with_expected_type
 
@@ -59,22 +59,31 @@ class DatasetLabelMutationMixin:
     async def delete_dataset_labels(
         self, info: Info[Context, None], input: DeleteDatasetLabelsInput
     ) -> DeleteDatasetLabelsMutationPayload:
-        dataset_label_ids = [
-            from_global_id_with_expected_type(dataset_label_id, DatasetLabel.__name__)
-            for dataset_label_id in input.dataset_label_ids
-        ]
+        dataset_label_row_ids: dict[int, None] = {}
+        for dataset_label_node_id in input.dataset_label_ids:
+            try:
+                dataset_label_row_id = from_global_id_with_expected_type(
+                    dataset_label_node_id, DatasetLabel.__name__
+                )
+            except ValueError:
+                raise BadRequest(f"Unknown dataset label: {dataset_label_node_id}")
+            dataset_label_row_ids[dataset_label_row_id] = None
         async with info.context.db() as session:
             stmt = (
                 delete(models.DatasetLabel)
-                .where(models.DatasetLabel.id.in_(dataset_label_ids))
+                .where(models.DatasetLabel.id.in_(dataset_label_row_ids.keys()))
                 .returning(models.DatasetLabel)
             )
             deleted_dataset_labels = (await session.scalars(stmt)).all()
-            if len(deleted_dataset_labels) < len(dataset_label_ids):
+            if len(deleted_dataset_labels) < len(dataset_label_row_ids):
                 await session.rollback()
-                raise NotFound(f"Could not find dataset labels with IDs: {dataset_label_ids}")
+                raise NotFound("Could not find one or more dataset labels with given IDs")
+        deleted_dataset_labels_by_id = {
+            dataset_label.id: dataset_label for dataset_label in deleted_dataset_labels
+        }
         return DeleteDatasetLabelsMutationPayload(
             dataset_labels=[
-                to_gql_dataset_label(dataset_label) for dataset_label in deleted_dataset_labels
+                to_gql_dataset_label(deleted_dataset_labels_by_id[dataset_label_row_id])
+                for dataset_label_row_id in dataset_label_row_ids
             ]
         )
