0c49666647f52e20eb44084ff86d458d0a4e953a
Tony Powell <apowell@arize.com>
2025-10-03 12:06:35 -0400

    Refactor DownloadExperimentActionMenu

--- app/src/pages/experiments/DownloadExperimentActionMenu.tsx
+++ app/src/pages/experiments/DownloadExperimentActionMenu.tsx
@@ -1,7 +1,12 @@
-import { ActionMenu, Item } from "@arizeai/components";
-
-import { Icon, Icons } from "@phoenix/components";
-import { StopPropagation } from "@phoenix/components/StopPropagation";
+import {
+  Button,
+  Icon,
+  Icons,
+  Menu,
+  MenuItem,
+  MenuTrigger,
+  Popover,
+} from "@phoenix/components";
 import { prependBasename } from "@phoenix/utils/routingUtils";
 interface DownloadExperimentActionMenuProps {
   experimentId: string;
@@ -11,29 +16,32 @@ export function DownloadExperimentActionMenu({
   experimentId,
 }: DownloadExperimentActionMenuProps) {
   return (
-    <StopPropagation>
-      <ActionMenu
-        buttonSize="compact"
-        align="end"
-        icon={<Icon svg={<Icons.DownloadOutline />} />}
-        onAction={(action) => {
-          if (action === "csv") {
-            window.open(
-              prependBasename(`/v1/experiments/${experimentId}/csv`),
-              "_blank"
-            );
-          }
-          if (action === "json") {
-            window.open(
-              prependBasename(`/v1/experiments/${experimentId}/json`),
-              "_blank"
-            );
-          }
-        }}
-      >
-        <Item key="csv">Download CSV</Item>
-        <Item key="json">Download JSON</Item>
-      </ActionMenu>
-    </StopPropagation>
+    <MenuTrigger>
+      <Button
+        size="S"
+        leadingVisual={<Icon svg={<Icons.DownloadOutline />} />}
+      />
+      <Popover>
+        <Menu
+          onAction={(action) => {
+            if (action === "csv") {
+              window.open(
+                prependBasename(`/v1/experiments/${experimentId}/csv`),
+                "_blank"
+              );
+            }
+            if (action === "json") {
+              window.open(
+                prependBasename(`/v1/experiments/${experimentId}/json`),
+                "_blank"
+              );
+            }
+          }}
+        >
+          <MenuItem id="csv">Download CSV</MenuItem>
+          <MenuItem id="json">Download JSON</MenuItem>
+        </Menu>
+      </Popover>
+    </MenuTrigger>
   );
 }
