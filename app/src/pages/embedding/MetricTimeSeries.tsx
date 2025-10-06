5dc35499aab88f117dc0f8d83e839a594d984aa4
Tony Powell <apowell@arize.com>
2025-10-06 15:12:54 -0400

    Port ContextualHelp to phoenix components with new tooltip

--- app/src/pages/embedding/MetricTimeSeries.tsx
+++ app/src/pages/embedding/MetricTimeSeries.tsx
@@ -16,9 +16,13 @@ import {
 import { CategoricalChartFunc } from "recharts/types/chart/types";
 import { css } from "@emotion/react";
 
-import { Content, ContextualHelp } from "@arizeai/components";
-
-import { Heading, Icon, Icons, Text } from "@phoenix/components";
+import {
+  ContextualHelp,
+  Heading,
+  Icon,
+  Icons,
+  Text,
+} from "@phoenix/components";
 import {
   ChartTooltip,
   ChartTooltipDivider,
@@ -360,7 +364,7 @@ export function MetricTimeSeries({
         {metricDescription != null ? (
           <ContextualHelp>
             <Heading level={4}>{metricShortName}</Heading>
-            <Content>{metricDescription}</Content>
+            <Text>{metricDescription}</Text>
           </ContextualHelp>
         ) : null}
       </Heading>
