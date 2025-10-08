f44efb74cebc50d02e79642c3c13513489a6b077
Donny Li <geli2001@berkeley.edu>
2025-10-08 15:07:09 -0700

    wip

--- app/src/components/dataset/DatasetLabelConfigButton.tsx
+++ app/src/components/dataset/DatasetLabelConfigButton.tsx
@@ -145,7 +145,7 @@ export function DatasetLabelSelectionContent(props: {
 
   return (
     <>
-      <DatasetLabelSelectionDialogContentStyled
+      <DatasetLabelSelectionDialogContent
         {...props}
         onNewLabelPress={() => setShowNewLabelDialog(true)}
       />
@@ -172,32 +172,7 @@ export function DatasetLabelSelectionContent(props: {
   );
 }
 
-function DatasetLabelSelectionDialogContentStyled(props: {
-  datasetId: string;
-  onNewLabelPress: () => void;
-  onClose: () => void;
-}) {
-  const { datasetId } = props;
-  const query = useLazyLoadQuery<DatasetLabelConfigButtonQuery>(
-    graphql`
-      query DatasetLabelConfigButtonQuery($datasetId: ID!) {
-        ...DatasetLabelConfigButton_allLabels
-        dataset: node(id: $datasetId) {
-          ... on Dataset {
-            ...DatasetLabelConfigButton_datasetLabels
-          }
-        }
-      }
-    `,
-    { datasetId }
-  );
-
-  return (
-    <DatasetLabelListStyled query={query} dataset={query.dataset} {...props} />
-  );
-}
-
-function DatasetLabelListStyled({
+function DatasetLabelList({
   query,
   dataset,
   onNewLabelPress,
@@ -416,11 +391,7 @@ function DatasetLabelListStyled({
 
       {/* Footer section */}
       <View padding="size-100" borderTopColor="dark" borderTopWidth="thin">
-        <Flex
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
-        >
+        <Flex direction="row" justifyContent="space-between" alignItems="center">
           <LinkButton variant="quiet" size="S" to="/settings/datasets">
             Manage Labels
           </LinkButton>
@@ -435,225 +406,6 @@ function DatasetLabelListStyled({
         </Flex>
       </View>
     </>
-  );
-}
-
-function DatasetLabelList({
-  query,
-  dataset,
-  onNewLabelPress,
-  onClose,
-}: {
-  dataset: DatasetLabelConfigButton_datasetLabels$key;
-  query: DatasetLabelConfigButton_allLabels$key;
-  onNewLabelPress: () => void;
-  onClose: () => void;
-}) {
-  const notifyError = useNotifyError();
-  const datasetData = useFragment<DatasetLabelConfigButton_datasetLabels$key>(
-    graphql`
-      fragment DatasetLabelConfigButton_datasetLabels on Dataset {
-        id
-        labels {
-          id
-        }
-      }
-    `,
-    dataset
-  );
-  const labelData = useFragment<DatasetLabelConfigButton_allLabels$key>(
-    graphql`
-      fragment DatasetLabelConfigButton_allLabels on Query
-      @argumentDefinitions(first: { type: "Int", defaultValue: 100 }) {
-        datasetLabels(first: $first)
-          @connection(key: "DatasetLabelConfigButtonAllLabels_datasetLabels") {
-          edges {
-            node {
-              id
-              name
-              color
-            }
-          }
-        }
-      }
-    `,
-    query
-  );
-
-  const selectedLabelIds = datasetData?.labels?.map((label) => label.id) || [];
-  const [search, setSearch] = useState("");
-  const [selected, setSelected] = useState<Selection>(
-    () => new Set(selectedLabelIds)
-  );
-  const [hasChanges, setHasChanges] = useState(false);
-
-  const [setDatasetLabels] =
-    useMutation<DatasetLabelConfigButtonSetLabelsMutation>(graphql`
-      mutation DatasetLabelConfigButtonSetLabelsMutation(
-        $datasetIds: [ID!]!
-        $datasetLabelIds: [ID!]!
-      ) {
-        setDatasetLabels(
-          input: { datasetIds: $datasetIds, datasetLabelIds: $datasetLabelIds }
-        ) {
-          query {
-            datasets(first: 100) @connection(key: "DatasetsTable_datasets") {
-              edges {
-                node {
-                  id
-                  labels {
-                    id
-                    name
-                    color
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    `);
-
-  const [unsetDatasetLabels] =
-    useMutation<DatasetLabelConfigButtonUnsetLabelsMutation>(graphql`
-      mutation DatasetLabelConfigButtonUnsetLabelsMutation(
-        $datasetIds: [ID!]!
-        $datasetLabelIds: [ID!]!
-      ) {
-        unsetDatasetLabels(
-          input: { datasetIds: $datasetIds, datasetLabelIds: $datasetLabelIds }
-        ) {
-          query {
-            datasets(first: 100) @connection(key: "DatasetsTable_datasets") {
-              edges {
-                node {
-                  id
-                  labels {
-                    id
-                    name
-                    color
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    `);
-
-  const labels = labelData.datasetLabels.edges
-    .map((edge) => edge.node)
-    .filter((label) => {
-      return label.name.toLowerCase().includes(search.toLowerCase());
-    });
-
-  const onSelectionChange = (selection: Selection) => {
-    if (selection === "all") {
-      return;
-    }
-    setSelected(selection);
-
-    // Check if there are changes from the original selection
-    const newLabelIds = [...selection] as string[];
-    const originalSet = new Set(selectedLabelIds);
-    const newSet = new Set(newLabelIds);
-
-    const hasActualChanges =
-      originalSet.size !== newSet.size ||
-      [...originalSet].some((id) => !newSet.has(id)) ||
-      [...newSet].some((id) => !originalSet.has(id));
-
-    setHasChanges(hasActualChanges);
-  };
-
-  const handleSave = () => {
-    const newLabelIds = [...selected] as string[];
-    const labelIdsToAdd: string[] = newLabelIds.filter(
-      (id) => !selectedLabelIds.includes(id)
-    );
-    const labelIdsToRemove: string[] = selectedLabelIds.filter(
-      (id) => !newLabelIds.includes(id)
-    );
-
-    const promises = [];
-
-    if (labelIdsToAdd.length) {
-      promises.push(
-        setDatasetLabels({
-          variables: {
-            datasetIds: [datasetData.id],
-            datasetLabelIds: labelIdsToAdd,
-          },
-        })
-      );
-    }
-    if (labelIdsToRemove.length) {
-      promises.push(
-        unsetDatasetLabels({
-          variables: {
-            datasetIds: [datasetData.id],
-            datasetLabelIds: labelIdsToRemove,
-          },
-        })
-      );
-    }
-
-    // Close modal after all mutations complete
-    Promise.all(promises)
-      .then(() => {
-        setHasChanges(false);
-        onClose();
-      })
-      .catch(() => {
-        // Keep modal open on error so user can retry
-        notifyError({
-          title: "Failed to save label changes",
-          message: "Failed to save label changes. Please try again.",
-        });
-      });
-  };
-
-  return (
-    <Flex direction="column" gap="size-100">
-      <DebouncedSearch
-        autoFocus
-        aria-label="Search labels"
-        placeholder="Search labels..."
-        onChange={setSearch}
-      />
-      <ListBox
-        aria-label="labels"
-        items={labels}
-        selectionMode="multiple"
-        selectedKeys={selected}
-        onSelectionChange={onSelectionChange}
-        css={css`
-          height: 300px;
-          width: 100%;
-        `}
-        renderEmptyState={() => "No labels found"}
-      >
-        {(item) => <DatasetLabelListBoxItem key={item.id} item={item} />}
-      </ListBox>
-      <Flex direction="row" justifyContent="space-between" alignItems="center">
-        <Flex direction="row" gap="size-100">
-          <Button variant="quiet" size="S" onPress={onNewLabelPress}>
-            Create New Label
-          </Button>
-          <LinkButton variant="quiet" size="S" to="/settings/datasets">
-            Manage Labels
-          </LinkButton>
-        </Flex>
-        <Button
-          variant="primary"
-          size="S"
-          onPress={handleSave}
-          isDisabled={!hasChanges}
-        >
-          Save Changes
-        </Button>
-      </Flex>
-    </Flex>
   );
 }
 
