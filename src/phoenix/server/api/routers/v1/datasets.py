a6d8d841c50a051deae76d3a2f5d82d128bf511c
Jonathan Beltran <jonathanbeltran35@gmail.com>
2025-10-08 10:42:23 -0400

    fix integration tests

--- src/phoenix/server/api/routers/v1/datasets.py
+++ src/phoenix/server/api/routers/v1/datasets.py
@@ -685,7 +685,7 @@ class DatasetExample(V1RoutesBaseModel):
     output: dict[str, Any]
     metadata: dict[str, Any]
     updated_at: datetime
-    split_ids: list[str]
+    split_ids: list[str] = UNDEFINED
 
 
 class ListDatasetExamplesData(V1RoutesBaseModel):
