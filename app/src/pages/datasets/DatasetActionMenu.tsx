ff874124753f8c07de2d78fd64b6a52e7d98cbb9
Donny Li <77590974+GeLi2001@users.noreply.github.com>
2025-10-09 17:56:25 -0700

    feat: dataset edit labels (#9791)
    * wip
    
    * wip
    
    * wip
    
    * refactor
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * Update app/src/pages/datasets/DatasetActionMenu.tsx
    
    Co-authored-by: Xander Song <axiomofjoy@gmail.com>
    
    * Update app/src/components/dataset/DatasetLabelConfigButton.tsx
    
    Co-authored-by: Tony Powell <apowell@arize.com>
    
    * prettier
    
    * wip
    
    ---------
    
    Co-authored-by: Xander Song <axiomofjoy@gmail.com>
    Co-authored-by: Tony Powell <apowell@arize.com>

--- app/src/pages/datasets/DatasetActionMenu.tsx
+++ app/src/pages/datasets/DatasetActionMenu.tsx
@@ -1,8 +1,20 @@
-import { useState } from "react";
+import { Suspense, useState } from "react";
 
 import { ActionMenu, Item } from "@arizeai/components";
 
-import { Flex, Icon, Icons, Text } from "@phoenix/components";
+import {
+  Button,
+  Dialog,
+  DialogTrigger,
+  Flex,
+  Icon,
+  Icons,
+  Loading,
+  Popover,
+  PopoverArrow,
+  Text,
+} from "@phoenix/components";
+import { DatasetLabelSelectionContent } from "@phoenix/components/dataset/DatasetLabelConfigButton";
 
 import { DeleteDatasetDialog } from "./DeleteDatasetDialog";
 import { EditDatasetDialog } from "./EditDatasetDialog";
@@ -20,6 +32,7 @@ type DatasetActionMenuProps = {
 enum DatasetAction {
   DELETE = "deleteDataset",
   EDIT = "editDataset",
+  LABELS = "configureLabels",
 }
 
 export function DatasetActionMenu(props: DatasetActionMenuProps) {
@@ -35,53 +48,107 @@ export function DatasetActionMenu(props: DatasetActionMenuProps) {
   } = props;
   const [isDeleteOpen, setIsDeleteOpen] = useState(false);
   const [isEditOpen, setIsEditOpen] = useState(false);
+  const [isLabelsOpen, setIsLabelsOpen] = useState(false);
 
   return (
-    <div
-      // TODO: add this logic to the ActionMenu component
-      onClick={(e) => {
-        // prevent parent anchor link from being followed
-        e.preventDefault();
-        e.stopPropagation();
-      }}
-    >
-      <ActionMenu
-        align="end"
-        buttonSize="compact"
-        onAction={(action) => {
-          switch (action) {
-            case DatasetAction.DELETE:
-              setIsDeleteOpen(true);
-              break;
-            case DatasetAction.EDIT:
-              setIsEditOpen(true);
-              break;
-          }
+    <>
+      <div
+        // TODO: add this logic to the ActionMenu component
+        onClick={(e) => {
+          // prevent parent anchor link from being followed
+          e.preventDefault();
+          e.stopPropagation();
         }}
+        style={{ position: "relative" }}
       >
-        <Item key={DatasetAction.EDIT}>
-          <Flex
-            direction={"row"}
-            gap="size-75"
-            justifyContent={"start"}
-            alignItems={"center"}
-          >
-            <Icon svg={<Icons.Edit2Outline />} />
-            <Text>Edit</Text>
-          </Flex>
-        </Item>
-        <Item key={DatasetAction.DELETE}>
-          <Flex
-            direction={"row"}
-            gap="size-75"
-            justifyContent={"start"}
-            alignItems={"center"}
-          >
-            <Icon svg={<Icons.TrashOutline />} />
-            <Text>Delete</Text>
-          </Flex>
-        </Item>
-      </ActionMenu>
+        {/* Action Menu for Edit, Labels, and Delete */}
+        <ActionMenu
+          align="end"
+          buttonSize="compact"
+          onAction={(action) => {
+            switch (action) {
+              case DatasetAction.DELETE:
+                setIsDeleteOpen(true);
+                break;
+              case DatasetAction.EDIT:
+                setIsEditOpen(true);
+                break;
+              case DatasetAction.LABELS:
+                setIsLabelsOpen(true);
+                break;
+            }
+          }}
+        >
+          <Item key={DatasetAction.EDIT}>
+            <Flex
+              direction={"row"}
+              gap="size-75"
+              justifyContent={"start"}
+              alignItems={"center"}
+            >
+              <Icon svg={<Icons.Edit2Outline />} />
+              <Text>Edit</Text>
+            </Flex>
+          </Item>
+          <Item key={DatasetAction.LABELS}>
+            <Flex
+              direction={"row"}
+              gap="size-75"
+              justifyContent={"start"}
+              alignItems={"center"}
+            >
+              <Icon svg={<Icons.PriceTagsOutline />} />
+              <Text>Label</Text>
+            </Flex>
+          </Item>
+          <Item key={DatasetAction.DELETE}>
+            <Flex
+              direction={"row"}
+              gap="size-75"
+              justifyContent={"start"}
+              alignItems={"center"}
+            >
+              <Icon svg={<Icons.TrashOutline />} />
+              <Text>Delete</Text>
+            </Flex>
+          </Item>
+        </ActionMenu>
+
+        {/* Invisible anchor positioned over ActionMenu button for popover positioning */}
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            right: 0,
+            pointerEvents: "none",
+          }}
+        >
+          <DialogTrigger isOpen={isLabelsOpen} onOpenChange={setIsLabelsOpen}>
+            <Button
+              variant="quiet"
+              size="S"
+              aria-label="Configure dataset labels"
+              style={{
+                visibility: "hidden",
+                pointerEvents: "none",
+              }}
+            >
+              <Icon svg={<Icons.SettingsOutline />} />
+            </Button>
+            <Popover placement="bottom end">
+              <PopoverArrow />
+              <Dialog>
+                <Suspense fallback={<Loading />}>
+                  <DatasetLabelSelectionContent
+                    datasetId={datasetId}
+                    onClose={() => setIsLabelsOpen(false)}
+                  />
+                </Suspense>
+              </Dialog>
+            </Popover>
+          </DialogTrigger>
+        </div>
+      </div>
 
       {/* Delete Dataset Dialog */}
       <DeleteDatasetDialog
@@ -104,6 +171,6 @@ export function DatasetActionMenu(props: DatasetActionMenuProps) {
         isOpen={isEditOpen}
         onOpenChange={setIsEditOpen}
       />
-    </div>
+    </>
   );
 }
