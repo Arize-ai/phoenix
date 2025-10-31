9af4f5d597a6c8b8d7c72773c6ebab4e1e015502
zurfjereluhmie <jeremie.zurfluh@camptocamp.com>
2025-10-31 08:06:43 +0100

    refactor: change location of the preference card

--- app/src/pages/settings/index.tsx
+++ app/src/pages/settings/index.tsx
@@ -2,5 +2,6 @@ export * from "./SettingsPage";
 export * from "./settingsGeneralPageLoader";
 export * from "./settingsAIProvidersPageLoader";
 export * from "./SettingsDataPage";
+export * from "./SettingsDisplayPreferencesPage";
 export * from "./prompts";
 export * from "./datasets";
