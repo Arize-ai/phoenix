1ff26fa4037952c0be226c4c8c7ec820ff0cea48
Mikyo King <mikyo@arize.com>
2025-11-10 17:01:05 -0700

    make now be idempotent

--- app/src/hooks/useCurrentTime.ts
+++ app/src/hooks/useCurrentTime.ts
@@ -1,28 +1,36 @@
 import { useEffect, useState } from "react";
 
-let _nowEpochMs = Date.now();
-
 type UseCurrentTimeProps = {
   /**
    * The interval at which the current time will be updated. If set to null, the current time will stay fixed.
+   * This is by design as we want the component that uses the hook to have explicit control over it's rendering behavior
    * @default null
    */
   updateIntervalMs?: number | null;
 };
-export function useCurrentTime(props: UseCurrentTimeProps) {
+
+type CurrentTimeValue = {
+  nowEpochMs: number;
+};
+/**
+ * A react hook that makes makes the current time idenponent
+ * @param props - The props for the hook
+ * @returns The current time value
+ */
+export function useCurrentTime(
+  props: UseCurrentTimeProps = {}
+): CurrentTimeValue {
   const { updateIntervalMs = null } = props;
-  const [nowEpochMs, setNowEpochMs] = useState<number>(_nowEpochMs);
+  const [nowEpochMs, setNowEpochMs] = useState<number>(() => Date.now());
   useEffect(() => {
     if (typeof updateIntervalMs !== "number") {
       return;
     }
     const interval = setInterval(() => {
-      // update the global so that subsiquent mounts
-      _nowEpochMs = Date.now();
       // update via state callback to avoid setting state in the effect
-      setNowEpochMs(_nowEpochMs);
+      setNowEpochMs(Date.now());
     }, updateIntervalMs);
     return () => clearInterval(interval);
   }, [updateIntervalMs]);
-  return nowEpochMs;
+  return { nowEpochMs };
 }
