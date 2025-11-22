02ff7b9a5f73c3b7483032332ec8d9720e9c133b
Alexander Song <axiomofjoy@gmail.com>
2025-11-22 14:23:26 -0800

    compile into evals

---
+++ packages/phoenix-evals/__generated__/classification_evaluator_configs/__init__.py
@@ -0,0 +1,10 @@
+# This file is generated. Do not edit by hand.
+
+from ._HallucinationClassificationEvaluatorConfig import HallucinationClassificationEvaluatorConfig
+from ._models import ClassificationEvaluatorConfig, PromptMessage
+
+__all__ = [
+    "ClassificationEvaluatorConfig",
+    "PromptMessage",
+    "HallucinationClassificationEvaluatorConfig",
+]
