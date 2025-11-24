f08730b78a4201d1d0c4dcee77ab2f31c5f88079
Xander Song <axiomofjoy@gmail.com>
2025-11-24 13:21:39 -0800

    refactor (#10384)

--- packages/phoenix-evals/src/phoenix/evals/__generated__/classification_evaluator_configs/__init__.py
+++ packages/phoenix-evals/src/phoenix/evals/__generated__/classification_evaluator_configs/__init__.py
@@ -1,5 +1,8 @@
 # This file is generated. Do not edit by hand.
 
+from ._document_relevance_classification_evaluator_config import (
+    DOCUMENT_RELEVANCE_CLASSIFICATION_EVALUATOR_CONFIG,
+)
 from ._hallucination_classification_evaluator_config import (
     HALLUCINATION_CLASSIFICATION_EVALUATOR_CONFIG,
 )
@@ -8,5 +11,6 @@ from ._models import ClassificationEvaluatorConfig, PromptMessage
 __all__ = [
     "ClassificationEvaluatorConfig",
     "PromptMessage",
+    "DOCUMENT_RELEVANCE_CLASSIFICATION_EVALUATOR_CONFIG",
     "HALLUCINATION_CLASSIFICATION_EVALUATOR_CONFIG",
 ]
