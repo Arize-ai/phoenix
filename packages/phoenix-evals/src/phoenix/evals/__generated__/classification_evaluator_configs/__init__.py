bb4c7246c5bc8e2cce39601f99886e96330d9824
Xander Song <axiomofjoy@gmail.com>
2025-11-24 08:54:06 -0800

    ci: compile prompts into python code (#10359)
    * mvp
    
    * tox
    
    * ci
    
    * more
    
    * more
    
    * tox
    
    * test
    
    * more
    
    * test
    
    * clean
    
    * test
    
    * clean
    
    * clean
    
    * test
    
    * test
    
    * clean
    
    * test
    
    * move
    
    * test ci
    
    * clean
    
    * test
    
    * clean
    
    * clean
    
    * compile into evals
    
    * test ci fails
    
    * fix
    
    * clean
    
    * clean
    
    * clean
    
    * clean
    
    * simplify tox

---
+++ packages/phoenix-evals/src/phoenix/evals/__generated__/classification_evaluator_configs/__init__.py
@@ -0,0 +1,12 @@
+# This file is generated. Do not edit by hand.
+
+from ._hallucination_classification_evaluator_config import (
+    HALLUCINATION_CLASSIFICATION_EVALUATOR_CONFIG,
+)
+from ._models import ClassificationEvaluatorConfig, PromptMessage
+
+__all__ = [
+    "ClassificationEvaluatorConfig",
+    "PromptMessage",
+    "HALLUCINATION_CLASSIFICATION_EVALUATOR_CONFIG",
+]
