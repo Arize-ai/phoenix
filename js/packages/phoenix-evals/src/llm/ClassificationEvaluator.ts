630e7cdaee1c42c63498b1368aeeecb349eaa13e
Mikyo King <mikyo@arize.com>
2025-11-24 11:36:38 -0700

    feat(js): input mapping for evaluators (#10360)
    * cleanup
    
    * cleanup
    
    * feat: bindEvaluator and input mapping support for ts evals
    
    * cleanup
    
    * fix types
    
    * cleanup
    
    * cleanup
    
    * cleanup
    
    * docs
    
    * add to readme

--- js/packages/phoenix-evals/src/llm/ClassificationEvaluator.ts
+++ js/packages/phoenix-evals/src/llm/ClassificationEvaluator.ts
@@ -1,13 +1,18 @@
 import { getTemplateVariables } from "../template";
 import {
+  ClassificationChoicesMap,
   CreateClassificationEvaluatorArgs,
   EvaluatorFn,
   PromptTemplate,
   WithPromptTemplate,
 } from "../types";
+import { ObjectMapping } from "../types/data";
+import { remapObject } from "../utils/objectMappingUtils";
 
 import { createClassifierFn } from "./createClassifierFn";
 import { LLMEvaluator } from "./LLMEvaluator";
+
+import { LanguageModel } from "ai";
 
 /**
  * An LLM evaluator that performs evaluation via classification
@@ -18,14 +23,34 @@ export class ClassificationEvaluator<RecordType extends Record<string, unknown>>
 {
   readonly evaluatorFn: EvaluatorFn<RecordType>;
   readonly promptTemplate: PromptTemplate;
+  /**
+   * A dynamically computed set of prompt template variables
+   */
   private _promptTemplateVariables: string[] | undefined;
-  constructor(args: CreateClassificationEvaluatorArgs) {
+  /**
+   * The model to use for classification
+   */
+  readonly model: LanguageModel;
+  /**
+   * The choices to classify the example into
+   */
+  readonly choices: ClassificationChoicesMap;
+
+  constructor(args: CreateClassificationEvaluatorArgs<RecordType>) {
     super(args);
     this.promptTemplate = args.promptTemplate;
-    this.evaluatorFn = createClassifierFn<RecordType>(args);
+    this.model = args.model;
+    this.choices = args.choices;
+    this.evaluatorFn = createClassifierFn<RecordType>({
+      ...args,
+    });
   }
   evaluate = (example: RecordType) => {
-    return this.evaluatorFn(example);
+    return this.evaluatorFn(
+      this.inputMapping
+        ? remapObject<RecordType>(example, this.inputMapping)
+        : example
+    );
   };
   /**
    * List out the prompt template variables needed to perform evaluation
@@ -39,5 +64,16 @@ export class ClassificationEvaluator<RecordType extends Record<string, unknown>>
     }
     // Give a copy of the variables
     return [...this._promptTemplateVariables];
+  }
+  /**
+   * Binds the input mapping to the evaluator. It makes a a copy of the evaluator and returns it.
+   */
+  bindInputMapping(
+    inputMapping: ObjectMapping<RecordType>
+  ): ClassificationEvaluator<RecordType> {
+    return new ClassificationEvaluator({
+      ...this,
+      inputMapping,
+    });
   }
 }
--- js/packages/phoenix-evals/src/types/otel.ts
+++ js/packages/phoenix-evals/src/types/otel.ts
@@ -1,17 +1,19 @@
 import { Tracer } from "@opentelemetry/api";
 
+export type TelemetryConfig = {
+  /**
+   * Whether OpenTelemetry is enabled on the call.
+   * Defaults to true for visibility into the evals calls.
+   * @default true
+   */
+  isEnabled?: boolean;
+  /**
+   * The tracer to use for the call.
+   * If not provided, the traces will get picked up by the global tracer.
+   */
+  tracer?: Tracer;
+};
+
 export type WithTelemetry = {
-  telemetry?: {
-    /**
-     * Whether OpenTelemetry is enabled on the call.
-     * Defaults to true for visibility into the evals calls.
-     * @default true
-     */
-    isEnabled?: boolean;
-    /**
-     * The tracer to use for the call.
-     * If not provided, the traces will get picked up by the global tracer.
-     */
-    tracer?: Tracer;
-  };
+  telemetry?: TelemetryConfig;
 };
