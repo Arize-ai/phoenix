6f42acb4689a37796b92303465271a154b469548
Mikyo King <mikyo@arize.com>
2025-11-25 12:11:59 -0700

    test: add viewer role to tests (#10398)
    * fix: add viewer role to tests
    
    * global setup for viewer
    
    * add global setup
    
    * fix global setup
    
    * cleanup
    
    * Update app/tests/viewer-access.spec.ts
    
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>

--- app/global-setup.ts
+++ app/global-setup.ts
@@ -23,9 +23,9 @@ async function globalSetup(config: FullConfig) {
   await page.waitForURL("**/projects");
   await page.goto(`${baseURL}/settings/general`);
   await page.waitForURL("**/settings/general");
-  await page.getByRole("button", { name: "Add User" }).click();
 
   // Add member user
+  await page.getByRole("button", { name: "Add User" }).click();
   await page.getByLabel("Email").fill("member@localhost.com");
   await page.getByLabel("Username").fill("member");
   await page.getByLabel("Password", { exact: true }).fill("member");
@@ -38,11 +38,25 @@ async function globalSetup(config: FullConfig) {
     .getByRole("button", { name: "Add User" })
     .click();
 
+  // Add viewer user
+  await page.getByRole("button", { name: "Add User" }).click();
+  await page.getByLabel("Email").fill("viewer@localhost.com");
+  await page.getByLabel("Username").fill("viewer");
+  await page.getByLabel("Password", { exact: true }).fill("viewer");
+  await page.getByLabel("Confirm Password").fill("viewer");
+
+  await page.getByRole("dialog").getByLabel("member", { exact: true }).click();
+  await page.getByRole("option", { name: "viewer" }).click();
+  await page
+    .getByRole("dialog")
+    .getByRole("button", { name: "Add User" })
+    .click();
+
   // Log out of admin account
   await page.getByRole("button", { name: "Log Out" }).click();
 
   // Log in as member
-  page.goto(`${baseURL}/login`);
+  await page.goto(`${baseURL}/login`);
   await page.getByLabel("Email").fill("member@localhost.com");
   await page.getByLabel("Password").fill("member");
   await page.getByRole("button", { name: "Log In", exact: true }).click();
@@ -52,6 +66,19 @@ async function globalSetup(config: FullConfig) {
   await page.getByLabel("Old Password").fill("member");
   await page.getByLabel("New Password").fill("member123");
   await page.getByLabel("Confirm Password").fill("member123");
+  await page.getByRole("button", { name: "Reset Password" }).click();
+
+  // Log in as viewer
+  await page.goto(`${baseURL}/login`);
+  await page.getByLabel("Email").fill("viewer@localhost.com");
+  await page.getByLabel("Password").fill("viewer");
+  await page.getByRole("button", { name: "Log In", exact: true }).click();
+
+  // Reset viewer password
+  await page.waitForURL("**/reset-password");
+  await page.getByLabel("Old Password").fill("viewer");
+  await page.getByLabel("New Password").fill("viewer123");
+  await page.getByLabel("Confirm Password").fill("viewer123");
   await page.getByRole("button", { name: "Reset Password" }).click();
 }
 
