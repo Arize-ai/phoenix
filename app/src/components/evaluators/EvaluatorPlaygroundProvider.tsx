e8409de4b15f9b6d632eb4699273bc1d48362a04
Mikyo King <mikyo@arize.com>
2025-12-02 10:44:18 -0700

    refactor: add preview toggle for evaluator template (#10432)

---
+++ app/src/components/evaluators/EvaluatorPlaygroundProvider.tsx
@@ -0,0 +1,81 @@
+import { PropsWithChildren, useMemo } from "react";
+import { graphql, useLazyLoadQuery } from "react-relay";
+
+import { EvaluatorPlaygroundProviderQuery } from "@phoenix/components/evaluators/__generated__/EvaluatorPlaygroundProviderQuery.graphql";
+import { makeLLMEvaluatorInstance } from "@phoenix/components/evaluators/EvaluatorChatTemplate/utils";
+import { usePreferencesContext } from "@phoenix/contexts";
+import { PlaygroundProvider } from "@phoenix/contexts/PlaygroundContext";
+import { fetchPlaygroundPrompt_promptVersionToInstance_promptVersion$key } from "@phoenix/pages/playground/__generated__/fetchPlaygroundPrompt_promptVersionToInstance_promptVersion.graphql";
+import { promptVersionToInstance } from "@phoenix/pages/playground/fetchPlaygroundPrompt";
+import { NoInstalledProvider } from "@phoenix/pages/playground/NoInstalledProvider";
+import { generateInstanceId } from "@phoenix/store";
+
+export const EvaluatorPlaygroundProvider = ({
+  children,
+  promptVersionRef,
+  promptVersionTag,
+  promptName,
+  promptId,
+}: PropsWithChildren<{
+  promptId?: string;
+  promptName?: string;
+  promptVersionRef?: fetchPlaygroundPrompt_promptVersionToInstance_promptVersion$key;
+  promptVersionTag?: string;
+}>) => {
+  const { modelProviders } = useLazyLoadQuery<EvaluatorPlaygroundProviderQuery>(
+    graphql`
+      query EvaluatorPlaygroundProviderQuery {
+        modelProviders {
+          name
+          dependenciesInstalled
+          dependencies
+        }
+      }
+    `,
+    {}
+  );
+  const modelConfigByProvider = usePreferencesContext(
+    (state) => state.modelConfigByProvider
+  );
+
+  const hasInstalledProvider = modelProviders.some(
+    (provider) => provider.dependenciesInstalled
+  );
+
+  const defaultInstances = useMemo(() => {
+    if (promptId && promptName && promptVersionRef) {
+      return [
+        {
+          ...promptVersionToInstance({
+            promptId,
+            promptName,
+            promptVersionRef,
+            promptVersionTag: promptVersionTag ?? null,
+            supportedInvocationParameters: [],
+          }),
+          id: generateInstanceId(),
+        },
+      ];
+    }
+    return makeLLMEvaluatorInstance(modelConfigByProvider);
+  }, [
+    modelConfigByProvider,
+    promptId,
+    promptName,
+    promptVersionRef,
+    promptVersionTag,
+  ]);
+
+  if (!hasInstalledProvider) {
+    return <NoInstalledProvider availableProviders={modelProviders} />;
+  }
+
+  return (
+    <PlaygroundProvider
+      instances={defaultInstances}
+      modelConfigByProvider={modelConfigByProvider}
+    >
+      {children}
+    </PlaygroundProvider>
+  );
+};
