af1c007ac1cde0c99a9e259aef1ae60cb47a82ca
Mikyo King <mikyo@arize.com>
2025-12-02 10:44:18 -0700

    refactor: add preview toggle for evaluator template (#10432)

--- app/src/components/evaluators/EvaluatorChatTemplate/EvaluatorChatTemplate.tsx
+++ app/src/components/evaluators/EvaluatorChatTemplate/EvaluatorChatTemplate.tsx
@@ -1,92 +1,9 @@
-import { PropsWithChildren, useMemo } from "react";
-import { graphql, useLazyLoadQuery } from "react-relay";
-
-import { EvaluatorChatTemplateQuery } from "@phoenix/components/evaluators/EvaluatorChatTemplate/__generated__/EvaluatorChatTemplateQuery.graphql";
-import { makeLLMEvaluatorInstance } from "@phoenix/components/evaluators/EvaluatorChatTemplate/utils";
-import { usePreferencesContext } from "@phoenix/contexts";
-import {
-  PlaygroundProvider,
-  usePlaygroundContext,
-} from "@phoenix/contexts/PlaygroundContext";
-import { fetchPlaygroundPrompt_promptVersionToInstance_promptVersion$key } from "@phoenix/pages/playground/__generated__/fetchPlaygroundPrompt_promptVersionToInstance_promptVersion.graphql";
-import { promptVersionToInstance } from "@phoenix/pages/playground/fetchPlaygroundPrompt";
-import { NoInstalledProvider } from "@phoenix/pages/playground/NoInstalledProvider";
+import { usePlaygroundContext } from "@phoenix/contexts/PlaygroundContext";
 import { PlaygroundTemplate } from "@phoenix/pages/playground/PlaygroundTemplate";
-import { generateInstanceId } from "@phoenix/store";
-
-export const EvaluatorChatTemplateProvider = ({
-  children,
-  promptVersionRef,
-  promptVersionTag,
-  promptName,
-  promptId,
-}: PropsWithChildren<{
-  promptId?: string;
-  promptName?: string;
-  promptVersionRef?: fetchPlaygroundPrompt_promptVersionToInstance_promptVersion$key;
-  promptVersionTag?: string;
-}>) => {
-  const { modelProviders } = useLazyLoadQuery<EvaluatorChatTemplateQuery>(
-    graphql`
-      query EvaluatorChatTemplateQuery {
-        modelProviders {
-          name
-          dependenciesInstalled
-          dependencies
-        }
-      }
-    `,
-    {}
-  );
-  const modelConfigByProvider = usePreferencesContext(
-    (state) => state.modelConfigByProvider
-  );
-
-  const hasInstalledProvider = modelProviders.some(
-    (provider) => provider.dependenciesInstalled
-  );
-
-  const defaultInstances = useMemo(() => {
-    if (promptId && promptName && promptVersionRef) {
-      return [
-        {
-          ...promptVersionToInstance({
-            promptId,
-            promptName,
-            promptVersionRef,
-            promptVersionTag: promptVersionTag ?? null,
-            supportedInvocationParameters: [],
-          }),
-          id: generateInstanceId(),
-        },
-      ];
-    }
-    return makeLLMEvaluatorInstance(modelConfigByProvider);
-  }, [
-    modelConfigByProvider,
-    promptId,
-    promptName,
-    promptVersionRef,
-    promptVersionTag,
-  ]);
-
-  if (!hasInstalledProvider) {
-    return <NoInstalledProvider availableProviders={modelProviders} />;
-  }
-
-  return (
-    <PlaygroundProvider
-      instances={defaultInstances}
-      modelConfigByProvider={modelConfigByProvider}
-    >
-      {children}
-    </PlaygroundProvider>
-  );
-};
 
 export const EvaluatorChatTemplate = () => {
   const instances = usePlaygroundContext((state) => state.instances);
-  const instanceId = useMemo(() => instances[0].id, [instances]);
+  const instanceId = instances[0].id;
   return (
     <PlaygroundTemplate
       playgroundInstanceId={instanceId}
--- app/src/components/evaluators/__generated__/EvaluatorPlaygroundProviderQuery.graphql.ts
+++ app/src/components/evaluators/__generated__/EvaluatorPlaygroundProviderQuery.graphql.ts
@@ -1,5 +1,5 @@
 /**
- * @generated SignedSource<<74b56c4ecd52206d48506c984c289a89>>
+ * @generated SignedSource<<b6b043d1b954dae965bff4679669cdf3>>
  * @lightSyntaxTransform
  * @nogrep
  */
@@ -9,17 +9,17 @@
 // @ts-nocheck
 
 import { ConcreteRequest } from 'relay-runtime';
-export type EvaluatorChatTemplateQuery$variables = Record<PropertyKey, never>;
-export type EvaluatorChatTemplateQuery$data = {
+export type EvaluatorPlaygroundProviderQuery$variables = Record<PropertyKey, never>;
+export type EvaluatorPlaygroundProviderQuery$data = {
   readonly modelProviders: ReadonlyArray<{
     readonly dependencies: ReadonlyArray<string>;
     readonly dependenciesInstalled: boolean;
     readonly name: string;
   }>;
 };
-export type EvaluatorChatTemplateQuery = {
-  response: EvaluatorChatTemplateQuery$data;
-  variables: EvaluatorChatTemplateQuery$variables;
+export type EvaluatorPlaygroundProviderQuery = {
+  response: EvaluatorPlaygroundProviderQuery$data;
+  variables: EvaluatorPlaygroundProviderQuery$variables;
 };
 
 const node: ConcreteRequest = (function(){
@@ -62,7 +62,7 @@ return {
     "argumentDefinitions": [],
     "kind": "Fragment",
     "metadata": null,
-    "name": "EvaluatorChatTemplateQuery",
+    "name": "EvaluatorPlaygroundProviderQuery",
     "selections": (v0/*: any*/),
     "type": "Query",
     "abstractKey": null
@@ -71,20 +71,20 @@ return {
   "operation": {
     "argumentDefinitions": [],
     "kind": "Operation",
-    "name": "EvaluatorChatTemplateQuery",
+    "name": "EvaluatorPlaygroundProviderQuery",
     "selections": (v0/*: any*/)
   },
   "params": {
-    "cacheID": "848d5723c3176020a792a90e218e6440",
+    "cacheID": "d14405c14c659f9aa5edc1edb1aa694b",
     "id": null,
     "metadata": {},
-    "name": "EvaluatorChatTemplateQuery",
+    "name": "EvaluatorPlaygroundProviderQuery",
     "operationKind": "query",
-    "text": "query EvaluatorChatTemplateQuery {\n  modelProviders {\n    name\n    dependenciesInstalled\n    dependencies\n  }\n}\n"
+    "text": "query EvaluatorPlaygroundProviderQuery {\n  modelProviders {\n    name\n    dependenciesInstalled\n    dependencies\n  }\n}\n"
   }
 };
 })();
 
-(node as any).hash = "5c7603303ece465623ccbb5ec9426b05";
+(node as any).hash = "e2a15f3f5e2c1218d23664988d54db05";
 
 export default node;
