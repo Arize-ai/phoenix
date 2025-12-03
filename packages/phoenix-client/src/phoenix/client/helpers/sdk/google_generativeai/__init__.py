186c0270b069290cb984daf734646ee5aee662a0
Alexander Song <axiomofjoy@gmail.com>
2025-12-03 17:57:51 -0500

    commit

---
+++ packages/phoenix-client/src/phoenix/client/helpers/sdk/google_generativeai/__init__.py
@@ -0,0 +1,8 @@
+import warnings
+
+warnings.warn(
+    "The 'phoenix.client.helpers.sdk.google_generativeai' module is deprecated. "
+    "Use 'phoenix.client.helpers.sdk.google_genai' instead.",
+    DeprecationWarning,
+    stacklevel=2,
+)
