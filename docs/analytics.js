bc05dab51eb19a3b4e9f58a4d2fb59e2fb8bd00d
Mikyo King <mikyo@arize.com>
2025-12-10 11:26:13 -0700

    docs: add scarf analytics to docs

---
+++ docs/analytics.js
@@ -0,0 +1,6 @@
+// Scarf.sh to gain insight into how people are accessing documentation
+const img = document.createElement('img');
+img.src = 'https://static.scarf.sh/a.png?x-pxid=6bd245a7-3214-444b-a83b-24f73272b4ba';
+img.referrerPolicy = 'no-referrer-when-downgrade';
+img.id = 'scarf-sh-analytics';
+document.body.appendChild(img);
