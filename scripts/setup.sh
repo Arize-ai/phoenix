d4a9c6d7c2668269cd6e8a4c05ce23534b7148ad
Tony Powell <apowell@arize.com>
2025-12-10 10:47:22 -0500

    source nvm immediately

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -46,7 +46,8 @@ if [ ! -d "$HOME/.nvm" ]; then
     
     # Source nvm for current session
     export NVM_DIR="$HOME/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 else
     echo "nvm is already installed"
     # Source nvm if it exists
