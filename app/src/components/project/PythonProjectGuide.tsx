af2be2de13320c61665951222e18d4092b38957c
Srilakshmi Chavali <schavali03@gmail.com>
2025-12-11 11:27:51 -0800

    fixing link as per cursorbot suggestion

--- app/src/components/project/PythonProjectGuide.tsx
+++ app/src/components/project/PythonProjectGuide.tsx
@@ -38,7 +38,7 @@ function getEnvironmentVariablesPython({
   if (isHosted) {
     return `PHOENIX_CLIENT_HEADERS='api_key=<your-api-key>'\nPHOENIX_COLLECTOR_ENDPOINT='${HOSTED_PHOENIX_URL}'`;
   } else if (isAuthEnabled) {
-    return `PHOENIX_API_KEY='<your-api-key>'\nPHOENIX_COLLECTOR_ENDPOINT='${HOSTED_PHOENIX_URL}'`;
+    return `PHOENIX_API_KEY='<your-api-key>'\nPHOENIX_COLLECTOR_ENDPOINT='${BASE_URL}'`;
   }
   return `PHOENIX_COLLECTOR_ENDPOINT='${BASE_URL}'`;
 }
