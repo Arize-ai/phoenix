63f0d3f2587130f998345ab7f946ef739d5631c7
Mikyo King <mikyo@arize.com>
2025-12-16 17:57:51 -0700

    relay codegen

--- app/src/pages/dataset/__generated__/datasetLoaderQuery.graphql.ts
+++ app/src/pages/dataset/__generated__/datasetLoaderQuery.graphql.ts
@@ -8,7 +8,7 @@
 /* eslint-disable */
 // @ts-nocheck
 
-import { ConcreteRequest } from 'relay-runtime';
+import { ConcreteRequest } from "relay-runtime";
 export type datasetLoaderQuery$variables = {
   id: string;
 };
@@ -40,202 +40,199 @@ export type datasetLoaderQuery = {
   variables: datasetLoaderQuery$variables;
 };
 
-const node: ConcreteRequest = (function(){
-var v0 = [
-  {
-    "defaultValue": null,
-    "kind": "LocalArgument",
-    "name": "id"
-  }
-],
-v1 = [
-  {
-    "kind": "Variable",
-    "name": "id",
-    "variableName": "id"
-  }
-],
-v2 = {
-  "alias": null,
-  "args": null,
-  "kind": "ScalarField",
-  "name": "id",
-  "storageKey": null
-},
-v3 = {
-  "alias": null,
-  "args": null,
-  "kind": "ScalarField",
-  "name": "name",
-  "storageKey": null
-},
-v4 = {
-  "alias": null,
-  "args": null,
-  "kind": "ScalarField",
-  "name": "description",
-  "storageKey": null
-},
-v5 = {
-  "kind": "InlineFragment",
-  "selections": [
-    (v3/*: any*/),
-    (v4/*: any*/),
-    {
-      "alias": null,
-      "args": null,
-      "kind": "ScalarField",
-      "name": "exampleCount",
-      "storageKey": null
+const node: ConcreteRequest = (function () {
+  var v0 = [
+      {
+        defaultValue: null,
+        kind: "LocalArgument",
+        name: "id",
+      },
+    ],
+    v1 = [
+      {
+        kind: "Variable",
+        name: "id",
+        variableName: "id",
+      },
+    ],
+    v2 = {
+      alias: null,
+      args: null,
+      kind: "ScalarField",
+      name: "id",
+      storageKey: null,
     },
-    {
-      "alias": null,
-      "args": null,
-      "kind": "ScalarField",
-      "name": "experimentCount",
-      "storageKey": null
+    v3 = {
+      alias: null,
+      args: null,
+      kind: "ScalarField",
+      name: "name",
+      storageKey: null,
     },
-    {
-      "alias": null,
-      "args": null,
-      "concreteType": "DatasetLabel",
-      "kind": "LinkedField",
-      "name": "labels",
-      "plural": true,
-      "selections": [
-        (v2/*: any*/),
-        (v3/*: any*/),
+    v4 = {
+      alias: null,
+      args: null,
+      kind: "ScalarField",
+      name: "description",
+      storageKey: null,
+    },
+    v5 = {
+      kind: "InlineFragment",
+      selections: [
+        v3 /*: any*/,
+        v4 /*: any*/,
         {
-          "alias": null,
-          "args": null,
-          "kind": "ScalarField",
-          "name": "color",
-          "storageKey": null
-        }
-      ],
-      "storageKey": null
-    },
-    {
-      "alias": "latestVersions",
-      "args": [
-        {
-          "kind": "Literal",
-          "name": "first",
-          "value": 1
+          alias: null,
+          args: null,
+          kind: "ScalarField",
+          name: "exampleCount",
+          storageKey: null,
         },
         {
-          "kind": "Literal",
-          "name": "sort",
-          "value": {
-            "col": "createdAt",
-            "dir": "desc"
-          }
-        }
-      ],
-      "concreteType": "DatasetVersionConnection",
-      "kind": "LinkedField",
-      "name": "versions",
-      "plural": false,
-      "selections": [
+          alias: null,
+          args: null,
+          kind: "ScalarField",
+          name: "experimentCount",
+          storageKey: null,
+        },
         {
-          "alias": null,
-          "args": null,
-          "concreteType": "DatasetVersionEdge",
-          "kind": "LinkedField",
-          "name": "edges",
-          "plural": true,
-          "selections": [
+          alias: null,
+          args: null,
+          concreteType: "DatasetLabel",
+          kind: "LinkedField",
+          name: "labels",
+          plural: true,
+          selections: [
+            v2 /*: any*/,
+            v3 /*: any*/,
             {
-              "alias": "version",
-              "args": null,
-              "concreteType": "DatasetVersion",
-              "kind": "LinkedField",
-              "name": "node",
-              "plural": false,
-              "selections": [
-                (v2/*: any*/),
-                (v4/*: any*/),
-                {
-                  "alias": null,
-                  "args": null,
-                  "kind": "ScalarField",
-                  "name": "createdAt",
-                  "storageKey": null
-                }
-              ],
-              "storageKey": null
-            }
+              alias: null,
+              args: null,
+              kind: "ScalarField",
+              name: "color",
+              storageKey: null,
+            },
           ],
-          "storageKey": null
-        }
+          storageKey: null,
+        },
+        {
+          alias: "latestVersions",
+          args: [
+            {
+              kind: "Literal",
+              name: "first",
+              value: 1,
+            },
+            {
+              kind: "Literal",
+              name: "sort",
+              value: {
+                col: "createdAt",
+                dir: "desc",
+              },
+            },
+          ],
+          concreteType: "DatasetVersionConnection",
+          kind: "LinkedField",
+          name: "versions",
+          plural: false,
+          selections: [
+            {
+              alias: null,
+              args: null,
+              concreteType: "DatasetVersionEdge",
+              kind: "LinkedField",
+              name: "edges",
+              plural: true,
+              selections: [
+                {
+                  alias: "version",
+                  args: null,
+                  concreteType: "DatasetVersion",
+                  kind: "LinkedField",
+                  name: "node",
+                  plural: false,
+                  selections: [
+                    v2 /*: any*/,
+                    v4 /*: any*/,
+                    {
+                      alias: null,
+                      args: null,
+                      kind: "ScalarField",
+                      name: "createdAt",
+                      storageKey: null,
+                    },
+                  ],
+                  storageKey: null,
+                },
+              ],
+              storageKey: null,
+            },
+          ],
+          storageKey: 'versions(first:1,sort:{"col":"createdAt","dir":"desc"})',
+        },
       ],
-      "storageKey": "versions(first:1,sort:{\"col\":\"createdAt\",\"dir\":\"desc\"})"
-    }
-  ],
-  "type": "Dataset",
-  "abstractKey": null
-};
-return {
-  "fragment": {
-    "argumentDefinitions": (v0/*: any*/),
-    "kind": "Fragment",
-    "metadata": null,
-    "name": "datasetLoaderQuery",
-    "selections": [
-      {
-        "alias": "dataset",
-        "args": (v1/*: any*/),
-        "concreteType": null,
-        "kind": "LinkedField",
-        "name": "node",
-        "plural": false,
-        "selections": [
-          (v2/*: any*/),
-          (v5/*: any*/)
-        ],
-        "storageKey": null
-      }
-    ],
-    "type": "Query",
-    "abstractKey": null
-  },
-  "kind": "Request",
-  "operation": {
-    "argumentDefinitions": (v0/*: any*/),
-    "kind": "Operation",
-    "name": "datasetLoaderQuery",
-    "selections": [
-      {
-        "alias": "dataset",
-        "args": (v1/*: any*/),
-        "concreteType": null,
-        "kind": "LinkedField",
-        "name": "node",
-        "plural": false,
-        "selections": [
-          {
-            "alias": null,
-            "args": null,
-            "kind": "ScalarField",
-            "name": "__typename",
-            "storageKey": null
-          },
-          (v2/*: any*/),
-          (v5/*: any*/)
-        ],
-        "storageKey": null
-      }
-    ]
-  },
-  "params": {
-    "cacheID": "5ba8ebf4f5fb94654a8719703a46bac5",
-    "id": null,
-    "metadata": {},
-    "name": "datasetLoaderQuery",
-    "operationKind": "query",
-    "text": "query datasetLoaderQuery(\n  $id: ID!\n) {\n  dataset: node(id: $id) {\n    __typename\n    id\n    ... on Dataset {\n      id\n      name\n      description\n      exampleCount\n      experimentCount\n      labels {\n        id\n        name\n        color\n      }\n      latestVersions: versions(first: 1, sort: {col: createdAt, dir: desc}) {\n        edges {\n          version: node {\n            id\n            description\n            createdAt\n          }\n        }\n      }\n    }\n  }\n}\n"
-  }
-};
+      type: "Dataset",
+      abstractKey: null,
+    };
+  return {
+    fragment: {
+      argumentDefinitions: v0 /*: any*/,
+      kind: "Fragment",
+      metadata: null,
+      name: "datasetLoaderQuery",
+      selections: [
+        {
+          alias: "dataset",
+          args: v1 /*: any*/,
+          concreteType: null,
+          kind: "LinkedField",
+          name: "node",
+          plural: false,
+          selections: [v2 /*: any*/, v5 /*: any*/],
+          storageKey: null,
+        },
+      ],
+      type: "Query",
+      abstractKey: null,
+    },
+    kind: "Request",
+    operation: {
+      argumentDefinitions: v0 /*: any*/,
+      kind: "Operation",
+      name: "datasetLoaderQuery",
+      selections: [
+        {
+          alias: "dataset",
+          args: v1 /*: any*/,
+          concreteType: null,
+          kind: "LinkedField",
+          name: "node",
+          plural: false,
+          selections: [
+            {
+              alias: null,
+              args: null,
+              kind: "ScalarField",
+              name: "__typename",
+              storageKey: null,
+            },
+            v2 /*: any*/,
+            v5 /*: any*/,
+          ],
+          storageKey: null,
+        },
+      ],
+    },
+    params: {
+      cacheID: "5ba8ebf4f5fb94654a8719703a46bac5",
+      id: null,
+      metadata: {},
+      name: "datasetLoaderQuery",
+      operationKind: "query",
+      text: "query datasetLoaderQuery(\n  $id: ID!\n) {\n  dataset: node(id: $id) {\n    __typename\n    id\n    ... on Dataset {\n      id\n      name\n      description\n      exampleCount\n      experimentCount\n      labels {\n        id\n        name\n        color\n      }\n      latestVersions: versions(first: 1, sort: {col: createdAt, dir: desc}) {\n        edges {\n          version: node {\n            id\n            description\n            createdAt\n          }\n        }\n      }\n    }\n  }\n}\n",
+    },
+  };
 })();
 
 (node as any).hash = "d72f520af32d53dad5a6e33611650186";
