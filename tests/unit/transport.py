bb3a78ae9287a12a7dd49fda153c03599f37d5f5
Xander Song <axiomofjoy@gmail.com>
2025-12-19 12:03:21 -0500

    test(evaluator): test playground evaluators (#10641)
    * test
    
    * empty
    
    * cassette and api keys
    
    * fix
    
    * simplify
    
    * subscription test
    
    * mutation tests
    
    * eval not run on chat completion mutation when task errors
    
    * test eval not run on chat completion over dataset mutation when task errors
    
    * test eval not run on chat completion subscription when task errors
    
    * test eval not run on chat completion over subscription when task errors
    
    * clean
    
    * rename
    
    * fix
    
    * test
    
    * clean
    
    * clean
    
    * clean
    
    * fix types

--- tests/unit/transport.py
+++ tests/unit/transport.py
@@ -143,6 +143,8 @@ class ASGIWebSocketAsyncNetworkStream(AsyncNetworkStream):
                 await self.send({"type": "websocket.receive", "text": event.data})
             elif isinstance(event, wsproto.events.BytesMessage):
                 await self.send({"type": "websocket.receive", "bytes": event.data})
+            elif isinstance(event, wsproto.events.Ping):
+                await self.send({"type": "websocket.pong"})
             else:
                 raise UnhandledWebSocketEvent(event)
 
