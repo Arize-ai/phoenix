696d55df7be1080a26f7e616e43641d0c70056e4
Alexander Song <axiomofjoy@gmail.com>
2025-12-23 13:13:17 -0500

    tmp

--- js/packages/phoenix-evals/src/__generated__/default_templates/DOCUMENT_RELEVANCE_CLASSIFICATION_EVALUATOR_CONFIG.ts
+++ js/packages/phoenix-evals/src/__generated__/default_templates/DOCUMENT_RELEVANCE_CLASSIFICATION_EVALUATOR_CONFIG.ts
@@ -20,6 +20,7 @@ You are comparing a document to a question and trying to determine if the docume
 {{documentText}}
 </document_text>
 </data>
+
 Compare the question above to the document text. You must determine whether the document text contains information that can answer the question. Please focus on whether the very specific question can be answered by the information in the document text. Your response must be either "relevant" or "unrelated". "unrelated" means that the document text does not contain an answer to the question. "relevant" means the document text contains an answer to the question.
 `,
     },
--- src/phoenix/__generated__/classification_evaluator_configs/_document_relevance_classification_evaluator_config.py
+++ src/phoenix/__generated__/classification_evaluator_configs/_document_relevance_classification_evaluator_config.py
@@ -10,7 +10,7 @@ DOCUMENT_RELEVANCE_CLASSIFICATION_EVALUATOR_CONFIG = ClassificationEvaluatorConf
     messages=[
         PromptMessage(
             role="user",
-            content='You are comparing a document to a question and trying to determine if the document text contains information relevant to answering the question. Here is the data:\n\n<data>\n<question>\n{{input}}\n</question>\n<document_text>\n{{document_text}}\n</document_text>\n</data>\nCompare the question above to the document text. You must determine whether the document text contains information that can answer the question. Please focus on whether the very specific question can be answered by the information in the document text. Your response must be either "relevant" or "unrelated". "unrelated" means that the document text does not contain an answer to the question. "relevant" means the document text contains an answer to the question.',
+            content='You are comparing a document to a question and trying to determine if the document text contains information relevant to answering the question. Here is the data:\n\n<data>\n<question>\n{{input}}\n</question>\n<document_text>\n{{document_text}}\n</document_text>\n</data>\n\nCompare the question above to the document text. You must determine whether the document text contains information that can answer the question. Please focus on whether the very specific question can be answered by the information in the document text. Your response must be either "relevant" or "unrelated". "unrelated" means that the document text does not contain an answer to the question. "relevant" means the document text contains an answer to the question.',
         )
     ],
     choices={"relevant": 1.0, "unrelated": 0.0},
