5b53d4718233819c7d9266209630de08846f865e
Alexander Song <axiomofjoy@gmail.com>
2025-12-23 13:11:33 -0500

    clean

--- js/packages/phoenix-evals/src/__generated__/default_templates/DOCUMENT_RELEVANCE_CLASSIFICATION_EVALUATOR_CONFIG.ts
+++ js/packages/phoenix-evals/src/__generated__/default_templates/DOCUMENT_RELEVANCE_CLASSIFICATION_EVALUATOR_CONFIG.ts
@@ -13,8 +13,12 @@ export const DOCUMENT_RELEVANCE_CLASSIFICATION_EVALUATOR_CONFIG: ClassificationE
 You are comparing a document to a question and trying to determine if the document text contains information relevant to answering the question. Here is the data:
 
 <data>
-<question> {{input}} </question>
-<document_text> {{documentText}} </document_text>
+<question>
+{{input}}
+</question>
+<document_text>
+{{documentText}}
+</document_text>
 </data>
 Compare the question above to the document text. You must determine whether the document text contains information that can answer the question. Please focus on whether the very specific question can be answered by the information in the document text. Your response must be either "relevant" or "unrelated". "unrelated" means that the document text does not contain an answer to the question. "relevant" means the document text contains an answer to the question.
 `,
