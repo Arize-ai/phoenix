05ecb91f8b8328669d7863145a24d44e01781ee5
Mikyo King <mikyo@arize.com>
2025-12-22 18:43:16 -0700

    feat: add key icon

--- app/src/components/icon/Icons.tsx
+++ app/src/components/icon/Icons.tsx
@@ -1584,6 +1584,24 @@ export const InfoOutline = () => (
 
 //J
 //K
+export const KeyOutline = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path
+      fill="none"
+      d="M21 2l-2 2m-7.61 7.61a5.5 5.5 0 1 1-7.778 7.778 5.5 5.5 0 0 1 7.777-7.777zm0 0L15.5 7.5m0 0l3 3L22 7l-3-3m-3.5 3.5L19 4"
+    ></path>
+  </svg>
+);
 //L
 export const LassoOutline = () => (
   <svg
