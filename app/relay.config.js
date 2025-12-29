9c39d39424d9cd9791bbf2951e0c6c3f13cc338f
Roger Yang <roger.yang@arize.com>
2025-12-29 14:33:32 -0800

    Add SecretString type for generative credentials

--- app/relay.config.js
+++ app/relay.config.js
@@ -11,6 +11,7 @@ module.exports = {
     UUID: "string",
     Identifier: "string",
     CronExpression: "string",
+    SecretString: "string",
   },
   typescriptExcludeUndefinedFromNullableUnion: true,
 };
