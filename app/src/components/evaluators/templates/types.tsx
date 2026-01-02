2ac2011e3eb0d8f7a436daa2cf7f78b39e774a63
Mikyo King <mikyo@arize.com>
2026-01-02 14:47:26 -0700

    feat: add default output config for llm evaluators (#10775)

--- app/src/components/evaluators/templates/types.tsx
+++ app/src/components/evaluators/templates/types.tsx
@@ -1,4 +1,7 @@
+import type { ClassificationEvaluatorAnnotationConfig } from "@phoenix/types";
+
 export type LLMEvaluatorTemplate = Readonly<{
   systemPrompt: string;
   userPrompt: string;
+  outputConfig: Readonly<ClassificationEvaluatorAnnotationConfig>;
 }>;
