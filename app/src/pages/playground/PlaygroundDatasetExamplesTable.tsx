e3723ddbfe97bc23c36469fc6918c58befd3b954
Tony Powell <apowell@arize.com>
2026-01-06 16:04:35 -0500

    Display evaluation errors in list

--- app/src/pages/playground/PlaygroundDatasetExamplesTable.tsx
+++ app/src/pages/playground/PlaygroundDatasetExamplesTable.tsx
@@ -10,6 +10,7 @@ import {
   useRef,
   useState,
 } from "react";
+import { Pressable } from "react-aria";
 import {
   Disposable,
   graphql,
@@ -103,7 +104,9 @@ import PlaygroundDatasetExamplesTableSubscription, {
   PlaygroundDatasetExamplesTableSubscription$data,
 } from "./__generated__/PlaygroundDatasetExamplesTableSubscription.graphql";
 import {
+  type EvaluationError,
   ExampleRunData,
+  type ExperimentRunEvaluation,
   InstanceResponses,
   usePlaygroundDatasetExamplesTableContext,
 } from "./PlaygroundDatasetExamplesTableContext";
@@ -309,6 +312,22 @@ function ExampleOutputContent({
     onViewTracePress,
   ]);
 
+  const successfulEvaluations = useMemo(() => {
+    return (
+      evaluations?.filter(
+        (e): e is ExperimentRunEvaluation => !("__typename" in e)
+      ) ?? []
+    );
+  }, [evaluations]);
+  const evaluationErrors = useMemo(() => {
+    return (
+      evaluations?.filter(
+        (e): e is EvaluationError =>
+          "__typename" in e && e.__typename === "EvaluationErrorChunk"
+      ) ?? []
+    );
+  }, [evaluations]);
+
   return (
     <Flex direction="column" height="100%">
       <CellTop extra={spanControls}>
@@ -359,9 +378,9 @@ function ExampleOutputContent({
           </View>
         ) : null}
       </Flex>
-      {evaluations != null && evaluations.length > 0 && (
+      {successfulEvaluations != null && successfulEvaluations.length > 0 && (
         <ExperimentRunCellAnnotationsList
-          annotations={evaluations}
+          annotations={successfulEvaluations}
           annotationOutputConfigs={evaluatorOutputConfigs}
           onTraceClick={({ traceId, projectId, annotationName }) => {
             if (traceId && projectId) {
@@ -369,6 +388,54 @@ function ExampleOutputContent({
             }
           }}
         />
+      )}
+      {evaluationErrors != null && evaluationErrors.length > 0 && (
+        <ul
+          css={css`
+            display: flex;
+            flex-direction: column;
+            gap: var(--ac-global-dimension-static-size-100);
+          `}
+        >
+          {evaluationErrors.map((error, index) => {
+            return (
+              <li key={`${error.evaluatorName}-${index}`}>
+                <TooltipTrigger delay={0}>
+                  <Pressable>
+                    <button
+                      className="button--reset"
+                      css={css`
+                        cursor: pointer;
+                        padding: var(--ac-global-dimension-size-50)
+                          var(--ac-global-dimension-size-100);
+                        flex: 1 1 auto;
+                        border-radius: var(--ac-global-rounding-small);
+                        width: 100%;
+                        min-width: 0;
+                        &:hover {
+                          background-color: var(--ac-global-color-grey-200);
+                        }
+                      `}
+                    >
+                      <Flex
+                        direction="row"
+                        gap="size-100"
+                        alignItems="center"
+                        justifyContent="start"
+                      >
+                        <Text color="danger">{error.evaluatorName}</Text>
+                      </Flex>
+                    </button>
+                  </Pressable>
+                  <Tooltip placement="top">
+                    <TooltipArrow />
+                    <Text>{error.message}</Text>
+                  </Tooltip>
+                </TooltipTrigger>
+              </li>
+            );
+          })}
+        </ul>
       )}
     </Flex>
   );
@@ -702,9 +769,11 @@ export function PlaygroundDatasetExamplesTable({
             if (chatCompletion.datasetExampleId == null) {
               return;
             }
-            notifyError({
-              title: `Evaluator "${chatCompletion.evaluatorName}" failed`,
-              message: chatCompletion.message,
+            appendExampleDataEvaluationChunk({
+              instanceId,
+              exampleId: chatCompletion.datasetExampleId,
+              repetitionNumber: chatCompletion.repetitionNumber ?? 1,
+              evaluationChunk: chatCompletion,
             });
             incrementEvalsCompleted(instanceId);
             break;
@@ -724,7 +793,6 @@ export function PlaygroundDatasetExamplesTable({
       incrementEvalsCompleted,
       incrementRunsCompleted,
       incrementRunsFailed,
-      notifyError,
       updateExampleData,
       updateInstance,
     ]
