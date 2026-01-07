4201ff534274f5d4c26cd026d1cff3d9f4816339
Mikyo King <mikyo@arize.com>
2026-01-06 19:03:25 -0700

    add python

--- packages/phoenix-evals/src/phoenix/evals/metrics/__init__.py
+++ packages/phoenix-evals/src/phoenix/evals/metrics/__init__.py
@@ -1,3 +1,4 @@
+from .correctness import CorrectnessEvaluator
 from .document_relevance import DocumentRelevanceEvaluator
 from .exact_match import exact_match
 from .hallucination import HallucinationEvaluator
@@ -5,8 +6,9 @@ from .matches_regex import MatchesRegex
 from .precision_recall import PrecisionRecallFScore
 
 __all__ = [
-    "exact_match",
+    "CorrectnessEvaluator",
     "DocumentRelevanceEvaluator",
+    "exact_match",
     "HallucinationEvaluator",
     "MatchesRegex",
     "PrecisionRecallFScore",
