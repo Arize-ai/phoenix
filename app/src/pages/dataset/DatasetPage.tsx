33948a50f2652957cf79a9f4564ab6ffadeaf07e
Rick Steele <rsteele@arize.com>
2026-01-12 13:28:37 -0800

    fix: preserve dataset tables' tab and selection state (#10928)

--- app/src/pages/dataset/DatasetPage.tsx
+++ app/src/pages/dataset/DatasetPage.tsx
@@ -135,10 +135,13 @@ function isTabName(name: unknown): name is TabName {
 }
 
 function getTabIndexFromPathname(pathname: string): number {
-  // We only need the last part of the path
-  const path = pathname.split("/").at(-1);
-  if (isTabName(path)) {
-    return TABS_LIST.indexOf(path);
+  // Check all path segments for a valid tab name
+  // This handles nested routes like /datasets/:id/examples/:exampleId
+  const segments = pathname.split("/");
+  for (const segment of segments) {
+    if (isTabName(segment)) {
+      return TABS_LIST.indexOf(segment);
+    }
   }
   return 0;
 }
