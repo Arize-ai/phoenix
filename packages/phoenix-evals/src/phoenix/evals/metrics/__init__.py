07e3980909870f5e42d836cadec98a510e2ccc3b
ehutt <ehutton44@gmail.com>
2026-01-13 18:21:54 -0800

    update python evals library

--- packages/phoenix-evals/src/phoenix/evals/metrics/__init__.py
+++ packages/phoenix-evals/src/phoenix/evals/metrics/__init__.py
@@ -1,7 +1,8 @@
 from .correctness import CorrectnessEvaluator
 from .document_relevance import DocumentRelevanceEvaluator
 from .exact_match import exact_match
-from .hallucination import HallucinationEvaluator
+from .faithfulness import FaithfulnessEvaluator
+from .hallucination import HallucinationEvaluator  # Deprecated alias
 from .matches_regex import MatchesRegex
 from .precision_recall import PrecisionRecallFScore
 
@@ -9,7 +10,8 @@ __all__ = [
     "CorrectnessEvaluator",
     "DocumentRelevanceEvaluator",
     "exact_match",
-    "HallucinationEvaluator",
+    "FaithfulnessEvaluator",
+    "HallucinationEvaluator",  # Deprecated: use FaithfulnessEvaluator
     "MatchesRegex",
     "PrecisionRecallFScore",
 ]
