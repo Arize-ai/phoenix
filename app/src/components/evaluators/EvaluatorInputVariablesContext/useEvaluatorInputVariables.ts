8d827cb6e1f1173a6bc59612551f4e35da2c0014
Roger Yang <80478925+RogerHYang@users.noreply.github.com>
2026-01-28 13:52:54 -0800

    fix(app): upgrade @apollo/client to 4.1.3 for multipart subscription cancellation (#11155)
    Apollo Client 4.1.3 includes native support for aborting multipart subscriptions,
    removing the need for our custom createSubscribe implementation.
    
    Changes:
    - Upgrade @apollo/client from 3.13.9 to 4.1.3
    - Revert to using Apollo's createFetchMultipartSubscription
    - Replace @apollo/client/utilities/globals import with tiny-invariant

--- app/src/components/evaluators/EvaluatorInputVariablesContext/useEvaluatorInputVariables.ts
+++ app/src/components/evaluators/EvaluatorInputVariablesContext/useEvaluatorInputVariables.ts
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { invariant } from "@apollo/client/utilities/globals";
+import invariant from "tiny-invariant";
 
 import { EvaluatorInputVariablesContext } from "@phoenix/components/evaluators/EvaluatorInputVariablesContext/evaluatorInputVariablesContext";
 
