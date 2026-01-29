18ab1d375c87380f8c882d617bd5a63726cc7afd
Mikyo King <mikyo@arize.com>
2026-01-29 17:40:45 +0900

    feat: add EvaluatorKindToken

---
+++ app/src/components/evaluators/EvaluatorKindToken.tsx
@@ -0,0 +1,26 @@
+import { useMemo } from "react";
+
+import { Token, TokenProps } from "@phoenix/components/token";
+import type { EvaluatorKind } from "@phoenix/types";
+
+export function EvaluatorKindToken(props: {
+  kind: EvaluatorKind;
+  size?: TokenProps["size"];
+}) {
+  const { kind, size = "M" } = props;
+  const color = useMemo<TokenProps["color"]>(() => {
+    switch (kind) {
+      case "LLM":
+        return "var(--ac-global-color-orange-500)";
+      case "CODE":
+        return "var(--ac-global-color-blue-500)";
+      default:
+        return "var(--ac-global-color-grey-300)";
+    }
+  }, [kind]);
+  return (
+    <Token color={color} size={size}>
+      {kind}
+    </Token>
+  );
+}
