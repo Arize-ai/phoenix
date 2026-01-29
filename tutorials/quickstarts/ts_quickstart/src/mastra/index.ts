bd65e4fb9daaca20fabd6583342f0d49e866bc80
Srilakshmi Chavali <schavali03@gmail.com>
2026-01-28 21:44:47 -0800

    testing mastra updates

--- tutorials/quickstarts/ts_quickstart/src/mastra/index.ts
+++ tutorials/quickstarts/ts_quickstart/src/mastra/index.ts
@@ -8,13 +8,10 @@ if (!process.env.OTEL_ATTRIBUTE_VALUE_LENGTH_LIMIT) {
 import { Mastra } from "@mastra/core/mastra";
 import { Observability } from "@mastra/observability";
 import { ArizeExporter } from "@mastra/arize";
-import type { SpanOutputProcessor } from "@mastra/core/observability";
-import { SpanType } from "@mastra/core/observability";
 import { financialOrchestratorAgent } from "./agents/financial-orchestrator-agent";
 import { financialResearcherAgent } from "./agents/financial-researcher-agent";
 import { financialWriterAgent } from "./agents/financial-writer-agent";
 import { createArizeConfig } from '@mastra/arize';
-
 
 export const mastra = new Mastra({
   agents: {
@@ -25,18 +22,14 @@ export const mastra = new Mastra({
   observability: new Observability({
     configs: {
       arize: createArizeConfig({
-        exporter: {
-          endpoint: process.env.PHOENIX_ENDPOINT!,
-          apiKey: process.env.PHOENIX_API_KEY,
-          projectName: process.env.PHOENIX_PROJECT_NAME,
-        },
-        //  serializationOptions: {
-        //  },
-        //  spanProcessors: [
-        //  ],
+        exporters: [
+          new ArizeExporter({ 
+            endpoint: process.env.PHOENIX_ENDPOINT!,
+            apiKey: process.env.PHOENIX_API_KEY,
+            projectName: process.env.PHOENIX_PROJECT_NAME
+          }),
+        ],
       }),
-    },
- }),
   // observability: new Observability({
   //   configs: {
   //     arize: {
@@ -55,22 +48,7 @@ export const mastra = new Mastra({
   //         maxArrayLength: 1000,
   //         maxObjectKeys: 1000,
   //       },
-  //       spanOutputProcessors: [
-  //         {
-  //           name: "workflow-loop-filter",
-  //           process: (span) =>
-  //             [
-  //               SpanType.WORKFLOW_LOOP,
-  //               SpanType.WORKFLOW_PARALLEL,
-  //               SpanType.WORKFLOW_CONDITIONAL,
-  //               SpanType.WORKFLOW_CONDITIONAL_EVAL,
-  //             ].includes((span as any)?.type)
-  //               ? undefined
-  //               : span,
-  //           shutdown: async () => {},
-  //         } satisfies SpanOutputProcessor,
-  //       ],
-  //     },
-  //   },
-  // }),
+    //   },
+    },
+  }),
 });
