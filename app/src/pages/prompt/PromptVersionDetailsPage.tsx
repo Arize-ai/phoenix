d875155de0241b569f8d1176684cadc52e62c33b
Mikyo King <mikyo@arize.com>
2026-02-12 01:49:54 +0900

    feat: preserve prompt id and version / tag info in the URL on playground (#11327)
    * feat: preserve prompt selection in the URL
    
    * feat: preserve prompt selection in the URL
    
    * refactor to be cleaner
    
    * cleanup
    
    * add coding style
    
    * cleanup
    
    * remove legacy code
    
    * add playwright tests
    
    * remove export
    
    * remove timeout

--- app/src/pages/prompt/PromptVersionDetailsPage.tsx
+++ app/src/pages/prompt/PromptVersionDetailsPage.tsx
@@ -77,7 +77,7 @@ function PromptVersionDetailsPageContent({
                   <LinkButton
                     variant="primary"
                     leadingVisual={<Icon svg={<Icons.PlayCircleOutline />} />}
-                    to="playground"
+                    to={`/playground?promptId=${encodeURIComponent(promptId)}&promptVersionId=${encodeURIComponent(promptVersion.id)}`}
                     size="S"
                     data-testid="open-prompt-version-in-playground-button"
                     aria-label="Open this Prompt version in Playground"
