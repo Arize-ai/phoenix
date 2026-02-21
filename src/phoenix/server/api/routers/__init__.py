4a74bbd4a80ae3886850801bc8d2f5d6a48c034c
Alexander Song <axiomofjoy@gmail.com>
2026-02-21 18:24:03 -0500

    agents first commit

--- src/phoenix/server/api/routers/__init__.py
+++ src/phoenix/server/api/routers/__init__.py
@@ -1,8 +1,10 @@
 from .auth import create_auth_router
 from .oauth2 import router as oauth2_router
 from .v1 import create_v1_router
+from .vercel_chat_stream import create_vercel_chat_stream_router
 
 __all__ = [
+    "create_vercel_chat_stream_router",
     "create_auth_router",
     "create_v1_router",
     "oauth2_router",
