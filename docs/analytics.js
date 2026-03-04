aa14c7d08178314a6b29e3eec39c099bcfe07bf5
Laurie Voss <github@seldo.com>
2026-03-04 09:34:52 -0800

    chore: add reo.dev for doc analytics (#11859)

--- docs/analytics.js
+++ docs/analytics.js
@@ -4,3 +4,6 @@ img.src = 'https://static.scarf.sh/a.png?x-pxid=6bd245a7-3214-444b-a83b-24f73272
 img.referrerPolicy = 'no-referrer-when-downgrade';
 img.id = 'scarf-sh-analytics';
 document.body.appendChild(img);
+
+// Reo.dev to do... the same thing
+!function(){var e,t,n;e="e465671c1b14374",t=function(){Reo.init({clientID:"e465671c1b14374"})},(n=document.createElement("script")).src="https://static.reo.dev/"+e+"/reo.js",n.defer=!0,n.onload=t,document.head.appendChild(n)}();
