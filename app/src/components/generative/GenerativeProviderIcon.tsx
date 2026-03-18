1b2ddf602bead49c622a244b3a5f05aea57cfc03
Rick Steele <rsteele@arize.com>
2026-03-18 11:02:25 -0700

    fix: respect height property

--- app/src/components/generative/GenerativeProviderIcon.tsx
+++ app/src/components/generative/GenerativeProviderIcon.tsx
@@ -125,8 +125,8 @@ const OllamaSVG = ({ height }: { height: number }) => (
 );
 const BedrockSVG = ({ height }: { height: number }) => (
   <svg
-    width="24"
-    height="24"
+    width={height}
+    height={height}
     viewBox="0 0 24 24"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
