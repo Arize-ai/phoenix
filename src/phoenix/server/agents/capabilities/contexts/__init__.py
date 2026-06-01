ejentum/phoenix-ejentum

feat(pxi): code-evaluator authoring tools + smoke specs
Adds three PXI tools so users can author code evaluators end-to-end
from the assistant: read_code_evaluator_draft / edit_code_evaluator_draft
(form co-pilot mode — drives an open Create/Edit Code Evaluator dialog
with an inline accept/reject diff) and create_code_evaluator
(direct-authoring mode — persists a brand-new evaluator via the existing
createCodeEvaluator GraphQL mutation). The two modes are mutually
exclusive: the form-open context advertises a code_evaluator context to
PXI, which toggles which tools are visible. create_code_evaluator
accepts an optional freeform output_config (optimization direction,
threshold, lower/upper bounds) mirroring the form's single-freeform
shape, so the user can specify the annotation surface in the same
conversation.

Two new Playwright smoke specs cover both surfaces end-to-end against
a real PXI session with an LLM judge, including revision-staleness
guards at both propose-time and accept-time, BadRequest surfacing for
unparseable evaluate() signatures, and output_config round-trip via
GraphQL.

src/phoenix/server/agents/capabilities/contexts/__init__.py
@@ -5,6 +5,9 @@ from pydantic_ai.capabilities import AbstractCapability, CapabilityFunc, Combine
 
 from phoenix.server.agents.capabilities.base import AbstractDynamicCapability
 from phoenix.server.agents.capabilities.contexts.app import AppContextCapability
+from phoenix.server.agents.capabilities.contexts.code_evaluator import (
+    CodeEvaluatorContextCapability,
+)
 from phoenix.server.agents.capabilities.contexts.graphql_mutations import (
     GraphQLMutationsCapability,
 )
@@ -33,6 +36,7 @@ def get_context_capability_function(
         TraceContextCapability(instructions=instructions.trace_context),
         SpanContextCapability(instructions=instructions.span_context),
         PlaygroundContextCapability(instructions=instructions.playground_context),
+        CodeEvaluatorContextCapability(instructions=instructions.code_evaluator_context),
         GraphQLMutationsCapability(instructions=instructions.graphql_mutations),
     ]
 
@@ -45,6 +49,7 @@ def get_context_capability_function(
 
 __all__ = [
     "AppContextCapability",
+    "CodeEvaluatorContextCapability",
     "GraphQLMutationsCapability",
     "PlaygroundContextCapability",
     "ProjectContextCapability",

src/phoenix/server/agents/capabilities/tools/external/__init__.py
@@ -12,7 +12,10 @@ from phoenix.server.agents.capabilities.tools.external import (
     ask_user,
     bash,
     clone_prompt_instance,
+    create_code_evaluator,
+    edit_code_evaluator_draft,
     edit_prompt_instance,
+    read_code_evaluator_draft,
     read_prompt_instance,
     render_generative_ui,
     set_spans_filter,
@@ -23,8 +26,17 @@ from phoenix.server.agents.capabilities.tools.external.bash import BashCapabilit
 from phoenix.server.agents.capabilities.tools.external.clone_prompt_instance import (
     ClonePromptInstanceCapability,
 )
+from phoenix.server.agents.capabilities.tools.external.create_code_evaluator import (
+    CreateCodeEvaluatorCapability,
+)
+from phoenix.server.agents.capabilities.tools.external.edit_code_evaluator_draft import (
+    EditCodeEvaluatorDraftCapability,
+)
 from phoenix.server.agents.capabilities.tools.external.edit_prompt_instance import (
     EditPromptInstanceCapability,
+)
+from phoenix.server.agents.capabilities.tools.external.read_code_evaluator_draft import (
+    ReadCodeEvaluatorDraftCapability,
 )
 from phoenix.server.agents.capabilities.tools.external.read_prompt_instance import (
     ReadPromptInstanceCapability,
@@ -47,7 +59,10 @@ _EXTERNAL_TOOL_DEFINITIONS_BY_NAME: dict[str, ToolDefinition] = {
         ask_user.TOOL_DEFINITION,
         bash.TOOL_DEFINITION,
         clone_prompt_instance.TOOL_DEFINITION,
+        create_code_evaluator.TOOL_DEFINITION,
+        edit_code_evaluator_draft.TOOL_DEFINITION,
         edit_prompt_instance.TOOL_DEFINITION,
+        read_code_evaluator_draft.TOOL_DEFINITION,
         read_prompt_instance.TOOL_DEFINITION,
         render_generative_ui.RENDER_GENERATIVE_UI_TOOL_DEFINITION,
         set_spans_filter.TOOL_DEFINITION,
@@ -80,6 +95,9 @@ def get_external_tool_capability_function(
         ReadPromptInstanceCapability(instructions=instructions.read_prompt_instance_tool),
         ClonePromptInstanceCapability(instructions=instructions.clone_prompt_instance_tool),
         EditPromptInstanceCapability(instructions=instructions.edit_prompt_instance_tool),
+        ReadCodeEvaluatorDraftCapability(instructions=instructions.read_code_evaluator_draft_tool),
+        EditCodeEvaluatorDraftCapability(instructions=instructions.edit_code_evaluator_draft_tool),
+        CreateCodeEvaluatorCapability(instructions=instructions.create_code_evaluator_tool),
     ]
 
     def _build(ctx: RunContext[AgentDependencies]) -> AbstractCapability[AgentDependencies]:
@@ -93,7 +111,10 @@ __all__ = [
     "AskUserCapability",
     "BashCapability",
     "ClonePromptInstanceCapability",
+    "CreateCodeEvaluatorCapability",
+    "EditCodeEvaluatorDraftCapability",
     "EditPromptInstanceCapability",
+    "ReadCodeEvaluatorDraftCapability",
     "ReadPromptInstanceCapability",
     "RenderGenerativeUICapability",
     "SetSpansFilterCapability",

Arize-ai/phoenix

src/phoenix/server/agents/capabilities/contexts/__init__.py
@@ -5,6 +5,9 @@ from pydantic_ai.capabilities import AbstractCapability, CapabilityFunc, Combine
 
 from phoenix.server.agents.capabilities.base import AbstractDynamicCapability
 from phoenix.server.agents.capabilities.contexts.app import AppContextCapability
+from phoenix.server.agents.capabilities.contexts.code_evaluator import (
+    CodeEvaluatorContextCapability,
+)
 from phoenix.server.agents.capabilities.contexts.graphql_mutations import (
     GraphQLMutationsCapability,
 )
@@ -33,6 +36,7 @@ def get_context_capability_function(
         TraceContextCapability(instructions=prompts.trace_context),
         SpanContextCapability(instructions=prompts.span_context),
         PlaygroundContextCapability(instructions=prompts.playground_context),
+        CodeEvaluatorContextCapability(instructions=prompts.code_evaluator_context),
         GraphQLMutationsCapability(instructions=prompts.graphql_mutations),
     ]
 
@@ -45,6 +49,7 @@ def get_context_capability_function(
 
 __all__ = [
     "AppContextCapability",
+    "CodeEvaluatorContextCapability",
     "GraphQLMutationsCapability",
     "PlaygroundContextCapability",
     "ProjectContextCapability",

src/phoenix/server/agents/capabilities/tools/external/__init__.py
@@ -13,7 +13,10 @@ from phoenix.server.agents.capabilities.tools.external import (
     bash,
     batch_span_annotate,
     clone_prompt_instance,
+    create_code_evaluator,
+    edit_code_evaluator_draft,
     edit_prompt_instance,
+    read_code_evaluator_draft,
     read_playground_output,
     read_prompt_instance,
     read_prompt_tools,
@@ -33,8 +36,17 @@ from phoenix.server.agents.capabilities.tools.external.batch_span_annotate impor
 from phoenix.server.agents.capabilities.tools.external.clone_prompt_instance import (
     ClonePromptInstanceCapability,
 )
+from phoenix.server.agents.capabilities.tools.external.create_code_evaluator import (
+    CreateCodeEvaluatorCapability,
+)
+from phoenix.server.agents.capabilities.tools.external.edit_code_evaluator_draft import (
+    EditCodeEvaluatorDraftCapability,
+)
 from phoenix.server.agents.capabilities.tools.external.edit_prompt_instance import (
     EditPromptInstanceCapability,
+)
+from phoenix.server.agents.capabilities.tools.external.read_code_evaluator_draft import (
+    ReadCodeEvaluatorDraftCapability,
 )
 from phoenix.server.agents.capabilities.tools.external.read_playground_output import (
     ReadPlaygroundOutputCapability,
@@ -74,7 +86,10 @@ _EXTERNAL_TOOL_DEFINITIONS_BY_NAME: dict[str, ToolDefinition] = {
         batch_span_annotate.TOOL_DEFINITION,
         bash.TOOL_DEFINITION,
         clone_prompt_instance.TOOL_DEFINITION,
+        create_code_evaluator.TOOL_DEFINITION,
+        edit_code_evaluator_draft.TOOL_DEFINITION,
         edit_prompt_instance.TOOL_DEFINITION,
+        read_code_evaluator_draft.TOOL_DEFINITION,
         read_prompt_instance.TOOL_DEFINITION,
         read_prompt_tools.TOOL_DEFINITION,
         read_playground_output.TOOL_DEFINITION,
@@ -120,6 +135,9 @@ def get_external_tool_capability_function(
         WritePromptToolsCapability(instructions=prompts.write_prompt_tools_tool),
         RunPlaygroundCapability(instructions=prompts.run_playground_tool),
         SetVariableValuesCapability(instructions=prompts.set_variable_values_tool),
+        ReadCodeEvaluatorDraftCapability(instructions=prompts.read_code_evaluator_draft_tool),
+        EditCodeEvaluatorDraftCapability(instructions=prompts.edit_code_evaluator_draft_tool),
+        CreateCodeEvaluatorCapability(instructions=prompts.create_code_evaluator_tool),
     ]
 
     def _build(ctx: RunContext[AgentDependencies]) -> AbstractCapability[AgentDependencies]:
@@ -134,7 +152,10 @@ __all__ = [
     "BatchSpanAnnotateCapability",
     "BashCapability",
     "ClonePromptInstanceCapability",
+    "CreateCodeEvaluatorCapability",
+    "EditCodeEvaluatorDraftCapability",
     "EditPromptInstanceCapability",
+    "ReadCodeEvaluatorDraftCapability",
     "ReadPromptInstanceCapability",
     "ReadPromptToolsCapability",
     "ReadPlaygroundOutputCapability",