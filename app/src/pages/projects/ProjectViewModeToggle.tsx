ab2cd6535c7e9b497a6fefee63ed5701cfdf2fbc
Mikyo King <mikeldking@gmail.com>
2026-06-11 21:54:10 -0700

    fix(ui): use design system tooltip for project view mode toggle

--- app/src/pages/projects/ProjectViewModeToggle.tsx
+++ app/src/pages/projects/ProjectViewModeToggle.tsx
@@ -1,13 +1,12 @@
 import { css } from "@emotion/react";
-import { Tooltip, TooltipTrigger } from "react-aria-components";
 
 import {
   Icon,
   Icons,
-  Text,
   ToggleButton,
   ToggleButtonGroup,
-  View,
+  Tooltip,
+  TooltipTrigger,
 } from "@phoenix/components";
 import { usePreferencesContext } from "@phoenix/contexts";
 import type { ProjectViewMode } from "@phoenix/store/preferencesStore";
@@ -41,16 +40,7 @@ export const ProjectViewModeToggle = () => {
           aria-label="Grid view"
           leadingVisual={<Icon svg={<Icons.Grid />} />}
         />
-        <Tooltip offset={10}>
-          <View
-            padding="size-100"
-            borderColor="default"
-            borderWidth="thin"
-            borderRadius="small"
-          >
-            <Text>View projects in a grid</Text>
-          </View>
-        </Tooltip>
+        <Tooltip>View projects in a grid</Tooltip>
       </TooltipTrigger>
       <TooltipTrigger delay={100}>
         <ToggleButton
@@ -58,16 +48,7 @@ export const ProjectViewModeToggle = () => {
           aria-label="Table view"
           leadingVisual={<Icon svg={<Icons.ListOutline />} />}
         />
-        <Tooltip offset={10}>
-          <View
-            padding="size-100"
-            borderColor="default"
-            borderWidth="thin"
-            borderRadius="small"
-          >
-            <Text>View projects in a table</Text>
-          </View>
-        </Tooltip>
+        <Tooltip>View projects in a table</Tooltip>
       </TooltipTrigger>
     </ToggleButtonGroup>
   );
