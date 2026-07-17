686781ca56fc142dd4475f199e793e2a2a06f7ab
Mikyo King <mikeldking@gmail.com>
2026-07-17 10:43:45 -0700

    style(cli): fix oxfmt formatting in test setup

--- js/packages/phoenix-cli/test/setup.ts
+++ js/packages/phoenix-cli/test/setup.ts
@@ -1,7 +1,6 @@
 import * as fs from "fs";
 import * as os from "os";
 import * as path from "path";
-
 import { ENV_PHOENIX_DISCOVER_CONFIG } from "@arizeai/phoenix-config";
 
 // Disable `.env.phoenix` file discovery so a developer's real credential file
