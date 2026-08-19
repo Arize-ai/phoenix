ab8e6aece0ae2c0662a873156d1c392a249573cb
Tony Powell <apowell@arize.com>
2026-08-19 14:11:01 -0400

    format

--- js/app/tests/settings-tables.spec.ts
+++ js/app/tests/settings-tables.spec.ts
@@ -16,7 +16,6 @@ test.describe("Settings Tables", () => {
 
     await expect(page.getByRole("heading", { name: "Users" })).toBeVisible();
 
-
     const userHeader = page.getByRole("columnheader", { name: "user" }).first();
     await clickSortableHeaderAndExpect(userHeader, "ascending");
   });
