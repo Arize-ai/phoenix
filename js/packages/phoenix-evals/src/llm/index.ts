b7e3a589551cb8456b1938d6c57fd0d20ec126d7
Mora Vigo Malusardi <93168878+MoraVigoMalusardi@users.noreply.github.com>
2026-08-26 09:53:07 -0300

    removed comment on index.ts

--- js/packages/phoenix-evals/src/llm/index.ts
+++ js/packages/phoenix-evals/src/llm/index.ts
@@ -5,7 +5,7 @@ export * from "./createConcisenessEvaluator";
 export * from "./createCorrectnessEvaluator";
 export * from "./createDocumentRelevanceEvaluator";
 export * from "./createFaithfulnessEvaluator";
-export * from "./createHallucinationEvaluator"; // Deprecated: use createFaithfulnessEvaluator
+export * from "./createHallucinationEvaluator";
 export * from "./createPiiDetectionEvaluator";
 export * from "./createRefusalEvaluator";
 export * from "./createRetrievalRelevanceEvaluator";
