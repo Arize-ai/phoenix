74fe8511a05a39a5ff519a60e134b2f1de0164f7
Mikyo King <mikeldking@gmail.com>
2026-09-11 12:31:51 -0600

    refactor(hooks): rename useUnsavedChangesBlocker to useConfirmNavigation

--- js/app/src/hooks/index.ts
+++ js/app/src/hooks/index.ts
@@ -18,4 +18,4 @@ export * from "./usePersistedState";
 export * from "./useOwnedPreloadedQuery";
 export * from "./useLabelFilterSearchParams";
 export * from "./useMediaQuery";
-export * from "./useUnsavedChangesBlocker";
+export * from "./useConfirmNavigation";
